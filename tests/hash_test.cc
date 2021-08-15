f3e631eecba96fee5a802248e823edbaaa0c3e10
abj7 <alan@thirdai.com>
2021-08-14 20:30:46 -0500

    Add murmurhash avalanche tests

--- tests/hash_test.cc
+++ tests/hash_test.cc
@@ -2,6 +2,7 @@
 #include "tabulationhash/UniversalHash.h"
 #include "murmurhash/MurmurHash3.h"
 #include <algorithm>
+#include <bitset>
 #include <chrono>
 #include <iostream>
 #include <random>
@@ -31,6 +32,7 @@ namespace ThirdAI {
             static uint64_t int_keys[100000];
             static std::string str_keys[100000]; 
             static const uint64_t seed = 1;
+            static UniversalHash universal_hash;
 
             // Initialize all cross-test parameters (integer and string keys)
             static void SetUpTestSuite() {
@@ -41,13 +43,24 @@ namespace ThirdAI {
                     str_keys[i] = generate_random_string();
                 }
             }
+
+        public:
+            // Converts input string to bitstring (bitset).
+            std::string convert_to_bitstring(std::string str) {
+                std::string bitstring = "";
+                for (char& _c : str) {
+                    bitstring += std::bitset<8>(_c).to_string();
+                }
+                return bitstring;
+            }
     };
 
     uint64_t HashTest::int_keys[100000];
     std::string HashTest::str_keys[100000];
+    UniversalHash HashTest::universal_hash(time(NULL));
 
     TEST_F(HashTest, MurmurHashTimeTest) {
-        // Allocate 64 bits for murmurhash output of both keys. TODO(alan): Reused for now. Avalanche testing later.
+        // Allocate 64 bits for output of both keys.
         uint32_t murmurhash_output[2];
         // Test speed of MurmurHash.
         auto start = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
@@ -58,12 +71,11 @@ namespace ThirdAI {
         auto end = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
         std::cout << "MurmurHash output ex: " << murmurhash_output[0] << " " << murmurhash_output[1] << std::endl;
         std::cout << "MurmurHash time (ms): " << end - start << std::endl;
-        EXPECT_LE(end - start, 20);
+        EXPECT_LE(end - start, 100);
     }
 
     TEST_F(HashTest, TabulationHashTimeTest) {
-        UniversalHash universal_hash(time(NULL));
-        // Allocate 64 bits for tabulation hash output of both keys. TODO(alan): Reused for now. Avalanche testing later.
+        // Allocate 64 bits for output of both keys.
         uint32_t tabulation_output[2];
         // Test speed of Tabulation Hashing.
         auto start = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
@@ -74,6 +86,108 @@ namespace ThirdAI {
         auto end = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
         std::cout << "Tabulation output ex: " << tabulation_output[0] << " " << tabulation_output[1] << std::endl;
         std::cout << "Tabulation Hash time (ms): " << end - start << std::endl;
-        EXPECT_LE(end - start, 20);
+        EXPECT_LE(end - start, 100);
     }
+
+    TEST_F(HashTest, MurmurHashStringKeyAvalancheTest) {
+        // Allocate 64 bits for output of both keys.
+        uint32_t murmurhash_output[2];
+        uint32_t res[48][32] = {0};
+        for (int i = 0; i < 100000; i++) {
+            MurmurHash3_x86_32(str_keys[i].c_str(), (uint64_t)strlen(str_keys[i].c_str()), seed, &(murmurhash_output[0]));
+            for (int j = 0; j < 48; j++) {
+                std::bitset<48> str_key_flipped_bitarray(convert_to_bitstring(str_keys[i]));
+                std::string str_key_flipped = str_key_flipped_bitarray.flip(j).to_string();
+                // std::cout << convert_to_bitstring(str_keys[i]) << std::endl;
+                // std::cout << str_key_flipped << std::endl;
+                MurmurHash3_x86_32(str_key_flipped.c_str(), (uint64_t)strlen(str_key_flipped.c_str()), seed, &(murmurhash_output[1]));
+                for (int k = 0; k < 32; k++) {
+                    res[j][k] += ((murmurhash_output[0] ^ murmurhash_output[1]) >> k) & 1;
+                }
+            }
+        }
+        // TODO(alan): Could find better way for gtest assertion.
+        for (int j = 0; j < 48; j++) {
+            for (int k = 0; k < 32; k++) {
+                // Expect ~0.5 probability over all 100000 keys.
+                EXPECT_NEAR(res[j][k], 50000, 1000);
+            }
+        }
+    }
+
+    TEST_F(HashTest, MurmurHashIntegerKeyAvalancheTest) {
+        // Allocate 64 bits for output of both keys.
+        uint32_t murmurhash_output[2];
+        uint32_t res[8][32] = {0};
+        for (int i = 0; i < 100000; i++) {
+            MurmurHash3_x86_32((void *)&(int_keys[i]), sizeof(uint64_t), seed, &(murmurhash_output[1]));
+            for (int j = 0; j < 8; j++) {
+                // std::cout << convert_to_bitstring(str_keys[i]) << std::endl;
+                // std::cout << str_key_flipped << std::endl;
+                uint64_t int_key_flipped = int_keys[i] ^ (1 << j);
+                MurmurHash3_x86_32((void *)&(int_key_flipped), sizeof(uint64_t), seed, &(murmurhash_output[1]));
+                for (int k = 0; k < 32; k++) {
+                    res[j][k] += ((murmurhash_output[0] ^ murmurhash_output[1]) >> k) & 1;
+                }
+            }
+        }
+        // TODO(alan): Could find better way for gtest assertion.
+        for (int j = 0; j < 8; j++) {
+            for (int k = 0; k < 32; k++) {
+                // Expect ~0.5 probability over all 100000 keys.
+                EXPECT_NEAR(res[j][k], 50000, 10000);
+            }
+        }
+    }
+
+    // TEST_F(HashTest, TabulationHashStringKeyAvalancheTest) {
+    //     // Allocate 64 bits for output of both keys.
+    //     uint32_t tabulation_output[2];
+    //     uint32_t res[48][32] = {0};
+    //     for (int i = 0; i < 100000; i++) {
+    //         tabulation_output[0] = universal_hash.gethash(str_keys[i]);
+    //         for (int j = 0; j < 48; j++) {
+    //             std::bitset<48> str_key_flipped_bitarray(convert_to_bitstring(str_keys[i]));
+    //             std::string str_key_flipped = str_key_flipped_bitarray.flip(j).to_string();
+    //             // std::cout << convert_to_bitstring(str_keys[i]) << std::endl;
+    //             // std::cout << str_key_flipped << std::endl;
+    //             tabulation_output[1] = universal_hash.gethash(str_key_flipped);
+    //             for (int k = 0; k < 32; k++) {
+    //                 res[j][k] += ((tabulation_output[0] ^ tabulation_output[1]) >> k) & 1;
+    //             }
+    //         }
+    //     }
+    //     // TODO(alan): Could find better way for gtest assertion.
+    //     for (int j = 0; j < 48; j++) {
+    //         for (int k = 0; k < 32; k++) {
+    //             // Expect ~0.5 probability over all 100000 keys.
+    //             EXPECT_NEAR(res[j][k], 50000, 1000);
+    //         }
+    //     }
+    // }
+
+    // TEST_F(HashTest, TabulationHashIntegerKeyAvalancheTest) {
+    //     // Allocate 64 bits for output of both keys.
+    //     uint32_t tabulation_output[2];
+    //     uint32_t res[8][32] = {0};
+    //     for (int i = 0; i < 100000; i++) {
+    //         tabulation_output[0] = universal_hash.gethash(int_keys[i]);
+    //         for (int j = 0; j < 8; j++) {
+    //             // std::cout << convert_to_bitstring(str_keys[i]) << std::endl;
+    //             // std::cout << str_key_flipped << std::endl;
+    //             uint64_t int_key_flipped = int_keys[i] ^ (1 << j);
+    //             tabulation_output[0] = universal_hash.gethash(int_key_flipped);
+    //             for (int k = 0; k < 32; k++) {
+    //                 res[j][k] += ((tabulation_output[0] ^ tabulation_output[1]) >> k) & 1;
+    //             }
+    //         }
+    //     }
+    //     // TODO(alan): Could find better way for gtest assertion.
+    //     for (int j = 0; j < 8; j++) {
+    //         for (int k = 0; k < 32; k++) {
+    //             // Expect ~0.5 probability over all 100000 keys.
+    //             EXPECT_NEAR(res[j][k], 50000, 10000);
+    //         }
+    //     }
+    // }
 }
