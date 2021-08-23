5e46ae4f3b374e831413796a271670d38527dbdc
Anshu <anshu@thirdai.com>
2021-08-23 14:52:21 -0500

    Starting Utils for Slide

---
+++ Anshu_Utils_Slide/SparseBatchData.h
@@ -0,0 +1,31 @@
+#include <string>
+#include <fstream> 
+#include <memory>
+
+namespace ThirdAI{
+
+ enum SPARSE_DATA_FORMAT { SVM, CSV};
+
+	class SparseBatchData{
+	 private:
+		 std::string filename; 
+		 std::shared_ptr<std::ifstream> _file; 
+                 SPARSE_DATA_FORMAT _format;
+		 char* _buffer;
+
+         public:
+		 uint32_t* _indices, _lengths;
+		 float* _nonZeros;
+		 uint32_t batchSize;
+		 SparseBatchData(std::string filename, uint32_t dim, SPARSE_DATA_FORMAT format);
+		 /*
+		  * Reads the next batch into the same pointers of _indices, _lengths. It can be done in parallel 
+		  * and is shared among different threads.   
+		  */
+		 void ReadNextBatch(uint32_t batchSize); 
+		 ~SparseBatchData();
+                 
+
+	};
+
+}//namespace
