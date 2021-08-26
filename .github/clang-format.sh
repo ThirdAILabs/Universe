90a31b199c618d1fc7d72a7cf7b572cf8221ff04
alanThirdAI <alan@thirdai.com>
2021-08-26 15:43:27 -0500

    Update clang-format.sh

--- .github/clang-format.sh
+++ .github/clang-format.sh
@@ -1,5 +1,5 @@
 # Run clang-format on all files/directories recursively
-find ../ -iname '*.h' -o -iname '*.cc' | xargs clang-format -i -style=file
+find . -iname '*.h' -o -iname '*.cc' | xargs clang-format -i -style=file
 
 # Check git diff for any changes
 if git diff-index --quiet HEAD --
