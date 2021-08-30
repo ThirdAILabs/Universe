3673b38828aff4dd830e4aef8c4f52366103bb93
Josh Engels <josh@thirdai.com>
2021-08-30 16:18:52 -0500

    Updated mac installs

--- install-mac.sh
+++ install-mac.sh
@@ -5,6 +5,9 @@
 # Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# Make homebrew path come first
+echo "export PATH=/opt/homebrew/bin:\$PATH" >> $HOME/.bash_profile
+
 # Install clang-tidy, clang-format
 brew install llvm
 ln -s "$(brew --prefix llvm)/bin/clang-format" "/usr/local/bin/clang-format"
@@ -12,7 +15,8 @@ ln -s "$(brew --prefix llvm)/bin/clang-tidy" "/usr/local/bin/clang-tidy"
 ln -s "$(brew --prefix llvm)/bin/clang-apply-replacements" "/usr/local/bin/clang-apply-replacements"
 
 # Install gcc
-brew install gcc
+brew install brew
+ln -s /opt/homebrew/bin/g++-11 gcc
 
 # Install bazel
 brew install bazel
