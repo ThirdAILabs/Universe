3c6fee9d0740dd958a0ba5f1345849f041e666ef
Josh Engels <josh@thirdai.com>
2021-08-29 21:07:45 -0500

    Created mac install script

---
+++ install-mac.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Run this script with sudo
+
+# Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install clang-tidy, clang-format
+brew install llvm
+ln -s "$(brew --prefix llvm)/bin/clang-format" "/usr/local/bin/clang-format"
+ln -s "$(brew --prefix llvm)/bin/clang-tidy" "/usr/local/bin/clang-tidy"
+ln -s "$(brew --prefix llvm)/bin/clang-apply-replacements" "/usr/local/bin/clang-apply-replacements"
+
+# Install gcc
+brew install gcc
+
+# Install bazel
+brew install bazel
