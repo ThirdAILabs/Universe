906215d1f1f51dd60401ed31f6b11d0928fda66a
Joshua Engels <josh@thirdai.com>
2021-08-31 16:24:38 -0500

    Changing generate compile commands to work on mac (#3)

--- generate_compile_commands.sh
+++ generate_compile_commands.sh
@@ -7,7 +7,7 @@ outfile="$(bazel info bazel-bin)/compile_commands.json"
 # Command to replace the marker for exec_root in the file.
 execroot=$(bazel info execution_root)
 sed -i.bak "s@__EXEC_ROOT__@${execroot}@" "${outfile}"
-sed -i 's/-fno-canonical-system-headers/ /g' "${outfile}"
+sed -i '' -e 's/-fno-canonical-system-headers/ /g' "${outfile}"
 
 rm -f compile_commands.json
 cp ${outfile} compile_commands.json 
