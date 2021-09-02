bf3af1471d82c5827c24daa34ce12974a823ce63
benitoThree <89472452+benitoThree@users.noreply.github.com>
2021-09-02 17:24:27 -0500

    Dataset interface for loading multiple batches from file at once (#5)
    * First commit of utils top level folder, along with basic HashFunction and Dataset objects
    
    * Updating PR to include comments
    
    * Forgot to format this, but nice to know working as intended
    
    * Change to a Batch pointer
    
    * Adding formatting
    
    * gitignore and compile_commands
    
    * Dataset.h can load multiple batches at once
    
    * Dataset.h can load multiple batches at once
    
    * Dataset.h can load multiple batches at once
    
    * Made adjustments based on Josh's review
    
    * Made adjustments based on Josh's review
    
    * reverted git ignore, deleting compile_commands.json
    
    * reverted gitignore
    
    * hash function line 16 fix
    
    * hashFunction hashBatch() comment fix
    
    * hashFunction hashBatch() comment fix
    
    * hashFunction hashBatch() comment fix
    
    Co-authored-by: Josh Engels <josh@thirdai.com>
    Co-authored-by: Benito Geordie <benito@Benitos-MacBook-Air.local>
    Co-authored-by: Benito Geordie <benito@Benitos-Air.attlocal.net>

---
+++ utils/dataset/Dataset.h
@@ -0,0 +1,137 @@
+#pragma once
+
+#include <cassert>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <optional>
+#include <sstream>
+#include <string>
+#include <vector>
+
+namespace thirdai::utils {
+
+enum class BATCH_TYPE { SPARSE, SPARSE_LABELED, DENSE, DENSE_LABELED };
+
+struct Batch {
+ public:
+  // TODO: Comment these methods
+  uint32_t _batch_size{0};
+  uint32_t** _indices{nullptr};
+  float** _values{nullptr};
+  uint32_t* _lens{nullptr};
+  uint32_t** _labels{nullptr};
+  uint32_t* _label_lens{nullptr};
+  BATCH_TYPE _type;
+  uint32_t _dim;
+
+  /** Creates a new Batch object with a size, data dimension, and data type */
+  Batch(uint64_t batch_size, BATCH_TYPE type, uint32_t dim) {
+    assert(_dim != 0);
+
+    _type = type;
+    _batch_size = batch_size;
+    _values = new float*[_batch_size];
+    _dim = dim;
+
+    if (_type == BATCH_TYPE::SPARSE || _type == BATCH_TYPE::SPARSE_LABELED) {
+      _indices = new uint32_t*[_batch_size];
+      _lens = new uint32_t[_batch_size];
+    }
+    if (_type == BATCH_TYPE::SPARSE_LABELED ||
+        _type == BATCH_TYPE::DENSE_LABELED) {
+      _labels = new uint32_t*[_batch_size];
+      _label_lens = new uint32_t[_batch_size];
+    }
+  }
+
+  // No copy constructor
+  Batch(const Batch& other) = delete;
+
+  // Move constructor
+  Batch(Batch&& other) noexcept
+      : _batch_size(other._batch_size),
+        _indices(other._indices),
+        _values(other._values),
+        _lens(other._lens),
+        _labels(other._labels),
+        _label_lens(other._label_lens),
+        _type(other._type),
+        _dim(other._dim) {
+    // Set fields to null so we do not delete the fields of our current object
+    other._batch_size = 0;
+    other._indices = nullptr;
+    other._values = nullptr;
+    other._lens = nullptr;
+    other._labels = nullptr;
+    other._label_lens = nullptr;
+  }
+
+  // No copy assignment operator
+  Batch& operator=(const Batch& other) = delete;
+
+  // Move assignment operator
+  Batch& operator=(Batch&& other) noexcept {
+    _batch_size = other._batch_size;
+    _indices = other._indices;
+    _values = other._values;
+    _lens = other._lens;
+    _labels = other._labels;
+    _label_lens = other._label_lens;
+
+    other._batch_size = 0;
+    other._indices = nullptr;
+    other._values = nullptr;
+    other._lens = nullptr;
+    other._labels = nullptr;
+    other._label_lens = nullptr;
+    return *this;
+  }
+
+  ~Batch() {
+    delete[] _indices;
+    delete[] _lens;
+    delete[] _values;
+    delete[] _labels;
+    delete[] _label_lens;
+  }
+};
+
+class Dataset {
+ public:
+  /**
+   * target_batch_num_per_read is the number of batches loaded from file each
+   * time. set to 0 to load entire file.
+   */
+  Dataset(uint64_t target_batch_size, uint64_t target_batch_num_per_load)
+      : _target_batch_size(target_batch_size),
+        _target_batch_num_per_load(target_batch_num_per_load){};
+
+  /**
+   * The batch is only going to exist until the next call to loadNextBatchSet();
+   */
+  const Batch& operator[](uint64_t i) const {
+    assert(i <= _num_batches);
+    return _batches[i];
+  }
+
+  /**
+   * Load n batches from file
+   * Frees any memory for currently existing batches and updates _numBatches.
+   */
+  virtual void loadNextBatchSet() = 0;
+
+  /**
+   * Number of batches currently loaded
+   */
+  uint64_t numBatches() { return _num_batches; };
+
+  ~Dataset() { delete[] _batches; }
+
+ protected:
+  const uint64_t _target_batch_size, _target_batch_num_per_load;
+  uint64_t _num_batches;
+  Batch* _batches;
+};
+
+}  // namespace thirdai::utils
