f880fc0d2c1782cf9e5e42a964606a5a0c2b96e9
Henry Zhang <junyanzhang@Henrys-MacBook-Air.local>
2021-09-07 16:34:32 -0500

    added interfaces for TfidfDataset and GlobalFreq

--- utils/dataset/GlobalFreq.h
+++ utils/dataset/GlobalFreq.h
@@ -1,39 +1,37 @@
 #pragma once
 
 #include <cstdint>
-#include <optional>
-#include <string>
-#include <vector>
 #include <fstream>
 #include <iostream>
+#include <optional>
 #include <sstream>
+#include <string>
 #include <unordered_map>
+#include <vector>
 
 namespace thirdai::utils {
-    
-class GlobalFreq
-{
-    /*
-        This class is responsible for calculating (inverse) word/document frequencies on a given data corpus. 
-    */
-    private:
-        std::unordered_map<std::string, int> _idfMap;
-    
-    public:
-        GlobalFreq(std::string[]);
 
-        inline int getIdf(std::string word);
+class GlobalFreq {
+  /*
+      This class is responsible for calculating (inverse) word/document
+     frequencies on a given data corpus.
+  */
+ private:
+  std::unordered_map<std::string, int> _idfMap;
 
-        // Can have a parallel version of getIdf like:
-        void getIdfPar(std::vector<std::string> wordvec, int* freqs);
+ public:
+  GlobalFreq(std::string[]);
 
-        ~GlobalFreq();
-    };
-    
-    GlobalFreq::GlobalFreq(std::string files[]) {
-    }
-    
-    GlobalFreq::~GlobalFreq() {
-    }
-    
-}
+  inline int getIdf(std::string word);
+
+  // Can have a parallel version of getIdf like:
+  void getIdfPar(std::vector<std::string> wordvec, int* freqs);
+
+  ~GlobalFreq();
+};
+
+GlobalFreq::GlobalFreq(std::string files[]) {}
+
+GlobalFreq::~GlobalFreq() {}
+
+}  // namespace thirdai::utils
--- utils/dataset/TfidfDataset.h
+++ utils/dataset/TfidfDataset.h
@@ -1,32 +1,35 @@
 #include "Dataset.h"
 #include "GlobalFreq.h"
-#include <stdio.h>
 #include <chrono>
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <stdio.h>
 
 namespace thirdai::utils {
 
-
 class TfidfDataset : public Dataset {
-private:
-    // TODO: Store a hash function(eg. murmurHash) that maps tokens to token ids (indices), we can also use default natural numbers.
+ private:
+  // TODO: Store a hash function(eg. murmurHash) that maps tokens to token ids
+  // (indices), we can also use default natural numbers.
 
-    int _tfDefault = -1;    // Specifies whether we should use a default value for the TF of a word.
-    GlobalFreq* _globalFreq;
+  int _tfDefault = -1;  // Specifies whether we should use a default value for
+                        // the TF of a word.
+  GlobalFreq* _globalFreq;
 
-    // TODO: Also need a tokenization tool/object for word-unigram and char-trigram;
+  // TODO: Also need a tokenization tool/object for word-unigram and
+  // char-trigram;
 
-public:
-    TfidfDataset(uint64_t target_batch_size, uint64_t target_batch_num_per_read, GlobalFreq* globalFreq, int tfDefault = -1)
-            : Dataset(target_batch_size, target_batch_num_per_read) {
-        _tfDefault = tfDefault;
-        _globalFreq = globalFreq;
-    };
+ public:
+  TfidfDataset(uint64_t target_batch_size, uint64_t target_batch_num_per_read,
+               GlobalFreq* globalFreq, int tfDefault = -1)
+      : Dataset(target_batch_size, target_batch_num_per_read) {
+    _tfDefault = tfDefault;
+    _globalFreq = globalFreq;
+  };
 
-    void readDataset(std::string files[]);
-    void loadNextBatchSet();
+  void readDataset(std::string files[]);
+  void loadNextBatchSet();
 };
 
-}
+}  // namespace thirdai::utils
