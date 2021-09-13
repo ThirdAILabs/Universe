62ed0c4cd9f193f9bf6333b80ec290f675258a05
AIHenry <89421348+AIHenry@users.noreply.github.com>
2021-09-13 15:17:33 -0500

    update henry_dev to string-dataset branch (#48)
    * interfaces for TfidfDataset and GlobalFreq--an object that extracts global frequency info for text data
    
    * added interfaces for TfidfDataset and GlobalFreq
    
    * interfaces for TfidfDataset and GlobalFreq--an object that extracts global frequency info for text data
    
    * added interfaces for TfidfDataset and GlobalFreq
    update with main branch.
    
    * start coding TfidfDataset.cc
    
    * working on the proper workflow
    
    * interfaces for TfidfDataset and GlobalFreq--an object that extracts global frequency info for text data
    rebase 9/10
    
    * ready to merge with stringdataset branch
    
    * clang-format
    
    Co-authored-by: Henry Zhang <junyanzhang@Henrys-Air.attlocal.net>
    Co-authored-by: Henry Zhang <junyanzhang@Henrys-MacBook-Air.local>

---
+++ utils/dataset/GlobalFreq.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <optional>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+namespace thirdai::utils {
+
+class GlobalFreq {
+  /*
+      This class is responsible for calculating (inverse) word/document
+     frequencies on a given data corpus.
+  */
+ private:
+  std::unordered_map<std::string, int> _idfMap;
+
+ public:
+  GlobalFreq(std::vector<std::string> files);
+
+  int getIdf(std::string word);  // Should have a default value
+
+  // Can have a parallel version of getIdf like:
+  void getIdfPar(std::vector<std::string> wordvec, int* freqs);
+
+  ~GlobalFreq();
+};
+
+GlobalFreq::GlobalFreq(std::vector<std::string> files) {}
+
+GlobalFreq::~GlobalFreq() {}
+
+}  // namespace thirdai::utils
