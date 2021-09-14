a671c87a492cbe153fc1ab86282586311c5ba6c7
Joshua Engels <josh@thirdai.com>
2021-09-13 21:26:44 -0500

    Added HashFunction implementations to a Cmake library and tidied HashFunctions (#53)
    * Fixing up hash functions
    
    * Fixing lint errors in hashing code
    
    * Fixing up tidy problems
    
    * Removing extra tests we will add later
    
    * Small format change

--- utils/hashing/DensifiedMinHash.h
+++ utils/hashing/DensifiedMinHash.h
@@ -4,25 +4,25 @@
 #include <stdint.h>
 //#include "Hash_Function.h"
 
-namespace bolt {
+namespace thirdai::utils {
 
 class DensifiedMinHash {
  private:
-  uint32_t _K, _L, _num_hashes, _range, _binsize;
+  uint32_t _hashes_per_table, _num_tables, _num_hashes, _range, _binsize;
   uint32_t _randa, _rand_double_hash_seed;
   uint32_t *_random_hash, *_rand1, _log_num_hashes, *_binids;
   uint32_t _topK = 30;
 
  public:
-  constexpr uint32_t RandDoubleHash(int binid, int count) {
+  constexpr uint32_t RandDoubleHash(int binid, int count) const {
     uint32_t tohash = ((binid + 1) << 6) + count;
     uint32_t result =
         (_rand_double_hash_seed * tohash << 3) >> (32 - _log_num_hashes);
     return result;
   }
 
-  DensifiedMinHash(uint32_t input_dim, uint32_t _K, uint32_t _L,
-                   uint32_t range_pow);
+  DensifiedMinHash(uint32_t input_dim, uint32_t hashes_per_table,
+                   uint32_t num_tables, uint32_t range_pow);
 
   uint32_t* HashSparseVector(const uint32_t* indices, const float* values,
                              uint32_t len);
@@ -34,10 +34,10 @@ class DensifiedMinHash {
 
   void HashVector(const float* data, uint32_t len, uint32_t* final_hashes);
 
-  void DensifyHashes(uint32_t* hashes, uint32_t* final_hashes);
+  void DensifyHashes(const uint32_t* hashes, uint32_t* final_hashes);
 
   void getMap(int n, int* binid);
   ~DensifiedMinHash();
 };
 
-}  // namespace bolt
+}  // namespace thirdai::utils
