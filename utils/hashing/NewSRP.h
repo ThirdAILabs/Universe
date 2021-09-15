1de6b6c2c86090dcdff39f205c9014a4732236db
alanThirdAI <alan@thirdai.com>
2021-09-15 16:23:24 -0500

    Configure New SRP as subclass

--- utils/hashing/NewSRP.h
+++ utils/hashing/NewSRP.h
@@ -1,13 +1,14 @@
 #pragma once
 
-#include <stdint.h>
+#include "HashFunction.h"
+#include <cstdint>
 
 namespace thirdai::utils {
 
-class FastSRP {
+class FastSRP : public HashFunction {
  private:
-  uint32_t _hashes_per_table, _num_tables, _num_hashes, _log_num_hashes, _dim,
-      _binsize, _permute;
+  uint32_t _hashes_per_table, _num_tables, _num_hashes, _log_num_hashes, _range,
+      _dim, _binsize, _permute;
   uint32_t* _bin_map;
   uint32_t* _positions;
   uint32_t _rand_double_hash_seed;
@@ -20,21 +21,28 @@ class FastSRP {
     return result;
   }
 
-  void DensifyHashes(const uint32_t* hashes, uint32_t* final_hashes);
+  void densifyHashes(const uint32_t* hashes, uint32_t* final_hashes) const;
+
+  void hashSparseVector(const uint32_t* indices, const float* values,
+                        uint32_t len, uint32_t* final_hashes) const;
+
+  void hashDenseVector(const float* data, uint32_t len,
+                       uint32_t* final_hashes) const;
 
  public:
   FastSRP(uint32_t input_dim, uint32_t _hashes_per_table, uint32_t _num_tables,
           uint32_t range_pow);
 
-  uint32_t* HashSparseVector(const uint32_t* indices, const float* values,
-                             uint32_t len);
+  void hashSparse(uint64_t num_vectors, const uint32_t* const* indices,
+                  const float* const* values, const uint32_t* lengths,
+                  uint32_t* output) const override;
 
-  void HashSparseVector(const uint32_t* indices, const float* values,
-                        uint32_t len, uint32_t* final_hashes);
+  void hashDense(uint64_t num_vectors, uint64_t dim, const float* const* values,
+                 uint32_t* output) const override;
 
-  uint32_t* HashVector(const float* data, uint32_t len);
+  uint32_t numTables() const override { return _num_tables; }
 
-  void HashVector(const float* data, uint32_t len, uint32_t* final_hashes);
+  uint32_t range() const override { return _range; }
 
   ~FastSRP();
 };
