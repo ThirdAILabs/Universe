b7d8b69655c512cac8d6446d89297823903ec133
Henry Zhang <junyanzhang@Henrys-MacBook-Air.local>
2021-09-22 15:36:37 -0500

    imcomplete fix on unigram vectorizer

--- utils/dataset/string/vectorizers/UnigramVectorizer.cc
+++ utils/dataset/string/vectorizers/UnigramVectorizer.cc
@@ -8,35 +8,15 @@ virtual void UnigramVectorizer::vectorize(const std::string& str,
                                           std::vector<uint32_t>& indices,
                                           std::vector<float>& values,
                                           VECTOR_TYPE vector_type) {
-  switch (vector_type) {
-    case VECTOR_TYPE::MURMUR:
-      /* code */
-      break;
-    case VECTOR_TYPE::TFIDF:
-      indices.clear();
-      values.clear();
-      for (auto token : str) {
-        int idf = _globalFreq->getIdf(token);
-        int tf = _globalFreq->getTF(token, str);
-        int tokenID = _globalFreq->getTokenID(token);
-        indices.push_back(tokenID);
-        values.push_back(idf * tf);
-        // TODO: What about markers?
-      }
-      break;
-
-    default:
-      indices.clear();
-      values.clear();
-      for (auto token : str) {
-        int idf = _globalFreq->getIdf(token);
-        int tf = _globalFreq->getTF(token, str);
-        int tokenID = _globalFreq->getTokenID(token);
-        indices.push_back(tokenID);
-        values.push_back(idf * tf);
-        // TODO: What about markers?
-      }
-      break;
+    for (auto token : str) {
+      // TODO: make the frequency count similar to trigram
+      int idf = _globalFreq->getIdf(token);
+      int tf = _globalFreq->getTF(token, str);
+      int tokenID = _globalFreq->getTokenID(token);
+      indices.push_back(tokenID);
+      values.push_back(idf * tf);
+      // TODO: What about markers?
+    }
   }
-}
+
 }  // namespace thirdai::utils
