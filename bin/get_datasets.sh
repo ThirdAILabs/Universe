c4bfd04e331651d431c3509663bb0c0ddf57e30f
BenGeordie <school@bengeordie@gmail.com>
2021-09-22 08:58:19 -0500

    fix get_dataset

--- bin/get_datasets.sh
+++ bin/get_datasets.sh
@@ -4,6 +4,6 @@ BASEDIR=$(dirname "$0")
 BUILDDIR="$BASEDIR/../build"
 
 # Download and unzip data
-SVMDATDIR="$BUILDDIR/utils/tests/dataset/svm" 
+SVMDATADIR="$BUILDDIR/utils/tests/dataset/svm" 
 wget -P "$SVMDATADIR/" "https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/multilabel/bibtex.bz2"
 bzip2 -d $SVMDATADIR/bibtex.bz2
