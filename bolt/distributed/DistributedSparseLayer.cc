d072d36384eb25ca4d20f0b12de42fdd4316e5f2
Nicholas Meisburger <nmeisburger@Nicholass-MacBook-Air.local>
2021-09-23 10:46:07 -0500

    add NOLINT to MPI_IN_PLACE

--- bolt/distributed/DistributedSparseLayer.cc
+++ bolt/distributed/DistributedSparseLayer.cc
@@ -29,12 +29,10 @@ DistributedSparseLayer::DistributedSparseLayer(uint64_t dim, uint64_t prev_dim,
                                  sampling_config);
 }
 
-const void* SAFE_MPI_IN_PLACE = reinterpret_cast<const void*>(-1);
-
 void DistributedSparseLayer::ReduceErrors() {
   for (uint32_t b = 0; b < _batch_size; b++) {
-    MPI_Allreduce(SAFE_MPI_IN_PLACE, _errors[b], _total_active_lens[b],
-                  MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD);
+    MPI_Allreduce(MPI_IN_PLACE, _errors[b], _total_active_lens[b], MPI_FLOAT, // NOLINT
+                  MPI_SUM, MPI_COMM_WORLD);
 
     float* start = _errors[b] + _active_offsets[b][_rank];
     std::copy(start, start + _active_lens[b][_rank],
@@ -45,8 +43,8 @@ void DistributedSparseLayer::ReduceErrors() {
 void DistributedSparseLayer::GatherActivations() {
   for (uint32_t b = 0; b < _batch_size; b++) {
     _active_lens[b][_rank] = _local_layer->GetLen(b);
-    MPI_Allgather(SAFE_MPI_IN_PLACE, 1, MPI_INT, _active_lens[b] + _rank, 1,
-                  MPI_INT, MPI_COMM_WORLD);
+    MPI_Allgather(MPI_IN_PLACE, 1, MPI_INT, _active_lens[b] + _rank, 1, MPI_INT, // NOLINT
+                  MPI_COMM_WORLD);
 
     _active_offsets[b][0] = 0;
     _total_active_lens[b] = _active_lens[b][0];
@@ -66,7 +64,7 @@ void DistributedSparseLayer::GatherActivations() {
               _local_layer->GetValues(b) + _local_layer->GetLen(b),
               _activations[b] + _active_offsets[b][_rank]);
 
-    MPI_Allgatherv(SAFE_MPI_IN_PLACE, len, MPI_UNSIGNED, _active_neurons[b],
+    MPI_Allgatherv(MPI_IN_PLACE, len, MPI_UNSIGNED, _active_neurons[b], // NOLINT
                    _active_lens[b], _active_offsets[b], MPI_UNSIGNED,
                    MPI_COMM_WORLD);
 
