5ea3fae90072c76c804a79c147e59b147805dd67
Henry Zhang <junyanzhang@Henrys-Air.attlocal.net>
2021-09-24 13:01:12 -0500

    unigram support, not clang format yet

--- utils/dataset/string/vectorizers/UnigramVectorizer.cc
+++ utils/dataset/string/vectorizers/UnigramVectorizer.cc
@@ -2,20 +2,42 @@
 
 namespace thirdai::utils {
 
-UnigramVectorizer::~UnigramVectorizer() {}
+//UnigramVectorizer::~UnigramVectorizer() {}
 
-virtual void UnigramVectorizer::vectorize(const std::string& str,
+UnigramVectorizer::UnigramVectorizer(uint32_t start_idx, uint32_t max_dim) : StringVectorizer(start_idx, max_dim){}
+
+void UnigramVectorizer::vectorize(const std::string& str,
                                           std::vector<uint32_t>& indices,
                                           std::vector<float>& values,
                                           VECTOR_TYPE vector_type) {
-    for (auto token : str) {
-      // TODO: make the frequency count similar to trigram
-      int idf = _globalFreq->getIdf(token);
-      int tf = _globalFreq->getTF(token, str);
-      int tokenID = _globalFreq->getTokenID(token);
-      indices.push_back(tokenID);
-      values.push_back(idf * tf);
+    std::stringstream stream(str);
+    std::string temp;
+    std::unordered_map<uint32_t, float> ids;
+    while (stream >> temp) {
+      // Mapping to count frequencies of unique token ids.
+      const char *converted = temp.c_str();
+      u_int32_t len = temp.length();
+      u_int32_t hash = MurmurHash(converted, len, _murmur_seed);
+      if (vector_type == VECTOR_TYPE::TFIDF) {
+        //int idf = _globalFreq->getIdf(temp);
+        int idf = 1; // Placeholder
+        ids[hash] += idf;
+      }
+      else {
+        ids[hash] += 1;
+      }
+      
       // TODO: What about markers?
+    }
+    // Resize the vector to the number of unique token IDs.
+    indices.resize(ids.size());
+    values.resize(ids.size());
+    size_t i = 0;
+    // This overwrites the previous contents of indices and values.
+    for (auto kv : ids) {
+      indices[i] = kv.first;
+      values[i] = kv.second;
+      i++;
     }
   }
 
--- utils/dataset/string/vectorizers/UnigramVectorizer.h
+++ utils/dataset/string/vectorizers/UnigramVectorizer.h
@@ -5,8 +5,10 @@ namespace thirdai::utils {
 class UnigramVectorizer : public StringVectorizer {
  private:
   /* data */
+  u_int32_t _murmur_seed = 42;
  public:
-  UnigramVectorizer(GlobalFreq* globalFreq) : StringVectorizer(globalFreq);
+  //UnigramVectorizer(GlobalFreq* globalFreq) : StringVectorizer(globalFreq){}
+  UnigramVectorizer(uint32_t start_idx, uint32_t max_dim);
   virtual void vectorize(const std::string& str, std::vector<uint32_t>& indices,
                          std::vector<float>& values, VECTOR_TYPE vector_type);
 };
