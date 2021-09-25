cbae4674db8c7b5883803ef9d1a50e211b95fec4
benitoThree <89472452+benitoThree@users.noreply.github.com>
2021-09-25 12:21:35 -0500

    Adapted SVMDataset to fit dataset interface. Allowed sparse batches to take dim = 0 (#75)
    * Adapted SVMDataset utility to fit dataset interface
    
    * Allow sparse batches to accept dim = 0
    
    * added makelists
    
    * clang check
    
    * SVMDatasetTest attempt 1
    
    * It works now!
    
    * SVMDataset - Tested
    
    * Hopefully fixed testing for git
    
    * Clang check
    
    * Fixes based on Josh and Henry's reviews.\n - Added docstrings\n - Use exception instead of assertion in Batch constructor\n - minor cleanups\n\nTODO:\n -[ ] remove dataset and download through github actions
    
    * Fixes based on Josh and Henry's reviews.\n - Added docstrings\n - Use exception instead of assertion in Batch constructor\n - minor cleanups\n\nTODO:\n -[ ] remove dataset and download through github actions
    
    * get_dataset.sh, removed svmtest dataset from repo
    
    * Attempt to fix test.yml
    
    * get_dataset.sh
    
    * fix get_dataset, removing clang-tidy errors, adjusting var names
    
    * fix get_dataset
    
    * fix get_dataset
    
    * Fixed Batch exception
    
    * Fixed Batch exception
    
    * Fixed Batch exception
    
    Co-authored-by: BenGeordie <school@bengeordie@gmail.com>

--- utils/dataset/Dataset.h
+++ utils/dataset/Dataset.h
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <optional>
 #include <sstream>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
@@ -27,13 +28,18 @@ struct Batch {
   LABEL_TYPE _label_type;
   uint32_t _dim;
 
+  /** Default constructor */
+  Batch(){};
+
   /**
    * Creates a new Batch object with a size, data dimension, and data type
    * If sparse, dimension can be set to 0.
    */
   Batch(uint64_t batch_size, BATCH_TYPE batch_type, LABEL_TYPE label_type,
         uint32_t dim) {
-    assert(dim != 0);
+    if (batch_type == BATCH_TYPE::DENSE && dim == 0) {
+      throw std::invalid_argument("Dense batch does not accept dim = 0");
+    }
 
     _batch_type = batch_type;
     _label_type = label_type;
