4160e1e7cfa0bae67f376d6f3b0f97aebe630b37
Nicholas Meisburger <nmeisburger@Nicholass-MacBook-Air.local>
2021-09-25 20:40:54 -0500

    fix test script for boltg

--- bin/bolt_mnist_test.sh
+++ bin/bolt_mnist_test.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 curl https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/multiclass/mnist.bz2 --output mnist.bz2
 curl https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/multiclass/mnist.t.bz2 --output mnist.t.bz2
 
@@ -5,4 +7,4 @@ bzip2 -d mnist.bz2
 bzip2 -d mnist.t.bz2
 
 BASEDIR=$(dirname "$0")
-python3 $BASEDIR/../bolt/benchmarks/mnist.py ../data/mnist ../data/mnist.t
+python3 $BASEDIR/../bolt/benchmarks/mnist.py ./mnist ./mnist.t
