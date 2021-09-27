106d7c9844cc35bf8ed8646c8ad71bb3c59710ac
Josh Engels <josh@thirdai.com>
2021-09-27 17:33:51 -0500

    Fixing merge conflicts with main

---
+++ utils/dataset/svm/SVMDataset.cc
@@ -0,0 +1,102 @@
+#include "SVMDataset.h"
+
+namespace thirdai::utils {
+SVMDataset::SVMDataset(const std::string& filename, uint64_t target_batch_size,
+                       uint64_t target_batch_num_per_load)
+    : Dataset(target_batch_size, target_batch_num_per_load), _file(filename) {
+  if (_file.bad() || _file.fail() || !_file.good() || !_file.is_open()) {
+    throw std::runtime_error("Unable to open file '" + filename + "'");
+  }
+  _num_batches = 0;
+  _times_previously_loaded = 0;
+}
+
+void SVMDataset::loadNextBatchSet() {
+  readDataset();
+  _num_batches = (_num_vecs + _target_batch_size - 1) / _target_batch_size;
+
+  // Option 1. At each subsequent load, delete the previous batch array.
+  // Option 2. Keep the previous array and just modify it as necessary.
+  // Keeping option 2.
+  if (_times_previously_loaded > 0) {
+    if (_num_batches > 0) {
+      uint32_t size_of_last_batch_in_current_load =
+          std::min(_target_batch_size,
+                   _num_vecs - (_num_batches - 1) * _target_batch_size);
+      _batches[_num_batches - 1] =
+          Batch(size_of_last_batch_in_current_load, BATCH_TYPE::SPARSE,
+                LABEL_TYPE::LABELED, 0);
+    }
+  } else {
+    _batches = new Batch[_num_batches];
+
+    for (uint64_t i = 0; i < _num_batches; i++) {
+      uint32_t batch_size =
+          std::min(_target_batch_size, _num_vecs - i * _target_batch_size);
+      _batches[i] =
+          Batch(batch_size, BATCH_TYPE::SPARSE, LABEL_TYPE::LABELED, 0);
+    }
+  }
+
+  createBatches();
+  _times_previously_loaded++;
+}
+
+void SVMDataset::readDataset() {
+  _num_vecs = 0;
+  std::string line;
+  _label_markers.clear();
+  _labels.clear();
+  _markers.clear();
+  _indices.clear();
+  _values.clear();
+  while (((_target_batch_num_per_load > 0 &&
+           _num_vecs < _target_batch_num_per_load * _target_batch_size) ||
+          _target_batch_num_per_load == 0) &&
+         std::getline(_file, line)) {
+    std::stringstream stream(line);
+
+    _label_markers.push_back(_labels.size());
+
+    std::string labelstr;
+    stream >> labelstr;
+    size_t pos;
+    while ((pos = labelstr.find(',')) != std::string::npos) {
+      _labels.push_back(atoi(labelstr.substr(0, pos).c_str()));
+      labelstr = labelstr.substr(pos + 1);
+    }
+    _labels.push_back(atoi(labelstr.c_str()));
+
+    _markers.push_back(_indices.size());
+    std::string nonzero;
+    while (stream >> nonzero) {
+      pos = nonzero.find(':');
+      _indices.push_back(atoi(nonzero.substr(0, pos).c_str()));
+      _values.push_back(atof(nonzero.substr(pos + 1).c_str()));
+    }
+    _num_vecs++;
+  }
+
+  _label_markers.push_back(_labels.size());
+  _markers.push_back(_indices.size());
+}
+
+void SVMDataset::createBatches() {
+  uint32_t* indices_ptr = _indices.data();
+  float* values_ptr = _values.data();
+  uint32_t* labels_ptr = _labels.data();
+
+  for (uint64_t batch = 0; batch < _num_batches; batch++) {
+    for (uint64_t n = 0; n < _batches[batch]._batch_size; n++) {
+      uint64_t indx = batch * _target_batch_size + n;
+      _batches[batch]._indices[n] = indices_ptr + _markers[indx];
+      _batches[batch]._values[n] = values_ptr + _markers[indx];
+      _batches[batch]._lens[n] = _markers[indx + 1] - _markers[indx];
+
+      _batches[batch]._labels[n] = labels_ptr + _label_markers[indx];
+      _batches[batch]._label_lens[n] =
+          _label_markers[indx + 1] - _label_markers[indx];
+    }
+  }
+}
+}  // namespace thirdai::utils
---
+++ utils/dataset/svm/SVMDataset.h
@@ -0,0 +1,49 @@
+#include "../Dataset.h"
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+
+namespace thirdai::utils {
+class SVMDataset : public Dataset {
+ public:
+  SVMDataset(const std::string& filename, uint64_t target_batch_size,
+             uint64_t target_batch_num_per_load);
+
+  void loadNextBatchSet() override;
+
+  virtual ~SVMDataset(){
+      // _batches is deleted in superclass destructor.
+  };
+
+ private:
+  std::ifstream _file;
+  std::vector<uint32_t> _label_markers;
+  std::vector<uint32_t> _labels;
+  std::vector<uint32_t> _markers;
+  std::vector<uint32_t> _indices;
+  std::vector<float> _values;
+  uint64_t _num_vecs;
+  uint64_t _times_previously_loaded;
+
+  /**
+   * Helper function called in loadNextBatchSet().
+   * Reads lines from the dataset until one of the following is met:
+   * - EOF is reached
+   * - target_batch_num_per_load * target_batch_size vectors have been read (if
+   * target_batch_num_per_load > 0) Fills out _indices, _values, _labels,
+   * _label_markers and _markers.
+   */
+  void readDataset();
+
+  /**
+   * Helper function called in loadNextBatchSet().
+   * Formats information that is read into _indices, _values, _labels,
+   * _label_markers and _markers by readDataset() into an array of Batch
+   * objects.
+   */
+  void createBatches();
+};
+
+}  // namespace thirdai::utils
