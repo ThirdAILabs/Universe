17e93d9c847176c740523b4746cd5a900d932e8d
alanThirdAI <alan@thirdai.com>
2021-09-28 18:26:03 -0500

    test email

--- bin/regular-benchmark.sh
+++ bin/regular-benchmark.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-BASEDIR=$(dirname "$0")
-./$BASEDIR/tests.sh
-./$BASEDIR/get_datasets.sh
+./tests.sh
+./get_datasets.sh
 
 export DATE=$(date '+%Y-%m-%d')
-target=$BASEDIR/../../logs/$DATE
-mkdir $BASEDIR/../../logs/
+target= ../../logs/$DATE
+mkdir ../../logs/
 mkdir $target
 
 export NOW=$(date +"%T")
 
 # We need: Code version, machine information, run time, accuracy, hash seeds
-cd $BASEDIR/../build/
-echo "<html>" >> "../$target/$NOW.html"
-lscpu > "../$target/$NOW.html"
-echo "" >> "../$target/$NOW.html"
-echo "" >> "../$target/$NOW.html"
+cd ../build/
+echo "<html>" >> "$target/$NOW.html"
+lscpu > "$target/$NOW.html"
+echo "" >> "$target/$NOW.html"
+echo "" >> "$target/$NOW.html"
 
-echo "Current code version:" >> "../$target/$NOW.html"
-git describe --tag >> "../$target/$NOW.html"
-echo "" >> "../$target/$NOW.html"
+echo "Current code version:" >> "$target/$NOW.html"
+git describe --tag >> "$target/$NOW.html"
+echo "" >> "$target/$NOW.html"
 
-ctest -A >> "../$target/$NOW.html"
-echo "</html>" >> "../$target/$NOW.html"
+ctest -A >> "$target/$NOW.html"
+echo "</html>" >> "$target/$NOW.html"
