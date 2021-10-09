06fc7ad4dd80346996828518c365bdc233d3056d
alanThirdAI <alan@thirdai.com>
2021-10-08 19:43:14 -0500

    Fix datasets script

--- bin/get_datasets.sh
+++ bin/get_datasets.sh
@@ -5,7 +5,7 @@ BUILDDIR="$BASEDIR/../build"
 
 # Download and unzip data
 SVMDATADIR="$BUILDDIR/utils/tests/dataset/svm" 
-if [ ! -d "$SVMDATADIR" ]; then
+if [ ! -d "$SVMDATADIR/bibtex.bz2" ]; then
     curl "https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/multilabel/bibtex.bz2" --output $SVMDATADIR/bibtex.bz2
     bzip2 -d $SVMDATADIR/bibtex.bz2
     echo "Downloaded $SVMDATADIR/bibtex.bz2"
