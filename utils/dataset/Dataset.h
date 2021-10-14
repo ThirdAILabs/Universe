bf6eb1eed3079da67f91c4e44073983688a923fb
Nicholas Meisburger <nmeisburger@gmail.com>
2021-10-14 16:59:57 -0500

    Use dataset factory

--- utils/dataset/Dataset.h
+++ utils/dataset/Dataset.h
@@ -14,11 +14,31 @@
 namespace thirdai::utils {
 
 template <typename Batch_t>
+class Factory {
+ public:
+  virtual Batch_t parse(std::ifstream&, uint32_t, uint32_t) = 0;
+};
+
+template <typename Batch_t, typename Factory_t>
 class InMemoryDataset {
  public:
   // TODO (Nicholas, Josh, Geordie): Add constructor that takes in a vector of
   // filenames
-  InMemoryDataset(const std::string& filename, uint32_t batch_size);
+  template <typename... Args>
+  InMemoryDataset(const std::string& filename, uint32_t batch_size,
+                  Args&&... args) {
+    std::ifstream file(filename);
+
+    uint64_t curr_id = 0;
+    Factory_t factory(std::forward<Args>(args)...);
+    while (!file.eof()) {
+      _batches.push_back(factory.parse(file, batch_size, curr_id));
+      curr_id += _batches.back().getBatchSize();
+    }
+
+    file.close();
+    _len = curr_id;
+  }
 
   const Batch_t& operator[](uint32_t i) const { return _batches[i]; }
 
@@ -30,20 +50,37 @@ class InMemoryDataset {
 
   uint32_t numBatches() const { return _batches.size(); }
 
+  uint64_t len() const { return _len; }
+
  private:
   std::vector<Batch_t> _batches;
+  uint64_t _len;
 };
 
-template <typename Batch_t>
+template <typename Batch_t, typename Factory_t>
 class StreamedDataset {
  public:
   // TODO (Nicholas, Josh, Geordie): Add constructor that takes in a vector of
-  // filenames. For this dataset it will have to store a list of filenames, and
-  // whenever it reaches the end of one it can open the next one.
-  StreamedDataset(const std::string& filename, uint32_t batch_size)
-      : _file(filename), _batch_size(batch_size), _curr_id(0) {}
+  // filenames. For this dataset it will have to store a list of filenames,
+  // and whenever it reaches the end of one it can open the next one.
+  template <typename... Args>
+  StreamedDataset(const std::string& filename, uint32_t batch_size,
+                  Args&&... args)
+      : _file(filename),
+        _batch_size(batch_size),
+        _curr_id(0),
+        _factory(std::forward<Args>(args)...) {}
 
-  std::optional<Batch_t> nextBatch();
+  std::optional<Batch_t> nextBatch() {
+    if (_file.eof()) {
+      return std::nullopt;
+    }
+
+    Batch_t next(_file, _batch_size, _curr_id);
+    _curr_id += next.getBatchSize();
+
+    return next;
+  }
 
   ~StreamedDataset() { _file.close(); }
 
@@ -51,6 +88,7 @@ class StreamedDataset {
   std::ifstream _file;
   uint32_t _batch_size;
   uint64_t _curr_id;
+  Factory_t _factory;
 };
 
 }  // namespace thirdai::utils
