ef1870d96fc63f8958ce0c051b502898f533d8ec
Josh Engels <josh@thirdai.com>
2021-11-11 15:10:23 -0600

    Finishing basic image search benchmark script

--- flash/benchmarks/image_search.py
+++ flash/benchmarks/image_search.py
@@ -1,11 +1,15 @@
 import numpy as np
 import time
 import thirdai
+import sklearn
+from sklearn import metrics
 
 data_path = "/Users/josh/IndexChunks/"
+top_k_gt = 10
+top_k_flash = 100
 
-hf = thirdai.utils.SignedRandomProjection(input_dim=4096, hashes_per_table=15, num_tables=300)
-flash = thirdai.search.Flash(hf)
+hf = thirdai.utils.SignedRandomProjection(input_dim=4096, hashes_per_table=13, num_tables=500)
+flash = thirdai.search.Flash(hf, reservoir_size=100)
 
 max_chunk = 129
 num_vectors = 0
@@ -19,6 +23,10 @@ print("Loading and indexing %d vectors (40GB) took %f seconds." % (num_vectors, 
 
 queries = np.load(data_path + "test_embeddings.npy")
 start = time.perf_counter()
-results = flash.query(queries)
+results = flash.query(queries, top_k=top_k_flash)
 end = time.perf_counter()
-print("Querying %d vectors took %f seconds." % (len(queries), end - start))
+print("Querying %d vectors took %f seconds (%fms per query)." % (len(queries), end - start, (end - start) / len(queries) * 1000))
+
+gt = np.load(data_path + "ground_truth.npy")
+recals = [sum(gt[i] in result for i in range(top_k_gt)) / top_k_gt for result, gt in zip(results, gt)]
+print(f"R{top_k_gt}@{top_k_flash} = {sum(recals) / len(recals)}")
