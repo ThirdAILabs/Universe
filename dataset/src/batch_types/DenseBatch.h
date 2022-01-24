905e686631e6b18f1c6093cfea2ff2228303a81d
BenGeordie <school@bengeordie@gmail.com>
2022-01-24 16:33:13 -0600

    Support for user defined delimiters

--- dataset/src/batch_types/DenseBatch.h
+++ dataset/src/batch_types/DenseBatch.h
@@ -55,7 +55,21 @@ class DenseBatch {
 
 class CsvDenseBatchFactory : public Factory<DenseBatch> {
  public:
-  CsvDenseBatchFactory() {}
+  CsvDenseBatchFactory(char delimiter): _delimiter(delimiter) {
+    if (delimiter == '.' 
+        || delimiter == '0'
+        || delimiter == '1'
+        || delimiter == '2'
+        || delimiter == '3'
+        || delimiter == '4'
+        || delimiter == '5'
+        || delimiter == '6'
+        || delimiter == '7'
+        || delimiter == '8'
+        || delimiter == '9') {
+      throw std::invalid_argument("Invalid delimiter: " + delimiter);
+    }
+  }
 
   DenseBatch parse(std::ifstream& file, uint32_t target_batch_size,
                    uint64_t start_id) override {
@@ -65,6 +79,7 @@ class CsvDenseBatchFactory : public Factory<DenseBatch> {
 
     std::string line;
     while (batch._batch_size < target_batch_size && std::getline(file, line)) {
+      
       if (line.size() == 0) {
         continue;
       }
@@ -73,57 +88,37 @@ class CsvDenseBatchFactory : public Factory<DenseBatch> {
       const char* const line_end = line.c_str() + line.size();
       char* end;
 
-      while (start < line_end && *start != '\n' &&
-             (*start == ' ' || *start == '\t')) {
-        start++;
+      uint32_t label = std::strtoul(start, &end, 10);
+      if (start == end) {
+        throw std::invalid_argument("Invalid dataset file: Found a line that doesn't start with a label.");
       }
-
-      bool has_label = false;
-      if (start < line_end && *start != '\n') {
-        std::vector<uint32_t> labels;
-        labels.push_back(std::strtoul(start, &end, 10));
-        start = end;
-        batch._labels.push_back(std::move(labels));
-        has_label = true;
+      if (line_end - end == 1) {
+        throw std::invalid_argument("Invalid dataset file: The line only contains a label.");
       }
-
+      start = end;
       std::vector<float> values;
-      while (start < line_end && *start != '\n') {
-        while (start < line_end && *start != '\n' &&
-               (*start == ' ' || *start == '\t')) {
-          start++;
-        }
-
-        if (start < line_end && *start != '\n' && *start != ',') {
-          std::stringstream ss;
-          ss << "Elements are not delimited by commas: " << line << std::endl;
-          throw std::invalid_argument(ss.str());
+      while (start < line_end) {
+        if (*start != _delimiter) {
+          throw std::invalid_argument("Invalid dataset file: Found invalid character: " + *start);
         }
         start++;
-
-        if (start < line_end && *start != '\n') {
-          auto value = std::strtof(start, &end);
-          values.push_back(value);
-          start = end;
+        if (start == line_end) {
+          throw std::invalid_argument("Invalid dataset file: No number after delimiter.");
         }
-
-        while (start < line_end && *start != '\n' &&
-               (*start == ' ' || *start == '\t')) {
-          start++;
+        float value = std::strtof(start, &end);
+        if (start == end) {
+          throw std::invalid_argument("Invalid dataset file: Found invalid character: " + *start);
         }
+        values.push_back(value);
+        start = end;
       }
 
       if (dim != 0 && dim != values.size()) {
         throw std::invalid_argument(
-            "This file contains different-dimensional vectors.\n");
+            "Invalid dataset file: Contains different-dimensional vectors.\n");
       }
 
       dim = values.size();
-
-      if (dim == 0 && has_label == true) {
-        throw std::invalid_argument(
-            "This file contains a zero-dimensional vector.\n");
-      }
 
       DenseVector v(values.size());
       uint32_t cnt = 0;
@@ -137,6 +132,9 @@ class CsvDenseBatchFactory : public Factory<DenseBatch> {
     }
     return batch;
   }
+
+ private:
+  char _delimiter;
 };
 
 }  // namespace thirdai::dataset
