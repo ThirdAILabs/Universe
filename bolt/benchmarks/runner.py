65f10125120f1cd7a09da1b3fcd25c4d4732aad6
BenGeordie <school@bengeordie@gmail.com>
2022-01-24 14:44:26 -0600

    Adjusted python tests

--- bolt/benchmarks/runner.py
+++ bolt/benchmarks/runner.py
@@ -20,7 +20,7 @@ def train_mnist_sparse_output_layer(args):
     epoch_times = []
     epoch_accuracies = []
     for _ in range(args.epochs):
-        times = network.Train(train_data, args.lr, 1,
+        times = network.TrainSparse(train_data, args.lr, 1,
                               rehash=3000, rebuild=10000)
         epoch_times.append(times[0])
         acc = network.Test(test_data)
@@ -44,7 +44,7 @@ def train_mnist_sparse_hidden_layer(args):
     epoch_times = []
     epoch_accuracies = []
     for _ in range(args.epochs):
-        times = network.Train(train_data, args.lr, 1,
+        times = network.TrainSparse(train_data, args.lr, 1,
                               rehash=3000, rebuild=10000)
         epoch_times.append(times[0])
         acc = network.Test(test_data)
@@ -68,7 +68,7 @@ def train_amzn670(args):
     epoch_times = []
     epoch_accuracies = []
     for _ in range(args.epochs):
-        times = network.Train(train_data, args.lr, 1,
+        times = network.TrainSparse(train_data, args.lr, 1,
                               rehash=6400, rebuild=128000)
         epoch_times.append(times[0])
         acc = network.Test(test_data, batch_limit=20)
