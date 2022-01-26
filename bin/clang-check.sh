b588176823ae6e18e8aa020a1ade2edbebb7d5fd
Josh Engels <josh@thirdai.com>
2022-01-26 12:36:58 -0600

    Fixing script

--- bin/clang-check.sh
+++ bin/clang-check.sh
@@ -4,4 +4,4 @@ BASEDIR=$(dirname "$0")
 
 "$BASEDIR"/generate_compile_commands.sh
 find "$BASEDIR/../" -type f \( -iname '*.h' -o -iname '*.cc' \) -not -path "*/build/*" | xargs clang-format -i -style=file
-"$BASEDIR"/tidy-check.sh
+"$BASEDIR"/linting.sh
