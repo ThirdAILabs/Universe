5d8f966a5d7725c7e3dd3a9e0b6aa745ad1a9f08
Josh Engels <josh@thirdai.com>
2022-02-01 12:04:05 -0600

    More benchmark progress

--- bin/benchmarks.sh
+++ bin/benchmarks.sh
@@ -1,10 +1,31 @@
-payload='{
-    "text": "Hello, world."
-}'
+BASEDIR=$(dirname "$0")
+BENCHMARKING_FOLDER=$BASEDIR/../benchmarking
 
-# Send Slack Notification
-# Webhook URL for benchmarks channel
-URL="https://hooks.slack.com/services/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"
-# curl -X POST -H 'Content-type: application/json' \
-# --data "$payload" $URL
-curl -d @flash_recall_imagenet.png $URL
+RUN_MAGSEARCH=y
+
+# --------------- Mag Search ---------------
+if [ "$RUN_MAGSEARCH" == "y" ]
+then
+  # Image net embedding search
+  # /media/scratch/ImageNetDemo/IndexFiles $IMAGENET_FOLDER \
+  IMAGENET_FOLDER=$BENCHMARKING_FOLDER/magsearch/imagenet
+  mkdir -p $IMAGENET_FOLDER
+  python3 $BASEDIR/../flash/benchmarks/image_search.py \
+    /Users/josh/IndexChunks $IMAGENET_FOLDER \
+    > $IMAGENET_FOLDER/stdout 2> $IMAGENET_FOLDER/stderr
+fi
+
+# --------------- Bolt ---------------
+if [ "$RUN_BOLT" == "y" ]
+then
+  BOLT_FOLDER=$BENCHMARKING_FOLDER/bolt
+  mkdir $BOLT_FOLDER
+  # Amazon 670k extreme classification
+  AMAZON_FOLDER=$BOLT_FOLDER/amazon670k
+  mkdir $AMAZON_FOLDER
+  # TODO: ADD HERE
+fi
+
+# python3 $BASEDIR/../benchmarking/aggregate_request.py $BENCHMARKING_FOLDER
+
+# python3 $BASEDIR/../benchmarking/send_request_to_slack.py $BENCHMARKING_FOLDER/request.json
--- flash/benchmarks/image_search.py
+++ flash/benchmarks/image_search.py
@@ -1,57 +1,121 @@
 import numpy as np
 import time
 import thirdai
-import sklearn
-from sklearn import metrics
+import argparse
 
-reservoir_sizes = [100, 200, 500, 1000]
-hashes_per_table = [8, 10, 12, 14, 16]
+parser = argparse.ArgumentParser(description="Run MagSearch VGG Image Net Benchmark.")
+parser.add_argument(
+    "data_folder", help="The path to the folder containing the 129 imagenet .npy files."
+)
+parser.add_argument(
+    "output_folder",
+    help="The path to the folder where this script saves graphs and text results.",
+)
+args = parser.parse_args()
+
+top_k_gt = 10
+top_k_search = 100
+max_numpy_chunk_exclusive = 129
+
+
+def run_trial(reservoir_size, hashes_per_table, num_tables):
+    print(
+        f"Creating index with reservoir size {res} and {tables} hash"
+        + f" tables, with {per_table} concatenated hashes per table.",
+        flush=True,
+    )
+
+    hf = thirdai.hashing.SignedRandomProjection(
+        input_dim=4096, hashes_per_table=hashes_per_table, num_tables=num_tables
+    )
+    index = thirdai.search.MagSearch(hf, reservoir_size=reservoir_size)
+
+    num_vectors = 0
+    start = time.perf_counter()
+    num_bytes = 0
+    for chunk_num in range(0, max_numpy_chunk_exclusive):
+        batch = np.load("%s/chunk-ave%d.npy" % (args.data_folder, chunk_num))
+        num_bytes += batch.nbytes
+        index.add(dense_data=batch, starting_index=num_vectors)
+        num_vectors += len(batch)
+    end = time.perf_counter()
+    indexing_time = end - start
+    print(
+        "Loading and indexing %d vectors (%d bytes) took:%f"
+        % (num_bytes, num_vectors, end - start),
+        flush=True,
+    )
+
+    queries = np.load(args.data_folder + "/test_embeddings.npy")
+    start = time.perf_counter()
+    results = index.query(queries, top_k=top_k_search)
+    end = time.perf_counter()
+    querying_time = end - start
+    print(
+        "Querying %d vectors took:%f" % (len(queries), querying_time),
+        flush=True,
+    )
+
+    gt = np.load(args.data_folder + "/ground_truth.npy")
+    recals = [
+        sum(gt[i] in result for i in range(top_k_gt)) / top_k_gt
+        for result, gt in zip(results, gt)
+    ]
+    total_recall = sum(recals) / len(recals)
+    print(f"R{top_k_gt}@{top_k_search} = {total_recall}", flush=True)
+    return (
+        res,
+        hashes_per_table,
+        num_tables,
+        indexing_time,
+        querying_time,
+        total_recall,
+    )
+
+
+def get_pareto(values):
+    results = []
+    for v in sorted(values):
+        if len(results) == 0 or results[-1][1] < v[1]:
+            results.append(v)
+    return results
+
+
+reservoir_sizes = [100, 200, 500]
+hashes_per_table = [12, 14, 16]
 num_tables = [10, 50, 100, 200, 500]
-
-data_path = "/Users/josh/IndexChunks/"
-# data_path = "/media/scratch/ImageNetDemo/IndexFiles/"
-
+results = []
 for res in reservoir_sizes:
     for per_table in hashes_per_table:
         for tables in num_tables:
+            results.append(run_trial(res, per_table, tables))
 
-            print((res, per_table, tables), flush=True)
+query_time_v_recall = [(result[4], result[5]) for result in results]
+pareto = get_pareto(query_time_v_recall)
 
-            top_k_gt = 10
-            top_k_flash = 100
+fastest_with_recall_80_percent = [p for p in pareto if p[1] > 0.8][0]
+with open(args.output_folder + "/imagenet_results.txt") as f:
+    f.write(
+        f"""
+	Succesfully ran {len(results)} trials of MagSearch on the ImageNet embedding
+	dataset. The fastest trial with a recall of 0.8 took 
+	{fastest_with_recall_80_percent} seconds for 10,000 queries 
+	({10000 / fastest_with_recall_80_percent} queries per second). """
+    )
 
-            hf = thirdai.hashing.SignedRandomProjection(
-                input_dim=4096, hashes_per_table=per_table, num_tables=tables
-            )
-            flash = thirdai.search.MagSearch(hf, reservoir_size=res)
 
-            max_chunk = 129
-            num_vectors = 0
-            start = time.perf_counter()
-            for chunk_num in range(0, max_chunk):
-                batch = np.load("%schunk-ave%d.npy" % (data_path, chunk_num))
-                flash.add(dense_data=batch, starting_index=num_vectors)
-                num_vectors += len(batch)
-            end = time.perf_counter()
-            print(
-                "Loading and indexing %d vectors (40GB) took:%f"
-                % (num_vectors, end - start),
-                flush=True,
-            )
+import matplotlib.pyplot as plt
+import math
 
-            queries = np.load(data_path + "test_embeddings.npy")
-            start = time.perf_counter()
-            results = flash.query(queries, top_k=top_k_flash)
-            end = time.perf_counter()
-            print(
-                "Querying %d vectors took:%f" % (len(queries), end - start), flush=True
-            )
-
-            gt = np.load(data_path + "ground_truth.npy")
-            recals = [
-                sum(gt[i] in result for i in range(top_k_gt)) / top_k_gt
-                for result, gt in zip(results, gt)
-            ]
-            print(
-                f"R{top_k_gt}@{top_k_flash} = {sum(recals) / len(recals)}", flush=True
-            )
+plt.plot(
+    [p[1] for p in pareto],
+    [math.log10(10000 / p[0]) for p in pareto],
+)
+titlefontsize = 22
+axisfontsize = 18
+labelfontsize = 12
+ls = "--"
+plt.xlabel("Recall (R10@100)", fontsize=axisfontsize)
+plt.ylabel("Queries per second (log 10)", fontsize=axisfontsize)
+plt.title("Flash Recall on ImageNet Vgg Embeddings", fontsize=titlefontsize)
+plt.savefig(args.output_folder + "/r10_at_100_recall_vs_time.png")
