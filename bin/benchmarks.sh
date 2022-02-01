4326ab3c22558045b5e2f4d05a9a583de1641dee
Josh Engels <josh@thirdai.com>
2022-02-01 13:11:30 -0600

    Creating aggregate request logic

--- bin/benchmarks.sh
+++ bin/benchmarks.sh
@@ -8,7 +8,7 @@ if [ "$RUN_MAGSEARCH" == "y" ]
 then
   # Image net embedding search
   # /media/scratch/ImageNetDemo/IndexFiles $IMAGENET_FOLDER \
-  IMAGENET_FOLDER=$BENCHMARKING_FOLDER/magsearch/imagenet
+  IMAGENET_FOLDER=$BENCHMARKING_FOLDER/MagSearch/ImageNet
   mkdir -p $IMAGENET_FOLDER
   python3 $BASEDIR/../flash/benchmarks/image_search.py \
     /Users/josh/IndexChunks $IMAGENET_FOLDER \
@@ -18,14 +18,17 @@ fi
 # --------------- Bolt ---------------
 if [ "$RUN_BOLT" == "y" ]
 then
-  BOLT_FOLDER=$BENCHMARKING_FOLDER/bolt
+  BOLT_FOLDER=$BENCHMARKING_FOLDER/Bolt
   mkdir $BOLT_FOLDER
   # Amazon 670k extreme classification
-  AMAZON_FOLDER=$BOLT_FOLDER/amazon670k
+  AMAZON_FOLDER=$BOLT_FOLDER/Amazon670k
   mkdir $AMAZON_FOLDER
   # TODO: ADD HERE
 fi
 
-# python3 $BASEDIR/../benchmarking/aggregate_request.py $BENCHMARKING_FOLDER
+# Aggregate request into benchmarking/request.json
+python3 $BASEDIR/../benchmarking/aggregate_request.py $BENCHMARKING_FOLDER
 
-# python3 $BASEDIR/../benchmarking/send_request_to_slack.py $BENCHMARKING_FOLDER/request.json
+# Send Slack Notification
+URL="https://hooks.slack.com/services/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"
+curl -X POST -H 'Content-type: application/json' -d @$BASEDIR/../request.json $URL
