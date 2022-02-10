8365a3689dbac8609229f67edafb90122ea76583
Joshua Engels <josh@thirdai.com>
2022-02-10 13:57:56 -0600

    Integrating MlFlow with our benchmarks (#221)
    * First exploratary changes
    
    * More benchmark progress
    
    * Creating aggregate request logic
    
    * Adding bolt benchmarks, first test messages working
    
    * Choosing good hyperparameters
    
    * Temp state to check out capitilization problem
    
    * Adding mlflow
    
    * Copying into log folder
    
    * Fixing up benchmarks
    
    * Cleaning up gitignore
    
    * Renaming to be a verb
    
    * Don't need this file anymore
    
    * Reformatting
    
    * Going back to origin file name
    
    * Removing bad import
    
    * Fixing link to benchmark script
    
    * Installing needed libraries
    
    * Removing bad import
    
    * Maybe this will work?
    
    * Sshing into node 11
    
    * Will this end the github action
    
    * Giving up on running asynchronously
    
    * Adding new image net path
    
    * Setting a tracking server
    
    * Cleaning up gitignore
    
    * Adding accuracy 0 at the beginning
    
    * Refactoring to use single logging file
    
    * Adding readme to explain logging
    
    * Removing run name
    
    * Fixing queries per second
    
    * Small fix
    
    * Adding hashes per table as self var
    
    * Adding self to log_start_training
    
    * Fixing logger
    
    * Changing experiment names

--- bolt/benchmarks/helpers.py
+++ bolt/benchmarks/helpers.py
@@ -7,7 +7,6 @@ def add_arguments(
     """
     Add arguments to each benchmark script.
     """
-    parser.add_argument("dataset", type=str, help="dataset name")
     parser.add_argument(
         "--train",
         default=train,
@@ -17,13 +16,6 @@ def add_arguments(
     )
     parser.add_argument(
         "--test", default=test, type=str, required=False, help="file path of test data"
-    )
-    parser.add_argument(
-        "--enable_checks",
-        default=False,
-        required=False,
-        help="train with error checking",
-        action="store_true",
     )
     parser.add_argument(
         "--runs",
@@ -60,33 +52,3 @@ def add_arguments(
         "--lr", default=lr, type=float, required=False, help="learning rate"
     )
     return parser.parse_args()
-
-
-def train(
-    args,
-    train_fn,
-    accuracy_threshold,
-    epoch_time_threshold=100,
-    total_time_threshold=10000,
-):
-    final_accuracies = []
-    final_epoch_times = []
-    total_times = []
-
-    for _ in range(args.runs):
-
-        final_accuracy, accuracies_per_epoch, time_per_epoch = train_fn(args)
-        final_accuracies.append(final_accuracy)
-        final_epoch_times.append(time_per_epoch[-1])
-        total_times.append(sum(time_per_epoch))
-
-        print(
-            f"Result of training {args.dataset} for {args.epochs} epochs:\n\tFinal epoch accuracy: {final_accuracy}\n\tFinal epoch time: {time_per_epoch}"
-        )
-
-        if args.enable_checks:
-            assert final_accuracies[-1] > accuracy_threshold
-            assert final_epoch_times[-1] < epoch_time_threshold
-            assert total_times[-1] < total_time_threshold
-
-    return final_accuracies, final_epoch_times
