be77558acd96397f1c01a275775cf63bf87e5645
MinghaoYan <minghaoyan2@gmail.com>
2022-03-18 14:59:33 -0500

    DLRM api change

--- bolt/src/networks/FullyConnectedNetwork.h
+++ bolt/src/networks/FullyConnectedNetwork.h
@@ -69,7 +69,7 @@ class FullyConnectedNetwork final : public Model<dataset::BoltInputBatch> {
     }
   }
 
-  BoltBatch getOutputs(uint32_t batch_size, bool force_dense, int layer_no = -1) final {
+  BoltBatch getOutputs(uint32_t batch_size, bool force_dense, int layer_no) final {
     if (layer_no == -1) {
       return _layers.back()->createBatchState(batch_size,
                                             useDenseComputations(force_dense));
