a9b659b28701f2488ff9f7a5e91faae110719d02
MinghaoYan <minghaoyan2@gmail.com>
2022-03-18 14:54:15 -0500

    DLRM api change

--- bolt/src/networks/DLRM.cc
+++ bolt/src/networks/DLRM.cc
@@ -73,7 +73,8 @@ void DLRM::initializeNetworkState(uint32_t batch_size, bool force_dense) {
 
 void DLRM::forward(uint32_t batch_index,
                    const dataset::ClickThroughBatch& inputs,
-                   BoltVector& output) {
+                   BoltVector& output, int layer_no) {
+  (void) layer_no;
   _bottom_mlp.forward(batch_index, inputs[batch_index],
                       _bottom_mlp_output[batch_index], nullptr);
 
--- bolt/src/networks/DLRM.h
+++ bolt/src/networks/DLRM.h
@@ -20,7 +20,7 @@ class DLRM final : public Model<dataset::ClickThroughBatch> {
 
  private:
   void forward(uint32_t batch_index, const dataset::ClickThroughBatch& inputs,
-               BoltVector& output) final;
+               BoltVector& output, int layer_no) final;
 
   void backpropagate(uint32_t batch_index, dataset::ClickThroughBatch& inputs,
                      BoltVector& output) final;
@@ -50,7 +50,7 @@ class DLRM final : public Model<dataset::ClickThroughBatch> {
 
   uint32_t outputDim() const final { return _top_mlp.outputDim(); }
 
-  BoltBatch getOutputs(uint32_t batch_size, bool force_dense) final {
+  BoltBatch getOutputs(uint32_t batch_size, bool force_dense, int layer_no = -1) final {
     return _top_mlp.getOutputs(batch_size, force_dense);
   }
 
