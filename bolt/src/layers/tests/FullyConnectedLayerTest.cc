351c31f0d6d764e12ddf8a8a812af2c4df80323f
Nicholas Meisburger <ncm5@rice.edu>
2022-03-22 11:06:22 -0500

    Make test stable

--- bolt/src/layers/tests/FullyConnectedLayerTest.cc
+++ bolt/src/layers/tests/FullyConnectedLayerTest.cc
@@ -38,7 +38,7 @@ class FullyConnectedLayerTestFixture : public testing::Test {
       : _rng(329),
         _layer(FullyConnectedLayerConfig(LAYER_DIM, 0.25,
                                          ActivationFunction::Linear,
-                                         SamplingConfig(1, 1, 3, 10)),
+                                         SamplingConfig(1, 64, 3, 10)),
                INPUT_DIM) {}
 
   void SetUp() override {
