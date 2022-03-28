b025cc98deff4552bc6b54e00388873d5bcd52cd
Josh Engels <josh@thirdai.com>
2022-03-28 16:46:43 -0500

    Slight changes to API

--- deps/ColBERT/colbert_wrapper.py
+++ deps/ColBERT/colbert_wrapper.py
@@ -4,13 +4,12 @@ from modeling.checkpoint import Checkpoint
 class Colbert:
     def __init__(self, checkpoint_path):
         self.checkpoint = Checkpoint(checkpoint_path).cpu()
-        print(self.checkpoint.colbert_config)
 
     def configure(self, **kw_args):
         self.config.configure(**kw_args)
 
-    def encodeQuery(self, queries):
-        return self.checkpoint.queryFromText(queries)
+    def encodeQuery(self, query):
+        return self.checkpoint.queryFromText([query]).numpy()[0]
 
-    def encodeDoc(self, docs):
-        return self.checkpoint.docFromText(docs)
+    def encodeDocs(self, docs):
+        return self.checkpoint.docFromText(docs).numpy()
