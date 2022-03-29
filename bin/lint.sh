c1b5017a205b967a88b32ee2d74ff0e01cb57106
Joshua Engels <josh@thirdai.com>
2022-03-29 14:11:35 -0500

    Cleaning up clang tidy, ended up doing this when adding eigen (#338)
    * Cleaning up clang tidy, ended up doing for eigen
    
    * Adding parenthesis in script
    
    * Fixing exit command
    
    * Adding empty SparseVector to fix linting problem
    
    * Reverted change to just lint source files again
    
    * Reverting changes to Vectors.h

--- bin/lint.sh
+++ bin/lint.sh
@@ -6,7 +6,7 @@ BASEDIR=$(dirname "$0")
 $BASEDIR/generate_compile_commands.sh
 
 # Check all files have pragma once
-NEED_PRAGMA=$(grep -L -r "pragma once" $BASEDIR/../. --include \*.h --exclude-dir=build)
+NEED_PRAGMA=$(grep -L -r "pragma once" $BASEDIR/../. --include \*.h --exclude-dir=build --exclude-dir=deps)
 if [ -n "$NEED_PRAGMA" ]
 then
   echo "The following files need pragma once:"
@@ -14,5 +14,20 @@ then
   exit 1
 fi
 
+# cd to Universe
 cd $BASEDIR/../
-python3 bin/run-clang-tidy.py
+
+num_failed=0
+
+# TODO(josh): Do we want to check headers too? Makes it take longer but 
+# could easily add -iname \*.h -o
+# Run clang tidy on all headers and source file, ignoring the build and deps directories
+# Note this will break if a filename has a new line in it
+find . -type f -name "*.cc" \
+  -not -path "./deps/*" -not -path "./build/*" \
+    | while read fname; do 
+      echo $fname 
+      clang-tidy $fname || ((num_failed++))
+    done
+
+exit $num_failed
