535d5563fc8807377e963deb37502e555afe3049
Josh Engels <josh@thirdai.com>
2022-03-30 15:11:21 -0500

    Fixing python formatting

--- flash/python_tests/test_doc_retrieval.py
+++ flash/python_tests/test_doc_retrieval.py
@@ -2,6 +2,8 @@ import pytest
 import thirdai
 from doc_retrieval_helpers import get_build_and_run_functions_random
 from doc_retrieval_helpers import get_build_and_run_functions_restful
+import numpy as np
+import time
 
 
 @pytest.mark.unit
@@ -56,7 +58,6 @@ def expect_error_on_construction(
 
 @pytest.mark.unit
 def test_error_inputs():
-    import time
 
     start = time.time()
     expect_error_on_construction(num_tables=0)
@@ -75,6 +76,76 @@ def test_error_inputs():
     # not e.g. build any big objects with invalid input
     assert end - start < 0.01
 
+
+@pytest.mark.unit
+def test_add_doc_find_centroids_is_fast():
+    """
+    The idea of this test is the time to add a doc without the centroids shouldn't
+    be that much more than the time to add a doc if we find the centroids with
+    numpy and pass them in (this uses the assumption that finding the centroids is
+    the slowest part). Note the query process uses the same code to find
+    the closest centroids so this also will test the performance of that code.
+    This could be flaky if suddenly the machine gets increased load in the middle
+    of the test, but this seems unlikely.
+    """
+
+    num_centroids = 2**18  # 262144
+    data_dim = 128
+    words_per_doc = 256
+    num_docs = 10
+    max_percent_slowdown = 0.05
+    centroids = np.random.rand(num_centroids, data_dim)
+    centroids_transposed = centroids.transpose().copy()
+    docs = np.random.rand(num_docs, words_per_doc, data_dim)
+
+    doc_index_precomputed_centroids = thirdai.search.DocRetrieval(
+        centroids=centroids,
+        hashes_per_table=8,
+        num_tables=8,
+        dense_input_dimension=data_dim,
+    )
+    doc_index_compute_centroids = thirdai.search.DocRetrieval(
+        centroids=centroids,
+        hashes_per_table=8,
+        num_tables=8,
+        dense_input_dimension=data_dim,
+    )
+
+    numpy_start = time.time()
+    centroid_ids_list = []
+    for doc in docs:
+        dot = doc.dot(centroids_transposed)
+        centroid_ids_list.append(np.argmax(dot, axis=1))
+    avg_numpy_time = (time.time() - numpy_start) / len(docs)
+
+    precomputed_start = time.time()
+    for i, (doc, centroid_ids) in enumerate(zip(docs, centroid_ids_list)):
+        doc_index_precomputed_centroids.add_doc(
+            doc_id=str(i),
+            doc_text="test",
+            doc_embeddings=np.array(doc),
+            doc_centroid_ids=centroid_ids.flatten(),
+        )
+    avg_precomputed_time = (time.time() - precomputed_start) / len(docs)
+
+    with_compute_start = time.time()
+    for i, doc in enumerate(docs):
+        doc_index_compute_centroids.add_doc(
+            doc_id=str(i), doc_text="test", doc_embeddings=np.array(doc)
+        )
+    avg_with_compute_time = (time.time() - with_compute_start) / len(docs)
+
+    print(avg_numpy_time, avg_precomputed_time, avg_with_compute_time)
+    print(
+        (avg_with_compute_time - avg_precomputed_time) / avg_numpy_time,
+        (1 + max_percent_slowdown),
+    )
+    assert (avg_with_compute_time - avg_precomputed_time) / avg_numpy_time < (
+        1 + max_percent_slowdown
+    )
+
+
+test_add_doc_find_centroids_is_fast()
 
 # TOOD(josh): Add the following tests:
 # 1. Test with lots of elements to ensure centroid reranking is reasonably fast
