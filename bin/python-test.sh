1d6196ae071c473345fdf5746fd084c3f9e4d40f
Joshua Engels <josh@thirdai.com>
2022-03-30 11:28:55 -0500

    Adding git submodules for each of our dependencies (#348)
    * Adding eigen
    
    * More dependencies
    
    * Removing pybind11 submodule briefly
    
    * Small changes
    
    * Adding pybind on correct branch
    
    * Adding recursive checkout to github actions
    
    * Removing comments
    
    * Adding secret token
    
    * Fixing format and docker problems
    
    * For some reason plain add isn't working in the slim Dockerfile
    
    * Finding python
    
    * Fixing python tests
    
    * Linking to notion and adding submodule explanation to notion

--- bin/python-test.sh
+++ bin/python-test.sh
@@ -5,4 +5,4 @@ BASEDIR=$(dirname "$0")
 # Run python tests. Switch to build directory first so we can use the so file.
 # Forward all arguments.
 cd $BASEDIR/../build/
-python3 -m pytest ../ -s "$@"
+python3 -m pytest ../ --ignore-glob=../deps -s "$@"
