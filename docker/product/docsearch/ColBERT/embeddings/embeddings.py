9b6e7fdc4ff7dde54d08d5f491abc4f98ef963f6
Josh <josh@thirdai.com>
2022-04-05 21:07:15 +0000

    Updating paths

--- docker/product/docsearch/ColBERT/embeddings/embeddings.py
+++ docker/product/docsearch/ColBERT/embeddings/embeddings.py
@@ -5,9 +5,9 @@ import numpy as np
 
 class DocSearchModel:
     def __init__(self):
-        checkpoint_path = "saved/downloads/colbertv2.0"
+        checkpoint_path = "/home/thirdai/saved/downloads/colbertv2.0"
         self.checkpoint = Checkpoint(checkpoint_path).cpu()
-        self.centroids = np.load("saved/downloads/centroids.npy")
+        self.centroids = np.load("/home/thirdai/saved/downloads/centroids.npy")
 
     def configure(self, **kw_args):
         self.config.configure(**kw_args)
