49c1742cfd44b01d11371ac3290a168c794c14fa
Vihan Lakshman <vihan@thirdai.com>
2022-04-05 16:57:19 +0000

    Add mlflow hooks for TF and pytorch

--- benchmarks/bolt_benchmarks/mnist/mnist_pytorch.py
+++ benchmarks/bolt_benchmarks/mnist/mnist_pytorch.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import datasets, transforms
+from mlflow_logger import ExperimentLogger
+
 torch.manual_seed(101)
 
 input_size = 784 # 28x28
@@ -11,18 +13,6 @@ num_epochs = 2
 batch_size = 100
 learning_rate = 0.001
 
-
-train_dataset = datasets.MNIST(root='./data',
-                                           train=True,
-                                           transform=transforms.ToTensor(),  
-                                           download=True)
-
-
-test_dataset = datasets.MNIST(root='./data', train=False, transform=transforms.ToTensor())
-
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
 
 class NeuralNet(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
@@ -40,37 +30,65 @@ class NeuralNet(nn.Module):
          return out
 
 
-model = NeuralNet(input_size, hidden_size, num_classes)
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate) 
+def train(train_loader, test_loader, mlflow_logger):
+    model = NeuralNet(input_size, hidden_size, num_classes)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate) 
 
-n_total_steps = len(train_loader)
-for epoch in range(num_epochs):
-    for i, (images, labels) in enumerate(train_loader):
-        images = images.reshape(-1, 28*28)
-        labels = labels
-        # Forward pass
-        outputs = model(images)
-        loss = criterion(outputs, labels)
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+    import mlflow.pytorch
+    mlflow.pytorch.autolog()
 
-        if (i+1) % 100 == 0:
-             print (f'Epoch [{epoch+1}/{num_epochs}], Step[{i+1}/{n_total_steps}], Loss: {loss.item():.4f}') 
+    n_total_steps = len(train_loader)
+    for epoch in range(num_epochs):
+        for i, (images, labels) in enumerate(train_loader):
+            images = images.reshape(-1, 28*28)
+            labels = labels
+            # Forward pass
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-with torch.no_grad():
-    n_correct = 0
-    n_samples = 0
-    for images, labels in test_loader:
-        images = images.reshape(-1, 28*28)
-        labels = labels
-        outputs = model(images)
-        # max returns (value ,index)
-        _, predicted = torch.max(outputs.data, 1)
-        n_samples += labels.size(0)
-        n_correct += (predicted == labels).sum().item()
+            if (i+1) % 100 == 0:
+                print (f'Epoch [{epoch+1}/{num_epochs}], Step[{i+1}/{n_total_steps}], Loss: {loss.item():.4f}') 
 
-acc = 100.0 * n_correct / n_samples
-print(f'Accuracy of the network on the 10000 test images: {acc} %') 
+    with torch.no_grad():
+        n_correct = 0
+        n_samples = 0
+        for images, labels in test_loader:
+            images = images.reshape(-1, 28*28)
+            labels = labels
+            outputs = model(images)
+            # max returns (value ,index)
+            _, predicted = torch.max(outputs.data, 1)
+            n_samples += labels.size(0)
+            n_correct += (predicted == labels).sum().item()
+
+    acc = 100.0 * n_correct / n_samples
+    print(f'Accuracy of the network on the 10000 test images: {acc} %') 
+
+def main():
+
+    train_dataset = datasets.MNIST(root='./data',
+                                            train=True,
+                                            transform=transforms.ToTensor(),  
+                                            download=True)
+
+
+    test_dataset = datasets.MNIST(root='./data', train=False, transform=transforms.ToTensor())
+
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
+
+    with ExperimentLogger(
+        experiment_name="MNIST Benchmark",
+        dataset="mnist",
+        algorithm="PyTorch",
+    ) as mlflow_logger:
+        train(train_loader, test_loader, mlflow_logger)
+
+
+if __name__ == "__main__":
+    main()
--- benchmarks/bolt_benchmarks/mnist/mnist_tensorflow.py
+++ benchmarks/bolt_benchmarks/mnist/mnist_tensorflow.py
@@ -1,7 +1,38 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
+from mlflow_logger import ExperimentLogger
 
-(ds_train, ds_test), ds_info = tfds.load(
+
+
+def normalize_img(image, label):
+  """Normalizes images: `uint8` -> `float32`."""
+  return tf.cast(image, tf.float32) / 255., label
+
+def train(ds_train, ds_test, mlflow_logger):
+    model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10)
+    ])
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(0.001),
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+    )
+
+    import mlflow.tensorflow
+    mlflow.tensorflow.autolog(every_n_iter=2)
+
+    model.fit(
+        ds_train,
+        epochs=6,
+        validation_data=ds_test,
+    )
+
+
+def main():
+
+    (ds_train, ds_test), ds_info = tfds.load(
     'mnist',
     split=['train', 'test'],
     shuffle_files=True,
@@ -9,38 +40,28 @@ import tensorflow_datasets as tfds
     with_info=True,
 )
 
-def normalize_img(image, label):
-  """Normalizes images: `uint8` -> `float32`."""
-  return tf.cast(image, tf.float32) / 255., label
+    ds_train = ds_train.map(normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+    ds_train = ds_train.cache()
+    ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
+    ds_train = ds_train.batch(128)
+    ds_train = ds_train.prefetch(tf.data.AUTOTUNE)
 
-ds_train = ds_train.map(
-    normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
-ds_train = ds_train.cache()
-ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
-ds_train = ds_train.batch(128)
-ds_train = ds_train.prefetch(tf.data.AUTOTUNE)
+    ds_test = ds_test.map(normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+    ds_test = ds_test.batch(128)
+    ds_test = ds_test.cache()
+    ds_test = ds_test.prefetch(tf.data.AUTOTUNE)
 
-ds_test = ds_test.map(
-    normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
-ds_test = ds_test.batch(128)
-ds_test = ds_test.cache()
-ds_test = ds_test.prefetch(tf.data.AUTOTUNE)
+    with ExperimentLogger(
+        experiment_name="MNIST Benchmark",
+        dataset="mnist",
+        algorithm="TensorFlow",
+    ) as mlflow_logger:
+        train(ds_train, ds_test, mlflow_logger)
 
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dense(10)
-])
-model.compile(
-    optimizer=tf.keras.optimizers.Adam(0.001),
-    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
-)
 
-model.fit(
-    ds_train,
-    epochs=6,
-    validation_data=ds_test,
-)
+
+if __name__ == "__main__":
+    main()
+
 
 
