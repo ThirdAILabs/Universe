08bde85ade213f707a5c57ccf8aee6489baf750c
Josh <josh@thirdai.com>
2022-04-05 20:41:24 +0000

    Reorganizing to make setup work

--- docker/product/docsearch/ColBERT/embeddings/embeddings.py
+++ docker/product/docsearch/ColBERT/embeddings/embeddings.py
@@ -5,10 +5,9 @@ import numpy as np
 
 class DocSearchModel:
     def __init__(self):
-        path_to_this_file = pathlib.Path(__file__).parent.resolve()
-        checkpoint_path = str(path_to_this_file) + "/downloads/colbertv2.0"
+        checkpoint_path = "saved/downloads/colbertv2.0"
         self.checkpoint = Checkpoint(checkpoint_path).cpu()
-        self.centroids = np.load(checkpoint_path + "/downloads/centroids.npy")
+        self.centroids = np.load("saved/downloads/centroids.npy")
 
     def configure(self, **kw_args):
         self.config.configure(**kw_args)
