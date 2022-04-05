d81081c9d0f42796cec682a7d13df893ae441157
Josh Engels <josh@thirdai.com>
2022-04-05 13:32:02 -0500

    Moving ColBERT to docker product dir

---
+++ docker/product/docsearch/ColBERT/embeddings.py
@@ -0,0 +1,23 @@
+from modeling.checkpoint import Checkpoint
+import pathlib
+import numpy as np
+
+class DocSearchModel:
+    def __init__(self, checkpoint_path=None):
+        if not checkpoint_path:
+          path_to_this_file = pathlib.Path(__file__).parent.resolve()
+          checkpoint_path = str(path_to_this_file) + "/downloads/colbertv2.0"
+        self.checkpoint = Checkpoint(checkpoint_path).cpu()
+        self.centroids = np.load(checkpoint_path + "/centroids.npy")
+
+    def configure(self, **kw_args):
+        self.config.configure(**kw_args)
+
+    def encodeQuery(self, query):
+        return self.checkpoint.queryFromText([query]).numpy()[0]
+
+    def encodeDocs(self, docs):
+        return self.checkpoint.docFromText(docs).numpy()
+
+    def getCentroids(self):
+      return self.centroids
--- docker/product/docsearch/download.sh
+++ docker/product/docsearch/download.sh
@@ -3,7 +3,10 @@
 BASEDIR=$(dirname "$0")
 cd $BASEDIR
 
-wget https://downloads.cs.stanford.edu/nlp/data/colbert/colbertv2/colbertv2.0.tar.gz -P downloads/
+
+wget https://downloads.cs.stanford.edu/nlp/data/colbert/colbertv2/colbertv2.0.tar.gz -P ColBERT/downloads/
 tar -xvzf downloads/colbertv2.0.tar.gz -C downloads/
+
+cp /share/josh/msmarco/centroids.npy 
 
 cp artifact.metadata downloads/colbertv2.0/
