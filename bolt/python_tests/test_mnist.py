2c78c63a20228244c5ab76acdc37da8af7b6b274
Nicholas Meisburger <nmeisburger@gmail.com>
2022-04-07 11:04:44 -0500

    Refactor bolt python tests

--- bolt/python_tests/test_mnist.py
+++ bolt/python_tests/test_mnist.py
@@ -1,9 +1,6 @@
 import os
-import time
 from thirdai import bolt, dataset
-import sys
 import pytest
-from collections import namedtuple
 
 LEARNING_RATE = 0.0001
 
@@ -22,267 +19,31 @@ def setup_module():
         os.system("bzip2 -d mnist.t.bz2")
 
 
-def train_mnist_sparse_output_layer(args):
+def build_sparse_output_layer_network():
     layers = [
-        bolt.LayerConfig(dim=256, activation_function=bolt.ActivationFunctions.ReLU),
+        bolt.LayerConfig(
+            dim=256, activation_function=bolt.ActivationFunctions.ReLU),
         bolt.LayerConfig(
             dim=10,
-            load_factor=args.sparsity,
+            load_factor=0.4,
             activation_function=bolt.ActivationFunctions.Softmax,
             sampling_config=bolt.SamplingConfig(
-                hashes_per_table=args.hashes_per_table,
-                num_tables=args.num_tables,
-                range_pow=args.hashes_per_table * 3,
+                hashes_per_table=1,
+                num_tables=32,
+                range_pow=3,
                 reservoir_size=10,
             ),
         ),
     ]
     network = bolt.Network(layers=layers, input_dim=784)
-
-    train_data = dataset.load_bolt_svm_dataset(args.train, 250)
-    test_data = dataset.load_bolt_svm_dataset(args.test, 250)
-    epoch_times = []
-    epoch_accuracies = []
-    for _ in range(args.epochs):
-        times = network.train(
-            train_data,
-            bolt.CategoricalCrossEntropyLoss(),
-            args.lr,
-            1,
-            rehash=3000,
-            rebuild=10000,
-            metrics=[],
-            verbose=False,
-        )
-        epoch_times.append(times["epoch_times"][0])
-        acc, _ = network.predict(
-            test_data, metrics=["categorical_accuracy"], verbose=False
-        )
-        epoch_accuracies.append(acc["categorical_accuracy"][0])
-
-    return epoch_accuracies[-1], epoch_accuracies, epoch_times
+    return network
 
 
-def train_mnist_sparse_hidden_layer(args):
+def build_sparse_hidden_layer_network(dim, sparsity):
     layers = [
         bolt.LayerConfig(
-            dim=20000,
-            load_factor=args.sparsity,
-            activation_function=bolt.ActivationFunctions.ReLU,
-            sampling_config=bolt.SamplingConfig(
-                hashes_per_table=args.hashes_per_table,
-                num_tables=args.num_tables,
-                range_pow=args.hashes_per_table * 3,
-                reservoir_size=32,
-            ),
-        ),
-        bolt.LayerConfig(dim=10, activation_function=bolt.ActivationFunctions.Softmax),
-    ]
-    network = bolt.Network(layers=layers, input_dim=784)
-
-    train_data = dataset.load_bolt_svm_dataset(args.train, 250)
-    test_data = dataset.load_bolt_svm_dataset(args.test, 250)
-    epoch_times = []
-    epoch_accuracies = []
-    for _ in range(args.epochs):
-        times = network.train(
-            train_data,
-            bolt.CategoricalCrossEntropyLoss(),
-            args.lr,
-            1,
-            rehash=3000,
-            rebuild=10000,
-            metrics=[],
-            verbose=False,
-        )
-        epoch_times.append(times["epoch_times"][0])
-        acc, _ = network.predict(
-            test_data, metrics=["categorical_accuracy"], verbose=False
-        )
-        epoch_accuracies.append(acc["categorical_accuracy"][0])
-    return epoch_accuracies[-1], epoch_accuracies, epoch_times
-
-
-def train_mnist_sparse_hidden_inference(args):
-    layers = [
-        bolt.LayerConfig(
-            dim=20000,
-            load_factor=args.sparsity,
-            activation_function=bolt.ActivationFunctions.ReLU,
-            sampling_config=bolt.SamplingConfig(
-                hashes_per_table=args.hashes_per_table,
-                num_tables=args.num_tables,
-                range_pow=args.hashes_per_table * 3,
-                reservoir_size=32,
-            ),
-        ),
-        bolt.LayerConfig(dim=10, activation_function=bolt.ActivationFunctions.Softmax),
-    ]
-    network = bolt.Network(layers=layers, input_dim=784)
-
-    train_data = dataset.load_bolt_svm_dataset(args.train, 250)
-    test_data = dataset.load_bolt_svm_dataset(args.test, 250)
-    epoch_times = []
-    inf_times = []
-    epoch_accuracies = []
-    for i in range(args.epochs):
-        if i == (args.epochs - 1):
-            network.enable_sparse_inference()
-
-        times = network.train(
-            train_data,
-            bolt.CategoricalCrossEntropyLoss(),
-            args.lr,
-            1,
-            rehash=3000,
-            rebuild=10000,
-            metrics=[],
-            verbose=False,
-        )
-        epoch_times.append(times["epoch_times"][0])
-        t0 = time.time()
-        acc, _ = network.predict(
-            test_data, metrics=["categorical_accuracy"], verbose=False
-        )
-        t1 = time.time()
-        inf_times.append(t1 - t0)
-        epoch_accuracies.append(acc["categorical_accuracy"][0])
-    return epoch_accuracies[-1], epoch_accuracies, epoch_times, inf_times
-
-
-def train(
-    args,
-    train_fn,
-    accuracy_threshold,
-    epoch_time_threshold=100,
-    total_time_threshold=10000,
-):
-    final_accuracies = []
-    final_epoch_times = []
-    total_times = []
-
-    for _ in range(args.runs):
-
-        final_accuracy, accuracies_per_epoch, time_per_epoch = train_fn(args)
-        final_accuracies.append(final_accuracy)
-        final_epoch_times.append(time_per_epoch[-1])
-        total_times.append(sum(time_per_epoch))
-
-        print(
-            f"Result of training {args.dataset} for {args.epochs} epochs:\n\tFinal epoch accuracy: {final_accuracy}\n\tFinal epoch time: {time_per_epoch}"
-        )
-
-        assert final_accuracies[-1] > accuracy_threshold
-        assert final_epoch_times[-1] < epoch_time_threshold
-        assert total_times[-1] < total_time_threshold
-
-    return final_accuracies, final_epoch_times
-
-
-def train_with_inference(
-    args,
-    train_fn,
-    accuracy_threshold,
-    speed_multiplier_threshold=5,
-    epoch_time_threshold=100,
-    total_time_threshold=10000,
-):
-    final_accuracies = []
-    final_epoch_times = []
-    total_times = []
-
-    for _ in range(args.runs):
-
-        final_accuracy, accuracies_per_epoch, time_per_epoch, inf_times = train_fn(args)
-        final_accuracies.append(final_accuracy)
-        final_epoch_times.append(time_per_epoch[-1])
-        total_times.append(sum(time_per_epoch))
-
-        print(
-            f"Result of training {args.dataset} for {args.epochs} epochs:\n\tFinal epoch accuracy: {final_accuracy}\n\tFinal epoch time: {time_per_epoch}"
-        )
-
-        assert final_accuracies[-1] > accuracy_threshold
-        assert final_epoch_times[-1] < epoch_time_threshold
-        assert total_times[-1] < total_time_threshold
-        sparse_inf_time = inf_times[-1]
-        avg_dense_inf_time = sum(inf_times[:-1]) / (args.epochs - 1)
-        assert avg_dense_inf_time >= sparse_inf_time * speed_multiplier_threshold
-
-    return final_accuracies, final_epoch_times
-
-
-@pytest.mark.integration
-def test_mnist_sparse_output():
-    args = {
-        "dataset": "mnist",
-        "train": "mnist",
-        "test": "mnist.t",
-        "epochs": 10,
-        "hashes_per_table": 1,
-        "num_tables": 32,
-        "sparsity": 0.4,
-        "lr": LEARNING_RATE,
-        "enable_checks": True,
-        "runs": 1,
-    }
-    # Turn the dictionary into the format expected by the train method, fields
-    # referencable by e.g. args.train
-    args = namedtuple("args", args.keys())(*args.values())
-    accs, times = train(
-        args, train_fn=train_mnist_sparse_output_layer, accuracy_threshold=0.95
-    )
-
-
-@pytest.mark.integration
-def test_mnist_sparse_hidden():
-    args = {
-        "dataset": "mnist",
-        "train": "mnist",
-        "test": "mnist.t",
-        "epochs": 10,
-        "hashes_per_table": 3,
-        "num_tables": 64,
-        "sparsity": 0.01,
-        "lr": LEARNING_RATE,
-        "enable_checks": True,
-        "runs": 1,
-    }
-    args = namedtuple("args", args.keys())(*args.values())
-    accs, times = train(
-        args, train_fn=train_mnist_sparse_hidden_layer, accuracy_threshold=0.95
-    )
-
-
-@pytest.mark.integration
-def test_mnist_sparse_inference():
-    args = {
-        "dataset": "mnist",
-        "train": "mnist",
-        "test": "mnist.t",
-        "epochs": 4,
-        "hashes_per_table": 3,
-        "num_tables": 64,
-        "sparsity": 0.01,
-        "lr": 0.0001,
-        "enable_checks": True,
-        "runs": 1,
-    }
-    args = namedtuple("args", args.keys())(*args.values())
-
-    accs, times = train_with_inference(
-        args,
-        train_fn=train_mnist_sparse_hidden_inference,
-        accuracy_threshold=0.90,
-        speed_multiplier_threshold=5,
-    )
-
-
-def test_load_save_fc_network():
-    layers = [
-        bolt.LayerConfig(
-            dim=1000,
-            load_factor=0.2,
+            dim=dim,
+            load_factor=sparsity,
             activation_function=bolt.ActivationFunctions.ReLU,
             sampling_config=bolt.SamplingConfig(
                 hashes_per_table=3,
@@ -291,23 +52,116 @@ def test_load_save_fc_network():
                 reservoir_size=32,
             ),
         ),
-        bolt.LayerConfig(dim=10, activation_function=bolt.ActivationFunctions.Softmax),
+        bolt.LayerConfig(
+            dim=10, activation_function=bolt.ActivationFunctions.Softmax),
     ]
     network = bolt.Network(layers=layers, input_dim=784)
+    return network
 
-    train_data = dataset.load_bolt_svm_dataset("mnist", 250)
-    test_data = dataset.load_bolt_svm_dataset("mnist.t", 250)
 
+def train_network(network, train_data, epochs, learning_rate=LEARNING_RATE):
     times = network.train(
         train_data,
         bolt.CategoricalCrossEntropyLoss(),
-        LEARNING_RATE,
-        2,
+        learning_rate,
+        epochs,
         rehash=3000,
         rebuild=10000,
         metrics=[],
-        verbose=False,
+        verbose=True
     )
+    return times
+
+
+def load_mnist():
+    train_data = dataset.load_bolt_svm_dataset("mnist", 250)
+    test_data = dataset.load_bolt_svm_dataset("mnist.t", 250)
+    return train_data, test_data
+
+
+@pytest.mark.integration
+def test_mnist_sparse_output_layer():
+    network = build_sparse_output_layer_network()
+
+    train, test = load_mnist()
+
+    train_network(network, train_data=train, epochs=10)
+
+    acc, _ = network.predict(
+        test, metrics=["categorical_accuracy"], verbose=False
+    )
+
+    assert acc['categorical_accuracy'][0] >= 0.95
+
+
+@pytest.mark.integration
+def test_mnist_sparse_hidden_layer():
+    network = build_sparse_hidden_layer_network(20000, 0.01)
+
+    train, test = load_mnist()
+
+    train_network(network, train_data=train, epochs=10)
+
+    acc, _ = network.predict(
+        test, metrics=["categorical_accuracy"], verbose=False
+    )
+
+    assert acc['categorical_accuracy'][0] >= 0.95
+
+
+@pytest.mark.integration
+def test_mnist_sparse_inference():
+    network = build_sparse_hidden_layer_network(20000, 0.01)
+
+    train, test = load_mnist()
+
+    # train_network(network, train_data=train, epochs=10)
+    for i in range(10):
+        network.train(
+            train,
+            bolt.CategoricalCrossEntropyLoss(),
+            LEARNING_RATE,
+            1,
+            rehash=3000,
+            rebuild=10000,
+            metrics=[],
+            verbose=True
+        )
+        dense_predict, _ = network.predict(
+            test, metrics=["categorical_accuracy"], verbose=True
+        )
+
+    dense_predict, _ = network.predict(
+        test, metrics=["categorical_accuracy"], verbose=True
+    )
+
+    assert dense_predict['categorical_accuracy'][0] >= 0.95
+
+    network.enable_sparse_inference()
+
+    train_network(network, train_data=train, epochs=1)
+
+    sparse_predict, _ = network.predict(
+        test, metrics=["categorical_accuracy"], verbose=True
+    )
+
+    assert sparse_predict["categorical_accuracy"][0] >= 0.9
+
+    dense_time = dense_predict["test_time"][0]
+    sparse_time = sparse_predict["test_time"][0]
+
+    SPARSE_INFERENCE_SPEED_MULTIPLIER = 5
+
+    assert (sparse_time * SPARSE_INFERENCE_SPEED_MULTIPLIER) < dense_time
+
+
+@pytest.mark.integration
+def test_load_save_fc_network():
+    network = build_sparse_hidden_layer_network(1000, 0.2)
+
+    train_data, test_data = load_mnist()
+
+    train_network(network, train_data=train_data, epochs=2)
 
     original_acc, _ = network.predict(
         test_data, metrics=["categorical_accuracy"], verbose=False
@@ -318,6 +172,7 @@ def test_load_save_fc_network():
     if os.path.exists(save_loc):
         os.remove(save_loc)
 
+    # Save network and load as a new network
     network.save(save_loc)
 
     new_network = bolt.Network.load(save_loc)
@@ -329,16 +184,7 @@ def test_load_save_fc_network():
     assert new_acc["categorical_accuracy"][0] == original_acc["categorical_accuracy"][0]
 
     # Continue to train loaded network
-    new_network.train(
-        train_data,
-        bolt.CategoricalCrossEntropyLoss(),
-        LEARNING_RATE,
-        2,
-        rehash=3000,
-        rebuild=10000,
-        metrics=[],
-        verbose=False,
-    )
+    train_network(new_network, train_data=train_data, epochs=2)
 
     another_acc, _ = new_network.predict(
         test_data, metrics=["categorical_accuracy"], verbose=False
