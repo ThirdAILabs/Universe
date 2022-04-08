fdc48775a6d9f8829bbce3979d1d76be4c3ee79f
Josh Engels <josh@thirdai.com>
2022-04-08 13:11:04 -0500

    Renaming more scripts

--- docker/product/build_all_product_dockers.sh
+++ docker/product/build_all_product_dockers.sh
@@ -7,10 +7,10 @@ cd $BASEDIR
 BASEDIR=$(pwd)
 
 slim/build_image.sh $1
-base_jupyter/build_image.sh $1
+base_jupyter/build_interactive_image.sh $1
 docsearch/build_interactive_image.sh $1
 docsearch/build_inference_image.sh $1
-text_classification/build_image.sh $1
+text_classification/build_interactive_image.sh $1
 
 # Ensure all images built correctly
 REV_TAG=$(git log -1 --pretty=format:%h)
