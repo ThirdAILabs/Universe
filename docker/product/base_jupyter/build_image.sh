75b17f49498ce4b769250a91eb3e7fcc1d41d5a1
Josh Engels <josh@thirdai.com>
2022-04-08 13:09:27 -0500

    Updating base jupter and slim dockers too

--- docker/product/base_jupyter/build_image.sh
+++ docker/product/base_jupyter/build_image.sh
@@ -15,7 +15,7 @@ cd $BASEDIR
 echo "================================================================="
 echo "Building docker image thirdai_jupyter_interactive:${REV_TAG} with platform $PLATFORM"
 echo "================================================================="
-docker build --platform=$PLATFORM --build-arg REV_TAG=${REV_TAG} . -t thirdai_jupyter_interactive:${REV_TAG}
+docker build --platform=$PLATFORM --build-arg REV_TAG=${REV_TAG} . -t thirdai_jupyter_interactive:${REV_TAG} -f Interactive.Dockerfile
 echo "================================================================="
 echo "Built docker image thirdai_jupyter_interactive:${REV_TAG} with platform $PLATFORM"
 echo "================================================================="
