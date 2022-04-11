ffad2fe59a8e5633c749330722783c2690c236a7
Josh <josh@thirdai.com>
2022-04-11 23:03:24 +0000

    Fixing logging and changing not to run on our own self hosting for now

--- docker/product/docsearch/docsearch_flask_app.py
+++ docker/product/docsearch/docsearch_flask_app.py
@@ -17,32 +17,32 @@ import logging
 
 gunicorn_logger = logging.getLogger("gunicorn.error")
 app.logger.handlers = gunicorn_logger.handlers
-app.logger.setLevel(logging.DEBUG)
+app.logger.setLevel(logging.INFO)
 
-app.logger.debug("Loading index and model")
+app.logger.info("Loading index and model")
 embedding_model = DocSearchModel()
 index_to_query = DocRetrieval.deserialize_from_file("/home/thirdai/index")
-app.logger.debug("Index and model loaded")
+app.logger.info("Index and model loaded")
 
 
 @app.route("/documents", methods=["GET"])
 def perform_query_top_1():
 
-    app.logger.debug("Handling document search query")
-
     if "query" not in request.args:
-        app.logger.debug(f"query not in args, args were {request.args}, aborting")
+        app.logger.info(f"query not in args, args were {request.args}, aborting")
         abort(400)
 
     if "top_k" not in request.args:
-        app.logger.debug(f"top_k not in args, args were {request.args}, aborting")
+        app.logger.info(f"top_k not in args, args were {request.args}, aborting")
         abort(400)
 
     query_text = request.args.get("query")
 
     top_k = int(request.args.get("top_k"))
 
-    app.logger.debug(f'Query text is "{query_text}"')
+    app.logger.info(
+        f'Handling document search query: query="{query_text}", top_k="{top_k}"'
+    )
 
     query_embedding = embedding_model.encodeQuery(query_text)
 
@@ -52,6 +52,10 @@ def perform_query_top_1():
 
     result = index_to_query.query(query_embedding, top_k=internal_top_k)
 
-    # We return the second element in each item of the results because
-    # the first element is the doc_id and the second is the doc_text
-    return jsonify(results=[r[1] for r in result[:top_k]])
+    app.logger.info(
+        f'For query="{query_text}", found {len(result)} results, returning {min(len(result), top_k)} results'
+    )
+
+    return jsonify(
+        doc_ids=[r[0] for r in result[:top_k]], doc_texts=[r[1] for r in result[:top_k]]
+    )
