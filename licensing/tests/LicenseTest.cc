80f9dc33c635488deea5f9ade11dd914b850b087
Josh Engels <josh@thirdai.com>
2022-04-14 17:16:26 -0500

    We have a failing test!

--- licensing/tests/LicenseTest.cc
+++ licensing/tests/LicenseTest.cc
@@ -1,14 +1,14 @@
-// #include <gtest/gtest.h>
+#include <gtest/gtest.h>
 #include <cryptopp/osrng.h>
 #include <cryptopp/rsa.h>
-// #include <licensing/src/DecodeLicense.h>
-// #include <licensing/src/EncodeLicense.h>
-// #include <licensing/src/License.h>
+#include <licensing/src/DecodeLicense.h>
+#include <licensing/src/EncodeLicense.h>
+#include <licensing/src/License.h>
 #include <unordered_map>
 
 namespace thirdai::licensing {
 
-void test() {
+TEST(LicenseTest, EncodeDecodeTest) {
   // See https://www.cryptopp.com/wiki/RSA_Cryptography (much of this code is
   // taken from their examples)
 
@@ -26,19 +26,19 @@ void test() {
   // Create License
   std::unordered_map<std::string, std::string> metadata{
       {"company", "abc"}, {"person", "def"}, {"machine type", "ghi"}};
-  // License license = License::createLicenseWithNDaysLeft(metadata, 30);
+  License license = License::createLicenseWithNDaysLeft(metadata, 30);
 
-  // // Encode License
-  // auto encoded_license = encodeLicense(license, private_key);
+  // Encode License
+  auto encoded_license = encodeLicense(license, private_key);
 
-  // // Decode License
-  // License decoded_license = decodeLicense(encoded_license, public_key);
+  // Decode License
+  License decoded_license = decodeLicense(encoded_license, public_key);
 
   // Assert License was decode correctly
-  // ASSERT_EQ(decoded_license.getExpireTimeMillis(), license.getExpireTimeMillis());
-  // ASSERT_EQ(decoded_license.getMetadataValue("company"), "abc");
-  // ASSERT_EQ(decoded_license.getMetadataValue("person"), "def");
-  // ASSERT_EQ(decoded_license.getMetadataValue("machine_type"), "ghi");
+  ASSERT_EQ(decoded_license.getExpireTimeMillis(), license.getExpireTimeMillis());
+  ASSERT_EQ(decoded_license.getMetadataValue("company"), "abc");
+  ASSERT_EQ(decoded_license.getMetadataValue("person"), "def");
+  ASSERT_EQ(decoded_license.getMetadataValue("machine_type"), "ghi");
 }
 
 }  // namespace thirdai::licensing
