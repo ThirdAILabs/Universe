220d69cc7b3d0d4547227ee2d4489d5e19d9af7b
David Torres <david@thirdai.com>
2022-04-27 14:13:54 -0500

    push to blade

--- bolt/experiments/TryConv.py
+++ bolt/experiments/TryConv.py
@@ -1,31 +1,40 @@
-from thirdai import bolt
+from thirdai import bolt, dataset
 
-def _create_conv_network():
+# TODO: make sure it works with birds
+# TODO: calculate num patches in conv layer
+# TODO: default sampling config when its not passed in
+# TODO: integrate with MLFlow
+
+def _define_network(args):
     layers = [
         bolt.Conv(
-            dim=200,
+            num_filters=200,
             load_factor=1,
             activation_function=bolt.ActivationFunctions.ReLU,
-            sampling_config=bolt.SamplingConfig(
-                hashes_per_table=3, num_tables=64, range_pow=9, reservoir_size=5
-            ),
+            sampling_config=bolt.SamplingConfig(),
+            kernel_size=(4, 4),
+            num_patches=3136,
         ),
-        bolt.Conv(
-            dim=400,
-            load_factor=0.1,
-            activation_function=bolt.ActivationFunctions.ReLU,
-            sampling_config=bolt.SamplingConfig(
-                hashes_per_table=3, num_tables=64, range_pow=9, reservoir_size=5
-            ),
-        ),
-        bolt.Conv(
-            dim=800,
-            load_factor=0.05,
-            activation_function=bolt.ActivationFunctions.ReLU,
-            sampling_config=bolt.SamplingConfig(
-                hashes_per_table=3, num_tables=64, range_pow=9, reservoir_size=5
-            ),
-        ),
+        # bolt.Conv(
+        #     num_filters=400,
+        #     load_factor=0.1,
+        #     activation_function=bolt.ActivationFunctions.ReLU,
+        #     sampling_config=bolt.SamplingConfig(
+        #         hashes_per_table=3, num_tables=64, range_pow=9, reservoir_size=5
+        #     ),
+        #     kernel_size=(4, 4),
+        #     num_patches=196,
+        # ),
+        # bolt.Conv(
+        #     num_filters=800,
+        #     load_factor=0.05,
+        #     activation_function=bolt.ActivationFunctions.ReLU,
+        #     sampling_config=bolt.SamplingConfig(
+        #         hashes_per_table=3, num_tables=64, range_pow=9, reservoir_size=5
+        #     ),
+        #     kernel_size=(2, 2),
+        #     num_patches=49,
+        # ),
         bolt.FullyConnected(
             dim=20000, 
             load_factor=0.05,
@@ -40,10 +49,31 @@ def _create_conv_network():
     network = bolt.Network(layers=layers, input_dim=224*224*3)
     return network
      
+def train_conv_birds_325():
+    network = _define_network()
 
+    train_data = dataset.load_bolt_svm_dataset(args.train, 256)
+    test_data = dataset.load_bolt_svm_dataset(args.test, 256)
+
+    for _ in range(args.epochs):
+        network.train(
+            train_data,
+            bolt.CategoricalCrossEntropyLoss(),
+            args.lr,
+            epochs=1,
+            rehash=6400,
+            rebuild=128000,
+        )
+        acc, __ = network.predict(
+            test_data, metrics=["categorical_accuracy"], verbose=False
+        )
+
+    final_accuracy, __ = network.predict(
+        test_data, metrics=["categorical_accuracy"], verbose=False
+    )
 
 def main():
-    pass
+    train_conv_birds_325()
 
 if __name__ == "__main__":
     main()
