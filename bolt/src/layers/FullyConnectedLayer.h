914279328ea38b6da804aae2e94a6b7231ad400c
Joshua Engels <josh@thirdai.com>
2022-04-27 16:15:40 -0500

    Splitting updateParameters into sparse/dense prev/current layer cases (#448)
    * Did this get slower?
    
    * This is kind of ugly but I'm interested in feedback
    
    * Removing extra scripts
    
    * Responding to comments

--- bolt/src/layers/FullyConnectedLayer.h
+++ bolt/src/layers/FullyConnectedLayer.h
@@ -106,16 +106,54 @@ class FullyConnectedLayer final : public SequentialLayer {
   std::vector<float> _b_momentum;
   std::vector<float> _b_velocity;
 
-  std::vector<bool> _is_active;
-
   SamplingConfig _sampling_config;
   std::unique_ptr<hashing::DWTAHashFunction> _hasher;
   std::unique_ptr<hashtable::SampledHashTable<uint32_t>> _hash_table;
   std::vector<uint32_t> _rand_neurons;
 
+  using ActiveNeuronsPair =
+      std::pair<std::vector<uint64_t>, std::vector<uint64_t>>;
+
+  bool _prev_is_dense;
+  bool _this_is_dense;
+  // This is only used if _prev_is_dense == false and _this_is_dense == false
+  // This is a vector of unique_ptr so that the push_back in the critical
+  // region is just a pointer move and can be very fast
+  std::vector<std::unique_ptr<ActiveNeuronsPair>> _active_pairs;
+  // This is only used if _prev_is_dense == false
+  std::vector<bool> _prev_is_active;
+  // This is only used if _this_is_dense == false
+  std::vector<bool> _is_active;
+
   bool _force_sparse_for_inference;
 
  private:
+  inline void updateSparseSparseWeightParameters(float lr, float B1, float B2,
+                                                 float eps,
+                                                 float B1_bias_corrected,
+                                                 float B2_bias_corrected);
+  inline void updateSparseDenseWeightParameters(float lr, float B1, float B2,
+                                                float eps,
+                                                float B1_bias_corrected,
+                                                float B2_bias_corrected);
+  inline void updateDenseSparseWeightParameters(float lr, float B1, float B2,
+                                                float eps,
+                                                float B1_bias_corrected,
+                                                float B2_bias_corrected);
+  inline void updateDenseDenseWeightParameters(float lr, float B1, float B2,
+                                               float eps,
+                                               float B1_bias_corrected,
+                                               float B2_bias_corrected);
+  inline void updateSingleWeightParameters(uint64_t prev_neuron,
+                                           uint64_t cur_neuron, float lr,
+                                           float B1, float B2, float eps,
+                                           float B1_bias_corrected,
+                                           float B2_bias_corrected);
+  inline void updateBiasParameters(float lr, float B1, float B2, float eps,
+                                   float B1_bias_corrected,
+                                   float B2_bias_corrected);
+  inline void cleanupWithinBatchVars();
+
   template <bool DENSE, bool PREV_DENSE>
   void forwardImpl(const BoltVector& input, BoltVector& output,
                    const BoltVector* labels);
@@ -133,8 +171,9 @@ class FullyConnectedLayer final : public SequentialLayer {
   void serialize(Archive& archive) {
     archive(_dim, _prev_dim, _sparse_dim, _sparsity, _act_func, _weights,
             _w_gradient, _w_momentum, _w_velocity, _biases, _b_gradient,
-            _b_momentum, _b_velocity, _is_active, _sampling_config, _hasher,
-            _hash_table, _rand_neurons, _force_sparse_for_inference);
+            _b_momentum, _b_velocity, _sampling_config, _prev_is_active,
+            _is_active, _hasher, _hash_table, _rand_neurons,
+            _force_sparse_for_inference);
   }
 };
 
