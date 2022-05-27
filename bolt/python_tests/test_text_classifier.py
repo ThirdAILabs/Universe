3c1bdac6df5d9feb856a493924148251d41aba27
Nicholas Meisburger <nmeisburger@gmail.com>
2022-05-26 19:30:37 -0500

    python format

--- bolt/python_tests/test_text_classifier.py
+++ bolt/python_tests/test_text_classifier.py
@@ -11,21 +11,21 @@ PREDICTION_FILE = "./clinc_predictions.txt"
 
 
 def write_dataset_to_csv(dataset, filename, return_labels=False):
-    label_names = dataset.features['intent'].names
+    label_names = dataset.features["intent"].names
 
     data = []
     for item in dataset:
-        sentence = item['text']
-        label = item['intent']
+        sentence = item["text"]
+        label = item["intent"]
         label_name = label_names[label]
         data.append((sentence, label_name))
 
     random.shuffle(data)
 
-    with open(filename, 'w') as file:
-        file.write("\"text\",\"category\"\n")
+    with open(filename, "w") as file:
+        file.write('"text","category"\n')
         lines = [
-            "\"{}\",\"{}\"\n".format(sentence, label_name) for sentence, label_name in data
+            '"{}","{}"\n'.format(sentence, label_name) for sentence, label_name in data
         ]
         file.writelines(lines)
 
@@ -37,8 +37,7 @@ def write_dataset_to_csv(dataset, filename, return_labels=False):
 def download_clinc_dataset():
     clinc_dataset = datasets.load_dataset("clinc_oos", "small")
     write_dataset_to_csv(clinc_dataset["train"], TRAIN_FILE)
-    labels = write_dataset_to_csv(
-        clinc_dataset["test"], TEST_FILE, return_labels=True)
+    labels = write_dataset_to_csv(clinc_dataset["test"], TEST_FILE, return_labels=True)
 
     return (clinc_dataset["train"].features["intent"].num_classes, labels)
 
@@ -70,15 +69,9 @@ def test_text_classifier_clinc_dataset():
     (n_classes, test_labels) = download_clinc_dataset()
     classifier = bolt.TextClassifier(model_size="small", n_classes=n_classes)
 
-    classifier.train(
-        train_file=TRAIN_FILE,
-        epochs=5
-    )
+    classifier.train(train_file=TRAIN_FILE, epochs=5)
 
-    classifier.predict(
-        test_file=TEST_FILE,
-        output_file=PREDICTION_FILE
-    )
+    classifier.predict(test_file=TEST_FILE, output_file=PREDICTION_FILE)
 
     acc = compute_accuracy(test_labels, PREDICTION_FILE)
 
