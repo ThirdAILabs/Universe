45be117febc4d45dfbf51bbe3bd3e789634b67ee
Pratikq Pranav <pratikqpranav@Pratikqs-MacBook-Air.local>
2022-06-02 19:48:28 +0530

    Changed Test to run on windows too

--- bolt/python_tests/test_ctrl_c_event.py
+++ bolt/python_tests/test_ctrl_c_event.py
@@ -8,27 +8,26 @@ import sys
 
 def test_ctrl_c_functionality():
     # started a subprocesss to run a file to which SIGINT needed to be sent
-    if sys.platform.startswith("linux") or sys.platform.startswith("darwin"):
-        proc = subprocess.Popen(
-            ["python3", "../bolt/python_tests/ctrl_c_executable.py"],
-            shell=False,
-            stdout=DEVNULL,
-            stderr=DEVNULL,
-        )
+    proc = subprocess.Popen(
+        ["python3", "../bolt/python_tests/ctrl_c_executable.py"],
+        shell=False,
+        stdout=DEVNULL,
+        stderr=DEVNULL,
+    )
 
-        # making sure the program reaches the train function
-        time.sleep(3)
-        # sending the SIGINT Signal to the subprocess
+    # making sure the program reaches the train function
+    time.sleep(3)
+    # sending the SIGINT Signal to the subprocess
 
-        proc.send_signal(signal.SIGINT)
-        time.sleep(2)
-        # checking the status of the process, if it is still running,
-        # means that the SIGINT is not handled by the process, else
-        # it should be handled by the process
-        poll = proc.poll()
-        proc.kill()
-        proc.wait()
-        if poll is None:
-            assert False, f"CTRL+C Functionality not working correctly"
-        else:
-            assert True
+    proc.send_signal(signal.SIGINT)
+    time.sleep(2)
+    # checking the status of the process, if it is still running,
+    # means that the SIGINT is not handled by the process, else
+    # it should be handled by the process
+    poll = proc.poll()
+    proc.kill()
+    proc.wait()
+    if poll is None:
+        assert False, f"CTRL+C Functionality not working correctly"
+    else:
+        assert True
