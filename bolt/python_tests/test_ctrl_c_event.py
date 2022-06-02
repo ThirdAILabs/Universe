e884913ca0094654aff554d63ce1ade90377053b
Pratikq Pranav <pratikqpranav@Pratikqs-MacBook-Air.local>
2022-06-02 14:04:45 +0530

    Added a check the test ran only on linux and darwin

--- bolt/python_tests/test_ctrl_c_event.py
+++ bolt/python_tests/test_ctrl_c_event.py
@@ -4,34 +4,31 @@ import time
 import os
 from subprocess import DEVNULL
 import pytest
+import sys
 
-pytestmark = [pytest.mark.unit]
-
-@pytest.mark.unit
 def test_ctrl_c_functionality():
     # started a subprocesss to run a file to which SIGINT needed to be sent
-    proc = subprocess.Popen(
-        ["python3", "../bolt/python_tests/ctrl_c_executable.py"],
-        shell=False
-    )
+    if sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
+        proc = subprocess.Popen(
+            ["python3", "../bolt/python_tests/ctrl_c_executable.py"],
+            shell=False
+        )
 
-    # making sure the program reaches the train function
-    time.sleep(3)
-    # sending the SIGINT Signal to the subprocess
+        # making sure the program reaches the train function
+        time.sleep(3)
+        # sending the SIGINT Signal to the subprocess
 
-    proc.send_signal(signal.SIGINT)
-    time.sleep(2)
-    # checking the status of the process, if it is still running,
-    # means that the SIGINT is not handled by the process, else
-    # it should be handled by the process
-    poll = proc.poll()
-    if poll is None:
+        proc.send_signal(signal.SIGINT)
+        time.sleep(2)
+        # checking the status of the process, if it is still running,
+        # means that the SIGINT is not handled by the process, else
+        # it should be handled by the process
+        poll = proc.poll()
         proc.kill()
-        assert False, f"CTRL+C Functionality not working correctly"
-    elif poll < 0:
-        proc.kill()
-        assert True
-    else:
-        assert True
+        proc.wait()
+        if poll is None:
+            assert False, f"CTRL+C Functionality not working correctly"
+        else:
+            assert True
 
 
