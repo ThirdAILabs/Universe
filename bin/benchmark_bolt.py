fe97247dd50acd734220a052d330525ccfb6c147
Vihan Lakshman <vihan@thirdai.com>
2022-06-03 22:04:18 +0000

    Python formatting

--- bin/benchmark_bolt.py
+++ bin/benchmark_bolt.py
@@ -5,13 +5,16 @@ import os
 from datetime import date
 from pathlib import Path
 
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Build a target in Universe")
-    parser.add_argument("-l", "--configs", nargs='+', help="List of config file paths to benchmark")
+    parser.add_argument(
+        "-l", "--configs", nargs="+", help="List of config file paths to benchmark"
+    )
     args = parser.parse_args()
     return args
 
-    
+
 def main():
     cur_date = str(date.today())
     args = parse_args()
@@ -19,7 +22,9 @@ def main():
     for config in args.configs:
         p = Path(config)
         run_name = f"benchmark_{p.stem}_{cur_date}"
-        os.system(f"python3 ../benchmarks/bolt_benchmarks/bolt.py {config} --run_name {run_name}")
+        os.system(
+            f"python3 ../benchmarks/bolt_benchmarks/bolt.py {config} --run_name {run_name}"
+        )
 
 
 if __name__ == "__main__":
