ad5c594fd95190448029445603b7cf65c2dc2158
Nicholas Meisburger <nmeisburger@gmail.com>
2022-06-07 10:27:45 -0500

    comments

--- bolt/src/graph/Graph.h
+++ bolt/src/graph/Graph.h
@@ -17,28 +17,51 @@ using InputPtr = std::shared_ptr<Input>;
 
 class GraphContext : public std::enable_shared_from_this<GraphContext> {
  public:
+  // The graph is constructed with a list of input layers, the order of these
+  // input layers is used to define how training/test inputs are mapped to the
+  // specific layers. Using the output node the graph can be traversed backwards
+  // to discover a reverse ordering in which to execute the layers.
   GraphContext(std::vector<InputPtr> inputs, NodePtr output)
       : _output(std::move(output)), _inputs(std::move(inputs)) {}
 
+  // When the layers are initially defined the only have information about their
+  // own dimensions, parameters etc. During compile the layers can use the
+  // information from their predecessor(s) such as output dim do fully
+  // initialize their parameters. Note that successors could be added to nodes
+  // as well if that is needed for certain layers to initialize. Additionally in
+  // this function the different layers can preform different checks to make
+  // sure that the network is properly formatted. For instance if
+  // CategoricalCrossEntropy loss is used, then it can verify that the output
+  // layer has a softmax activation.
   void compile(std::shared_ptr<LossFunction> loss);
 
+  // Computes the forward pass through the graph.
   void forward(uint32_t batch_index, BoltBatch& inputs, BoltBatch& labels);
 
+  // Computes the backward pass through the graph.
   void backward(uint32_t batch_index, BoltBatch& inputs);
 
-  BoltVector& getLabels(uint32_t batch_index);
-
+  // This is to allow for pointers to the graph to be passed into the nodes
+  // themselves to access information about the context of the graph.
   std::shared_ptr<GraphContext> getPtr() { return shared_from_this(); }
 
  private:
+  // List of nodes(layers) in the order in which they should be computed.
   std::vector<NodePtr> _nodes;
 
+  // Output layer.
   NodePtr _output;
 
+  // Input layers. When train is called, the ith input is fed into the ith input
+  // layer.
   std::vector<InputPtr> _inputs;
 
+  // List of the sparse layers in the graph. This is so that when we want to do
+  // things like enable sparse inference, update hash tables, or update hash
+  // functions.
   std::vector<std::shared_ptr<FullyConnectedLayer>> _sparse_layers;
 
+  // The loss function the graph was compliled with.
   std::shared_ptr<LossFunction> _loss;
 };
 
--- bolt/src/graph/Node.h
+++ bolt/src/graph/Node.h
@@ -13,29 +13,47 @@ class Node {
     compile();
   }
 
-  virtual void forward(uint32_t batch_index) = 0;
+  // Computes the forward pass for the node. The node will access its inputs
+  // through the getOutput() method on is predecessor(s). The labels are an
+  // optional argument that will only be specified for the output layer in order
+  // for sparse layers to sample the labels as active neurons during training.
+  virtual void forward(uint32_t batch_index, const BoltVector* labels) = 0;
 
+  // Computes the backwards pass through the node.
   virtual void backpropagate(uint32_t batch_index) = 0;
 
+  // Returns the ith output of the node.
   virtual BoltVector& getOutput(uint32_t batch_index) = 0;
 
-  virtual uint32_t inputDim() const = 0;
-
+  // Returns the output dimension of the node. This is used for subsequent nodes
+  // during compilation.
   virtual uint32_t outputDim() const = 0;
 
+  // Returns if the output of the node is sparse.
+  virtual bool outputSparse() const = 0;
+
+  // Initializes any state that the node must store for computations that is not
+  // part of the nodes parameters. For instance this could be the
+  // activations/gradients for a batch, or some other internal state that must
+  // be allocated after the batch size is known.
   virtual void initializeState(uint32_t batch_size, bool is_inference) = 0;
 
+  // Add any sparse layers in the node to a list of sparse layers.
   virtual void addSparseLayers(
       std::vector<std::shared_ptr<FullyConnectedLayer>>& sparse_layers) = 0;
 
   virtual ~Node() = default;
 
  protected:
+  // Compilation for the node.
   virtual void compile() = 0;
 
+  // Pointer to the graph object itself in case the node needs to access
+  // additional context.
   GraphContextPtr _graph;
 };
 
+// A node subclass that contains a fully connected layer.
 class FullyConnectedLayerNode final : public Node {
  public:
   explicit FullyConnectedLayerNode(FullyConnectedLayerConfig& config)
@@ -48,18 +66,9 @@ class FullyConnectedLayerNode final : public Node {
     _predecessor = std::move(node);
   }
 
-  void compile() final {
-    if (_predecessor == nullptr) {
-      throw std::invalid_argument("");
-    }
-
-    _layer = std::make_shared<FullyConnectedLayer>(_config,
-                                                   _predecessor->outputDim());
-  }
-
-  void forward(uint32_t batch_index) final {
+  void forward(uint32_t batch_index, const BoltVector* labels) final {
     _layer->forward(_predecessor->getOutput(batch_index), _outputs[batch_index],
-                    &_graph->getLabels(batch_index));
+                    labels);
   }
 
   void backpropagate(uint32_t batch_index) final {
@@ -71,9 +80,12 @@ class FullyConnectedLayerNode final : public Node {
     return _outputs[batch_index];
   }
 
-  uint32_t inputDim() const final { return _layer->getInputDim(); }
-
   uint32_t outputDim() const final { return _layer->getDim(); }
+
+  bool outputSparse() const final {
+    // Need to check sparsity of layer.
+    return false;
+  }
 
   void initializeState(uint32_t batch_size, bool is_inference) final {
     _outputs = _layer->createBatchState(batch_size, is_inference);
@@ -84,6 +96,16 @@ class FullyConnectedLayerNode final : public Node {
     sparse_layers.push_back(_layer);
   }
 
+ protected:
+  void compile() final {
+    if (_predecessor == nullptr) {
+      throw std::invalid_argument("");
+    }
+
+    _layer = std::make_shared<FullyConnectedLayer>(_config,
+                                                   _predecessor->outputDim());
+  }
+
  private:
   std::shared_ptr<FullyConnectedLayer> _layer;
   FullyConnectedLayerConfig _config;
@@ -92,6 +114,65 @@ class FullyConnectedLayerNode final : public Node {
   NodePtr _predecessor;
 };
 
+class Concatenation final : public Node {
+ public:
+  explicit Concatenation(std::vector<NodePtr> inputs)
+      : _input_nodes(std::move(inputs)),
+        _concatenated_dim(0),
+        _sparse_output(false) {}
+
+  // This may be a no-op, or we may need to map sparse indices to disjoint
+  // ranges.
+  void forward(uint32_t batch_index, const BoltVector* labels) final;
+
+  // This may be  no-op or we may need to map disjoint ranges of sparse indices
+  // to the dim of each sub-layer.
+  void backpropagate(uint32_t batch_index) final;
+
+  BoltVector& getOutput(uint32_t batch_index) final {
+    return _outputs[batch_index];
+  }
+
+  uint32_t outputDim() const final { return _concatenated_dim; }
+
+  bool outputSparse() const final { return _sparse_output; }
+
+  void initializeState(uint32_t batch_size, bool is_inference) final {
+    // How do we handle sparsity in concatenation layers?
+    _outputs = BoltBatch(_concatenated_dim, batch_size, is_inference);
+  }
+
+  void addSparseLayers(
+      std::vector<std::shared_ptr<FullyConnectedLayer>>& sparse_layers) final {
+    for (auto& node : _input_nodes) {
+      node->addSparseLayers(sparse_layers);
+    }
+  }
+
+ protected:
+  void compile() final {
+    for (auto& node : _input_nodes) {
+      node->compile(this->_graph);
+      _concatenated_dim += node->outputDim();
+      _sparse_output = _sparse_output || node->outputSparse();
+    }
+
+    // How to allocate state if there are sparse inputs?
+  }
+
+ private:
+  std::vector<NodePtr> _input_nodes;
+  uint32_t _concatenated_dim;
+  BoltBatch _outputs;
+  bool _sparse_output;
+};
+
+// A node subclass for input layers. The input batch will be stored in this
+// layer so that subsequent layers can access the inputs through its getOutput()
+// method. This makes the interface simplier by generalizing the forward pass so
+// that other layers always just access the outputs of the previous layer rather
+// than have to worry if they they need to access an input directly or access
+// the outputs of a previous layer.
 class Input final : public Node {
  public:
   explicit Input(uint32_t expected_input_dim)
@@ -99,7 +180,10 @@ class Input final : public Node {
 
   void compile() final {}
 
-  void forward(uint32_t batch_index) final { (void)batch_index; }
+  void forward(uint32_t batch_index, const BoltVector* labels) final {
+    (void)labels;
+    (void)batch_index;
+  }
 
   void backpropagate(uint32_t batch_index) final { (void)batch_index; }
 
@@ -109,9 +193,14 @@ class Input final : public Node {
     return (*_input_batch)[batch_index];
   }
 
-  uint32_t inputDim() const final { return _expected_input_dim; }
+  uint32_t expectedInputDim() const { return _expected_input_dim; }
 
   uint32_t outputDim() const final { return _expected_input_dim; }
+
+  bool outputSparse() const final {
+    // Need to check sparsity of input.
+    return false;
+  }
 
   void initializeState(uint32_t batch_size, bool is_inference) final {
     (void)batch_size;
