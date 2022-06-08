a3894516d4df09f9a1696302c7529c3639dc82bf
Josh Engels <josh@thirdai.com>
2022-06-07 21:42:08 -0400

    Investigating why there are problems

--- search/python_tests/test_blolt.py
+++ search/python_tests/test_blolt.py
@@ -11,8 +11,16 @@ def test_glove():
     # num_groups_to_consider=3 sucks, and kinda so does 4. 6 gives good groups, but not as high accuracy.
     search_index = BoltSearch(
         estimated_dataset_size=10**6,
-        num_classifiers=3,
+        num_classifiers=2,
         input_dim=100,
         num_groups_to_consider=5,
     )
-    search_index.index(glove_data)
+    search_index.index(glove_data, num_epochs=2, fraction_to_use_for_training=0.25)
+
+    results = search_index.query(glove_queries, top_k=100)
+
+    recalled_in_100 = 0
+    for found, gt in zip(results, glove_neighbors):
+        if gt[0] in found:
+            recalled_in_100 += 1
+    print(recalled_in_100)
--- thirdai_python_package/_boltsearch.py
+++ thirdai_python_package/_boltsearch.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 from thirdai._thirdai import bolt
+from tqdm import tqdm
 
 # For now works on dense numpy vectors
 class BoltSearch:
@@ -33,6 +34,8 @@ class BoltSearch:
         batch_size=2048,
         learning_rate=0.01,
     ):
+        self.groups = []
+        self.num_points = len(dataset)
         train_dataset = dataset[: int(fraction_to_use_for_training * len(dataset))]
         current_assignments = [
             self._get_random_group_assignments(
@@ -41,11 +44,11 @@ class BoltSearch:
             for _ in range(self.num_classifiers)
         ]
 
-        for epoch in range(num_epochs):
-            for i, classifier in enumerate(self.classifiers):
+        for classifier_id, classifier in enumerate(self.classifiers):
+            for epoch in range(num_epochs):
                 classifier.train(
                     train_data=train_dataset,
-                    train_labels=current_assignments[i],
+                    train_labels=current_assignments[classifier_id],
                     loss_fn=bolt.CategoricalCrossEntropyLoss(),
                     learning_rate=learning_rate,
                     epochs=1,
@@ -55,24 +58,73 @@ class BoltSearch:
 
                 predictions = classifier.predict(
                     train_dataset,
-                    current_assignments[i],
-                    1024,
+                    current_assignments[classifier_id],
+                    2048,
                     ["categorical_accuracy"],
                 )
 
-                current_assignments[i] = self._get_new_group_assignments(
+                current_assignments[classifier_id] = self._get_new_group_assignments(
                     predicted_group_ids=predictions[1],
                     predicted_activations=predictions[2],
                 )
 
-    def query(self, batch):
-        pass
+            all_predictions = classifier.predict(dataset, None, 2048)
+            all_assignments = self._get_new_group_assignments(
+                predicted_group_ids=all_predictions[1],
+                predicted_activations=all_predictions[2],
+            )
+            group_memberships = [[] for _ in range(self.num_classes)]
+            group_lens = [0 for _ in range(self.num_classes)]
+            for vec_id, group_id in enumerate(all_assignments):
+                group_memberships[group_id].append(vec_id)
+                group_lens[group_id] += 1
+            self.groups += group_memberships
+
+            print(
+                f"Min group size {min(group_lens)}, max group size {max(group_lens)}, std group size {np.std(group_lens)}"
+            )
+
+    def query(self, batch, top_k, threshold_to_return=-1):
+        if threshold_to_return == -1:
+            threshold_to_return = self.num_classifiers
+        all_predictions = [
+            classifier.predict(batch, None, 2048, verbose=False)
+            for classifier in self.classifiers
+        ]
+        results = []
+        for vec_id in tqdm(range(len(batch))):
+            group_ids = np.concatenate(
+                [
+                    [self.num_classes * i + p for p in prediction[1][vec_id]]
+                    for i, prediction in enumerate(all_predictions)
+                ]
+            )
+            activations = np.concatenate(
+                [prediction[2][vec_id] for prediction in all_predictions]
+            )
+            group_ids = group_ids[activations.argsort()[::-1]]
+            results.append(
+                self._group_testing_inference(group_ids, top_k, threshold_to_return)
+            )
+        return results
+
+    def _group_testing_inference(self, group_ids, top_k, threshold):
+        result = []
+        point_counts = np.zeros(shape=(self.num_points,))
+        for group_id in group_ids:
+            for point_id in self.groups[group_id]:
+                point_counts[point_id] += 1
+                if point_counts[point_id] == threshold:
+                    result.append(point_id)
+                    if len(result) == top_k:
+                        return result
+        return result
 
     def _create_single_dense_classifier(
         self,
         input_dim,
         last_layer_dim,
-        last_layer_sparsity=0.01,
+        last_layer_sparsity=0.025,
         hidden_layer_dim=10000,
         hidden_layer_sparsity=0.01,
     ):
@@ -107,5 +159,9 @@ class BoltSearch:
 
             new_group_sizes[selected_group_id] += 1
             new_group_assignments.append(selected_group_id)
+
+        print(
+            f"Min group size {min(new_group_sizes)}, max group size {max(new_group_sizes)}, std group size {np.std(new_group_sizes)}"
+        )
 
         return np.array(new_group_assignments)
