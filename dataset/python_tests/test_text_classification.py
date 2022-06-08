baba564485751cfea7ed3db24f3ea88047263432
BenGeordie <school@bengeordie@gmail.com>
2022-06-08 14:41:44 +0530

    formatting

--- dataset/python_tests/test_text_classification.py
+++ dataset/python_tests/test_text_classification.py
@@ -4,6 +4,7 @@ import os
 from thirdai.dataset import load_text_classification_dataset
 from thirdai import bolt
 
+
 def generate_text_classification_dataset(filename):
     with open(filename, "w") as f:
         for i in range(10_000):
@@ -13,19 +14,22 @@ def generate_text_classification_dataset(filename):
             else:
                 f.write("0,bad\n")
 
+
 @pytest.mark.integration
 def test_text_classification():
     file = "test_text_classification.csv"
     generate_text_classification_dataset(file)
     [data, labels], input_dim = load_text_classification_dataset(file)
-    
+
     layers = [
         bolt.FullyConnected(
             dim=1000,
             load_factor=0.1,
             activation_function=bolt.ActivationFunctions.ReLU,
         ),
-        bolt.FullyConnected(dim=2, activation_function=bolt.ActivationFunctions.Softmax),
+        bolt.FullyConnected(
+            dim=2, activation_function=bolt.ActivationFunctions.Softmax
+        ),
     ]
 
     network = bolt.Network(layers=layers, input_dim=input_dim)
