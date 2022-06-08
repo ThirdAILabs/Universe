2b2201c3590f45ee967c4561b3bf26eafc18247a
Henry Zhang (air) <henry@thirdai.com>
2022-06-08 11:33:02 -0500

    added simple test

--- text_classification/cookie_monster.py
+++ text_classification/cookie_monster.py
@@ -2,25 +2,24 @@ from thirdai import bolt, dataset
 import numpy as np
 
 class CookieMonster():
-    def __init__(self, size="small"):
+    def __init__(self, size="2Gb", output_dimension=2):
         self.model_size = size
-        self.bolt_classifier = bolt.TextClassifier(model_size=size, n_classes=2)
+        self.bolt_classifier = bolt.TextClassifier(model_size=size, n_classes=output_dimension)
+        self.out_dim = output_dimension
     
     def load_hidden_weights(self, path_to_weights):
-        #TODO(Henry): Add wrapper in text classifier to set weights&biases
         # download weights from mlflow and load them into bolt
         pass
 
     def set_output_dimension(self, dimension):
-        #TODO(Henry): Add wrapper in text classifier to get weights&biases
-        weights = self.bolt_classifier.get_weights()
-        biases = self.bolt_classifier.get_biases()
+        weights = self.bolt_classifier.get_hidden_weights()
+        biases = self.bolt_classifier.get_hidden_biases()
         del(self.bolt_classifier) # delete old model
 
         self.bolt_classifier = bolt.TextClassifier(model_size=self.model_size, n_classes=dimension)
 
-        self.bolt_classifier.set_weights(weights)
-        self.bolt_classifier.set_biases(biases)
+        self.bolt_classifier.set_hidden_weights(weights)
+        self.bolt_classifier.set_hidden_biases(biases)
 
     
     
---
+++ text_classification/test_monster.py
@@ -0,0 +1,13 @@
+from http.cookiejar import Cookie
+from cookie_monster import *
+import pytest
+
+def test_new_dimension():
+    cookie_model = CookieMonster(output_dimension=64)
+    train_file = "/Users/junyanzhang/Desktop/Datasets/train_shuffled.csv"
+    cookie_model.bolt_classifier.train(train_file=train_file, epochs=2, learning_rate=0.01)
+
+    cookie_model.set_output_dimension(40)
+
+
+test_new_dimension()
