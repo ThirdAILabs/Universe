cb648f2b93ceacf9613b49f7ca053631b4e5563e
Benito Geordie <89472452+benitoThree@users.noreply.github.com>
2022-06-08 10:35:28 +0530

    [1 / 3] Data pipeline for amazon polarity: core components of data pipeline (#509)
    * tests
    
    * tests
    
    * fix cmakelists
    
    * minor fixes
    
    * cmakelists fixes
    
    * fix batch processor
    
    * remove increment at indices
    
    * format
    
    * parallel dataset export and support no target
    
    * format
    
    * fix
    
    * slight refactor
    
    * format
    
    * tokenize_to_svm is now in dataset
    
    * extendable vectors test!
    
    * comments
    
    * formatting
    
    * test cmakelists
    
    * fixes
    
    * tests! and subsequent bug fixes
    
    * responded to nick's comments
    
    * Modified to new bolt api with separate labels and examples
    
    * formatting
    
    * remove unnecessary file
    
    * added test case for no label
    
    * formatting
    
    * resolve David's comments (except extendable vector stuff)
    
    * comments
    
    * hopefully fix linting
    
    * hopefully fix linting
    
    * struct -> class
    
    * hopefully fix linting
    
    * hopefully fixed testpypi
    
    * resolve nick's review
    
    * bug fix
    
    * resolve Nick's requests and fixed bugs
    
    * resolved .has_value() comments
    
    * resolved .has_value() comments
    
    Co-authored-by: BenGeordie <school@bengeordie@gmail.com>

---
+++ dataset/src/core/BatchProcessor.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <bolt/src/layers/BoltVector.h>
+#include <dataset/src/Dataset.h>
+#include <dataset/src/blocks/BlockInterface.h>
+#include <dataset/src/bolt_datasets/BoltDatasets.h>
+#include <chrono>
+
+namespace thirdai::dataset {
+
+/**
+ * Encodes input samples – each represented by a sequence of strings –
+ * as input and target BoltVectors according to the given blocks.
+ * It processes these sequences in batches.
+ */
+class BatchProcessor {
+ public:
+  BatchProcessor(std::vector<std::shared_ptr<Block>>& input_blocks,
+                 std::vector<std::shared_ptr<Block>>& target_blocks,
+                 uint32_t output_batch_size);
+
+  /**
+   * Consumes a batch of input samples and encodes them
+   * as vectors.
+   */
+  void processBatch(std::vector<std::vector<std::string>>& batch);
+
+  /**
+   * Produces an DatasetWithLabels containing the
+   * vectors processed so far.
+   * This method can optionally produce a shuffled dataset.
+   */
+  std::pair<BoltDatasetPtr, BoltDatasetPtr> exportInMemoryDataset(
+      bool shuffle = false,
+      uint32_t shuffle_seed =
+          std::chrono::system_clock::now().time_since_epoch().count());
+
+ private:
+  /**
+   * Produces a mapping from the final position of a vector in
+   * the exported dataset to its original position based on the
+   * shuffle and shuffle_seed arguments.
+   */
+  static std::vector<uint32_t> makeFinalPositions(uint32_t n_exported,
+                                                  bool shuffle,
+                                                  uint32_t shuffle_seed);
+
+  /**
+   * Encodes a sample as a BoltVector according to the given blocks.
+   */
+  static bolt::BoltVector makeVector(
+      std::vector<std::string>& sample,
+      std::vector<std::shared_ptr<Block>>& blocks, bool blocks_dense);
+
+  uint32_t _batch_size;
+  bool _input_blocks_dense;
+  bool _target_blocks_dense;
+  std::vector<bolt::BoltVector> _input_vectors;
+  std::optional<std::vector<bolt::BoltVector>> _target_vectors;
+  std::vector<std::shared_ptr<Block>>& _input_blocks;
+  std::vector<std::shared_ptr<Block>>& _target_blocks;
+};
+
+}  // namespace thirdai::dataset
