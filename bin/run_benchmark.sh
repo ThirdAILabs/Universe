e15d43681f42bdc90fd38e9f5b45a9ebf5ecb544
Vihan Lakshman <vihan@thirdai.com>
2022-06-14 01:59:37 +0000

    Fix bad path

--- bin/run_benchmark.sh
+++ bin/run_benchmark.sh
@@ -1,3 +1,3 @@
 
 python3 /Universe/bin/build.py
-python3 /Universe/benchmark_bolt.py -l benchmarks/bolt_benchmarks/configs/mnist_dense.txt --test_run
+python3 /Universe/bin/benchmark_bolt.py -l benchmarks/bolt_benchmarks/configs/mnist_dense.txt --test_run
