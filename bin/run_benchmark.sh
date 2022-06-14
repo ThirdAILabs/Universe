d9c7890e981303e77096edbc4793d864bcb35d82
Vihan Lakshman <vihan@thirdai.com>
2022-06-14 01:49:21 +0000

    Add absolute path

--- bin/run_benchmark.sh
+++ bin/run_benchmark.sh
@@ -1,2 +1,3 @@
-python3 build.py
-python3 bin/benchmark_bolt.py -l benchmarks/bolt_benchmarks/configs/mnist_dense.txt --test_run
+
+python3 /Universe/bin/build.py
+python3 /Universe/benchmark_bolt.py -l benchmarks/bolt_benchmarks/configs/mnist_dense.txt --test_run
