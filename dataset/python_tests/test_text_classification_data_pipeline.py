452a3e6e015efdcf918437b6ed3b7afd03b5ae55
BenGeordie <school@bengeordie@gmail.com>
2022-06-14 17:14:34 +0530

    added delimiter test case

--- dataset/python_tests/test_text_classification_data_pipeline.py
+++ dataset/python_tests/test_text_classification_data_pipeline.py
@@ -7,26 +7,27 @@ from thirdai.dataset import text_encodings
 from thirdai import bolt
 
 
-def generate_text_classification_dataset(filename):
+def generate_text_classification_dataset(filename, delim):
     with open(filename, "w") as f:
         for i in range(15_000):
             sentiment = i % 10 / 10
             if sentiment <= 0.3:
-                f.write("1,good stuff\n")
+                f.write(f"1{delim}good stuff\n")
             elif sentiment <= 0.6:
-                f.write("0,bad stuff\n")
+                f.write(f"0{delim}bad stuff\n")
             else:
-                f.write("2,neutral stuff\n")
+                f.write(f"2{delim}neutral stuff\n")
 
 
-def test_text_classification_data_pipeline(text_encoding):
+def test_text_classification_data_pipeline(text_encoding, delim):
     file = "test_text_classification.csv"
-    generate_text_classification_dataset(file)
+    generate_text_classification_dataset(file, delim)
     pipeline = DataPipeline(
         file,
         batch_size=256,
         input_blocks=[blocks.Text(1, text_encoding)],
         label_blocks=[blocks.Categorical(0, 3)],
+        delimiter=delim
     )
     [data, labels] = pipeline.load_in_memory()
 
@@ -70,14 +71,17 @@ def test_text_classification_data_pipeline(text_encoding):
 
 @pytest.mark.integration
 def test_text_classification_data_pipeline_with_unigrams():
-    test_text_classification_data_pipeline(text_encodings.UniGram(100_000))
+    test_text_classification_data_pipeline(text_encodings.UniGram(100_000), ',')
+    test_text_classification_data_pipeline(text_encodings.UniGram(100_000), '\t')
 
 
 @pytest.mark.integration
 def test_text_classification_data_pipeline_with_pairgrams():
-    test_text_classification_data_pipeline(text_encodings.PairGram(100_000))
+    test_text_classification_data_pipeline(text_encodings.PairGram(100_000), ',')
+    test_text_classification_data_pipeline(text_encodings.PairGram(100_000), '\t')
 
 
 @pytest.mark.integration
 def test_text_classification_data_pipeline_with_chartrigrams():
-    test_text_classification_data_pipeline(text_encodings.CharKGram(3, 100_000))
+    test_text_classification_data_pipeline(text_encodings.CharKGram(3, 100_000), ',')
+    test_text_classification_data_pipeline(text_encodings.CharKGram(3, 100_000), '\t')
