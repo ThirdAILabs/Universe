630868cb2dd80c69d9086f21de1abe9b72f61978
Vihan Lakshman <vihan@thirdai.com>
2022-06-14 02:11:20 +0000

    Add full path for benchmark config

--- bin/run_benchmark.sh
+++ bin/run_benchmark.sh
@@ -1,3 +1,3 @@
 
 python3 /Universe/bin/build.py
-python3 /Universe/bin/benchmark_bolt.py -l benchmarks/bolt_benchmarks/configs/mnist_dense.txt --test_run
+python3 /Universe/bin/benchmark_bolt.py -l /Universe/benchmarks/bolt_benchmarks/configs/mnist_dense.txt --test_run
