f50eb7c70e16e750a8865444198a218cbaf9b93b
Josh Engels <josh@thirdai.com>
2022-06-16 16:15:11 -0400

    Basic web parsing app

---
+++ webapps/parse_website_qa_demo/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template
+from parse_website import ParsedWebsite
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html", background_color="", console_output="")
+
+
+@app.route("/", methods=["POST"])
+def crawl_site():
+    url = request.form["url"]
+    depth = int(request.form["depth"])
+    if not url.startswith("http"):
+      url = "https://" + url
+
+    global parsed
+    parsed = ParsedWebsite(url, depth)
+
+    return render_template("home.html", background_color="", console_output="Parsed the following urls: \n" + "\n".join(list(parsed.seen_urls)))
+
+@app.route("/do-qa", methods=["POST"])
+def do_qa():
+    print("HERE", parsed)
+
+    return parsed.base_url
