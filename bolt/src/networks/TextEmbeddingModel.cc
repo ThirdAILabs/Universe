a314580b7e2df7690cea1e06aa302d8c99b1e7ab
Nicholas Meisburger <nmeisburger@gmail.com>
2022-06-20 13:23:24 -0500

    try fully connected model

--- bolt/src/networks/TextEmbeddingModel.cc
+++ bolt/src/networks/TextEmbeddingModel.cc
@@ -5,56 +5,62 @@ namespace thirdai::bolt {
 void TextEmbeddingModel::forward(uint32_t batch_index,
                                  const dataset::MaskedSentenceBatch& input,
                                  BoltVector& output, const BoltVector* labels) {
-  _sentence_embedding_model.forward(batch_index, input[batch_index],
-                                    _sentence_embedding_output[batch_index],
-                                    nullptr);
+  // _sentence_embedding_model.forward(batch_index, input[batch_index],
+  //                                   _sentence_embedding_output[batch_index],
+  //                                   nullptr);
 
-  _token_embedding_layers_used.at(input.maskedIndex(batch_index)) = true;
-  _token_embedding_layers.at(input.maskedIndex(batch_index))
-      .forward(_sentence_embedding_output[batch_index],
-               _token_embedding_output[batch_index], nullptr);
+  // _token_embedding_layers_used.at(input.maskedIndex(batch_index)) = true;
+  // _token_embedding_layers.at(input.maskedIndex(batch_index))
+  //     .forward(_sentence_embedding_output[batch_index],
+  //              _token_embedding_output[batch_index], nullptr);
 
-  _classifier.forward(batch_index, _token_embedding_output[batch_index], output,
-                      labels);
+  // _classifier.forward(batch_index, _token_embedding_output[batch_index],
+  // output,
+  //                     labels);
+
+  _classifier.forward(batch_index, input[batch_index], output, labels);
 }
 
 void TextEmbeddingModel::backpropagate(uint32_t batch_index,
                                        dataset::MaskedSentenceBatch& input,
                                        BoltVector& output) {
-  _classifier.backpropagate<false>(
-      batch_index, _token_embedding_output[batch_index], output);
+  _classifier.backpropagate<true>(batch_index, input[batch_index], output);
 
-  _token_embedding_layers.at(input.maskedIndex(batch_index))
-      .backpropagate(_sentence_embedding_output[batch_index],
-                     _token_embedding_output[batch_index]);
+  // _classifier.backpropagate<false>(
+  //     batch_index, _token_embedding_output[batch_index], output);
 
-  _sentence_embedding_model.backpropagate<true>(
-      batch_index, input[batch_index], _sentence_embedding_output[batch_index]);
+  // _token_embedding_layers.at(input.maskedIndex(batch_index))
+  //     .backpropagate(_sentence_embedding_output[batch_index],
+  //                    _token_embedding_output[batch_index]);
+
+  // _sentence_embedding_model.backpropagate<true>(
+  //     batch_index, input[batch_index],
+  //     _sentence_embedding_output[batch_index]);
 }
 
 void TextEmbeddingModel::updateParameters(float learning_rate, uint32_t iter) {
-  _sentence_embedding_model.updateParameters(learning_rate, iter);
+  // _sentence_embedding_model.updateParameters(learning_rate, iter);
 
-  for (uint32_t i = 0; i < _token_embedding_layers.size(); i++) {
-    if (_token_embedding_layers_used[i]) {
-      _token_embedding_layers[i].updateParameters(learning_rate, iter, BETA1,
-                                                  BETA2, EPS);
-      _token_embedding_layers_used[i] = false;
-    }
-  }
+  // for (uint32_t i = 0; i < _token_embedding_layers.size(); i++) {
+  //   if (_token_embedding_layers_used[i]) {
+  //     _token_embedding_layers[i].updateParameters(learning_rate, iter, BETA1,
+  //                                                 BETA2, EPS);
+  //     _token_embedding_layers_used[i] = false;
+  //   }
+  // }
 
   _classifier.updateParameters(learning_rate, iter);
 }
 
 void TextEmbeddingModel::initializeNetworkState(uint32_t batch_size,
                                                 bool force_dense) {
-  _sentence_embedding_model.initializeNetworkState(batch_size, force_dense);
+  // _sentence_embedding_model.initializeNetworkState(batch_size, force_dense);
 
-  _sentence_embedding_output =
-      _sentence_embedding_model.getOutputs(batch_size, force_dense);
+  // _sentence_embedding_output =
+  //     _sentence_embedding_model.getOutputs(batch_size, force_dense);
 
-  _token_embedding_output =
-      _token_embedding_layers[0].createBatchState(batch_size, force_dense);
+  // _token_embedding_output =
+  //     _token_embedding_layers[0].createBatchState(batch_size, force_dense);
 
   _classifier.initializeNetworkState(batch_size, force_dense);
 }
