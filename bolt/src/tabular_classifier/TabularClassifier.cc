61616b447bd591de149899f23a48ef0be28fb5fa
David Torres <david@thirdai.com>
2022-06-24 13:04:16 +0530

    constructor and load/save

--- bolt/src/tabular_classifier/TabularClassifier.cc
+++ bolt/src/tabular_classifier/TabularClassifier.cc
@@ -17,6 +17,21 @@
 namespace thirdai::bolt {
 
 TabularClassifier::TabularClassifier(const std::string& model_size,
-                                     uint32_t n_classes) {}
+                                     uint32_t n_classes) {
+  // TODO(david) autotune these size depending on model_size and benchmark
+  // results
+  uint32_t input_dim = 100000;
+  uint32_t hidden_layer_size = 1000;
+  uint32_t hidden_layer_sparsity = 0.1;
+
+  SequentialConfigList configs = {
+      std::make_shared<FullyConnectedLayerConfig>(
+          hidden_layer_size, hidden_layer_sparsity, ActivationFunction::ReLU),
+      std::make_shared<FullyConnectedLayerConfig>(n_classes,
+                                                  ActivationFunction::Softmax)};
+
+  _model =
+      std::make_unique<FullyConnectedNetwork>(std::move(configs), input_dim);
+}
 
 }  // namespace thirdai::bolt
--- bolt/src/tabular_classifier/TabularClassifier.h
+++ bolt/src/tabular_classifier/TabularClassifier.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cereal/archives/binary.hpp>
 #include <bolt/src/networks/FullyConnectedNetwork.h>
 #include <memory>
 
@@ -15,31 +16,30 @@ class TabularClassifier {
   // void predict(const std::string& filename,
   //              const std::optional<std::string>& output_filename);
 
-  // void save(const std::string& filename) {
-  //   std::ofstream filestream(filename, std::ios::binary);
-  //   cereal::BinaryOutputArchive oarchive(filestream);
-  //   oarchive(*this);
-  // }
+  void save(const std::string& filename) {
+    std::ofstream filestream(filename, std::ios::binary);
+    cereal::BinaryOutputArchive oarchive(filestream);
+    oarchive(*this);
+  }
 
-  // static std::unique_ptr<TabularClassifier> load(const std::string& filename)
-  // {
-  //   std::ifstream filestream(filename, std::ios::binary);
-  //   cereal::BinaryInputArchive iarchive(filestream);
-  //   std::unique_ptr<TabularClassifier> deserialize_into(
-  //       new TabularClassifier());
-  //   iarchive(*deserialize_into);
-  //   return deserialize_into;
-  // }
+  static std::unique_ptr<TabularClassifier> load(const std::string& filename) {
+    std::ifstream filestream(filename, std::ios::binary);
+    cereal::BinaryInputArchive iarchive(filestream);
+    std::unique_ptr<TabularClassifier> deserialize_into(
+        new TabularClassifier());
+    iarchive(*deserialize_into);
+    return deserialize_into;
+  }
 
-  // // Private constructor for cereal
-  // TabularClassifier() {}
+  // Private constructor for cereal
+  TabularClassifier() {}
 
-  // // Tell Cereal what to serialize. See https://uscilab.github.io/cereal/
-  // friend class cereal::access;
-  // template <class Archive>
-  // void serialize(Archive& archive) {
-  //   archive(_model);
-  // }
+  // Tell Cereal what to serialize. See https://uscilab.github.io/cereal/
+  friend class cereal::access;
+  template <class Archive>
+  void serialize(Archive& archive) {
+    archive(_model);
+  }
 
   std::unique_ptr<FullyConnectedNetwork> _model;
 };
