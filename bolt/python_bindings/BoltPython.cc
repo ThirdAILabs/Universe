011e61e461cc6c5ee9d278735096d0c01cb7b926
Pratikq Pranav <pratikqpranav@Pratikqs-MacBook-Air.local>
2022-06-24 18:27:49 +0530

    Did Some changes to pybindings

--- bolt/python_bindings/BoltPython.cc
+++ bolt/python_bindings/BoltPython.cc
@@ -819,9 +819,9 @@ void createBoltSubmodule(py::module_& module) {
            "Sets the weight matrix at the given layer index to the given 2D "
            "Numpy matrix. Throws an error if the dimension of the given weight "
            "matrix does not match the layer's current weight matrix.")
-      .def("set_layer_sparsity", &PyNetwork::setLayerSparsity,
+      .def("set_layer_sparsity", &DistributedPyNetwork::setLayerSparsity,
            py::arg("layer_index"), py::arg("sparsity"),
-           py::arg("hash_seed") = time(nullptr), py::arg("shuffle_ptr") = time(nullptr),
+          py::arg("hash_seed"), py::arg("shuffle_ptr"),
            "Sets the sparsity of the layer at the given index. The 0th layer "
            "is the first layer after the input layer. Note that this will "
            "autotune the sampling config to work for the new sparsity.");
