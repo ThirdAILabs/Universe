e4d862c463dddbc2b242fbfb43846736132081d8
David Torres <david@thirdai.com>
2022-06-24 12:58:42 +0530

    initial commit

---
+++ bolt/src/tabular_classifier/TabularClassifier.cc
@@ -0,0 +1,22 @@
+#include "TabularClassifier.h"
+#include <bolt/src/layers/BoltVector.h>
+#include <bolt/src/layers/LayerConfig.h>
+#include <bolt/src/layers/LayerUtils.h>
+#include <bolt/src/loss_functions/LossFunctions.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <algorithm>
+#include <fstream>
+#include <memory>
+#include <optional>
+#include <regex>
+#include <stdexcept>
+#include <string>
+#include <thread>
+
+namespace thirdai::bolt {
+
+TabularClassifier::TabularClassifier(const std::string& model_size,
+                                     uint32_t n_classes) {}
+
+}  // namespace thirdai::bolt
---
+++ bolt/src/tabular_classifier/TabularClassifier.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <bolt/src/networks/FullyConnectedNetwork.h>
+#include <memory>
+
+namespace thirdai::bolt {
+
+class TabularClassifier {
+ public:
+  TabularClassifier(const std::string& model_size, uint32_t n_classes);
+
+  // void train(const std::string& filename, uint32_t epochs, float
+  // learning_rate);
+
+  // void predict(const std::string& filename,
+  //              const std::optional<std::string>& output_filename);
+
+  // void save(const std::string& filename) {
+  //   std::ofstream filestream(filename, std::ios::binary);
+  //   cereal::BinaryOutputArchive oarchive(filestream);
+  //   oarchive(*this);
+  // }
+
+  // static std::unique_ptr<TabularClassifier> load(const std::string& filename)
+  // {
+  //   std::ifstream filestream(filename, std::ios::binary);
+  //   cereal::BinaryInputArchive iarchive(filestream);
+  //   std::unique_ptr<TabularClassifier> deserialize_into(
+  //       new TabularClassifier());
+  //   iarchive(*deserialize_into);
+  //   return deserialize_into;
+  // }
+
+  // // Private constructor for cereal
+  // TabularClassifier() {}
+
+  // // Tell Cereal what to serialize. See https://uscilab.github.io/cereal/
+  // friend class cereal::access;
+  // template <class Archive>
+  // void serialize(Archive& archive) {
+  //   archive(_model);
+  // }
+
+  std::unique_ptr<FullyConnectedNetwork> _model;
+};
+
+}  // namespace thirdai::bolt
