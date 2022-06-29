9ab1371bafdaf6c16a071ca11c8a79066735d6b7
David Torres <david@thirdai.com>
2022-06-29 22:59:47 +0530

    tests and cmakelists

---
+++ bolt/src/tabular_classifier/tests/TabularClassifierTest.cc
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+namespace thirdai::bolt {
+
+class TabularClassifierTestFixture : public testing::Test {
+ public:
+  // define static methods here
+};
+
+TEST_F(TabularClassifierTestFixture, TestOddCsvFormatsWithTabularClassifier) {}
+
+}  // namespace thirdai::bolt
