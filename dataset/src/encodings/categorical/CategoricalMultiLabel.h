a9c65cb9dca8cbcc633d1e3460debf53b04ac806
Nicholas Meisburger <nmeisburger@gmail.com>
2022-07-07 12:38:46 -0500

    Categorical multi label block

---
+++ dataset/src/encodings/categorical/CategoricalMultiLabel.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "CategoricalEncodingInterface.h"
+#include <dataset/src/blocks/BlockInterface.h>
+#include <stdexcept>
+#include <string>
+
+namespace thirdai::dataset {
+
+class CategoricalMultiLabel final : public CategoricalEncoding {
+ public:
+  explicit CategoricalMultiLabel(uint32_t max_label) : _max_label(max_label) {}
+
+  void encodeCategory(std::string_view labels,
+                      SegmentedFeatureVector& vec) final {
+    const char* start = labels.data();
+    char* end;
+    do {
+      uint32_t label = std::strtoul(start, &end, 10);
+      // TODO(Nicholas, Geordie): It would be nice if we could throw an
+      // exception here if the label is out of range, but right now its unsafe
+      // because exceptions in openmp is undefined.
+      vec.addSparseFeatureToSegment(label, 1.0);
+      start = end;
+    } while ((*start++) == ',');
+  }
+
+  bool isDense() const final { return false; }
+
+  uint32_t featureDim() const final { return _max_label; }
+
+ private:
+  uint32_t _max_label;
+};
+
+}  // namespace thirdai::dataset
