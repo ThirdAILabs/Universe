e402c32372e482cd358dd5054049ec11ea6a9399
BenGeordie <school@bengeordie@gmail.com>
2022-07-09 21:10:29 +0700

    tests

--- dataset/tests/blocks/TrendBlockTests.cc
+++ dataset/tests/blocks/TrendBlockTests.cc
@@ -1,202 +1,129 @@
-// #include "BlockTest.h"
-// #include <gtest/gtest.h>
-// #include <dataset/src/blocks/BlockInterface.h>
-// #include <dataset/src/blocks/Trend.h>
-// #include <dataset/src/utils/TimeUtils.h>
-// #include <sys/types.h>
-// #include <ctime>
-// #include <limits>
-// #include <memory>
-// #include <sstream>
-// #include <unordered_map>
-// #include <valarray>
-// #include <vector>
+#include "BlockTest.h"
+#include <gtest/gtest.h>
+#include <dataset/src/blocks/BlockInterface.h>
+#include <dataset/src/blocks/Trend.h>
+#include <dataset/src/utils/TimeUtils.h>
+#include <sys/types.h>
+#include <cmath>
+#include <ctime>
+#include <limits>
+#include <memory>
+#include <sstream>
+#include <unordered_map>
+#include <valarray>
+#include <vector>
 
-// namespace thirdai::dataset {
+namespace thirdai::dataset {
 
-// class TrendBlockTests : public BlockTest {
-//  public:
-//   static StringMatrix makeTrivialSamples(uint32_t n_ids, uint32_t n_days,
-//                                          uint32_t day_offset,
-//                                          bool inc_by_id = false) {
-//     StringMatrix samples;
-//     for (uint32_t day = 0; day < n_days; day++) {
-//       for (uint32_t id = 0; id < n_ids; id++) {
-//         std::vector<std::string> sample;
-//         std::stringstream id_ss;
-//         id_ss << id;
-//         sample.push_back(id_ss.str());
+class TrendBlockTests : public BlockTest {
+ public:
+  static constexpr float default_delta = 1E-7;
 
-//         time_t timestamp =
-//             static_cast<time_t>(day + day_offset) *
-//             TimeUtils::SECONDS_IN_DAY;
-//         // Add offset to prevent overflow
-//         // due to timezone differences.
-//         auto* tm = std::localtime(&timestamp);
-//         std::string timestamp_str;
-//         timestamp_str.resize(10);
-//         std::strftime(timestamp_str.data(), 10, "%Y-%m-%d", tm);
-//         sample.push_back(timestamp_str);
+  static void assertFloatEq(float f1, float f2, float delta = default_delta) {
+    ASSERT_LT(std::abs(f1 - f2), delta);
+  }
+};
 
-//         auto count = inc_by_id ? id : 1;
-//         count = (day % 2) * count;
-//         std::stringstream count_ss;
-//         count_ss << count;
-//         sample.push_back(count_ss.str());
+TEST_F(TrendBlockTests, CorrectOutputWindowSizeOneNoGraph) {
+  std::vector<std::shared_ptr<Block>> blocks{std::make_shared<TrendBlock>(
+      /* has_count_col = */ true, /* id_col = */ 0,
+      /* timestamp_col = */ 1, /* count_col = */ 2,
+      /* horizon = */ 0, /* lookback = */ 1, /* period = */ 7)};
 
-//         samples.push_back(sample);
-//       }
-//     }
-//     return samples;
-//   }
+  StringMatrix mock_data {
+    {"id_1", "2022-01-01", "0.3"}, // Trivial
+    {"id_1", "2022-01-02", "0.5"}, // Check same period gets aggregated
+    {"id_1", "2022-01-08", "0.1"}, // Check counted as new period
+    {"id_2", "2022-01-08", "0.45"}, // Check handle multiple tracking id
+  };
 
-//   static void checkSegment(std::unordered_map<uint32_t, float>& entries,
-//                            size_t lookback, bool can_check_mean, float delta,
-//                            uint32_t id, bool neighbor,
-//                            uint32_t neighbor_idx = 0) {
-//     uint32_t offset = neighbor ? (neighbor_idx + 1) * (lookback + 1) : 0;
-//     auto mean = entries.at(offset + lookback);
-//     if (can_check_mean) {
-//       ASSERT_FLOAT_EQ(mean, static_cast<float>(id) / 2);
-//     }
-//     float sum = 0;
-//     for (size_t i = 0; i < lookback; i++) {
-//       sum += entries[i];
-//     }
-//     ASSERT_LE(std::abs(sum - 0.0), delta);
+  auto vecs =
+      makeSparseSegmentedVecs(mock_data, blocks);
+  
+  auto vec_0_entries = vectorEntries(vecs[0]);
+  ASSERT_EQ(vec_0_entries.size(), 1);
+  std::cout << vec_0_entries.at(0);
+  assertFloatEq(vec_0_entries.at(0), 0.3);
 
-//     float expected_sum = mean * lookback;
-//     float actual_sum = 0;
-//     for (size_t i = 0; i < lookback; i++) {
-//       actual_sum += entries[i] * expected_sum + mean;
-//     }
-//     ASSERT_FLOAT_EQ(expected_sum, actual_sum);
-//   }
-// };
+  auto vec_1_entries = vectorEntries(vecs[1]);
+  ASSERT_EQ(vec_1_entries.size(), 1);
+  assertFloatEq(vec_1_entries.at(0), 0.8);
 
-// TEST_F(TrendBlockTests, Trivial) {
-//   std::vector<std::shared_ptr<Block>> blocks{std::make_shared<TrendBlock>(
-//       /* has_count_col = */ true, /* id_col = */ 0,
-//       /* timestamp_col = */ 1, /* count_col = */ 2,
-//       /* horizon = */ 0, /* lookback = */ 2)};
+  auto vec_2_entries = vectorEntries(vecs[2]);
+  ASSERT_EQ(vec_2_entries.size(), 1);
+  assertFloatEq(vec_2_entries.at(0), 0.1);
 
-//   auto samples = makeTrivialSamples(/* n_ids = */ 1, /* n_days = */ 3650,
-//                                     /* day_offset = */ 365);
-//   auto vecs =
-//       makeSparseSegmentedVecs(samples, blocks, /* batch_interval = */ 10);
+  auto vec_3_entries = vectorEntries(vecs[3]);
+  ASSERT_EQ(vec_3_entries.size(), 1);
+  assertFloatEq(vec_3_entries.at(0), 0.45);
+}
 
-//   size_t i = 0;
-//   for (auto& vec : vecs) {
-//     auto entries = vectorEntries(vec);
-//     ASSERT_EQ(entries.size(), 3);
-//     if (i == 0) {
-//       ASSERT_EQ(entries.at(0), 0);
-//       ASSERT_EQ(entries.at(1), 0);
-//       ASSERT_EQ(entries.at(2), 0);
-//     } else {
-//       ASSERT_EQ(entries.at(0), (i % 2) - 0.5);
-//       ASSERT_EQ(entries.at(1), ((i + 1) % 2) - 0.5);
-//       ASSERT_EQ(entries.at(2), 0.5);
-//     }
-//     i++;
-//   }
-// }
+TEST_F(TrendBlockTests, CorrectOutputWindowSizeOneWithGraph) {
+  Graph graph {
+    {"id_1", {"id_2"}},
+    {"id_2", {"id_1"}},
+  };
 
-// TEST_F(TrendBlockTests, CorrectCenteringAndNormalization) {
-//   size_t n_ids = 100;
-//   size_t lookback = 10;
-//   std::vector<std::shared_ptr<Block>> blocks{std::make_shared<TrendBlock>(
-//       /* has_count_col = */ true, /* id_col = */ 0,
-//       /* timestamp_col = */ 1, /* count_col = */ 2,
-//       /* horizon = */ 0, /* lookback = */ lookback)};
+  std::vector<std::shared_ptr<Block>> blocks{std::make_shared<TrendBlock>(
+      /* has_count_col = */ true, /* id_col = */ 0,
+      /* timestamp_col = */ 1, /* count_col = */ 2,
+      /* horizon = */ 0, /* lookback = */ 1, /* period = */ 7,
+      std::make_shared<Graph>(std::move(graph)), /* max_n_neighbors = */ 1)};
 
-//   auto samples =
-//       makeTrivialSamples(/* n_ids = */ n_ids, /* n_days = */ 365,
-//                          /* day_offset = */ 365, /* inc_by_id */ true);
-//   auto vecs = makeSparseSegmentedVecs(samples, blocks, 256);
+  StringMatrix mock_data {
+    {"id_1", "2022-01-01", "0.3"}, // Trivial
+    {"id_2", "2022-01-01", "0.35"}, // Check handle neighbor
+    {"id_3", "2022-01-01", "0.45"}, // Check handle not in graph
+    {"id_1", "2022-01-02", "0.5"}, // Check same period gets aggregated
+  };
 
-//   bool found_nonzero_mean = false;
-//   float delta = 1e-6;
+  auto vecs =
+      makeSparseSegmentedVecs(mock_data, blocks);
+  
+  auto vec_0_entries = vectorEntries(vecs[0]);
+  ASSERT_EQ(vec_0_entries.size(), 2);
+  std::cout << vec_0_entries.at(0);
+  assertFloatEq(vec_0_entries.at(0), 0.3);
+  assertFloatEq(vec_0_entries.at(1), 0.0);
 
-//   size_t i = 0;
-//   for (auto& vec : vecs) {
-//     auto entries = vectorEntries(vec);
-//     ASSERT_EQ(entries.size(), lookback + 1);
-//     auto mean = entries.at(lookback);
-//     if (i >= lookback * n_ids) {
-//       ASSERT_FLOAT_EQ(mean, static_cast<float>(i % 100) / 2);
-//     }
-//     float sum = 0;
-//     for (size_t i = 0; i < lookback; i++) {
-//       sum += entries[i];
-//     }
-//     ASSERT_LE(std::abs(sum - 0.0), delta);
+  auto vec_1_entries = vectorEntries(vecs[1]);
+  ASSERT_EQ(vec_1_entries.size(), 2);
+  assertFloatEq(vec_1_entries.at(0), 0.35);
+  assertFloatEq(vec_1_entries.at(1), 0.3);
 
-//     float expected_sum = mean * lookback;
-//     float actual_sum = 0;
-//     for (size_t i = 0; i < lookback; i++) {
-//       actual_sum += entries[i] * expected_sum + mean;
-//     }
-//     ASSERT_FLOAT_EQ(expected_sum, actual_sum);
+  auto vec_2_entries = vectorEntries(vecs[2]);
+  ASSERT_EQ(vec_2_entries.size(), 1);
+  assertFloatEq(vec_2_entries.at(0), 0.45);
 
-//     if (mean != 0.0) {
-//       found_nonzero_mean = true;
-//     }
-//     i++;
-//   }
+  auto vec_3_entries = vectorEntries(vecs[3]);
+  ASSERT_EQ(vec_3_entries.size(), 2);
+  assertFloatEq(vec_3_entries.at(0), 0.8);
+  assertFloatEq(vec_3_entries.at(1), 0.35);
+}
 
-//   ASSERT_TRUE(found_nonzero_mean);
-// }
+TEST_F(TrendBlockTests, CorrectOutputLargerWindow) {
+  std::vector<std::shared_ptr<Block>> blocks{std::make_shared<TrendBlock>(
+      /* has_count_col = */ true, /* id_col = */ 0,
+      /* timestamp_col = */ 1, /* count_col = */ 2,
+      /* horizon = */ 0, /* lookback = */ 4, /* period = */ 1)};
 
-// TEST_F(TrendBlockTests, Graph) {
-//   size_t n_ids = 100;
-//   size_t horizon = 1;
-//   size_t lookback = 10;
-//   size_t graph_max_n_neighbors = 10;
-//   size_t block_max_n_neighbors = 8;
+  StringMatrix mock_data {
+    {"id_1", "2022-01-01", "0.1"}, // Trivial
+    {"id_1", "2022-01-02", "0.2"}, // Check handle neighbor
+    {"id_1", "2022-01-03", "0.3"}, // Check handle not in graph
+    {"id_1", "2022-01-04", "0.4"}, // Check same period gets aggregated
+  };
 
-//   auto graph = buildGraph(n_ids, graph_max_n_neighbors);
+  auto vecs =
+      makeSparseSegmentedVecs(mock_data, blocks);
+  
+  auto vec_3_entries = vectorEntries(vecs[3]);
+  ASSERT_EQ(vec_3_entries.size(), 4);
+  float l2_norm = std::sqrt(2 * (0.15*0.15 + 0.05* 0.05));
+  assertFloatEq(vec_3_entries.at(0), 0.15 / l2_norm);
+  assertFloatEq(vec_3_entries.at(1), 0.05 / l2_norm);
+  assertFloatEq(vec_3_entries.at(2), -0.05 / l2_norm);
+  assertFloatEq(vec_3_entries.at(3), -0.15 / l2_norm);
+}
 
-//   std::vector<std::shared_ptr<Block>> blocks{std::make_shared<TrendBlock>(
-//       /* has_count_col = */ true, /* id_col = */ 0,
-//       /* timestamp_col = */ 1, /* count_col = */ 2, horizon, lookback, graph,
-//       block_max_n_neighbors)};
-
-//   auto samples =
-//       makeTrivialSamples(/* n_ids = */ n_ids, /* n_days = */ 365,
-//                          /* day_offset = */ 365, /* inc_by_id */ true);
-//   auto vecs = makeSparseSegmentedVecs(samples, blocks, 256);
-
-//   float delta = 1e-6;
-
-//   size_t i = 0;
-
-//   for (auto& vec : vecs) {
-//     size_t id = i % n_ids;
-//     auto nbrs = getIntNeighbors(id, *graph);
-//     size_t expected_n_neighbors = std::min(nbrs.size(),
-//     block_max_n_neighbors);
-
-//     auto entries = vectorEntries(vec);
-//     ASSERT_EQ(entries.size(), (expected_n_neighbors + 1) * (lookback + 1));
-//     bool can_check_mean = i >= (lookback + horizon + 1) * n_ids;
-//     checkSegment(entries, lookback, can_check_mean, delta, id,
-//                  /* neighbor */ false);
-//     for (uint32_t i = 0; i < expected_n_neighbors; i++) {
-//       checkSegment(entries, lookback, can_check_mean, delta, nbrs[i],
-//                    /* neighbor */ true, /* neighbor_idx = */ i);
-//     }
-//     i++;
-//   }
-// }
-
-// // TODO(Geordie): Should change lifetime to check number of samples too
-// because
-// // otherwise if we have a window size of 1 and lag of zero we keep making new
-// // sketches. In fact, maybe only check number of samples instead of the
-// dates.
-// // This allows for some neat things, like e.g. instead of having a critical
-// // section within the add vector segment method, we can do it before the
-// // parallel for loop.
-
-// }  // namespace thirdai::dataset
+}  // namespace thirdai::dataset
