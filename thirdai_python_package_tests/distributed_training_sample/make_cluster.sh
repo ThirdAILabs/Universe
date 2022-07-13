374605754077155a1d8c75651b475da7795a89ef
Pratik Pranav <pratik@thirdai.com>
2022-07-13 13:37:13 +0000

    Added instructions to run

--- thirdai_python_package_tests/distributed_training_sample/make_cluster.sh
+++ thirdai_python_package_tests/distributed_training_sample/make_cluster.sh
@@ -4,5 +4,5 @@
 for i in $1; do
     scp start_ray_on_nodes.sh $USER@$i:/home/$USER
     ssh $i './start_ray_on_nodes.sh' $2
-    ssh $i 'rm start_ray_on_nodes.sh' $2
+    ssh $i 'rm start_ray_on_nodes.sh'
 done
