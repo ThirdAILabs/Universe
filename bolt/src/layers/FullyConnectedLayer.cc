448da96734de18474655a0cd3669c8baef70e7ee
Yashwanth Adunukota <yashwanthadunukota@Yashwanths-MacBook-Air.local>
2022-07-14 14:16:03 +0530

    some changes

--- bolt/src/layers/FullyConnectedLayer.cc
+++ bolt/src/layers/FullyConnectedLayer.cc
@@ -266,6 +266,7 @@ void FullyConnectedLayer::selectActiveNeurons(const BoltVector& input,
     _hasher->hashSingleSparse(input.active_neurons, input.activations,
                               input.len, hashes.data());
   }
+  
   srand(hashes[0]);
   if (_sampling_mode == LSHSamplingMode::FreezeHashTablesWithInsertions) {
     /**
