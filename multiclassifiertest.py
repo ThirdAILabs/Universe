0475c93bd35abed03a97cf08dccf9735c6d3918d
Benjamin Meisberger <ben@thirdai.com>
2022-07-19 10:49:45 -0500

    updated test file

--- multiclassifiertest.py
+++ multiclassifiertest.py
@@ -4,6 +4,7 @@ from thirdai.bolt import MultiLabelTextClassifier
 
 classifier = MultiLabelTextClassifier(input_dim=100000, hidden_layer_dim=1024, n_classes=931)
 
-classifier.train(train_file="wayfair_train.txt", epochs=1, learning_rate=0.001)
+classifier.train(train_file="wayfair_train.txt", epochs=3, learning_rate=0.001)
+classifier.train(train_file="wayfair_train.txt", epochs=2, learning_rate=0.0001)
 
-classifier.predict(test_file="wayfair_test.txt", output_file="wayfair_out.txt", threshold=0.8)
+classifier.predict(test_file="wayfair_test.txt", output_file="wayfair_out.txt", threshold=0.5)
