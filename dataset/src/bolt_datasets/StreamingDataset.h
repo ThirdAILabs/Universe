2c9ed9593a1552b7ce73f2a449e37ce19ae8fbd6
Josh Engels <josh@thirdai.com>
2022-07-20 09:36:37 -0500

    Trying fix

--- dataset/src/bolt_datasets/StreamingDataset.h
+++ dataset/src/bolt_datasets/StreamingDataset.h
@@ -86,10 +86,10 @@ class StreamingDataset {
     // template that maps each vector of batches to an InMemoryDataset.
     std::tuple<std::shared_ptr<InMemoryDataset<BATCH_Ts>>...> dataset_ptrs =
         std::apply(
-            [&](auto&... batch_lists) {
+            [&](auto&... batch_lists_arg) {
               return std::make_tuple(
                   std::make_shared<InMemoryDataset<BATCH_Ts>>(
-                      std::move(batch_lists))...);
+                      std::move(batch_lists_arg))...);
             },
             batch_lists);
 
