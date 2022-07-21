a17a055f43ab58d4849aa5aa9dba453f9f626799
Yashwanth Adunukota <yashwanthadunukota@Yashwanths-MacBook-Air.local>
2022-07-21 17:40:47 +0530

    some changes

--- bolt/python_tests/test_input_gradients.py
+++ bolt/python_tests/test_input_gradients.py
@@ -69,43 +69,74 @@ def assert_ratio(network, x1, labels):
     assert (max_times / total) > 0.7
 
 
-@pytest.mark.unit
-def test_input_gradients_sample_data():
-    """
-    for "[1,0,0,0]" these type of inputs, we expect gradients
-    to be max for non-zero index, and for [1,1,0,0] the label mentioned '1' but for
-    [1,0,0,0] label mentioned '0' so the second index element has most influence over
-    the input to flip the label, so expected to have high gradient.
-    """
-    network = initialize_network()
+def get_vector(k=-1):
     x1 = np.array(
         [
             [1, 0, 0, 0],
             [0, 1, 0, 0],
             [0, 0, 1, 0],
             [0, 0, 0, 1],
-            [1, 1, 0, 0],
-            [1, 0, 1, 0],
-            [0, 1, 0, 1],
         ]
     ).astype("float32")
-    labels = np.array([0, 1, 2, 3, 1, 2, 1]).astype("uint32")
-    assert_ratio(network, x1, labels)
+    if k == -1:
+        return x1
+    return x1[k]
+
+
+@pytest.mark.unit
+def test_input_gradients():
+    """
+    For a given input and a fixed label on output, the INCREASE in activation of that label,
+    when we add a small EPS to the input at each index seperately, should be in the
+    same order as the input gradients. For example, let us have an input vector [1,0,0,0] and we choose output label as 1.
+    If the input gradients are in the order <2,3,1,0> (the order is on the input indices), then the increase in activation for label 1
+    should also be in same order, when we add small EPS at each position seperately.
+    """
+    network = initialize_network()
+    inputs = get_vector()
+    gradients, indices = network.get_input_gradients(
+        inputs,
+        bolt.CategoricalCrossEntropyLoss(),
+        required_labels=np.array([1, 1, 1, 1]).astype("uint32"),
+    )
+    _, act = network.predict(inputs, None)
+    """
+    For every vector in input,we modify the vector at every position(by adding EPS), and we check the above assertion.
+    """
+    for input_num in range(len(inputs)):
+        modified_vectors = []
+        for i in range(len(inputs[input_num])):
+            vec = get_vector(input_num)
+            vec[i] = vec[i] + 0.001
+            modified_vectors.append(vec)
+        modified_vectors = np.array(modified_vectors)
+        _, vecs_act = network.predict(modified_vectors, None)
+        difference = [
+            np.array(vec_act) - np.array(act[input_num]) for vec_act in vecs_act
+        ]
+        activation_difference = [diff[1] for diff in difference]
+        assert (
+            np.array_equal(
+                np.argsort(activation_difference), np.argsort(gradients[input_num])
+            )
+            == True
+        )
 
 
 @pytest.mark.unit
 def test_input_gradients_random_data():
     """
-    we check the same thing as above but for bigger and random dataset.
+    for "[1,0,0,0]" when these type of vectors given as input and output,
+    we expect gradients to be max for non-zero index.
     """
     network = initialize_network()
-    x1, labels = gen_training_data(4, 24000, 0.05)
+    inputs, labels = gen_training_data(4, 24000, 0.05)
     times = network.train(
-        x1,
+        inputs,
         labels,
         bolt.CategoricalCrossEntropyLoss(),
         learning_rate=0.001,
         epochs=5,
         batch_size=256,
     )
-    assert_ratio(network, x1, labels)
+    assert_ratio(network, inputs, labels)
