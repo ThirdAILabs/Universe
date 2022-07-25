98dd805c2384e4f4a9b7fbde4a95b0a6812c2ecb
Josh <josh@thirdai.com>
2022-07-25 13:34:45 -0500

    Correctly checking for perf

--- bin/create_flamegraph.sh
+++ bin/create_flamegraph.sh
@@ -3,7 +3,7 @@
 
 # TODO(anyone): This just works on linux. Add support for mac?
 # See https://stackoverflow.com/questions/592620/how-can-i-check-if-a-program-exists-from-a-bash-script
-type foo >/dev/null 2>&1 || { echo >&2 "This script requires perf but it's not installed.  Aborting."; exit 1; }
+type perf >/dev/null 2>&1 || { echo >&2 "This script requires perf but it's not installed.  Aborting."; exit 1; }
 
 perf record -F 99 --call-graph dwarf -o perf.out "$@"
 
