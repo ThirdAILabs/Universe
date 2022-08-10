b817fb4c0905359e52291322fad6380cd2e72792
shubh3ai <shubh@thirdai.com>
2022-08-10 10:09:31 +0000

    changes before rebasing with main

--- thirdai_python_package_tests/distributed_training_sample/train_distributed_amzn670k.py
+++ thirdai_python_package_tests/distributed_training_sample/train_distributed_amzn670k.py
@@ -1,8 +1,8 @@
 from thirdai.distributed_bolt import DistributedBolt
 
 # config_filename = './amzn670k_distributed.txt'
-config_filename='./amazon_polarity.txt'
-# config_filename = './mnist.txt'
+# config_filename='./amazon_polarity.txt'
+config_filename = './mnist.txt'
 head = DistributedBolt(2, config_filename) 
 head.train(circular=False,compression=None,compression_density=0.05)
 acc , _ = head.predict() 
