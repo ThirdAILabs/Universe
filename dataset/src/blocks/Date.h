7bb240e234d33cc8af938169616db56c056b887e
BenGeordie <school@bengeordie@gmail.com>
2022-08-10 17:42:39 -0500

    hope it looks good to david now

--- dataset/src/blocks/Date.h
+++ dataset/src/blocks/Date.h
@@ -2,11 +2,23 @@
 
 #include "BlockInterface.h"
 #include <dataset/src/utils/TimeUtils.h>
+#include <exception>
+#include <stdexcept>
 
 namespace thirdai::dataset {
 
 class DateBlockTests;
 
+/**
+ * Parses a date string in "YYYY-MM-DD" format
+ * and encodes in a vector the day of the week,
+ * the month of the year, the week of the month,
+ * and the week of the year, each one-hot
+ * encoded in a separate segment of the vector.
+ *
+ * TODO(Geordie): try out other features
+ * such as day of month, day of year, year, etc.
+ */
 class DateBlock : public Block {
   friend DateBlockTests;
 
@@ -20,54 +32,50 @@ class DateBlock : public Block {
 
   bool isDense() const final { return false; };
 
-  uint32_t expectedNumColumns() const final { return _col + 1; };
+  uint32_t expectedNumColumns() const final {
+    // _col is the column index, so we expect at least
+    // _col + 1 columns in each input row.
+    return _col + 1;
+  };
 
  protected:
   static constexpr uint32_t day_of_week_dim = 7;
   static constexpr uint32_t month_of_year_dim = 12;
   static constexpr uint32_t week_of_month_dim = 5;
-  static constexpr uint32_t week_of_year_dim = 49;
+  static constexpr uint32_t week_of_year_dim = 53;
 
   std::exception_ptr buildSegment(
       const std::vector<std::string_view>& input_row,
       SegmentedFeatureVector& vec) final {
-    struct tm time;
-    auto exception = TimeUtils::timeStringToTimeObject(input_row[_col], time);
-    if (exception) {
-      return exception;
+    TimeObject time;
+
+    try {
+      time = TimeObject(input_row[_col]);
+    } catch (const std::invalid_argument& e) {
+      return std::make_exception_ptr(e);
     }
 
-    uint32_t epoch_time = TimeUtils::timeToEpoch(time);
+    uint32_t epoch_time = time.secondsSinceEpoch();
     uint32_t offset = 0;
 
-    vec.addSparseFeatureToSegment(offset + dayOfWeek(epoch_time), 1.0);
+    uint32_t day_of_week = (epoch_time / TimeObject::SECONDS_IN_DAY) % 7;
+    vec.addSparseFeatureToSegment(offset + day_of_week, 1.0);
     offset += day_of_week_dim;
 
-    vec.addSparseFeatureToSegment(offset + time.tm_mon, 1.0);
+    vec.addSparseFeatureToSegment(offset + time.month(), 1.0);
     offset += month_of_year_dim;
 
-    vec.addSparseFeatureToSegment(offset + weekOfMonth(time), 1.0);
+    uint32_t week_of_month = time.dayOfMonthZeroIndexed() / 7;
+    vec.addSparseFeatureToSegment(offset + week_of_month, 1.0);
     offset += week_of_month_dim;
 
-    vec.addSparseFeatureToSegment(offset + weekOfYear(time), 1.0);
+    uint32_t week_of_year = time.dayOfYear() / 7;
+    vec.addSparseFeatureToSegment(offset + week_of_year, 1.0);
 
     return nullptr;
   }
 
  private:
-  static constexpr uint32_t dayOfWeek(uint32_t epoch_time) {
-    return (epoch_time / TimeUtils::SECONDS_IN_DAY) % 7;
-  }
-
-  static constexpr uint32_t weekOfMonth(struct tm& time) {
-    return (time.tm_mday - 1) / 7;  // tm_mday starts at 1.
-  }
-
-  static constexpr uint32_t weekOfYear(struct tm& time) {
-    uint32_t weeks_to_current_month = time.tm_mon * 4;
-    return weeks_to_current_month + weekOfMonth(time);
-  }
-
   uint32_t _col;
 };
 
