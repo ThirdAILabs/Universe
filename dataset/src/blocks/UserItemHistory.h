5b69ab8d454ec835d9527d3eee8eed53c3c0e9ab
BenGeordie <school@bengeordie@gmail.com>
2022-08-12 18:04:02 -0500

    user item history block

--- dataset/src/blocks/UserItemHistory.h
+++ dataset/src/blocks/UserItemHistory.h
@@ -6,38 +6,37 @@
 #include <optional>
 #include <unordered_map>
 namespace thirdai::dataset {
+struct ItemRecord {
+  bool valid;
+  uint32_t item;
+  int64_t timestamp;
+};
 
-template <typename ELEMENT_T>
 class UserItemBuffer {
  public:
   explicit UserItemBuffer(size_t size) : _buffer(size), _idx(0) {}
 
-  void insert(ELEMENT_T element) {
-    _buffer[getAndUpdateIdx()] = std::move(element);
-  }
+  UserItemBuffer(const UserItemBuffer& other)
+      : _buffer(other._buffer), _idx(other._idx) {}
 
-  const std::vector<std::atomic<ELEMENT_T>>& view() { return _buffer; }
+  void insert(ItemRecord element) { _buffer[getAndUpdateIdx()] = element; }
+
+  const std::vector<ItemRecord>& view() { return _buffer; }
 
  private:
   uint32_t getAndUpdateIdx() {
-    uint32_t cur_idx = _idx.load();
-    uint32_t new_idx = nextIdx(cur_idx);
-    while (!_idx.compare_exchange_weak(cur_idx, new_idx)) {
-      new_idx = nextIdx(cur_idx);
-    }
+    uint32_t cur_idx = _idx;
+    _idx = nextIdx(cur_idx);
     return cur_idx;
   }
 
   uint32_t nextIdx(uint32_t idx) { return (idx + 1) % _buffer.size(); }
 
-  std::vector<std::atomic<ELEMENT_T>> _buffer;
-  std::atomic_uint32_t _idx;
+  std::vector<ItemRecord> _buffer;
+  uint32_t _idx;
 };
 
-struct ItemRecord {
-  uint32_t item;
-  int64_t timestamp;
-};
+using UserItemHistoryRecords = std::vector<UserItemBuffer>;
 
 /**
  * Tracks up to the last N items associated with each user.
@@ -49,8 +48,7 @@ class UserItemHistoryBlock final : public Block {
       uint32_t track_last_n,
       std::shared_ptr<std::unordered_map<std::string, uint32_t>> user_id_map,
       std::shared_ptr<std::unordered_map<std::string, uint32_t>> item_id_map,
-      std::shared_ptr<std::vector<UserItemBuffer<std::optional<ItemRecord>>>>
-          records)
+      std::shared_ptr<UserItemHistoryRecords> records)
       : _user_col(user_col),
         _item_col(item_col),
         _timestamp_col(timestamp_col),
@@ -58,6 +56,12 @@ class UserItemHistoryBlock final : public Block {
         _user_id_map(std::move(user_id_map)),
         _item_id_map(std::move(item_id_map)),
         _records(std::move(records)) {}
+
+  static std::shared_ptr<UserItemHistoryRecords> makeEmptyRecord(
+      uint32_t n_users, uint32_t track_last_n) {
+    UserItemHistoryRecords records(n_users, UserItemBuffer(track_last_n));
+    return std::make_shared<UserItemHistoryRecords>(std::move(records));
+  }
 
   uint32_t featureDim() const final { return _item_id_map->size(); }
 
@@ -82,34 +86,33 @@ class UserItemHistoryBlock final : public Block {
       uint32_t item_id = _item_id_map->at(item_str);
       int64_t epoch_timestamp = TimeObject(timestamp_str).secondsSinceEpoch();
 
-      encodeTrackedItems(user_id, item_id, epoch_timestamp, vec);
+#pragma omp critical(user_item_history_block)
+      {
+        encodeTrackedItems(user_id, epoch_timestamp, vec);
 
-      // Insert new item after adding to the vector to not give away new
-      _records->at(user_id).insert(std::make_optional<ItemRecord>(
-          {/* item = */ item_id, /* timestamp = */ epoch_timestamp}));
+        // Insert new item after adding to the vector to not give away new
+        _records->at(user_id).insert({/* valid = */ true, /* item = */ item_id,
+                                      /* timestamp = */ epoch_timestamp});
+      }
 
-    } catch (std::exception_ptr& except) {
-      return except;
+    } catch (std::exception& except) {
+      return std::make_exception_ptr(except);
     }
     return nullptr;
   }
 
  private:
-  void encodeTrackedItems(uint32_t user_id, uint32_t item_id,
-                          int64_t epoch_timestamp,
+  void encodeTrackedItems(uint32_t user_id, int64_t epoch_timestamp,
                           SegmentedFeatureVector& vec) {
     uint32_t added = 0;
-    
 
-    for (const auto& atomic_item_opt : _records->at(user_id).view()) {
+    for (const auto& item : _records->at(user_id).view()) {
       if (added >= _track_last_n) {
         break;
       }
 
-      auto item_opt = atomic_item_opt.load();
-      if (item_opt.has_value() &&
-          item_opt.value().timestamp <= epoch_timestamp) {
-        vec.addSparseFeatureToSegment(item_opt.value().item, 1.0);
+      if (item.valid && item.timestamp <= epoch_timestamp) {
+        vec.addSparseFeatureToSegment(item.item, 1.0);
         added++;
       }
     }
@@ -123,8 +126,7 @@ class UserItemHistoryBlock final : public Block {
   std::shared_ptr<std::unordered_map<std::string, uint32_t>> _user_id_map;
   std::shared_ptr<std::unordered_map<std::string, uint32_t>> _item_id_map;
 
-  std::shared_ptr<std::vector<UserItemBuffer<std::optional<ItemRecord>>>>
-      _records;
+  std::shared_ptr<UserItemHistoryRecords> _records;
 };
 
 }  // namespace thirdai::dataset
--- dataset/tests/UserItemHistoryBlock.cc
+++
@@ -1,3 +0,0 @@
-// Test works fine when sequential.
-// Test works fine when parallel
-    // - "fine" defined as all the tracked items' timestamps < latest item timestamp.
