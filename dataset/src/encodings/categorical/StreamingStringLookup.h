b9831382c2af5ecda6e40cbf1b7069ce3b23f41d
BenGeordie <school@bengeordie@gmail.com>
2022-08-12 02:20:02 -0500

    test quirks

--- dataset/src/encodings/categorical/StreamingStringLookup.h
+++ dataset/src/encodings/categorical/StreamingStringLookup.h
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <iostream>
 #include <optional>
 #include <sstream>
@@ -16,7 +17,7 @@ class StreamingStringLookup {
     _registration_signatures.reserve(n_unique);
   }
 
-  uint32_t lookup(std::string& string) {
+  std::pair<uint32_t, std::string> lookup(std::string& string) {
     if (completelyRegistered(string)) {
       /* 
         It is safe to call unordered_map::at() here since
@@ -31,7 +32,13 @@ class StreamingStringLookup {
 
         TODO: May need to explain about C++ reference cannot be invalidated.
       */
-      return _string_to_uid.at(string);
+      
+      auto uid = _string_to_uid.at(string);
+      if (_uid_to_string[uid] == string) {
+        std::stringstream explanation;
+        explanation << "Mapped " << string << " to " << uid << ". Supposedly completely registered.";
+        return {uid, explanation.str()};
+      }
     }
     
     return registerNewString(string);
@@ -46,10 +53,17 @@ class StreamingStringLookup {
     return _uid_to_string[uid];
   }
 
+  void writeToFile(std::ofstream& out) {
+    for (uint32_t uid = 0; uid < _expected_n_unique; uid++) {
+      out << uid << " : " << _uid_to_string[uid] << std::endl;
+    }
+  }
+
  private:
 
-  uint32_t registerNewString(std::string& string) {
+  std::pair<uint32_t, std::string> registerNewString(std::string& string) {
     uint32_t uid = _expected_n_unique;
+    std::stringstream explanation;
 #pragma omp critical(streaming_string_lookup)
     {
       // We need to double check because another thread
@@ -58,19 +72,22 @@ class StreamingStringLookup {
         // No need to check that the string registration is 
         // signed since this is in a critical section.
         uid = _string_to_uid.at(string);
+        explanation << "Mapped " << string << " to " << uid << ". In critical section but supposedly seen before.";
+
       } else {
         uid = _string_to_uid.size();
 
         if (uid < _expected_n_unique) {
           auto dependency_signature = updateMapsAndGetDepSignature(uid, string);
           signRegistrationComplete(string, dependency_signature);
+          explanation << "Mapped " << string << " to " << uid << ". Added new.";
         } else {
           rejectRegistration();
         }
       }
     }
     
-    return uid;
+    return {uid, explanation.str()};
   }
 
   inline uint32_t updateMapsAndGetDepSignature(uint32_t uid, std::string& string) {
@@ -91,7 +108,7 @@ class StreamingStringLookup {
   }
 
   inline bool completelyRegistered(std::string& string) const {
-    return _registration_signatures.count(string) > 0;
+    return _registration_signatures.count(string) > 0 ;
   } 
 
   inline void rejectRegistration() const {
--- dataset/tests/StreamingStringLookupTests.cc
+++ dataset/tests/StreamingStringLookupTests.cc
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <cstddef>
 #include <cstdlib>
+#include <fstream>
 #include <random>
 #include <string>
 #include <vector>
@@ -35,13 +36,16 @@ static std::vector<std::string> generateRandomStrings(size_t n_unique, size_t re
   return strings;
 }
 
-std::vector<uint32_t> getUidsInParallel(StreamingStringLookup& lookup, std::vector<std::string>& strings) {
+std::pair<std::vector<uint32_t>, std::vector<std::string>> getUidsInParallel(StreamingStringLookup& lookup, std::vector<std::string>& strings) {
   std::vector<uint32_t> uids(strings.size());
-#pragma omp parallel for default(none) shared(strings, uids, lookup)
+  std::vector<std::string> explanations(strings.size());
+#pragma omp parallel for default(none) shared(strings, uids, lookup, explanations)
   for (uint32_t idx = 0; idx < strings.size(); idx++) {
-    uids[idx] = lookup.lookup(strings[idx]);
+    auto res = lookup.lookup(strings[idx]);
+    uids[idx] = res.first;
+    explanations[idx] = res.second;
   }
-  return uids;
+  return {uids, explanations};
 }
 
 std::vector<std::string> backToStrings(StreamingStringLookup& lookup, std::vector<uint32_t>& uids) {
@@ -54,10 +58,46 @@ std::vector<std::string> backToStrings(StreamingStringLookup& lookup, std::vecto
   return strings;
 }
 
-void assertStringsEqual(std::vector<std::string>& strings_1, std::vector<std::string>& strings_2) {
+void assertStringsEqual(std::vector<std::string>& strings_1, std::vector<std::string>& strings_2, std::vector<uint32_t>& uids, StreamingStringLookup& lookup, std::vector<std::string>& explanations) {
   ASSERT_EQ(strings_1.size(), strings_2.size());
   for (uint32_t idx = 0; idx < strings_1.size(); idx++) {
-    ASSERT_EQ(strings_1[idx], strings_2[idx]);
+    if (strings_1[idx] != strings_2[idx]) {
+      std::cout << "Error with lookup with explanation: " << explanations[idx] << std::endl;
+      std::cout << "Index: " << idx << std::endl;
+      std::cout << "String 1: " << strings_1[idx] << std::endl;
+      std::cout << "String 2: " << strings_2[idx] << std::endl;
+      std::cout << "Uid: " << uids[idx] << std::endl;
+      std::cout << "First occurrence of " << strings_1[idx] << " in source is ";
+      for (uint32_t first_idx = 0; first_idx < strings_1.size(); first_idx++) {
+        if (strings_1[first_idx] == strings_1[idx]) {
+          std::cout << first_idx << std::endl;
+          std::cout << "Explanation: " << explanations[first_idx] << std::endl;
+          break;
+        }
+      }
+      std::cout << "First occurrence of " << strings_2[idx] << " in source is ";
+      for (uint32_t first_idx = 0; first_idx < strings_2.size(); first_idx++) {
+        if (strings_1[first_idx] == strings_2[idx]) {
+          std::cout << first_idx << std::endl;
+          std::cout << "Explanation: " << explanations[first_idx] << std::endl;
+          break;
+        }
+      }
+      std::cout << "Previous occurrence of " << strings_1[idx] << " in source is ";
+      for (int prev_idx = idx - 1; prev_idx >= 0; prev_idx--) {
+        if (strings_1[prev_idx] == strings_1[idx]) {
+          std::cout << prev_idx << std::endl;
+          std::cout << "Explanation: " << explanations[prev_idx] << std::endl;
+          break;
+        }
+      }
+      std::stringstream ss;
+      ss << "log" << idx << ".txt";
+      std::ofstream out(ss.str());
+      lookup.writeToFile(out);
+      out.close();
+    }
+    // ASSERT_EQ(strings_1[idx], strings_2[idx]);
   }
 }
 
@@ -65,11 +105,12 @@ void assertStringsEqual(std::vector<std::string>& strings_1, std::vector<std::st
 
 TEST(StreamingStringLookupTests, DoesNotBreak) {
   for (uint32_t trial = 0; trial < 100; trial++) {
+    std::cout << "new trial" << std::endl;
     auto strings = generateRandomStrings(/* n_unique = */ 1000, /* repetitions = */ 1000, /* len = */ 10);
     StreamingStringLookup lookup(/* n_unique = */ 1000);
-    auto uids = getUidsInParallel(lookup, strings);
+    auto [uids, explanations] = getUidsInParallel(lookup, strings);
     auto reverted_strings = backToStrings(lookup, uids);
-    assertStringsEqual(strings, reverted_strings);
+    assertStringsEqual(strings, reverted_strings, uids, lookup, explanations);
   }
 }
 
