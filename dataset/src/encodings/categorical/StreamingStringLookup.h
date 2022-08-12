78a777e59db9f6f704c18cc3bb3510a988509f82
BenGeordie <school@bengeordie@gmail.com>
2022-08-12 01:07:23 -0500

    streaming string lookup class

---
+++ dataset/src/encodings/categorical/StreamingStringLookup.h
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <optional>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+namespace thirdai::dataset {
+
+class StreamingStringLookup {
+ public:
+  explicit StreamingStringLookup(uint32_t n_unique)
+      : _uid_to_string(n_unique), _expected_n_unique(n_unique) {
+    _string_to_uid.reserve(n_unique);
+    _registration_signatures.reserve(n_unique);
+  }
+
+  uint32_t lookup(std::string& string) {
+    if (completelyRegistered(string)) {
+      /* 
+        It is safe to call unordered_map::at() here since
+        1) we reserved enough buckets for n_unique elements
+        and we reject new strings after seeing n_unique unique
+        strings (see registerNewString), so the map's buckets
+        and hash function remain the same throughout.
+        2) From the C++ standard found in 
+        (http://eel.is/c++draft/unord.req#general-9), 
+        "Rehashing ... does not invalidate pointers or references 
+        to elements." This implies 
+
+        TODO: May need to explain about C++ reference cannot be invalidated.
+      */
+      return _string_to_uid.at(string);
+    }
+    
+    return registerNewString(string);
+  }
+
+  std::string originalString(uint32_t uid) {
+    uint32_t max_valid_uid = _string_to_uid.size() - 1;
+    if (uid > max_valid_uid) {
+      return "out-of-vocab";
+    }
+
+    return _uid_to_string[uid];
+  }
+
+ private:
+
+  uint32_t registerNewString(std::string& string) {
+    uint32_t uid = _expected_n_unique;
+#pragma omp critical(streaming_string_lookup)
+    {
+      // We need to double check because another thread
+      // may have registered this string before we got here.
+      if (_string_to_uid.count(string) > 0) {
+        // No need to check that the string registration is 
+        // signed since this is in a critical section.
+        uid = _string_to_uid.at(string);
+      } else {
+        uid = _string_to_uid.size();
+
+        if (uid < _expected_n_unique) {
+          auto dependency_signature = updateMapsAndGetDepSignature(uid, string);
+          signRegistrationComplete(string, dependency_signature);
+        } else {
+          rejectRegistration();
+        }
+      }
+    }
+    
+    return uid;
+  }
+
+  inline uint32_t updateMapsAndGetDepSignature(uint32_t uid, std::string& string) {
+    _string_to_uid[string] = uid;
+    _uid_to_string[uid] = string;
+    // The size of _string_to_uid is a value that is
+    // dependent on this operation's execution.
+    return _string_to_uid.size();
+  }
+
+  inline void signRegistrationComplete(std::string& string, uint32_t dependency_signature) {
+    /*
+      Dependency signature enforces that this operation is
+      executed after the operation that produces the signature.
+      (prevent unwanted instruction reordering by compiler)
+    */
+    _registration_signatures[string] = dependency_signature;
+  }
+
+  inline bool completelyRegistered(std::string& string) const {
+    return _registration_signatures.count(string) > 0;
+  } 
+
+  inline void rejectRegistration() const {
+    std::cerr << "[StreamingStringLookup] WARNING: expected " << _expected_n_unique
+              << " classes but found more. Clubbing extraneous classes to the "
+                 "same ID."
+              << std::endl;
+  }
+
+  std::unordered_map<std::string, uint32_t> _string_to_uid;
+  std::unordered_map<std::string, uint32_t> _registration_signatures;
+  std::vector<std::string> _uid_to_string;
+  uint32_t _expected_n_unique;
+};
+
+}  // namespace thirdai::dataset
---
+++ dataset/tests/StreamingStringLookupTests.cc
@@ -0,0 +1,78 @@
+#include <dataset/src/encodings/categorical/StreamingStringLookup.h>
+#include <gtest/gtest.h>
+#include <sys/types.h>
+#include <algorithm>
+#include <cstddef>
+#include <cstdlib>
+#include <random>
+#include <string>
+#include <vector>
+
+namespace thirdai::dataset {
+
+static std::vector<std::string> generateRandomStrings(size_t n_unique, size_t repetitions, size_t len) {
+  static const char alphanum[] =
+      "0123456789"
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      "abcdefghijklmnopqrstuvwxyz";
+
+  std::vector<std::string> strings(n_unique * repetitions);
+  for (uint32_t unique = 0; unique < n_unique; unique++) {
+    std::string random_string;
+    random_string.reserve(len);
+    
+    for (uint32_t i = 0; i < len; ++i) {
+      random_string += alphanum[std::rand() % (sizeof(alphanum) - 1)];
+    }
+
+    for (uint32_t rep = 0; rep < repetitions; rep++) {
+      strings[unique * repetitions + rep] = random_string;
+    }
+  }
+
+  auto rng = std::default_random_engine {};
+  std::shuffle(strings.begin(), strings.end(), rng);
+  return strings;
+}
+
+std::vector<uint32_t> getUidsInParallel(StreamingStringLookup& lookup, std::vector<std::string>& strings) {
+  std::vector<uint32_t> uids(strings.size());
+#pragma omp parallel for default(none) shared(strings, uids, lookup)
+  for (uint32_t idx = 0; idx < strings.size(); idx++) {
+    uids[idx] = lookup.lookup(strings[idx]);
+  }
+  return uids;
+}
+
+std::vector<std::string> backToStrings(StreamingStringLookup& lookup, std::vector<uint32_t>& uids) {
+  std::vector<std::string> strings;
+  strings.reserve(uids.size());
+  for (auto uid : uids) {
+    strings.push_back(lookup.originalString(uid));
+  }
+
+  return strings;
+}
+
+void assertStringsEqual(std::vector<std::string>& strings_1, std::vector<std::string>& strings_2) {
+  ASSERT_EQ(strings_1.size(), strings_2.size());
+  for (uint32_t idx = 0; idx < strings_1.size(); idx++) {
+    ASSERT_EQ(strings_1[idx], strings_2[idx]);
+  }
+}
+
+// TODO(Geordie): Compare with registerNewString() since it's basically the all-in-critical-section version of the lookup method.
+
+TEST(StreamingStringLookupTests, DoesNotBreak) {
+  auto strings = generateRandomStrings(/* n_unique = */ 1000, /* repetitions = */ 1000, /* len = */ 10);
+  StreamingStringLookup lookup(/* n_unique = */ 1000);
+  auto uids = getUidsInParallel(lookup, strings);
+  auto reverted_strings = backToStrings(lookup, uids);
+  assertStringsEqual(strings, reverted_strings);
+}
+
+TEST(StreamingStringLookupTests, LowOverheadWhenSingleThread) {}
+
+TEST(StreamingStringLookupTests, MuchFasterWhenMultiThread) {}
+
+} // namespace thirdai::dataset
