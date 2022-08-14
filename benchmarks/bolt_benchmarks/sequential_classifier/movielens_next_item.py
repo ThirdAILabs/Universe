8694a09029d5745854d321dce92228194b65c475
Benito Geordie <benito@thirdai.com>
2022-08-14 23:37:07 +0000

    script

---
+++ benchmarks/bolt_benchmarks/sequential_classifier/movielens_next_item.py
@@ -0,0 +1,28 @@
+from thirdai import bolt
+
+train_file = "/share/benito/movielens1m/train.csv"
+test_file = "/share/benito/movielens1m/test.csv"
+predict_file = "/share/benito/movielens1m/movielens1m_pred.csv"
+
+user_tup = ("userId", 6040)
+movie_tup = ("movieId",3706)
+
+
+def seq_with_temporal_feats():
+    return bolt.SequentialClassifier(
+        model_size="4Gb",
+        user=user_tup,
+        timestamp="timestamp",
+        target=movie_tup,
+        sequential=[("movieId", 3706, 5), ("movieId", 3706, 10), ("movieId", 3706, 25), ("movieId", 3706, 50)],
+    )
+
+def train_and_evaluate(seq):
+    seq.train(train_file, epochs=3, learning_rate=0.0001)
+    seq.predict(test_file, predict_file)
+
+def main():
+    train_and_evaluate(seq_with_temporal_feats())
+
+if __name__ == "__main__":
+    main()
