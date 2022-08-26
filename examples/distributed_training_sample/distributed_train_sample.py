eb882579ded2e773d4f632424174e1433974c452
Pratik <pratik@thirdai.com>
2022-08-26 09:59:54 +0000

    Changed folder name and introduced separate classes for communication

--- examples/distributed_training_sample/distributed_train_sample.py
+++ examples/distributed_training_sample/distributed_train_sample.py
@@ -4,10 +4,10 @@ import thirdai.distributed_bolt as DistributedBolt
 if __name__ == "__main__":
     config_filename = "./default_config.txt"
     head = DistributedBolt.FullyConnectedNetwork(
-        no_of_workers=2,
+        num_workers=2,
         config_filename=config_filename,
         num_cpus_per_node=20,
     )
-    head.train(circular=True)
+    head.train(circular=False)
     metrics, _ = head.predict()
     print(metrics["categorical_accuracy"])
