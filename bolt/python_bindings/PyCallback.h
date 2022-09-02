7322ecadae3c066444106687826a4fdde46d9c9f
David Torres <david@thirdai.com>
2022-09-02 17:08:26 -0500

    tests

--- bolt/python_bindings/PyCallback.h
+++ bolt/python_bindings/PyCallback.h
@@ -1,9 +1,10 @@
 #pragma once
 
 #include <bolt/src/graph/callbacks/Callback.h>
+#include <pybind11/pybind11.h>
 
 namespace thirdai::bolt {
-// For explanation of PYBIND11_OVERRIDE_PURE_NAME, see
+// For explanation of PYBIND11_OVERRIDE_PURE, see
 //  https://pybind11.readthedocs.io/en/stable/advanced/classes.html#overriding-virtual-functions-in-python
 //  Basically this allows us to define data loaders in python
 class PyCallback : public Callback {
@@ -12,64 +13,64 @@ class PyCallback : public Callback {
   using Callback::Callback;
 
   void onTrainBegin(BoltGraph& model) override {
-    PYBIND11_OVERRIDE_PURE(
-        void,             /* Return type */
-        Callback,         /* Parent class */
-        "on_train_begin", /* Name of function in C++ (must match Python name) */
-        model             /* Argument(s) */
+    PYBIND11_OVERRIDE_NAME(void,             /* Return type */
+                           Callback,         /* Parent class */
+                           "on_train_begin", /* Name of Python function */
+                           onTrainBegin,     /* Name of C++ function */
+                           model             /* Argument(s) */
     );
   }
 
   void onTrainEnd(BoltGraph& model) override {
-    PYBIND11_OVERRIDE_PURE(
-        void,           /* Return type */
-        Callback,       /* Parent class */
-        "on_train_end", /* Name of function in C++ (must match Python name) */
-        model           /* Argument(s) */
+    PYBIND11_OVERRIDE_NAME(void,           /* Return type */
+                           Callback,       /* Parent class */
+                           "on_train_end", /* Name of Python function */
+                           onTrainEnd,     /* Name of C++ function */
+                           model           /* Argument(s) */
     );
   }
 
   void onEpochBegin(BoltGraph& model) override {
-    PYBIND11_OVERRIDE_PURE(
-        void,             /* Return type */
-        Callback,         /* Parent class */
-        "on_epoch_begin", /* Name of function in C++ (must match Python name) */
-        model             /* Argument(s) */
+    PYBIND11_OVERRIDE_NAME(void,             /* Return type */
+                           Callback,         /* Parent class */
+                           "on_epoch_begin", /* Name of Python function */
+                           onEpochBegin,     /* Name of C++ function */
+                           model             /* Argument(s) */
     );
   }
 
   void onEpochEnd(BoltGraph& model) override {
-    PYBIND11_OVERRIDE_PURE(
-        void,           /* Return type */
-        Callback,       /* Parent class */
-        "on_epoch_end", /* Name of function in C++ (must match Python name) */
-        model           /* Argument(s) */
+    PYBIND11_OVERRIDE_NAME(void,           /* Return type */
+                           Callback,       /* Parent class */
+                           "on_epoch_end", /* Name of Python function */
+                           onEpochEnd,     /* Name of C++ function */
+                           model           /* Argument(s) */
     );
   }
 
   void onBatchBegin(BoltGraph& model) override {
-    PYBIND11_OVERRIDE_PURE(
-        void,             /* Return type */
-        Callback,         /* Parent class */
-        "on_batch_begin", /* Name of function in C++ (must match Python name) */
-        model             /* Argument(s) */
+    PYBIND11_OVERRIDE_NAME(void,             /* Return type */
+                           Callback,         /* Parent class */
+                           "on_batch_begin", /* Name of Python function */
+                           onBatchBegin,     /* Name of C++ function */
+                           model             /* Argument(s) */
     );
   }
 
   void onBatchEnd(BoltGraph& model) override {
-    PYBIND11_OVERRIDE_PURE(
-        void,           /* Return type */
-        Callback,       /* Parent class */
-        "on_batch_end", /* Name of function in C++ (must match Python name) */
-        model           /* Argument(s) */
+    PYBIND11_OVERRIDE_NAME(void,           /* Return type */
+                           Callback,       /* Parent class */
+                           "on_batch_end", /* Name of Python function */
+                           onBatchEnd,     /* Name of C++ function */
+                           model           /* Argument(s) */
     );
   }
 
   bool shouldStopTraining() override {
-    PYBIND11_OVERRIDE_PURE(bool,                   /* Return type */
+    PYBIND11_OVERRIDE_NAME(bool,                   /* Return type */
                            Callback,               /* Parent class */
-                           "should_stop_training", /* Name of function in C++
-                                                  (must match Python name) */
+                           "should_stop_training", /* Name of Python function */
+                           shouldStopTraining,     /* Name of C++ function */
                                                    /* Empty list of arguments */
     );
   }
--- bolt/python_tests/test_callbacks.py
+++ bolt/python_tests/test_callbacks.py
@@ -1,6 +1,7 @@
 import pytest
 from thirdai import bolt
 from utils import gen_numpy_training_data, get_simple_dag_model
+import os.path
 
 pytestmark = [pytest.mark.unit]
 
@@ -9,6 +10,7 @@ N_CLASSES = 10
 N_SAMPLES = 1000
 BATCH_SIZE = 100
 EPOCHS = 10
+SAVE_FILENAME = "fifthEpoch.model"
 
 
 def train_model_with_callback(callback):
@@ -36,36 +38,85 @@ def train_model_with_callback(callback):
     model.train(data, labels, train_config)
 
 
-class CountCallback(bolt.graph.Callback):
-    train_begin_count = 0
-    train_end_count = 0
-    epoch_begin_count = 0
-    epoch_end_count = 0
-    batch_begin_count = 0
-    batch_end_count = 0
+class CountCallback(bolt.graph.callbacks.Callback):
+    def __init__(self):
+        super().__init__()
+        self.train_begin_count = 0
+        self.train_end_count = 0
+        self.epoch_begin_count = 0
+        self.epoch_end_count = 0
+        self.batch_begin_count = 0
+        self.batch_end_count = 0
 
     def on_train_begin(self, model):
-        train_begin_count += 1
+        self.train_begin_count += 1
 
     def on_train_end(self, model):
-        train_end_count += 1
+        self.train_end_count += 1
 
     def on_epoch_begin(self, model):
-        epoch_begin_count += 1
+        self.epoch_begin_count += 1
 
     def on_epoch_end(self, model):
-        epoch_end_count += 1
+        self.epoch_end_count += 1
 
     def on_batch_begin(self, model):
-        batch_begin_count += 1
+        self.batch_begin_count += 1
 
     def on_batch_end(self, model):
-        batch_end_count += 1
+        self.batch_end_count += 1
 
 
-def test_count_callback():
+def test_callbacks_count_properly():
     count_callback = CountCallback()
 
     train_model_with_callback(count_callback)
 
     assert count_callback.train_begin_count == 1
+    assert count_callback.train_end_count == 1
+    assert count_callback.epoch_begin_count == EPOCHS
+    assert count_callback.epoch_end_count == EPOCHS
+    assert count_callback.batch_begin_count == (N_SAMPLES / BATCH_SIZE) * EPOCHS
+    assert count_callback.batch_end_count == (N_SAMPLES / BATCH_SIZE) * EPOCHS
+
+
+class SaveOnFifthEpoch(bolt.graph.callbacks.Callback):
+    def __init__(self):
+        super().__init__()
+        self.epoch_count = 0
+
+    def on_epoch_end(self, model):
+        self.epoch_count += 1
+        if (self.epoch_count == 5):
+            model.save(SAVE_FILENAME)
+        
+
+def test_callbacks_call_cpp_functions():
+    save_on_fifth_callback = SaveOnFifthEpoch()
+
+    train_model_with_callback(save_on_fifth_callback)
+
+    assert os.path.isfile(SAVE_FILENAME)
+
+    os.remove(SAVE_FILENAME) 
+
+class StopOnFifthEpoch(bolt.graph.callbacks.Callback):
+    def __init__(self):
+        super().__init__()
+        self.epoch_count = 0
+
+    def on_epoch_end(self, model):
+        self.epoch_count += 1
+    
+    def should_stop_training(self, model):
+        return True
+        if (self.epoch_count == 5):
+            return True
+
+
+def test_callbacks_stop_correctly():
+    stop_on_fifth_callback = SaveOnFifthEpoch()
+
+    train_model_with_callback(stop_on_fifth_callback)
+
+    assert stop_on_fifth_callback.epoch_count == 5
