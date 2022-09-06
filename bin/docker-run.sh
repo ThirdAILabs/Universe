f3c5c256164ea17b2c3592cad235e5b212151d88
Josh Engels <josh@thirdai.com>
2022-09-06 13:35:45 -0500

    Mounting AWS credentials

--- bin/docker-run.sh
+++ bin/docker-run.sh
@@ -12,11 +12,16 @@ if [ -d "${DATADIR}" ]; then
       -it --rm \
       --mount type=bind,source=${DATADIR},target=/data \
       --mount type=bind,source=${PWD},target=/Universe thirdai/universe_dev_build \
+      # This is a temporary workaround until we get IAM roles working on AWS
+      # It mounts the AWS credentials present on the home machine to the home
+      # directory of the docker image
+      -v $HOME/.aws/credentials:/root/.aws/credentials:ro
       bash
 else
     docker run \
       --privileged --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
       -it --rm \
       --mount type=bind,source=${PWD},target=/Universe thirdai/universe_dev_build \
+      -v $HOME/.aws/credentials:/root/.aws/credentials:ro \
       bash
 fi
