56df4b7d81807294b41578c41a88d2b4b2d6b80f
Josh Engels <josh@thirdai.com>
2022-09-06 14:10:48 -0500

    Moving comment up

--- bin/docker-run.sh
+++ bin/docker-run.sh
@@ -5,6 +5,9 @@ cd $BASEDIR/../
 # Run with --cap-add and --security-opt to be able to trace exectuion, see
 # https://stackoverflow.com/questions/19215177/how-to-solve-ptrace-operation-not-permitted-when-trying-to-attach-gdb-to-a-pro
 
+# Mounting the aws credentials is a temporary workaround until we get IAM roles 
+# working on AWS. It mounts the AWS credentials present on the home machine to 
+# the home directory of the docker image.
 DATADIR="/share/data"
 if [ -d "${DATADIR}" ]; then
     docker run \
@@ -12,9 +15,6 @@ if [ -d "${DATADIR}" ]; then
       -it --rm \
       --mount type=bind,source=${DATADIR},target=/data \
       --mount type=bind,source=${PWD},target=/Universe thirdai/universe_dev_build \
-      # This is a temporary workaround until we get IAM roles working on AWS
-      # It mounts the AWS credentials present on the home machine to the home
-      # directory of the docker image
       -v $HOME/.aws/credentials:/root/.aws/credentials:ro \
       bash
 else
