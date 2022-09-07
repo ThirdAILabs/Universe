8c059d65ed6834f68a4e902d88b38fbb91e76073
Pratik <pratik@thirdai.com>
2022-09-07 11:35:12 +0000

    Removed Distributed Bolt Files

--- thirdai_python_package/_distributed_bolt/__init__.py
+++
@@ -1,2 +0,0 @@
-from .distributed_fully_connected import *
-from .distributed_tabular_classifier import *
--- thirdai_python_package/_distributed_bolt/_models/model_type.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class ModelType(Enum):
-    FullyConnectedNetwork = 1
-    TabularClassifier = 2
