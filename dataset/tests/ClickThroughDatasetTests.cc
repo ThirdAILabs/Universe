b17c48224c1ca15741f75fcddc2c2a96f1387257
Nicholas Meisburger <nmeisburger@gmail.com>
2022-09-08 09:52:22 -0500

    Fix test

--- dataset/tests/ClickThroughDatasetTests.cc
+++ dataset/tests/ClickThroughDatasetTests.cc
@@ -127,7 +127,10 @@ class ClickThroughDatasetTestFixture : public ::testing::Test {
       for (uint32_t i = 0; i < getNumDenseFeatures(); i++) {
         float val =
             _ground_truths_vectors.at(vec_count_base + v).dense_features.at(i);
-        ASSERT_EQ(dense_inputs[v].activations[i], log(val));
+        if (val != 0) {
+          val = std::log(val);
+        }
+        ASSERT_NEAR(dense_inputs[v].activations[i], val, 0.0005);
       }
     }
   }
