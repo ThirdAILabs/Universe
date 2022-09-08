1ef9d4bd98ad9d2d2338f4308a73242d41a061e4
Yashwanth Adunukota <yash@thirdai.com>
2022-09-08 16:24:56 +0530

    changed the tests as per new function

--- bolt/src/auto_classifiers/tests/SequentialClassifierTest.cc
+++ bolt/src/auto_classifiers/tests/SequentialClassifierTest.cc
@@ -21,18 +21,18 @@ void writeMockSequentialDataToFile(
 
   std::ofstream train_file(train_file_name);
   train_file << header << std::endl;
-  train_file << "0,0,2022-08-29,hello,0,1" << std::endl;
-  train_file << "0,1,2022-08-30,hello,1,2" << std::endl;
-  train_file << "0,0,2022-08-31,hello,2,3" << std::endl;
-  train_file << "0,1,2022-09-01,hello,3,4" << std::endl;
+  train_file << "0,0,2022-08-29,hello,0,A" << std::endl;
+  train_file << "0,1,2022-08-30,hello,1,B" << std::endl;
+  train_file << "0,0,2022-08-31,hello,2,C" << std::endl;
+  train_file << "0,1,2022-09-01,hello,3,D" << std::endl;
 
   if (test_file_name != std::nullopt) {
     std::ofstream test_file(*test_file_name);
     test_file << header << std::endl;
-    test_file << "0,0,2022-09-02,hello,0,5" << std::endl;
-    test_file << "0,1,2022-09-03,hello,1,2" << std::endl;
-    test_file << "0,0,2022-09-04,hello,2,3" << std::endl;
-    test_file << "0,1,2022-09-05,hello,3,4" << std::endl;
+    test_file << "0,0,2022-09-02,hello,0,E" << std::endl;
+    test_file << "0,1,2022-09-03,hello,1,B" << std::endl;
+    test_file << "0,0,2022-09-04,hello,2,C" << std::endl;
+    test_file << "0,1,2022-09-05,hello,3,D" << std::endl;
   }
 }
 
@@ -50,10 +50,10 @@ std::unordered_map<std::string, std::string>
 mockSequentialSampleForPredictSingle() {
   return {{"user", "0"},
           {"target", "0"},
-          {"timestamp", "2022-09-06"},
-          {"static_text", "hello"},
+          {"timestamp", "2022-09-01"},
+          {"static_text", "hello world"},
           {"static_categorical", "0"},
-          {"sequential", "2"}};
+          {"sequential", "B"}};
 }
 
 SequentialClassifier getTrainedClassifier(const char* train_file_name) {
@@ -64,7 +64,18 @@ SequentialClassifier getTrainedClassifier(const char* train_file_name) {
   return classifier;
 }
 
-void assert_column_names(std::vector<std::string> column_names) {
+std::vector<std::string> getWordsInTextColumn(const std::string& sentence) {
+  std::vector<std::string> text_reasons;
+  std::string token;
+  std::stringstream ss(sentence);
+  while (getline(ss, token, ' ')) {
+    text_reasons.push_back(token);
+  }
+  return text_reasons;
+}
+
+void assert_column_names(std::vector<std::string> column_names,
+                         std::unordered_map<std::string, std::string> input) {
   // here we should have 'timestamp' four times because we are using four values
   // in input from the timestamp. we should have 'sequential' three times
   // because we are tracking last three values in the schema. for remaining
@@ -82,6 +93,12 @@ void assert_column_names(std::vector<std::string> column_names) {
     } else if (column_name == "sequential") {
       ASSERT_EQ(
           std::count(column_names.begin(), column_names.end(), column_name), 3);
+    } else if (column_name == "static_text") {
+      std::vector<std::string> text_reasons =
+          getWordsInTextColumn(input["static_text"]);
+      ASSERT_EQ(
+          std::count(column_names.begin(), column_names.end(), column_name),
+          text_reasons.size());
     } else {
       ASSERT_EQ(
           std::count(column_names.begin(), column_names.end(), column_name), 1);
@@ -92,9 +109,11 @@ void assert_column_names(std::vector<std::string> column_names) {
 void assert_percentage_significance(
     std::vector<float> percentage_significances) {
   // assert the values are sorted in descending order of absolute values.
-  bool isSorted = std::is_sorted(
-      percentage_significances.begin(), percentage_significances.end(),
-      [](float pair1, float pair2) { return abs(pair1) >= abs(pair2); });
+  bool isSorted = std::is_sorted(percentage_significances.begin(),
+                                 percentage_significances.end(),
+                                 [](float value1, float value2) {
+                                   return std::abs(value1) >= std::abs(value2);
+                                 });
 
   ASSERT_TRUE(isSorted);
 
@@ -108,24 +127,30 @@ void assert_percentage_significance(
   ASSERT_GT(total_percentage_sum, 0.99);
 }
 
-void assert_is_between(uint32_t value, uint32_t upper_limit) {
-  ASSERT_GE(value, 0);
-  ASSERT_LE(value, upper_limit);
-}
-
-void assert_indices_within_block(std::vector<std::string> column_names,
-                                 std::vector<uint32_t> indices_within_block) {
-  // the upper limit is caluclated based on the values mentioned in schema.
-  // for the text block the default limit is 100000.
-  for (uint32_t i = 0; i < indices_within_block.size(); i++) {
+void assert_words_within_block(
+    const std::vector<std::string>& column_names,
+    std::unordered_map<std::string, std::string> input,
+    const std::vector<std::string>& words_responsible) {
+  std::vector<std::string> timestamp_reasons = {
+      "day_of_week", "week_of_month", "month_of_year", "week_of_year"};
+  // these sequential reasons based on values in the sequential column in train
+  // data.
+  std::vector<std::string> sequential_reasons = {"A", "B", "C", "D"};
+  std::vector<std::string> text_reasons =
+      getWordsInTextColumn(input["static_text"]);
+  for (uint32_t i = 0; i < words_responsible.size(); i++) {
     if (column_names[i] == "timestamp") {
-      assert_is_between(indices_within_block[i], 73);
+      ASSERT_TRUE(std::find(timestamp_reasons.begin(), timestamp_reasons.end(),
+                            words_responsible[i]) != timestamp_reasons.end());
     } else if (column_names[i] == "sequential") {
-      assert_is_between(indices_within_block[i], 3);
+      ASSERT_TRUE(std::find(sequential_reasons.begin(),
+                            sequential_reasons.end(),
+                            words_responsible[i]) != sequential_reasons.end());
     } else if (column_names[i] == "static_text") {
-      assert_is_between(indices_within_block[i], 100000);
+      ASSERT_TRUE(std::find(text_reasons.begin(), text_reasons.end(),
+                            words_responsible[i]) != text_reasons.end());
     } else {
-      assert_is_between(indices_within_block[i], 5);
+      ASSERT_TRUE(input[column_names[i]] == words_responsible[i]);
     }
   }
 }
@@ -167,19 +192,20 @@ TEST(SequentialClassifierTest, TestExplainMethod) {
 
   auto single_inference_input = mockSequentialSampleForPredictSingle();
 
-  auto [column_names, percentage_significances, indices_within_block] =
+  auto [column_names, percentage_significances, words_responsible] =
       classifier.explain(single_inference_input);
 
   // we will check how many times the column names are present in the vector.
-  assert_column_names(column_names);
+  assert_column_names(column_names, single_inference_input);
 
   // we will check the total percentage is close to 100 and the percentage
   // significance are sorted.
 
   assert_percentage_significance(percentage_significances);
 
-  // we will check the indices within each block are within the limits.
-  assert_indices_within_block(column_names, indices_within_block);
+  // we will check the words responsible are there in the input or not.
+  assert_words_within_block(column_names, single_inference_input,
+                            words_responsible);
 
   std::remove(train_file_name);
 }
