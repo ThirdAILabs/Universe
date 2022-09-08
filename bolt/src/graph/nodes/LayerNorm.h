bf550ce5024faca77f6e7925e1232bb64af2ac8d
Blaise Muhirwa <blaise@thirdai.com>
2022-09-07 23:37:08 -0500

    Account for Multiple Masked Tokens in MLM Batch Processor (#732)
    * edit masked sentence batch processor
    
    * edit unit test
    
    * python bindings
    
    * remove unnecessary comment
    
    * nits
    
    * edit batch processor for MLM
    
    * address PR comments

--- bolt/src/graph/nodes/LayerNorm.h
+++ bolt/src/graph/nodes/LayerNorm.h
@@ -90,7 +90,7 @@ class LayerNormNode final : public Node,
 
   // Computes the first and second moments {mean, variance} required
   // to normalize the input to this layer.
-  static std::pair<float, float> computeNormalizationMoments(
+  static std::tuple<float, float> computeNormalizationMoments(
       const BoltVector& bolt_vector) {
     uint32_t len = bolt_vector.len;
     float mean = 0, variance = 0;
@@ -106,7 +106,7 @@ class LayerNormNode final : public Node,
     }
     variance /= len;
 
-    return std::make_pair(mean, variance);
+    return {mean, variance};
   }
 
   void forwardImpl(uint32_t vec_index, const BoltVector* labels) final {
