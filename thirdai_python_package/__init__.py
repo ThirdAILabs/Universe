4d4046640c6e5d5cc68561735946fba7290f16ec
Jerin Philip <jerin@thirdai.com>
2022-09-12 16:13:06 +0530

    Add logging (sub)module to exports in pypackage

--- thirdai_python_package/__init__.py
+++ thirdai_python_package/__init__.py
@@ -7,7 +7,7 @@ __all__ = [
     "distributed_bolt",
     "set_thirdai_license_path",
     "set_global_num_threads",
-    "setup_logging",
+    "logging",
 ]
 
 # Include these so we can use them just by import the top level.
@@ -16,7 +16,7 @@ import thirdai.search
 import thirdai.dataset
 import thirdai.hashing
 
-from thirdai._thirdai import setup_logging
+from thirdai._thirdai import logging
 
 # Relay __version__ from C++
 from thirdai._thirdai import __version__
