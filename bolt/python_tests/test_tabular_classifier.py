46357e9a4381da49f275ea3e2923d632b42a791c
Nicholas Meisburger <43160219+nmeisburger@users.noreply.github.com>
2022-09-14 11:18:13 -0500

    [2/3] Batch predict (#772)
    * Generalized Auto Classifier
    
    * Use dataloaders as api for auto classifiers
    
    * Streaming training for auto classifier
    
    * add metrics to predict return
    
    * Close file on restart
    
    * use seek to restart file
    
    * Start implementing auto classifiers with new api
    
    * Rename and add serialize
    
    * Re-implement text classifier
    
    * Multi-label-text classifier working
    
    * Tabular classifier
    
    * Cleanup autoclassifier base
    
    * Add tabular classifier
    
    * rename predict single
    
    * did something
    
    * Change to const&
    
    * Implement tabular classifier
    
    * Fix construct numpy arrays
    
    * Tabular classifier tests working
    
    * Add sequential classifier
    
    * Remove thresholding from base class
    
    * Move threshold to multilabel text classifier
    
    * Finish resolving comments
    
    * Make pointer
    
    * Implement interface changes
    
    * Make get index static
    
    * Fix python bindings
    
    * minor bug fixes
    
    * Fix tests
    
    * remove duplicate method
    
    * Cleanup interface
    
    * Migrate tests
    
    * Tabular classifier experiment
    
    * Fix script
    
    * fix text classifier benchmark
    
    * Tune text classifier test
    
    * Fix test
    
    * Fix text classifier test
    
    * use factory methodsg
    
    * Predict single batch
    
    * Test for predict batch
    
    * C++ format
    
    * Predict single c++ tests
    
    * batch predict tabular classifier test
    
    * Store batch processor in tabular classifier
    
    * fix logic in tabular
    
    * resolve comments
    
    * add fmeasure
    
    * Update threshold method
    
    * Fix fmeasure
    
    * Add tabular tests
    
    * python format
    
    * Add comment
    
    * Remove delete files
    
    * Resolve comments
    
    * Merge
    
    * Restore multi label tests
    
    * python format
    
    * Restart gh actions

--- bolt/python_tests/test_tabular_classifier.py
+++ bolt/python_tests/test_tabular_classifier.py
@@ -3,7 +3,11 @@ from thirdai import bolt
 import pytest
 import os
 import pandas as pd
-from utils import remove_files, compute_accuracy_of_predictions
+from utils import (
+    remove_files,
+    compute_accuracy_of_predictions,
+    check_autoclassifier_predict_correctness,
+)
 
 pytestmark = [pytest.mark.integration, pytest.mark.release]
 
@@ -53,10 +57,6 @@ def setup_module():
             file.writelines([line.replace(".", "") for line in data[1:]])
 
 
-def teardown_module():
-    remove_files([TRAIN_FILE, TEST_FILE])
-
-
 def get_census_income_metadata():
     df = pd.read_csv(TEST_FILE)
     n_classes = df[df.columns[-1]].nunique()
@@ -104,9 +104,9 @@ def test_tabular_classifier_census_income_dataset():
 
     single_test_samples = create_single_test_samples()
 
-    for sample, original_prediction in zip(single_test_samples, predictions):
-        single_prediction = new_classifier.predict(sample)
-        assert single_prediction == original_prediction
+    check_autoclassifier_predict_correctness(
+        new_classifier, single_test_samples, predictions
+    )
 
 
 def create_single_test_samples():
@@ -114,8 +114,8 @@ def create_single_test_samples():
         lines = file.readlines()
 
         samples = []
-        # skip the header
-        for line in lines[1:]:
+        # Skip the header and the last line since it is empty.
+        for line in lines[1:-1]:
             # ignore the label column
             values = line.split(",")[:-1]
             samples.append(values)
