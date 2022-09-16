4d5a426976f2ad580ac30967b652dbde5af07b2c
David Torres <david@thirdai.com>
2022-09-15 23:06:39 -0500

    refactor tabular metadata

--- dataset/src/batch_processors/TabularMetadataProcessor.h
+++ dataset/src/batch_processors/TabularMetadataProcessor.h
@@ -17,136 +17,59 @@ enum class TabularDataType {
   Label
 };  // TODO(david) add datetime/text support
 
+/**
+ * This object stores metadata about a Tabular Dataset like column type
+ * information, max/min values for numeric columns, etc.
+ */
 class TabularMetadata {
  public:
-  /**
-   * This object stores metadata about a Tabular Dataset like column type
-   * information, max/min values for numeric columns, etc.
-   */
-  TabularMetadata(uint32_t label_col_index,
-                  std::vector<TabularDataType> column_dtypes,
+  TabularMetadata(std::vector<TabularDataType> column_dtypes,
                   std::unordered_map<uint32_t, double> col_to_max_val,
                   std::unordered_map<uint32_t, double> col_to_min_val,
-                  uint32_t max_salt_len)
-      : _num_non_empty_bins(10),
-        _label_col_index(label_col_index),
-        _max_salt_len(max_salt_len),
-        _column_dtypes(std::move(column_dtypes)),
+                  std::unordered_map<std::string, uint32_t> class_to_class_id,
+                  std::optional<std::unordered_map<uint32_t, uint32_t>>
+                      col_to_num_bins = std::nullopt)
+      : _column_dtypes(std::move(column_dtypes),
         _col_to_max_val(std::move(col_to_max_val)),
-        _col_to_min_val(std::move(col_to_min_val)) {}
-
-  void addClass(const std::string& class_name) {
-    uint32_t label = numClasses();
-    _class_to_class_id[class_name] = label;
-    _class_id_to_class.push_back(class_name);
-  }
-
-  void updateColMaxMin(uint32_t col, double val) {
-    if (_col_to_max_val[col] < val) {
-      _col_to_max_val[col] = val;
-    }
-    if (_col_to_min_val[col] > val) {
-      _col_to_min_val[col] = val;
+        _col_to_min_val(std::move(col_to_min_val)),
+        _class_to_class_id(std::move(class_to_class_id)),
+        _col_to_num_bins(std::move(col_to_num_bins)) {
+    auto _label_col_index = std::find(
+        _column_dtypes.begin(), _column_dtypes.end(), TabularDataType::Label);
+    if (_label_col_index == _column_dtypes.end()) {
+      throw std::invalid_argument("No label col passed in.");
     }
   }
 
-  void setColumnNames(
-      std::unordered_map<uint32_t, std::string> col_to_col_name) {
-    _col_to_col_name = col_to_col_name;
-    for (auto [col, col_name] : _col_to_col_name) {
-      _col_name_to_col[col_name] = col;
-    }
-  }
+  uint32_t getLabelCol() const {
+    return _label_col_index; }
 
-  uint32_t numColumns() const { return _column_dtypes.size(); }
-
-  TabularDataType getColType(uint32_t col) const { return _column_dtypes[col]; }
-
-  uint32_t getColFromName(const std::string& col_name) {
-    if (!_col_name_to_col.count(col_name)) {
-      throw std::invalid_argument("Recieved invalid column name: " + col_name);
-    }
-    return _col_name_to_col[col_name];
-  }
-
-  uint32_t numClasses() const { return _class_id_to_class.size(); }
-
-  bool containsClassName(const std::string& str_value) const {
-    return _class_to_class_id.count(str_value);
-  }
-
-  uint32_t getLabelCol() const { return _label_col_index; }
-
-  std::vector<std::string> getClassIdToNames() { return _class_id_to_class; }
+  uint32_t numColumns() const {
+    return _column_dtypes.size(); }
 
   std::unordered_map<std::string, uint32_t> getClassToIdMap() {
     return _class_to_class_id;
   }
 
-  uint32_t getStringHashValue(const std::string& str_val, uint32_t col) const {
-    std::string unique_category = str_val + getColSalt(col);
-    return TextEncodingUtils::computeUnigram(unique_category.data(),
-                                             unique_category.size());
-  }
+  TabularDataType colType(uint32_t col) {
+    return _column_dtypes[col]; }
 
-  uint32_t getNumericHashValue(uint32_t col, const std::string& str_val,
-                               std::exception_ptr& exception_ptr) {
-    uint32_t bin = getColBin(col, str_val, exception_ptr);
-    uint64_t uniqueBin =
-        static_cast<uint64_t>(bin) << 32 | static_cast<uint64_t>(col);
-    const char* val_to_hash = reinterpret_cast<const char*>(&uniqueBin);
-    return TextEncodingUtils::computeUnigram(val_to_hash, /* len = */ 8);
+  double colMax(uint32_t col) {
+    return _col_to_max_val[col]; }
+
+  double colMin(uint32_t col) {
+    return _col_to_min_val[col]; }
+
+  static constexpr uint32_t DEFAULT_NUM_BINS = 10;
+
+  uint32_t numBins(uint32_t col) {
+    if (_col_to_num_bins) {
+      return (*_col_to_num_bins)[col];
+    }
+    return DEFAULT_NUM_BINS;
   }
 
  private:
-  double getColMin(uint32_t col) { return _col_to_min_val[col]; }
-
-  double getColBinsize(uint32_t col) {
-    return (_col_to_max_val[col] - _col_to_min_val[col]) / _num_non_empty_bins;
-  }
-
-  uint32_t getColBin(uint32_t col, const std::string& str_val,
-                     std::exception_ptr& exception_ptr) {
-    // map empty values to their own bin
-    if (str_val.empty()) {
-      return _num_non_empty_bins;
-    }
-    double value;
-    try {
-      value = std::stod(str_val);
-    } catch (std::invalid_argument& e) {
-      exception_ptr = std::make_exception_ptr(std::invalid_argument(
-          "Could not process column " + std::to_string(col) +
-          " as type 'numeric.' Received value: '" + str_val + ".'"));
-
-      // Since we have set the block exception above, the program will
-      // fail once all threads finish. Since we can't throw an exception within
-      // a pragma thread, we just have to keep the program running until then.
-      // Thus we return some arbitrary value to do that.
-      return 0;
-    }
-    double binsize = getColBinsize(col);
-    if (binsize == 0) {
-      return 0;
-    }
-    return static_cast<uint32_t>(
-        std::round((value - getColMin(col)) / getColBinsize(col)));
-  }
-
-  /**
-   * To compute pairgrams of categories across columns we need all categories to
-   * be unique. This method returns "salt" to make each category unique
-   * dependent on its column. By adding the column number padded to a fixed
-   * length, we ensure this characteristic.
-   */
-  std::string getColSalt(uint32_t col) const {
-    std::string col_str = std::to_string(col);
-    if (col_str.size() < _max_salt_len) {
-      col_str.insert(0, _max_salt_len - col_str.size(), '0');
-    }
-    return col_str;
-  }
-
   // Private constructor for cereal
   TabularMetadata() {}
 
@@ -154,69 +77,52 @@ class TabularMetadata {
   friend class cereal::access;
   template <class Archive>
   void serialize(Archive& archive) {
-    archive(_num_non_empty_bins, _label_col_index, _max_salt_len,
-            _column_dtypes, _col_to_max_val, _col_to_min_val,
-            _class_to_class_id, _class_id_to_class, _col_to_col_name,
-            _col_name_to_col);
+    archive(_column_dtypes, _col_to_max_val, _col_to_min_val,
+            _class_to_class_id, _col_to_num_bins, _label_col_index);
   }
 
-  // one additional bin is reserved for empty values
-  uint32_t _num_non_empty_bins;
-  uint32_t _label_col_index;
-  uint32_t _max_salt_len;
   std::vector<TabularDataType> _column_dtypes;
   std::unordered_map<uint32_t, double> _col_to_max_val;
   std::unordered_map<uint32_t, double> _col_to_min_val;
   std::unordered_map<std::string, uint32_t> _class_to_class_id;
-  std::vector<std::string> _class_id_to_class;
-  std::unordered_map<uint32_t, std::string> _col_to_col_name;
-  std::unordered_map<std::string, uint32_t> _col_name_to_col;
+
+  // one additional bin is reserved for empty values
+  std::optional<std::unordered_map<uint32_t, uint32_t>> _col_to_num_bins;
+
+  uint32_t _label_col_index;
 };
 
 class TabularMetadataProcessor : public ComputeBatchProcessor {
  public:
-  explicit TabularMetadataProcessor(std::vector<std::string> given_column_types,
+  explicit TabularMetadataProcessor(std::vector<std::string> column_types,
                                     uint32_t n_classes)
       : _delimiter(','), _n_classes(n_classes) {
     bool found_label_column = false;
-    uint32_t label_col_index = 0;
-    std::vector<TabularDataType> column_dtypes;
-    std::unordered_map<uint32_t, double> col_to_max_val;
-    std::unordered_map<uint32_t, double> col_to_min_val;
 
-    for (uint32_t col_idx = 0; col_idx < given_column_types.size(); col_idx++) {
-      if (given_column_types[col_idx] == "label" && found_label_column) {
-        throw std::invalid_argument(
-            "Found multiple 'label' columns in dataset.");
-      }
-
-      if (given_column_types[col_idx] == "label") {
+    for (uint32_t col_idx = 0; col_idx < column_types.size(); col_idx++) {
+      std::string col_type = column_types[col_idx];
+      if (col_type == "label") {
+        if (found_label_column) {
+          throw std::invalid_argument(
+              "Found multiple 'label' columns in dataset.");
+        }
         found_label_column = true;
-        label_col_index = col_idx;
-        column_dtypes.push_back(TabularDataType::Label);
-
-      } else if (given_column_types[col_idx] == "categorical") {
-        column_dtypes.push_back(TabularDataType::Categorical);
-
-      } else if (given_column_types[col_idx] == "numeric") {
-        column_dtypes.push_back(TabularDataType::Numeric);
-        col_to_max_val[col_idx] = std::numeric_limits<double>::min();
-        col_to_min_val[col_idx] = std::numeric_limits<double>::max();
-
+        _column_dtypes.push_back(TabularDataType::Label);
+      } else if (col_type == "categorical") {
+        _column_dtypes.push_back(TabularDataType::Categorical);
+      } else if (col_type == "numeric") {
+        _column_dtypes.push_back(TabularDataType::Numeric);
+        _col_to_max_val[col_idx] = std::numeric_limits<double>::min();
+        _col_to_min_val[col_idx] = std::numeric_limits<double>::max();
       } else {
         throw std::invalid_argument(
-            "Received datatype '" + given_column_types[col_idx] +
+            "Received datatype '" + col_type +
             "' expected one of 'label', 'categorical', or 'numeric'.");
       }
     }
     if (!found_label_column) {
       throw std::invalid_argument("Dataset does not contain a 'label' column.");
     }
-    uint32_t max_salt_len = std::to_string(given_column_types.size()).size();
-
-    _metadata = std::make_shared<TabularMetadata>(label_col_index,
-                                                  column_dtypes, col_to_max_val,
-                                                  col_to_min_val, max_salt_len);
   }
 
   bool expectsHeader() const final { return true; }
@@ -226,19 +132,18 @@ class TabularMetadataProcessor : public ComputeBatchProcessor {
         ProcessorUtils::parseCsvRow(header, _delimiter);
     verifyNumColumns(column_names);
     std::unordered_map<uint32_t, std::string> col_to_col_name;
-    for (uint32_t col = 0; col < column_names.size(); col++) {
-      col_to_col_name[col] = column_names[col];
+    for (auto col_name : column_names) {
+      _column_names.push_back(std::string(col_name));
     }
-    _metadata->setColumnNames(col_to_col_name);
   }
 
   void processRow(const std::string& row) final {
     std::vector<std::string_view> values =
         ProcessorUtils::parseCsvRow(row, _delimiter);
     verifyNumColumns(values);
-    for (uint32_t col = 0; col < _metadata->numColumns(); col++) {
+    for (uint32_t col = 0; col < values.size(); col++) {
       std::string str_value(values[col]);
-      switch (_metadata->getColType(col)) {
+      switch (_column_dtypes[col]) {
         case TabularDataType::Numeric: {
           processNumeric(str_value, col);
           break;
@@ -253,7 +158,10 @@ class TabularMetadataProcessor : public ComputeBatchProcessor {
     }
   }
 
-  std::shared_ptr<TabularMetadata> getMetadata() { return _metadata; }
+  std::shared_ptr<TabularMetadata> getMetadata() {
+    return std::make_shared<TabularMetadata>(
+        _column_dtypes, _col_to_max_val, _col_to_min_val, _class_to_class_id);
+  }
 
  private:
   void processNumeric(const std::string& str_value, uint32_t col) {
@@ -266,7 +174,12 @@ class TabularMetadataProcessor : public ComputeBatchProcessor {
     }
     try {
       double val = std::stod(str_value);
-      _metadata->updateColMaxMin(col, val);
+      if (_col_to_max_val[col] < val) {
+        _col_to_max_val[col] = val;
+      }
+      if (_col_to_min_val[col] > val) {
+        _col_to_min_val[col] = val;
+      }
     } catch (std::invalid_argument& e) {
       throw std::invalid_argument(
           "Could not process column " + std::to_string(col) +
@@ -274,21 +187,21 @@ class TabularMetadataProcessor : public ComputeBatchProcessor {
     }
   }
 
-  void processLabel(const std::string& str_value) {
-    if (!_metadata->containsClassName(str_value)) {
-      if (_metadata->numClasses() == _n_classes) {
+  void processLabel(const std::string& class_name) {
+    if (!_class_to_class_id.count(class_name)) {
+      if (_class_to_class_id.size() == _n_classes) {
         throw std::invalid_argument(
             "Expected " + std::to_string(_n_classes) +
-            " classes but found an additional class: '" + str_value + ".'");
+            " classes but found an additional class: '" + class_name + ".'");
       }
-      _metadata->addClass(str_value);
+      _class_to_class_id[class_name] = _class_to_class_id.size();
     }
   }
 
   void verifyNumColumns(const std::vector<std::string_view>& values) {
-    if (values.size() != _metadata->numColumns()) {
+    if (values.size() != _column_dtypes.size()) {
       throw std::invalid_argument("Csv format error. Expected " +
-                                  std::to_string(_metadata->numColumns()) +
+                                  std::to_string(_column_dtypes.size()) +
                                   " columns but received " +
                                   std::to_string(values.size()) + " columns.");
     }
@@ -296,7 +209,11 @@ class TabularMetadataProcessor : public ComputeBatchProcessor {
 
   char _delimiter;
   uint32_t _n_classes;
-  std::shared_ptr<TabularMetadata> _metadata;
+  std::vector<TabularDataType> _column_dtypes;
+  std::unordered_map<uint32_t, double> _col_to_max_val;
+  std::unordered_map<uint32_t, double> _col_to_min_val;
+  std::vector<std::string> _column_names;
+  std::unordered_map<std::string, uint32_t> _class_to_class_id;
 };
 
 }  // namespace thirdai::dataset
--- dataset/src/blocks/TabularBlocks.h
+++ dataset/src/blocks/TabularBlocks.h
@@ -20,7 +20,10 @@ class TabularPairGram : public Block {
 
   bool isDense() const final { return false; };
 
-  uint32_t expectedNumColumns() const final { return _metadata->numColumns(); };
+  uint32_t expectedNumColumns() const final {
+    // metadata includes the label which this block doesn't need
+    return _metadata->numColumns() - 1;
+  };
 
  protected:
   // TODO(david) We should always include all unigrams but if the number of
@@ -33,10 +36,10 @@ class TabularPairGram : public Block {
     std::vector<uint32_t> unigram_hashes;
     for (uint32_t col = 0; col < input_row.size(); col++) {
       std::string str_val(input_row[col]);
-      switch (_metadata->getColType(col)) {
+      switch (_metadata->colType(col)) {
         case TabularDataType::Numeric: {
           std::exception_ptr err;
-          uint32_t unigram = _metadata->getNumericHashValue(col, str_val, err);
+          uint32_t unigram = getNumericHashValue(col, str_val, err);
           if (err) {
             return err;
           }
@@ -44,7 +47,7 @@ class TabularPairGram : public Block {
           break;
         }
         case TabularDataType::Categorical: {
-          uint32_t unigram = _metadata->getStringHashValue(str_val, col);
+          uint32_t unigram = getStringHashValue(str_val, col);
           unigram_hashes.push_back(unigram);
           break;
         }
@@ -64,6 +67,58 @@ class TabularPairGram : public Block {
         });
 
     return nullptr;
+  }
+
+  uint32_t getStringHashValue(const std::string& str_val, uint32_t col) const {
+    // to ensure hashes are unique across columns we add salt based on the col
+    const char* char_salt = reinterpret_cast<const char*>(&col);
+    std::string str_salt(char_salt, 4);
+    std::string unique_category = str_val + str_salt;
+    return TextEncodingUtils::computeUnigram(unique_category.data(),
+                                             unique_category.size());
+  }
+
+  uint32_t getNumericHashValue(uint32_t col, const std::string& str_val,
+                               std::exception_ptr& exception_ptr) {
+    uint32_t bin = getColBin(col, str_val, exception_ptr);
+    // to ensure hashes are unique across columns we add salt based on the col
+    uint64_t uniqueBin =
+        static_cast<uint64_t>(bin) << 32 | static_cast<uint64_t>(col);
+    const char* val_to_hash = reinterpret_cast<const char*>(&uniqueBin);
+    return TextEncodingUtils::computeUnigram(val_to_hash, /* len = */ 8);
+  }
+
+  double getColBinsize(uint32_t col) {
+    return (_metadata->colMax(col) - _metadata->colMin(col)) /
+           _metadata->numBins(col);
+  }
+
+  uint32_t getColBin(uint32_t col, const std::string& str_val,
+                     std::exception_ptr& exception_ptr) {
+    // map empty values to their own bin
+    if (str_val.empty()) {
+      return _metadata->numBins(col);
+    }
+    double value;
+    try {
+      value = std::stod(str_val);
+    } catch (std::invalid_argument& e) {
+      exception_ptr = std::make_exception_ptr(std::invalid_argument(
+          "Could not process column " + std::to_string(col) +
+          " as type 'numeric.' Received value: '" + str_val + ".'"));
+
+      // Since we have set the block exception above, the program will
+      // fail once all threads finish. Since we can't throw an exception within
+      // a pragma thread, we just have to keep the program running until then.
+      // Thus we return some arbitrary value to do that.
+      return 0;
+    }
+    double binsize = getColBinsize(col);
+    if (binsize == 0) {
+      return 0;
+    }
+    return static_cast<uint32_t>(
+        std::round((value - _metadata->colMin(col)) / binsize));
   }
 
  private:
