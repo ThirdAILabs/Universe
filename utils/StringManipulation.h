8ea7d50e514bd9cf2eb0b8c7cafded752c30a183
Josh Engels <josh@thirdai.com>
2022-09-22 16:51:54 -0500

    Adding support for string views

--- utils/StringManipulation.h
+++ utils/StringManipulation.h
@@ -1,4 +1,6 @@
+#pragma once
 #include <string>
+#include <string_view>
 
 namespace thirdai::utils {
 
@@ -6,6 +8,17 @@ namespace thirdai::utils {
  * Creates a copy of the original string where all characters are lowercase.
  */
 inline std::string lower(const std::string& str) {
+  std::string lower_name;
+  for (char c : str) {
+    lower_name.push_back(std::tolower(c));
+  }
+  return lower_name;
+}
+
+/**
+ * Creates a copy of the original string where all characters are lowercase.
+ */
+inline std::string lower(std::string_view str) {
   std::string lower_name;
   for (char c : str) {
     lower_name.push_back(std::tolower(c));
