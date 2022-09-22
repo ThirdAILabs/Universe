d05df2f48b0c52954785b9ffb0568602d402800b
shubh@3ai <shubh@thirdai.com>
2022-09-22 17:10:18 +0530

    Remove headers from test.cc files

--- compression/tests/CountSketchTest.cc
+++ compression/tests/CountSketchTest.cc
@@ -1,11 +1,8 @@
 #include <gtest/gtest.h>
-#include <compression/src/CompressedVector.h>
-#include <compression/src/CompressionUtils.h>
 #include <compression/src/CountSketch.h>
 #include <sys/types.h>
 #include <cstddef>
 #include <cstdint>
-#include <memory>
 #include <random>
 #include <vector>
 
--- compression/tests/DragonVectorTest.cc
+++ compression/tests/DragonVectorTest.cc
@@ -1,11 +1,8 @@
 #include <gtest/gtest.h>
-#include <compression/src/CompressedVector.h>
-#include <compression/src/CompressionUtils.h>
 #include <compression/src/DragonVector.h>
 #include <sys/types.h>
 #include <cstddef>
 #include <cstdint>
-#include <memory>
 #include <random>
 #include <vector>
 namespace thirdai::compression::tests {
