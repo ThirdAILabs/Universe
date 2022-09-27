b29588cb3edecd8416e3900b3d268d6b2db7f33a
Joshua Engels <josh@thirdai.com>
2022-09-27 14:34:37 -0500

    Example pybind docs using Google format (#831)
    * Docs generation basically working
    
    * Training example doc
    
    * Reformatting and uncommenting
    
    * Going to test on the current branch
    
    * Documenting special members like constructors too
    
    * Python formatting
    
    * Removing DocUpdates for build docs target

--- bolt/python_bindings/BoltGraphPython.cc
+++ bolt/python_bindings/BoltGraphPython.cc
@@ -295,53 +295,55 @@ void createBoltGraphSubmodule(py::module_& bolt_submodule) {
           },
           py::arg("train_data"), py::arg("train_labels"),
           py::arg("train_config"))
-      .def(
-          "train", &BoltGraph::train, py::arg("train_data"),
-          py::arg("train_labels"), py::arg("train_config"),
-          "Trains the network on the given training data.\n"
-          "Arguments:\n"
-          " * train_data: PyObject - Training data. This can be one of "
-          "three things. First, it can be a BoltDataset as loaded by "
-          "thirdai.dataset.load_bolt_svm_dataset or "
-          "thirdai.dataset.load_bolt_csv_dataset. Second, it can be a dense "
-          "numpy array of float32 where each row in the array is interpreted "
-          "as a vector. Thid, it can be a sparse dataset represented by a "
-          " tuple of three numpy arrays (indices, values, offsets), where "
-          "indices and offsets are uint32 and values are float32. In this case "
-          "indices is a 1D array of all the nonzero indices concatenated, "
-          "values is a 1D array of all the nonzero values concatenated, and "
-          "offsets are the start positions in the indices and values array of "
-          "each vector plus one extra element at the end of the array "
-          "representing the total number of nonzeros. This is so that "
-          "indices[offsets[i], offsets[i + 1]] contains the indices of the ith "
-          "vector and values[offsets[i], offsets[i+1] contains the values of "
-          "the ith vector. For example, if we have the vectors "
-          "{0.0, 1.5, 0.0, 9.0} and {0.0, 0.0, 0.0, 4.0}, then the indices "
-          "array is {1, 3, 3}, the values array is {1.5, 9.0, 4.0} and the "
-          "offsets array is {0, 2, 3}.\n"
-          " * train_labels: PyObject - Training labels. This can be one of "
-          "three things. First it can be a BoltDataset as loaded by "
-          "thirdai.dataset.load_bolt_svm_dataset or "
-          "thirdai.dataset.load_bolt_csv_dataset. Second, it can be a dense "
-          "numpy array of float32 where each row in the array is interpreted "
-          "as a label vector. Thid, it can be a set of sparse vectors (each "
-          "vector is a label vector) represented as three numpy arrays "
-          "(indices, values, offsets) where indices and offsets are uint32 "
-          "and values are float32. In this case indices is a 1D array of all "
-          "the nonzero indices concatenated, values is a 1D array of all the "
-          "nonzero values concatenated, and offsets are the start positions "
-          "in the indices and values array of each vector plus one extra "
-          "element at the end of the array representing the total number of "
-          "nonzeros. This is so that indices[offsets[i], offsets[i + 1]] "
-          "contains the indices of the ith vector and values[offsets[i], "
-          "offsets[i+1] contains the values of the ith vector. For example, if "
-          "we have the vectors {0.0, 1.5, 0.0, 9.0} and {0.0, 0.0, 0.0, 4.0}, "
-          "then the indices array is {1, 3, 3}, the values array is {1.5, "
-          "9.0, 4.0}, and the offsets array is {0, 2, 3}.\n"
-          " * train_config: TrainConfig - the additional training parameters. "
-          "See the TrainConfig documentation above.\n\n"
-          "Returns a mapping from metric names to an array of their values for "
-          "every epoch.")
+      .def("train", &BoltGraph::train, py::arg("train_data"),
+           py::arg("train_labels"), py::arg("train_config"),
+           R"pbdoc(  
+Trains the network on the given training data and labels with the given training
+config.
+
+Args:
+    train_data (List[BoltDataset] or BoltDataset): The data to train the model with. 
+        There should be exactly one BoltDataset for each Input node in the Bolt
+        model, and each BoltDataset should have the same total number of 
+        vectors and the same batch size. The batch size for training is the 
+        batch size of the passed in BoltDatasets (you can specify this batch 
+        size when loading or creating a BoltDataset).
+    train_labels (BoltDataset): The labels to use as ground truth during 
+        training. There should be the same number of total vectors and the
+        same batch size in this BoltDataset as in the train_data list.
+    train_config (TrainConfig): The object describing all other training
+        configuration details. See the TrainConfig documentation for more
+        information as to possible options. This includes the number of epochs
+        to train for, the verbosity of the training, the learning rate, and so
+        much more!
+
+Returns:
+    Dict[Str, List[float]]:
+    A dictionary from metric name to a list of the value of that metric 
+    for each epoch (this also always includes an entry for 'epoch_times'). The 
+    metrics that are returned are the metrics requested in the TrainConfig.
+
+Notes:
+    Sparse bolt training was originally based off of SLIDE. See [1]_ for more details
+
+References:
+    .. [1] "SLIDE : In Defense of Smart Algorithms over Hardware Acceleration for Large-Scale Deep Learning Systems" 
+            https://arxiv.org/pdf/1903.03129.pdf.
+
+Examples:
+    >>> train_config = (
+            bolt.graph.TrainConfig.make(learning_rate=0.001, epochs=3)
+            .with_metrics(["categorical_accuracy"])
+        )
+    >>> metrics = model.train(
+            train_data=train_data, train_labels=train_labels, train_config=train_config
+        )
+    >>> print(metrics)
+    {'epoch_times': [1.7, 3.4, 5.2], 'categorical_accuracy': [0.4665, 0.887, 0.9685]}
+
+That's all for now, folks! More docs coming soon :)
+
+)pbdoc")
 #if THIRDAI_EXPOSE_ALL
       .def(
           "get_input_gradients_single",
