9fb794f7e5a63913f8021fc24367fcc5141ec45d
BenGeordie <school@bengeordie@gmail.com>
2022-09-28 12:08:33 -0500

    pybind

--- bolt/python_bindings/BoltPython.cc
+++ bolt/python_bindings/BoltPython.cc
@@ -1,6 +1,7 @@
 #include "BoltPython.h"
 #include "BoltGraphPython.h"
 #include <bolt/python_bindings/ConversionUtils.h>
+#include <bolt/src/auto_classifiers/sequential_classifier/ConstructorUtilityTypes.h>
 #include <bolt/src/auto_classifiers/sequential_classifier/SequentialClassifier.h>
 #include <bolt/src/graph/Graph.h>
 #include <bolt/src/graph/Node.h>
@@ -94,96 +95,240 @@ py::module_ createBoltSubmodule(py::module_& module) {
       .def(py::init<>(),
            "Constructs a WeightedMeanAbsolutePercentageError object.");
 
+  auto oracle_types_submodule = bolt_submodule.def_submodule("types");
+
+  py::class_<sequential_classifier::DataType>(  // NOLINT
+      oracle_types_submodule, "Type", "Base class for bolt types.");
+
+  oracle_types_submodule.def("categorical",
+                             sequential_classifier::DataType::categorical,
+                             py::arg("n_unique_classes"));
+  oracle_types_submodule.def("numerical",
+                             sequential_classifier::DataType::numerical);
+  oracle_types_submodule.def("text", sequential_classifier::DataType::text,
+                             py::arg("average_n_words") = std::nullopt);
+  oracle_types_submodule.def("date", sequential_classifier::DataType::date);
+
+  auto oracle_temporal_submodule = bolt_submodule.def_submodule("temporal");
+
+  py::class_<sequential_classifier::TemporalConfig>(  // NOLINT
+      oracle_temporal_submodule, "TemporalConfig",
+      "Base class for temporal feature configs.");
+
+  oracle_temporal_submodule.def(
+      "categorical", sequential_classifier::TemporalConfig::categorical,
+      py::arg("column_name"), py::arg("track_last_n"),
+      py::arg("include_current_row") = false);
+  oracle_temporal_submodule.def(
+      "numerical", sequential_classifier::TemporalConfig::numerical,
+      py::arg("column_name"), py::arg("history_length"),
+      py::arg("include_current_row") = false);
+
   /**
    * Sequential Classifier
    */
-  py::class_<SequentialClassifier>(bolt_submodule, "SequentialClassifier",
+  py::class_<SequentialClassifier>(bolt_submodule, "Oracle",
                                    "Autoclassifier for sequential predictions.")
       .def(
-          py::init<
-              const std::pair<std::string, uint32_t>,
-              const std::pair<std::string, uint32_t>, const std::string,
-              const std::vector<std::string>,
-              const std::vector<std::pair<std::string, uint32_t>>,
-              const std::vector<std::tuple<std::string, uint32_t, uint32_t>>,
-              const std::vector<std::string>, std::optional<char>, std::string,
-              std::optional<uint32_t>, std::optional<uint32_t>>(),
-          py::arg("user"), py::arg("label"), py::arg("timestamp"),
-          py::arg("static_text") = std::vector<std::string>(),
-          py::arg("static_category") =
-              std::vector<std::pair<std::string, uint32_t>>(),
-          py::arg("track_categories") =
-              std::vector<std::tuple<std::string, uint32_t, uint32_t>>(),
-          py::arg("track_quantities") = std::vector<std::string>(),
-          py::arg("multi_class_delim") = std::nullopt,
-          py::arg("time_granularity") = "daily",
-          py::arg("time_to_predict_ahead") = std::nullopt,
-          py::arg("history_length_for_inference") = std::nullopt,
-          "Constructs a SequentialClassifier.\n"
-          "Arguments:\n"
-          " * user: Tup[str, int] - Column name for user IDs and the number of "
-          "unique user IDs.\n"
-          " * label: Tup[str, int] - Column name for label IDs and the number "
-          "of unique IDs.\n"
-          " * timestamp: str - Column name for timestamps. Timestamps must be "
-          "in YYYY-MM-DD format.\n"
-          " * static_text (optional): List[str] - List of column names for "
-          "static text information.\n"
-          " * static_category (optional): List[Tup[str, int]] - List of "
-          "(column name, num unique categories) pairs for static categorical "
-          "features.\n"
-          " * track_categories (optional): List[Tup[str, int, int]] - List of "
-          "(column name, num unique categories, max sequence length) triplets "
-          "for trackable categorical features. SequentialClassifier tracks the "
-          "last max_sequence_length categories associated with a user ID.\n"
-          " * track_quantities (optional): List[str] - List of column names "
-          "for trackable numerical features.\n",
-          " * multi_class_delim (optional): str - A single character to "
-          "delimit multi-class categorical feature columns. This delimiter "
-          "applies to columns specified in `static_category` and "
-          "`track_categories`. Defaults to None.\n"
-          " * time_granularity (optional): str - The granularity of quantity "
-          "tracking. Options: 'daily'/'d', 'weekly'/'w', 'biweekly'/'b', "
-          "'monthly'/'m'. E.g. If granularity is 'weekly' and there is a "
-          "record of a $50 transaction on Monday and a $100 transaction on "
-          "Tuesday, SequentialClassifier will treat this as a $150 transaction "
-          "during the week.\n"
-          " * time_to_predict_ahead (required only if track_quantities is "
-          "non-empty): int - How far ahead the model needs to learn to "
-          "predict. Time unit is in terms of the selected `time_granulartiy`. "
-          "E.g. time_to_predict_ahead=5 and granularity='weekly' means the "
-          "model learns to predict 5 weeks ahead.\n"
-          " * history_length_for_inference (required only if track_quantities "
-          "is non-empty): int - The length of history of tracked quantities "
-          "that the model can use to make predictions. Length is in terms of "
-          "the selected `time_granulartiy`. E.g. "
-          "history_length_for_inference=5 and granularity='weekly' means the "
-          "model uses the last 5 weeks of counts to make predictions.\n")
+          py::init<std::map<std::string, sequential_classifier::DataType>,
+                   std::map<std::string,
+                            std::vector<sequential_classifier::TemporalConfig>>,
+                   std::string, std::string, uint32_t>(),
+          py::arg("data_types"), py::arg("temporal_tracking_relationships"),
+          py::arg("target"), py::arg("time_granularity") = "daily",
+          py::arg("lookahead") = 0)
       .def("train", &SequentialClassifier::train, py::arg("train_file"),
            py::arg("epochs"), py::arg("learning_rate"),
-           py::arg("metrics") = std::vector<std::string>({"recall@1"}))
+           py::arg("metrics") = std::vector<std::string>({"recall@1"}),
+           "Trains a Sequential classifier Model using the data provided in "
+           "train_file"
+           "Arguments:\n"
+           " * train_file: str - The path to the training dataset. The dataset "
+           "has to be a CSV file with a header.\n"
+           " * epochs: int - Number of epochs to train the model.\n"
+           " * learning_rate: float - Learning rate\n"
+           " * metrics (Optional): List[Str] - Metrics to track during "
+           "training. Defaults "
+           "to ['recall@1'] Metrics are currently restricted to any 'recall@k' "
+           "where k is a positive (nonzero) integer.\n"
+           "Example:\n"
+           "```\n"
+           "from thirdai import bolt\n\n"
+           "model = bolt.SequentialClassifier(...)\n"
+           "model.train(\n"
+           "    'train_data.csv',\n"
+           "    epochs=3,\n"
+           "    learning_rate=0.0001,\n"
+           "    metrics=['recall@1', 'recall@10', 'recall@100']\n"
+           ")\n"
+           "```\n")
 #if THIRDAI_EXPOSE_ALL
-      .def("summarizeModel", &SequentialClassifier::summarizeModel)
+      .def("summarizeModel", &SequentialClassifier::summarizeModel,
+           "Deprecated\n")
 #endif
       .def("predict", &SequentialClassifier::predict, py::arg("test_file"),
            py::arg("metrics") = std::vector<std::string>({"recall@1"}),
-           py::arg("output_file") = std::nullopt, py::arg("print_last_k") = 1)
-      .def("predict_single", &SequentialClassifier::predictSingle,
-           py::arg("input_sample"), py::arg("top_k") = 1,
-           "Computes the top k classes and their probabilities for a single "
-           "input sample. "
-           "Returns a list of (class name. probability) pairs\n"
+           py::arg("output_file") = std::nullopt,
+           py::arg("write_top_k_to_file") = 1,
+           "Predicts the output classes and evaluates the predictions on a "
+           "test dataset. Optionally writes top k predictions to a file if "
+           "output file name is provided. This method cannot be called on an "
+           "untrained model.\n"
            "Arguments:\n"
-           " * sample: Dict[str, str] - The input sample as a dictionary where "
-           "the keys "
-           "are column names as specified in the schema and the values are the "
-           "respective "
-           "column values.\n"
-           " * k: Int (positive) - The number of top results to return.\n")
-      .def("save", &SequentialClassifier::save, py::arg("filename"))
-      .def_static("load", &SequentialClassifier::load, py::arg("filename"))
-      .def("explain", &SequentialClassifier::explain, py::arg("input_sample"),
-           py::arg("neuron_to_explain") = std::optional<uint32_t>());
+           " * test_file: str - The path to the test dataset. The dataset has "
+           "to be a CSV file with a header.\n"
+           " * metrics (optional): List[str] - Metrics to evaluate the "
+           "predictions. Defaults to ['recall@1']. Metrics are currently "
+           "restricted to any 'recall@k' where k is a positive (nonzero) "
+           "integer.\n"
+           " * output_file (optional): str: An optional path to a file to "
+           "write predictions to. If not provided, predictions will not be "
+           "written to file.\n"
+           " * write_top_k_to_file (optional): int: Number of top predictions "
+           "to write to file per input sample. Defaults to 1.\n"
+           "Example:\n"
+           "```\n"
+           "from thirdai import bolt\n\n"
+           "model = bolt.SequentialClassifier(...)\n"
+           "model.train(...)\n"
+           "model.predict(\n"
+           "    'test_data.csv',\n"
+           "    metrics=['recall@1', 'recall@10', 'recall@100']\n"
+           "    output_file='predictions.txt',\n"
+           "    write_top_k_to_file=10,\n"
+           ")\n"
+           "```\n")
+      .def(
+          "predict_single", &SequentialClassifier::predictSingle,
+          py::arg("input_sample"), py::arg("top_k") = 1,
+          "Computes the top k classes and their probabilities for a single "
+          "input sample. "
+          "Returns a list of (class name. probability) pairs\n"
+          "Arguments:\n"
+          " * input_sample: Dict[str, str] - The input sample as a dictionary "
+          "where the keys are column names as specified in the schema and the "
+          "values are the respective column values.\n"
+          " * k: Int (positive) - The number of top results to return.\n"
+          "Returns a list of (prediction, score) tuples."
+          "Example:\n"
+          "```\n"
+          "# Suppose we construct a SequentialClassifier as follows:\n"
+          "model = SequentialClassifier(\n"
+          "    user=('name', 500),\n"
+          "    label=('movie', 5001),\n"
+          "    timestamp='timestamp',\n"
+          "    static_categorical=[('age_group', 7)]\n"
+          "    track_categories=[('movie', 5001, 10)]\n"
+          "    track_quantities=['movie_duration']\n"
+          "    time_granularity='daily',\n"
+          "    time_to_predict_ahead=1,\n"
+          "    history_length_for_inference=30\n\n"
+          ")\n\n"
+          "# Suppose there is a user 'arun' for whome we want to recommend\n"
+          "# the next movie to watch, then the input sample would be as "
+          "follows."
+          "input_sample = {\n"
+          "    'name': 'arun',\n"
+          "    'timestamp': '2022-02-02',\n"
+          "    'age_group': '20-39',\n"
+          "    'movie': 'null_token'\n"
+          "    'movie_duration': '0'\n"
+          "})\n\n"
+          "model.predict_single(input_sample, top_k=10)\n"
+          "```\n"
+          "Notice that in the example above, we will not know the movie or "
+          "movie duration since they are both tied to the variable that we are "
+          "trying to predict. In this scenario, we can pass in random values "
+          "and the model automatically ignores them. If you pass a unique "
+          "'null token' in place of a tracked categorical feature for clarity, "
+          "the number of unique values for that column (passed into the "
+          "constructor) must include this 'null token'. In this example, there "
+          "are 5000 movies, but we define the number of unique movies as 5001 "
+          "to include the 'null token'.")
+      .def(
+          "explain", &SequentialClassifier::explain, py::arg("input_sample"),
+          py::arg("target_label") = std::nullopt,
+          "The Root Cause Analysis method which gives us relevant "
+          "explanations of the input with respect to the given target label.\n"
+          "Arguments:\n"
+          " * input_sample: Dict[str, str] - The input sample as a dictionary "
+          "where the keys are column names as specified in the schema and the "
+          "values are the respective column values.\n"
+          " * target_label (Optional): str - The label class with respect to "
+          "which we want the explanations for the input. Returns a list of "
+          "Explanation objects with the following fields: `column_number`, "
+          "`column_name`, `keyword`, and `percentage_significance`.\n"
+          "Example:\n"
+          "```\n"
+          "# Suppose we construct a SequentialClassifier as follows:\n"
+          "model = SequentialClassifier(\n"
+          "    user=('name', 500),\n"
+          "    label=('salary', 5),\n"
+          "    timestamp='timestamp',\n"
+          "    static_categorical=[('age_group', 7)]\n"
+          "    track_categories=[('expenditure_level', 7, 30)]\n"
+          ")\n\n"
+          "# Suppose there is a user identified as `arun` and we want to\n"
+          "# know why his salary is in the '<=50k' group, then we may call\n"
+          "# the explain(...) method as follows:\n\n"
+          "input_sample = {\n"
+          "    'name': 'arun',\n"
+          "    'timestamp': '2022-02-02',\n"
+          "    'age_group': '20-39',\n"
+          "    'expenditure_level': 'high'\n"
+          "})\n"
+          "explanations = model.explain(input_sample, target_label='<=50k')\n\n"
+          "# Let's now print the explanations\n\n"
+          "for explanation in explanations:\n"
+          "    print(explanation.column_num)\n"
+          "    print(explanation.column_name)\n"
+          "    print(explanation.percentage_significance)\n"
+          "    print(explanation.keyword)\n"
+          "```\n")
+      .def(
+          "index_single", &SequentialClassifier::indexSingle, py::arg("sample"),
+          "Indexes a single true sample to keep the SequentialClassifier's "
+          "internal quantity and category trackers up to date.\n"
+          "Arguments:\n"
+          " * input_sample: Dict[str, str] - The input sample as a dictionary "
+          "where the keys are column names as specified in the schema and the "
+          "values are the respective column values.\n"
+          "Example:\n"
+          "```\n"
+          "# Suppose we construct a SequentialClassifier as follows:\n"
+          "model = SequentialClassifier(\n"
+          "    user=('name', 500),\n"
+          "    label=('salary', 5),\n"
+          "    timestamp='timestamp',\n"
+          "    static_categorical=[('age_group', 7)]\n"
+          "    track_categories=[('expenditure_level', 7, 30)]\n"
+          ")\n\n"
+          "# Suppose we recorded a new sample with the following information:\n"
+          "input_sample = {\n"
+          "    'name': 'arun',\n"
+          "    'salary': '<=50k',\n"
+          "    'timestamp': '2022-02-02',\n"
+          "    'age_group': '20-39',\n"
+          "    'expenditure_level': 'high'\n"
+          "})\n\n"
+          "# Then we will call index_single as follows:\n"
+          "model.index_single(input_sample)\n"
+          "```\n")
+      .def("save", &SequentialClassifier::save, py::arg("filename"),
+           "Serializes the SequentialClassifier into a file on disk. Example:\n"
+           "```\n"
+           "from thirdai import bolt\n\n"
+           "model = bolt.SequentialClassifier(...)\n"
+           "model.save('seq_class_savefile.bolt')\n"
+           "```\n")
+      .def_static(
+          "load", &SequentialClassifier::load, py::arg("filename"),
+          "Loads a serialized SequentialClassifier from a file on disk. "
+          "Example:\n"
+          "```\n"
+          "from thirdai import bolt\n\n"
+          "model = bolt.SequentialClassifier.load('seq_class_savefile.bolt')\n"
+          "```\n");
 
   createBoltGraphSubmodule(bolt_submodule);
 
