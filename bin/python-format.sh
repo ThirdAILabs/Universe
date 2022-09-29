6f3fa9ce3fdf0b656cdd3b45438c2f73e88c86f6
Yashwanth Adunukota <yash@thirdai.com>
2022-09-29 23:18:15 +0530

    used profile as black to resolve conflicts

--- bin/python-format.sh
+++ bin/python-format.sh
@@ -3,4 +3,4 @@
 BASEDIR=$(dirname "$0")
 cd $BASEDIR/..
 black . --exclude "build|.venv|venv|.env|env|deps"
-isort . --skip-glob deps --skip _deps
+isort . --profile black --skip-glob deps --skip _deps
