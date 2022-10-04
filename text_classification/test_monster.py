39c0064fc17bc2b5bf9b3c860be1e9b780d16173
Jerin Philip <jerin@thirdai.com>
2022-10-04 16:06:31 +0530

    Add vocab to cookie-monster tests

--- text_classification/test_monster.py
+++ text_classification/test_monster.py
@@ -1,7 +1,12 @@
+import os
 import pytest
-from cookie_monster import *
+from cookie_monster import CookieMonster
+from thirdai.dataset import FixedVocabulary
 
 pytestmark = [pytest.mark.unit]
+
+BERT_VOCAB_PATH = "bert-base-uncased.vocab"
+BERT_VOCAB_URL = "https://huggingface.co/bert-base-uncased/resolve/main/vocab.txt"
 
 
 def setup_module():
@@ -12,10 +17,22 @@ def setup_module():
         f.write("0,This is a positive sentence\n")
         f.write("1,To train the model on classification tasks\n")
 
+    if not os.path.exists(BERT_VOCAB_PATH):
+        import urllib.request
+
+        response = urllib.request.urlopen(BERT_VOCAB_URL)
+        with open(BERT_VOCAB_PATH, "wb+") as bert_vocab_file:
+            bert_vocab_file.write(response.read())
+
 
 def test_new_dimension():
+    vocab = FixedVocabulary(BERT_VOCAB_PATH)
     model = CookieMonster(
-        100000, hidden_dimension=2000, hidden_sparsity=0.1, mlflow_enabled=False
+        vocab,
+        input_dimension=100000,
+        hidden_dimension=2000,
+        hidden_sparsity=0.1,
+        mlflow_enabled=False,
     )
     model.set_output_dimension(10)
     assert model.output_layer.get_dim() == 10
@@ -24,8 +41,13 @@ def test_new_dimension():
 
 
 def test_train():
+    vocab = FixedVocabulary(BERT_VOCAB_PATH)
     model = CookieMonster(
-        784, hidden_dimension=2000, hidden_sparsity=0.1, mlflow_enabled=False
+        vocab,
+        input_dimension=784,
+        hidden_dimension=2000,
+        hidden_sparsity=0.1,
+        mlflow_enabled=False,
     )
     model.eat_corpus("test_dir", False, True)
     model.evaluate("test_dir")
