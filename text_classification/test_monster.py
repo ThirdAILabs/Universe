d952e2d7b1ffb493353c7b8470ad7759f3094245
Jerin Philip <jerin@thirdai.com>
2022-10-04 17:23:50 +0530

    Add a vocabulary test

--- text_classification/test_monster.py
+++ text_classification/test_monster.py
@@ -6,8 +6,8 @@ from thirdai.dataset import FixedVocabulary
 
 pytestmark = [pytest.mark.unit]
 
-BERT_VOCAB_PATH = "bert-base-uncased.vocab"
-BERT_VOCAB_URL = "https://huggingface.co/bert-base-uncased/resolve/main/vocab.txt"
+BERT_VOCAB_PATH = "bert-large-uncased.vocab"
+BERT_VOCAB_URL = "https://huggingface.co/bert-large-uncased/resolve/main/vocab.txt"
 
 
 def setup_module():
@@ -52,3 +52,31 @@ def test_train():
     )
     model.eat_corpus("test_dir", False, True)
     model.evaluate("test_dir")
+
+
+def test_vocab():
+    vocab = FixedVocabulary.make(BERT_VOCAB_PATH)
+    samples = [
+        "popularity of thread ##ing has increased around 2003 , as the growth of the cpu frequency was replaced with the growth of number of cores , in turn requiring concurrency to utilize multiple cores .",
+        "arkansas highway 59 business is a business route in gentry .",
+        "before joining city of hope , rosen worked at northwestern university .",
+        "the third section ( 8 km ) was the bypass of s ##zcz ##uc ##zyn opened in november 2015 as a single carriage ##way road .",
+        "in may 2021 , the company left the swedish market due to a “ unfair and disc ##rim ##inatory treatment ” by the swedish tax agency .",
+        "since 2018 , the winner of the bon ##aire kam ##pio ##nat ##o , the top tier of football on the island qu ##ali ##fies for the caribbean club shield , a tertiary knockout tournament for developing caribbean football nations .",
+        "the tone of lin ##lit ##hg ##ow ' s warnings to am ##ery grew increasingly serious over the first half of 1943 , as did am ##ery ' s requests to the war cabinet ; on 4a ##ug ##ust 1943 am ##ery noted the spread of famine , and specifically stressed the effect upon calcutta and the potential effect on the morale of european troops .",
+        "during the first years of japanese occupation following the first sino - japanese war , the political divisions of the island were changed frequently .",
+        "october 27 – martin mu ##llen , 63 , right fielder in one game for the 1872 cleveland forest city ##s of the national association .",
+        "mora ##ta de ta ##jun ##a is a municipality of the community of madrid , spain .",
+    ]
+    for sample in samples:
+        pieces = vocab.encode(sample)
+
+        # Nothing maps to unknown, as the above is taken from BERT (thirdai)
+        assert vocab.unk_id() not in pieces
+        tokens = sample.split()
+        if "THIRDAI_TEST_DEBUG" in os.environ:
+            for piece, token in zip(pieces, tokens):
+                print("{}: {}".format(token, piece), end=" ")
+            print()
+
+        assert len(pieces) == len(tokens)
