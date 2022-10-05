b6caded769f11002ff4d00fda3374073a66ad868
Nicholas Meisburger <nmeisburger@gmail.com>
2022-10-05 13:38:00 -0500

    Validation test

--- auto_ml/python_tests/test_text_classifer.py
+++ auto_ml/python_tests/test_text_classifer.py
@@ -185,6 +185,24 @@ def batch_predictions(samples, original_predictions, batch_size=10):
     return batches
 
 
+def test_train_with_validation(trained_text_classifier):
+    predict_config = (
+        bolt.graph.PredictConfig.make()
+        .with_metrics(["categorical_accuracy"])
+        .enable_sparse_inference()
+    )
+
+    val_data, val_labels = trained_text_classifier.load_validation_data(TEST_FILE)
+
+    train_config = bolt.graph.TrainConfig.make(epochs=1, learning_rate=0.001).with_validation(
+        validation_data=val_data, validation_labels=val_labels, predict_config=predict_config, validation_frequency=10
+    ).silence()
+
+    trained_text_classifier.train(
+        filename=TEST_FILE, train_config=train_config, batch_size=256
+    )
+
+
 def test_model_save_and_load(trained_text_classifier, model_predictions, clinc_dataset):
     trained_text_classifier.save(SAVE_FILE)
 
