5c9e3da88d7cc8043965a23de2c48486ec67156b
Josh Engels <josh@thirdai.com>
2022-10-07 10:05:17 -0500

    Reverting back to correct API in compression tests

--- bolt/python_tests/utils.py
+++ bolt/python_tests/utils.py
@@ -346,19 +346,21 @@ def compressed_training(
     predict_config = (
         bolt.graph.PredictConfig.make().with_metrics(["categorical_accuracy"]).silence()
     )
-    while wrapped_model.compute_and_store_next_batch_gradients() < epochs:
-        compressed_weight_grads = get_compressed_weight_gradients(
-            wrapped_model,
-            compression_scheme=compression_scheme,
-            compression_density=compression_density,
-            seed_for_hashing=np.random.randint(100),
-            sample_population_size=sample_population_size,
-        )
-        set_compressed_weight_gradients(
-            wrapped_model,
-            compressed_weight_grads=compressed_weight_grads,
-        )
-        wrapped_model.update_parameters()
+    for epochs in range(epochs):
+        for batch_num in range(num_training_batches):
+            wrapped_model.compute_and_store_batch_gradients(batch_num)
+            compressed_weight_grads = get_compressed_weight_gradients(
+                wrapped_model,
+                compression_scheme=compression_scheme,
+                compression_density=compression_density,
+                seed_for_hashing=np.random.randint(100),
+                sample_population_size=sample_population_size,
+            )
+            set_compressed_weight_gradients(
+                wrapped_model,
+                compressed_weight_grads=compressed_weight_grads,
+            )
+            wrapped_model.update_parameters()
 
     wrapped_model.finish_training()
 
