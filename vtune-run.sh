102bcdc03184d075a9080756db24ecff20b91a0f
Jerin Philip <jerin@thirdai.com>
2022-10-07 03:45:44 +0000

    Add vtune hotstpot cmdline command

---
+++ vtune-run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -x;
+
+VERSION=$(python3 -c "import thirdai; print(thirdai.__version__)")
+RESULTS_BASE=perf/$VERSION
+
+mkdir -p $RESULTS_BASE;
+
+vtune -collect hotspots -result-dir $RESULTS_BASE/hotspots python3 benchmarks/bolt.py benchmarks/bolt_configs/criteo_dlrm.txt --disable_mlflow --log-to-stderr
