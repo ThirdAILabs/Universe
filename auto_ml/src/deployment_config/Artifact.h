a48b6a801b071d8b624d8c3c8fb18ef6672d076e
Benito Geordie <benito@thirdai.com>
2022-10-11 14:20:31 -0500

    comments

--- auto_ml/src/deployment_config/Artifact.h
+++ auto_ml/src/deployment_config/Artifact.h
@@ -5,7 +5,13 @@
 
 namespace thirdai::automl::deployment {
 
-// This variant lists all types that can be returned as artifacts.
+/*
+  This variant lists all types that can be returned as artifacts.
+  Remember to list the types according to Pybind's overload resolution order;
+  e.g. bool has to come before int because bool is a subclass of int and not
+  the other way around.
+  https://pybind11.readthedocs.io/en/stable/advanced/cast/stl.html#c-17-library-containers
+*/
 using Artifact = std::variant<TemporalContextPtr>;
 
 }  // namespace thirdai::automl::deployment
