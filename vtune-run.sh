444bdd2b7b91addbb2770eeee62fca5c047cf6a3
Jerin Philip <jerin@thirdai.com>
2022-10-11 06:03:59 +0000

    Update vtune-run.sh

--- vtune-run.sh
+++ vtune-run.sh
@@ -4,10 +4,14 @@ source /opt/intel/oneapi/vtune/latest/env/vars.sh
 
 set -x;
 
-
 VERSION=$(python3 -c "import thirdai; print(thirdai.__version__)")
 RESULTS_BASE=perf/$VERSION
 
-mkdir -p $RESULTS_BASE;
+mkdir -p $RESULTS_BASE/performance-snapshot
 
-vtune -collect hotspots -result-dir $RESULTS_BASE/hotspots python3 benchmarks/bolt.py benchmarks/bolt_configs/criteo_dlrm.txt --disable_mlflow --log-to-stderr
+
+CMD="python3 benchmarks/bolt.py benchmarks/bolt_configs/criteo_dlrm.txt --disable_mlflow --log-to-stderr"
+#vtune -collect hotspots -result-dir $RESULTS_BASE/hotspots $CMD
+vtune -collect performance-snapshot -result-dir $RESULTS_BASE/performance-snapshot $CMD
+
+# valgrind --tool=cachegrind --cachegrind-out-file=$RESULTS_BASE/cachegrind.out $CMD
