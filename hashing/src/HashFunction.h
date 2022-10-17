2be6b71c96cc1a4ab47f83b8c896d33f64bec20e
Blaise Muhirwa <blaise@thirdai.com>
2022-10-16 23:14:05 -0500

    remove unnecessary changes

--- hashing/src/HashFunction.h
+++ hashing/src/HashFunction.h
@@ -29,7 +29,7 @@ class HashFunction {
   }
 
   void hashBatchParallel(const BoltBatch& batch, uint32_t* output) const {
-#pragma omp parallel for default(none) shared(batch, output, std::cout)
+#pragma omp parallel for default(none) shared(batch, output)
     for (uint32_t v = 0; v < batch.getBatchSize(); v++) {
       if (batch[v].isDense()) {
         hashSingleDense(batch[v].activations, batch[v].len,
