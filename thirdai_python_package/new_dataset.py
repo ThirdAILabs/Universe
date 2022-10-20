4d517809445528137f98ad905f0f7aad3837b194
Josh Engels <josh@thirdai.com>
2022-10-20 14:45:35 -0500

    Updating comment

--- thirdai_python_package/new_dataset.py
+++ thirdai_python_package/new_dataset.py
@@ -5,15 +5,18 @@ import thirdai._thirdai.new_dataset
 from thirdai._thirdai.new_dataset import *
 
 
-# df is a pandas dataframe, but we don't type it as such to avoid a package
-# dependency on pandas.
-# This method assumes that integer type columns are sparse. If you want to
-# force those to be dense, pass them in as elements of the dense_int_cols set.
-# This method also assumes that any sparse columns have a range equal in size
-# to the largest value in the column. If this is not the case, pass in the
-# actual range in the int_col_ranges dictionary with the key of the column name.
-# Note also the pandas array should be named
 def pandas_to_columnmap(df, dense_int_cols=set(), int_col_ranges={}):
+    """
+    Converts a pandas dataframe to a ColumnMap object. This method assumes that
+    integer type columns are sparse. If you want to force an integer column to
+    be dense, pass it in as an element of the dense_int_cols set. This method
+    also assumes that any sparse columns have a range equal in size to the
+    largest value in the column. This may not be always be the case (e.g.
+    streaming or inference). You can explicitly pass in the actual range in the
+    int_col_ranges dictionary with the key of the column name. Finally, note
+    that the pandas array should have valid headers, as these will be the names
+    of the column in the ColumnMap.
+    """
     column_map = {}
     for column_name in df:
         column_np = df[column_name].values
