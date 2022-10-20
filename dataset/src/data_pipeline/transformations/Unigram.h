a68256fce54762dd685589e19ef74acc77c8ac9e
David Torres <david@thirdai.com>
2022-10-20 15:10:27 -0500

    idk

--- dataset/src/data_pipeline/transformations/Unigram.h
+++ dataset/src/data_pipeline/transformations/Unigram.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <hashing/src/MurmurHash.h>
-#include <_types/_uint32_t.h>
 #include <dataset/src/data_pipeline/Column.h>
 #include <dataset/src/data_pipeline/ColumnMap.h>
 #include <dataset/src/data_pipeline/Transformation.h>
@@ -11,38 +9,32 @@
 
 namespace thirdai::dataset {
 
-class CrossColumnPairgram : public Transformation {
-  CrossColumnPairgram(std::vector<std::string> input_column_names,
-                      std::string output_column_name, uint32_t output_range)
-      : _input_column_names(std::move(input_column_names)),
+class Unigram : public Transformation {
+  Unigram(std::string input_column_name, std::string output_column_name,
+          uint32_t output_range)
+      : _input_column_name(std::move(input_column_name)),
         _output_column_name(std::move(output_column_name)),
         _output_range(output_range) {}
 
   void apply(ColumnMap& column_map) final {
-    std::vector<std::shared_ptr<SparseValueColumn>> columns(
-        _input_column_names.size());
-    std::vector<uint32_t> column_hashes(_input_column_names.size());
-    for (const auto& col_name : _input_column_names) {
-      columns.push_back(column_map.getSparseValueColumn(col_name));
-      column_hashes.push_back(
-          TextEncodingUtils::computeUnigram(col_name.c_str(), col_name.size()));
-    }
+    auto column = column_map.getSparseValueColumn(_input_column_name);
 
 #pragma omp parallel for default(none) \
-    shared(column_map, columns, column_hashes, _output_range)
+    shared(column_map, columns, _output_range)
     for (uint32_t row_idx = 0; row_idx < column_map.numRows(); row_idx++) {
       std::vector<uint32_t> unigram_hashes(columns.size());
       uint32_t col_num = 0;
       for (const auto& column : columns) {
         // to avoid two identical values in different columns from having the
-        // same hash value we combine the hash with the name of the column of
-        // origin
-        const char* val_to_hash =
-            reinterpret_cast<const char*>(&(*column)[row_idx]);
-        uint32_t hashes_col_val =
-            TextEncodingUtils::computeUnigram(val_to_hash, /* len = */ 4);
-        unigram_hashes.push_back(hashing::HashUtils::combineHashes(
-            hashes_col_val, column_hashes[col_num]));
+        // same hash value we add the column number as "salt" to make it unique
+        // we use a 64 bit int, the first 32 bits being the value and the next
+        // 32 bits being the column number
+        uint64_t salted_value = static_cast<uint64_t>((*column)[row_idx])
+                                    << 32 |
+                                static_cast<uint64_t>(col_num);
+        const char* val_to_hash = reinterpret_cast<const char*>(&salted_value);
+        unigram_hashes.push_back(
+            TextEncodingUtils::computeUnigram(val_to_hash, /* len = */ 8));
         col_num++;
       }
 
@@ -60,7 +52,7 @@ class CrossColumnPairgram : public Transformation {
     }
   }
 
-  std::vector<std::string> _input_column_names;
+  std::string _input_column_name;
   std::string _output_column_name;
   uint32_t _output_range;
 };
