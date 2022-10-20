f36490fdbf2729421e3bc611d0c5c958d576f9df
David Torres <david@thirdai.com>
2022-10-19 23:20:08 -0500

    initial pairgram transform

---
+++ dataset/src/data_pipeline/transformations/CrossColumnPairgram.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <_types/_uint32_t.h>
+#include <dataset/src/data_pipeline/Column.h>
+#include <dataset/src/data_pipeline/ColumnMap.h>
+#include <dataset/src/data_pipeline/Transformation.h>
+#include <dataset/src/utils/TextEncodingUtils.h>
+#include <string>
+#include <vector>
+
+namespace thirdai::dataset {
+
+class CrossColumnPairgram : public Transformation {
+  CrossColumnPairgram(std::vector<std::string> input_column_names,
+                      std::string output_column_name, uint32_t output_range)
+      : _input_column_names(std::move(input_column_names)),
+        _output_column_name(std::move(output_column_name)),
+        _output_range(output_range) {}
+
+  void apply(ColumnMap& column_map) final {
+    std::vector<std::shared_ptr<IntegerValueColumn>> columns(
+        _input_column_names.size());
+    for (const auto& col_name : _input_column_names) {
+      columns.push_back(column_map.getIntegerValueColumn(col_name));
+    }
+
+    
+
+#pragma omp parallel for default(none) \
+    shared(column_map, columns, _output_range)
+    for (uint32_t row_idx = 0; row_idx < column_map.numRows(); row_idx++) {
+      std::vector<uint32_t> unigram_hashes(columns.size());
+      uint32_t col_num = 0;
+      for (const auto& column : columns) {
+        uint64_t salted_value = static_cast<uint64_t>((*column)[row_idx])
+                                    << 32 |
+                                static_cast<uint64_t>(col_num);
+        const char* val_to_hash = reinterpret_cast<const char*>(&salted_value);
+        unigram_hashes.push_back(
+            TextEncodingUtils::computeUnigram(val_to_hash, /* len = */ 8));
+        col_num++;
+      }
+
+      std::vector<uint32_t> pairgram_hashes =
+          TextEncodingUtils::computeRawPairgramsFromUnigrams(unigram_hashes,
+                                                             _output_range);
+
+      std::vector<uint32_t> pairgrams(pairgram_hashes.size());
+      TextEncodingUtils::sumRepeatedIndices(
+          pairgram_hashes, /* base_value = */ 1.0,
+          [&](uint32_t pairgram, float value) {
+            (void)value;
+            pairgrams.push_back(pairgram);
+          });
+    }
+  }
+
+  std::vector<std::string> _input_column_names;
+  std::string _output_column_name;
+  uint32_t _output_range;
+};
+
+}  // namespace thirdai::dataset
