881a8694bd13b0e2dc0091e3efe4a06bf87d8773
Nicholas Meisburger <nmeisburger@gmail.com>
2022-10-20 19:28:14 -0500

    Python format

--- bolt/python_tests/test_sgd.py
+++ bolt/python_tests/test_sgd.py
@@ -7,8 +7,12 @@ from utils import gen_numpy_training_data, get_simple_dag_model
 @pytest.mark.unit
 def test_sgd():
     n_classes = 20
-    train_data, train_labels = gen_numpy_training_data(n_classes=n_classes, n_samples=5000, batch_size_for_conversion=100)
-    test_data, test_labels = gen_numpy_training_data(n_classes=n_classes, n_samples=1000)
+    train_data, train_labels = gen_numpy_training_data(
+        n_classes=n_classes, n_samples=5000, batch_size_for_conversion=100
+    )
+    test_data, test_labels = gen_numpy_training_data(
+        n_classes=n_classes, n_samples=1000
+    )
 
     model = get_simple_dag_model(
         input_dim=n_classes,
@@ -18,11 +22,12 @@ def test_sgd():
         optimizer=bolt.optimizers.Sgd(),
     )
 
-    predict_config = bolt.graph.PredictConfig.make().with_metrics(["categorical_accuracy"])
+    predict_config = bolt.graph.PredictConfig.make().with_metrics(
+        ["categorical_accuracy"]
+    )
     train_config = bolt.graph.TrainConfig.make(epochs=2, learning_rate=1.0)
 
     model.train(train_data, train_labels, train_config)
     metrics = model.predict(test_data, test_labels, predict_config)[0]
 
     assert metrics["categorical_accuracy"] >= 0.9
-    
