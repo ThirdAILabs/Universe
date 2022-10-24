c8d264059250438513a01d264211eec4d7ac5f7c
David Torres <david@thirdai.com>
2022-10-24 15:12:46 -0500

    sentence unigram test

--- new_dataset/python_tests/test_pairgram_transformations.py
+++ new_dataset/python_tests/test_pairgram_transformations.py
@@ -1,45 +1,56 @@
 import pytest
+from collections import defaultdict
 from thirdai import new_dataset as dataset
 
 pytestmark = [pytest.mark.unit]
 
+
 def get_sentence_str_column(col_length):
-    return dataset.columns.StringColumn([f"value{i} value{i} value{i+1}" for i in range(col_length)])
+    return dataset.columns.StringColumn(
+        [f"value{i} value{i} value{i+1}" for i in range(col_length)]
+    )
 
-# tests that if 
+
+# tests that deduplication in the sentence unigram block doesn't actually change
+# resulting the boltvector
 def test_sentence_unigram_deduplication():
-col_length = 10000
-column = get_sentence_str_column(col_length)
+    col_length = 10000
+    column = get_sentence_str_column(col_length)
 
-columns = dataset.ColumnMap({"sentence": column})
+    columns = dataset.ColumnMap({"sentence": column})
 
-output_range = 100
-featurizer = dataset.FeaturizationPipeline(
-    transformations=[
-        dataset.transformations.SentenceUnigram(
-            input_column="sentence",
-            output_column="deduplicated",
-            output_range=output_range,
-            deduplicate=True,
-        ),
-        dataset.transformations.SentenceUnigram(
-            input_column="sentence",
-            output_column="not_deduplicated",
-            output_range=output_range,
-            deduplicate=False,
-        )
-    ]
-)
+    output_range = 100
+    featurizer = dataset.FeaturizationPipeline(
+        transformations=[
+            dataset.transformations.SentenceUnigram(
+                input_column="sentence",
+                output_column="deduplicated",
+                output_range=output_range,
+                deduplicate=True,
+            ),
+            dataset.transformations.SentenceUnigram(
+                input_column="sentence",
+                output_column="not_deduplicated",
+                output_range=output_range,
+                deduplicate=False,
+            ),
+        ]
+    )
 
-columns = featurizer.featurize(columns)
-deduped_dataset = columns.convert_to_dataset(["deduplicated"], batch_size=col_length)
-not_deduped_dataset = columns.convert_to_dataset(["not_deduplicated"], batch_size=col_length)
+    columns = featurizer.featurize(columns)
+    deduped_dataset = columns.convert_to_dataset(
+        ["deduplicated"], batch_size=col_length
+    )
+    not_deduped_dataset = columns.convert_to_dataset(
+        ["not_deduplicated"], batch_size=col_length
+    )
 
-    for i in range(col_length):
+    for row_idx in range(col_length):
+        indices, values = not_deduped_dataset[0][row_idx].to_numpy()
+        expected_values = defaultdict(int)
+        for index, value in zip(indices, values):
+            expected_values[index] += value
 
-
-    for i in range(10):
-        print(dataset_object2[0][i])
-
-
-
+        actual_indices, actual_values = deduped_dataset[0][row_idx].to_numpy()
+        for actual_index, actual_value in zip(actual_indices, actual_values):
+            assert expected_values[actual_index] == actual_value
