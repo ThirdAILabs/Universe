defa61f5d765140f30edcb7d4edd41b7acb9509b
Nicholas Meisburger <nmeisburger@gmail.com>
2022-10-26 11:48:49 -0500

    Fix c++ test compile error

--- bolt/src/graph/tests/MockNode.h
+++ bolt/src/graph/tests/MockNode.h
@@ -16,8 +16,6 @@ class MockNode : public Node {
 
   MOCK_METHOD(bool, isInputNode, (), (const override));
 
-  MOCK_METHOD(void, enableDistributedTraining, (), (override));
-
  private:
   MOCK_METHOD(void, compileImpl, (), (override));
 
