876f849d7b6b18ca097e83c64ca66a458db37a3c
Nicholas Meisburger <nmeisburger@gmail.com>
2022-10-28 15:11:47 -0500

    Signed momentum test

--- bolt/python_tests/test_optimizers.py
+++ bolt/python_tests/test_optimizers.py
@@ -29,3 +29,30 @@ def test_sgd():
     metrics = model.evaluate(test_data, test_labels, eval_config)[0]
 
     assert metrics["categorical_accuracy"] >= 0.9
+
+
+@pytest.mark.unit
+def test_signed_momentum():
+    n_classes = 20
+    train_data, train_labels = gen_numpy_training_data(
+        n_classes=n_classes, n_samples=5000, batch_size_for_conversion=100
+    )
+    test_data, test_labels = gen_numpy_training_data(
+        n_classes=n_classes, n_samples=1000
+    )
+
+    model = get_simple_dag_model(
+        input_dim=n_classes,
+        hidden_layer_dim=100,
+        hidden_layer_sparsity=1.0,
+        output_dim=n_classes,
+        optimizer=bolt.nn.optimizers.SignedMomentum(),
+    )
+
+    train_config = bolt.TrainConfig(epochs=2, learning_rate=0.01)
+    model.train(train_data, train_labels, train_config)
+
+    eval_config = bolt.EvalConfig().with_metrics(["categorical_accuracy"])
+    metrics = model.evaluate(test_data, test_labels, eval_config)[0]
+
+    assert metrics["categorical_accuracy"] >= 0.9
