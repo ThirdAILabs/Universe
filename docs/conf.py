ff3b329da6212d659a87bd4a774cad228ea28ac7
Jerin Philip <jerin@thirdai.com>
2022-10-28 15:18:49 +0530

    Reduce noise: omit _thirdai in type annotations and cleanup self arg

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,8 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import inspect
+
 
 # -- Project information -----------------------------------------------------
 
@@ -56,5 +58,57 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-
 html_static_path = []
+
+
+autodoc_typehints_format = "short"
+autodoc_class_signature = "separated"
+
+
+def fix_pybind11_signatures(
+    app, what, name, obj, options, signature, return_annotation
+):
+    def _remove_self(signature):
+        arguments = signature[1:-1].split(", ")
+        if arguments and arguments[0].startswith("self:"):
+            arguments.pop(0)
+        return "(%s)" % ", ".join(arguments)
+
+    def _reformat_typehints(content):
+        return content.replace(
+            "thirdai._thirdai.",
+            "thirdai." if autodoc_typehints_format == "fully-qualified" else "",
+        )
+
+    if signature is not None:
+        signature = _remove_self(signature)
+        signature = _reformat_typehints(signature)
+
+    if return_annotation is not None:
+        return_annotation = _reformat_typehints(return_annotation)
+
+    return (signature, return_annotation)
+
+
+def skip_pybind11_builtin_members(app, what, name, obj, skip, options):
+    skipped_entries = {
+        "__init__": ["self", "args", "kwargs"],
+        "__new__": ["args", "kwargs"],
+    }
+
+    ref_arguments = skipped_entries.get(name)
+    if ref_arguments is not None:
+        try:
+            arguments = list(inspect.signature(obj).parameters.keys())
+            if arguments == ref_arguments:
+                return True
+        except ValueError:
+            pass
+
+    return None
+
+
+def setup(app):
+    app.add_css_file("custom.css")
+    app.connect("autodoc-skip-member", skip_pybind11_builtin_members)
+    app.connect("autodoc-process-signature", fix_pybind11_signatures)
