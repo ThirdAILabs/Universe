ba1f965d4a0304f1cfe1d12ee1022d05858b6273
Blaise Muhirwa <blaise@thirdai.com>
2022-10-31 18:45:19 -0500

    Restore Flash  (#892)
    * add indexer python bindings
    
    * restore original flash codebase
    
    * add flash serialization/deserialization unit test
    
    * add initial python bindings for indexer
    
    * add loading and saving for flash index config
    
    * change constructor for flash index config
    
    * add python test for flash indexer
    
    * add serialization for flash and vector hash table
    
    * remove unnecessary changes
    
    * add python test for the indexer
    
    * temp commit
    
    * add documentation for indexer in deployment
    
    * move indexer and flash to bolt submodule
    
    * Rename flash.cc to Flash.cc
    
    * Rename flash.h to Flash.h
    
    * add further documentation
    
    * respond to PR comments
    
    * fix linting errors
    
    * fix memory leak in flash
    
    * add ids to queries mapping
    
    * fix cmake formatting errors
    
    * rename indexer to generator
    
    * fix unit test
    
    * augment python unit test coverage
    
    * use a fixed batch processor for the generator class
    
    * nits
    
    * fix memory leak
    
    * change class name from Generator to QueryCandidateGenerator
    
    * temporary commit
    
    * respond to PR comments
    
    * change python test file name
    
    * move generator class to search
    
    * respond to PR comments
    
    * respond to PR comments
    
    * remove verifyBatchSequentialIDs
    
    * remove labels from the generator class state
    
    * fix batch processor issues
    
    * add unit test to check unique label assignments for input queries
    
    * NOLINT for ASSERT_THROW since clang tidy doesn't like it
    
    * respond to pr comments
    
    * fix minor issue

---
+++ search/tests/FlashIndexTest.cc
@@ -0,0 +1,98 @@
+#include <cereal/archives/binary.hpp>
+#include <bolt_vector/src/BoltVector.h>
+#include <bolt_vector/tests/BoltVectorTestUtils.h>
+#include <hashing/src/DensifiedMinHash.h>
+#include <gtest/gtest.h>
+#include <dataset/src/Datasets.h>
+#include <search/src/Flash.h>
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <numeric>
+#include <random>
+#include <vector>
+
+using thirdai::hashing::DensifiedMinHash;
+using thirdai::search::Flash;
+
+namespace thirdai::tests {
+
+const uint32_t HASHES_PER_TABLE = 3;
+const uint32_t NUM_TABLES = 32;
+const uint32_t RANGE = 3000;
+const uint32_t NUM_VECTORS = 100;
+
+std::vector<uint32_t> createBatchLabels(uint32_t vector_size) {
+  std::vector<uint32_t> labels;
+  labels.reserve(vector_size);
+
+  std::iota(labels.begin(), labels.end(), 0);
+  return labels;
+}
+
+TEST(FlashIndexTest, FlashIndexSerializationTest) {
+  uint32_t input_vector_dimension = 50;
+
+  uint32_t batch_size = 5;
+  uint32_t num_queries = 12;
+  uint32_t top_k = 5;
+
+  auto vectors_to_be_indexed =
+      createRandomSparseVectors(input_vector_dimension, NUM_VECTORS,
+                                std::normal_distribution<float>(0, 1));
+
+  auto flash_index_batches = createBatches(vectors_to_be_indexed, batch_size);
+
+  auto query_vectors =
+      createRandomSparseVectors(input_vector_dimension, num_queries,
+                                std::normal_distribution<float>(0, 1));
+  auto query_batches = createBatches(query_vectors, batch_size);
+
+  // Create a Flash object
+  auto flash_index = Flash<uint32_t>(
+      std::make_shared<DensifiedMinHash>(HASHES_PER_TABLE, NUM_TABLES, RANGE));
+
+  auto labels = createBatchLabels(NUM_VECTORS);
+
+  for (BoltBatch& batch : flash_index_batches) {
+    flash_index.addBatch(batch, labels);
+  }
+
+  std::vector<std::vector<std::vector<uint32_t>>> query_outputs;
+
+  for (BoltBatch& batch : query_batches) {
+    auto output_vectors = flash_index.queryBatch(batch, top_k, true);
+    query_outputs.push_back(output_vectors);
+  }
+
+  // Serialization-Deserialization is not handled via load-save methods
+  // because Flash is not a top level class.
+  std::stringstream stream;
+  {
+    cereal::BinaryOutputArchive output_archive(stream);
+    output_archive(flash_index);
+  }
+
+  Flash<uint32_t> deserialized_flash_index;
+  {
+    cereal::BinaryInputArchive input_archive(stream);
+    input_archive(deserialized_flash_index);
+  }
+
+  std::vector<std::vector<std::vector<uint32_t>>>
+      deserialized_flash_query_outputs;
+
+  for (BoltBatch& batch : query_batches) {
+    auto output_vectors =
+        deserialized_flash_index.queryBatch(batch, top_k, true);
+    deserialized_flash_query_outputs.push_back(output_vectors);
+  }
+
+  for (uint32_t batch_index = 0; batch_index < query_batches.size();
+       batch_index++) {
+    ASSERT_TRUE(query_outputs[batch_index] ==
+                deserialized_flash_query_outputs[batch_index]);
+  }
+}
+}  // namespace thirdai::tests
