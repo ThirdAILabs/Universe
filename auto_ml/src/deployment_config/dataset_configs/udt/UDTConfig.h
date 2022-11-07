780c05e07ae2daedeb3bc1825609b161b855df9e
Benito Geordie <89472452+benitoThree@users.noreply.github.com>
2022-11-07 11:18:54 -0600

    Remove oracle python binding (#962)
    * Remove oracle python binding
    
    * Removed all traces of sequential classifier and renamed anything "oracle" to "udt" (#963)

--- auto_ml/src/deployment_config/dataset_configs/udt/UDTConfig.h
+++ auto_ml/src/deployment_config/dataset_configs/udt/UDTConfig.h
@@ -2,8 +2,7 @@
 
 #include <cereal/access.hpp>
 #include <cereal/types/string.hpp>
-#include "Aliases.h"
-#include <bolt/src/auto_classifiers/sequential_classifier/ConstructorUtilityTypes.h>
+#include "DataTypes.h"
 #include <dataset/src/utils/QuantityHistoryTracker.h>
 #include <utils/StringManipulation.h>
 #include <algorithm>
@@ -15,7 +14,7 @@
 
 namespace thirdai::automl::deployment {
 
-struct OracleConfig {
+struct UDTConfig {
   /**
    * data_types: mapping from column names (strings) to DataType objects,
    *   e.g. {"user_id_column": types.categorical(n_unique_classes=5)}
@@ -27,7 +26,7 @@ struct OracleConfig {
    *            "movie_id",
    *            temporal.categorical("movie_id", track_last_n=20)
    *        ]}
-   *   When a TemporalConfig object is provided, Oracle will track the column
+   *   When a TemporalConfig object is provided, UDT will track the column
    *   as manually configured. When a string is provided, the tracking
    *   configuration will be autotuned.
    *
@@ -37,20 +36,19 @@ struct OracleConfig {
    *   or `"monthly"`/`"m"`. Interval of time that we are interested in.
    *   Temporal numerical features are grouped according to this time
    *   granularity. E.g. if time_granularity="w" and the numerical values
-   *   on days 1 and 2 are 345.25 and 201.1 respectively, then Oracle
+   *   on days 1 and 2 are 345.25 and 201.1 respectively, then UDT
    *   captures a single numerical value of 546.26 for the week instead of
    *   individual values for the two days. Defaults to "d" (daily).
    *
-   * lookahead: How far in the future Oracle has to predict. The given number
+   * lookahead: How far in the future UDT has to predict. The given number
    *   is relative to the provided time_granularity.
-   *   e.g. if time_granularity = "w" and lookahead = 5, then Oracle learns to
+   *   e.g. if time_granularity = "w" and lookahead = 5, then UDT learns to
    *   predict 5 weeks into the future.
    */
-  OracleConfig(
-      ColumnDataTypes data_types,
-      UserProvidedTemporalRelationships temporal_tracking_relationships,
-      std::string target, std::string time_granularity = "d",
-      uint32_t lookahead = 0, char delimiter = ',')
+  UDTConfig(ColumnDataTypes data_types,
+            UserProvidedTemporalRelationships temporal_tracking_relationships,
+            std::string target, std::string time_granularity = "d",
+            uint32_t lookahead = 0, char delimiter = ',')
       : data_types(std::move(data_types)),
         provided_relationships(std::move(temporal_tracking_relationships)),
         target(std::move(target)),
@@ -68,7 +66,7 @@ struct OracleConfig {
 
  private:
   // Private constructor for Cereal.
-  OracleConfig() {}
+  UDTConfig() {}
 
   // Tell Cereal what to serialize. See https://uscilab.github.io/cereal/
   friend class cereal::access;
@@ -79,6 +77,6 @@ struct OracleConfig {
   }
 };
 
-using OracleConfigPtr = std::shared_ptr<OracleConfig>;
+using UDTConfigPtr = std::shared_ptr<UDTConfig>;
 
 }  // namespace thirdai::automl::deployment
