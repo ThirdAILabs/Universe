4be4c25f8fa8e9ad11fac501d4b385b5cd9f9919
Jerin Philip <jerin@thirdai.com>
2022-11-10 12:56:48 +0530

    Pick DistributedTrainingWrapper from old era

--- bolt/src/graph/DistributedTrainingWrapper.h
+++ bolt/src/graph/DistributedTrainingWrapper.h
@@ -8,8 +8,6 @@
 #include <bolt/src/graph/nodes/FullyConnected.h>
 #include <bolt/src/loss_functions/LossFunctions.h>
 #include <bolt/src/metrics/MetricAggregator.h>
-#include <optional>
-#include <stdexcept>
 #include <utility>
 #include <vector>
 
@@ -17,9 +15,12 @@ namespace thirdai::bolt {
 
 class DistributedTrainingWrapper {
  public:
-  DistributedTrainingWrapper(BoltGraphPtr bolt_graph, TrainConfig train_config)
+  DistributedTrainingWrapper(BoltGraphPtr bolt_graph,
+                             const dataset::BoltDatasetList& train_data,
+                             const dataset::BoltDatasetPtr& train_labels,
+                             TrainConfig train_config)
       : _bolt_graph(std::move(bolt_graph)),
-        _train_context(std::nullopt),
+        _train_context(DatasetContext(train_data, train_labels)),
         _train_config(std::move(train_config)),
         _metric_aggregator(_train_config.getMetricAggregator()) {
     _bolt_graph->verifyCanTrain(_train_context);
@@ -36,76 +37,32 @@ class DistributedTrainingWrapper {
     }
   }
 
-  void computeAndStoreBatchGradients(uint32_t batch_idx) {
-    requireTrainContext();
-    _train_context->setInputs(batch_idx, _bolt_graph->_inputs);
-    const BoltBatch& batch_labels = _train_context->labels()->at(batch_idx);
+  void computeAndSaveBatchGradients(uint32_t batch_idx) {
+    _train_context.setInputs(batch_idx, _bolt_graph->_inputs);
+    const BoltBatch& batch_labels = _train_context.labels()->at(batch_idx);
     _bolt_graph->processTrainingBatch(batch_labels, _metric_aggregator);
   }
 
   void updateParameters() {
-    requireTrainContext();
     _bolt_graph->updateParametersAndSampling(
         /* learning_rate = */ _train_config.learningRate(),
         /* rebuild_hash_tables_batch = */
         _train_config.getRebuildHashTablesBatchInterval(
-            _train_context->batchSize(), _train_context->len()),
+            _train_context.batchSize(), _train_context.len()),
         /* reconstruct_hash_functions_batch = */
         _train_config.getReconstructHashFunctionsBatchInterval(
-            _train_context->batchSize(), _train_context->len()));
-    _bolt_graph->logValidateAndSave(_train_context->batchSize(), _train_config,
+            _train_context.batchSize(), _train_context.len()));
+    _bolt_graph->logValidateAndSave(_train_context.batchSize(), _train_config,
                                     _metric_aggregator);
   }
 
   BoltGraphPtr getModel() { return _bolt_graph; }
 
-  void finishTraining() {
-    requireTrainContext();
-    _bolt_graph->cleanupAfterBatchProcessing();
-  }
-
-  uint64_t numBatches() {
-    if (!_train_context.has_value()) {
-      return 0;
-    }
-    return _train_context->numBatches();
-  }
-
-  void setDatasets(const dataset::BoltDatasetList& train_data,
-                   const dataset::BoltDatasetPtr& train_labels) {
-    DatasetContext new_context(train_data, train_labels);
-    _bolt_graph->verifyCanTrain(new_context);
-
-    if (!_train_context.has_value()) {
-      _bolt_graph->prepareToProcessBatches(new_context.batchSize(),
-                                           /* use_sparsity=*/true);
-      _train_context = new_context;
-      return;
-    }
-
-    if (new_context.batchSize() != _train_context->batchSize()) {
-      throw std::invalid_argument(
-          "New datasets must have the same batch size as the original "
-          "datasets");
-    }
-
-    // Since we have verified that the new DatasetContext has the same batch
-    // size as the original, we don't need to call prepareToProcessBatches
-    // again.
-    _train_context = new_context;
-  }
+  void finishTraining() { _bolt_graph->cleanupAfterBatchProcessing(); }
 
  private:
-  void requireTrainContext() {
-    if (!_train_context.has_value()) {
-      throw std::runtime_error(
-          "You must call setNewDataasets before you can train the wrapped "
-          "model");
-    }
-  }
-
   BoltGraphPtr _bolt_graph;
-  std::optional<DatasetContext> _train_context;
+  DatasetContext _train_context;
   TrainConfig _train_config;
   MetricAggregator _metric_aggregator;
 };
