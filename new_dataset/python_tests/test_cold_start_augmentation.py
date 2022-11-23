84b30e86cd7d632f712344a8b70e70d611d35a3c
= <bcoleman@thirdai.com>
2022-11-23 12:54:04 -0600

    Python format

--- new_dataset/python_tests/test_cold_start_augmentation.py
+++ new_dataset/python_tests/test_cold_start_augmentation.py
@@ -9,15 +9,15 @@ pytestmark = [pytest.mark.unit]
 
 
 def get_real_dataset():
-    '''
+    """
     This function returns strong, weak and label lists for a few entries of a
     real text dataset that contains the kind of phrases / punctuation commonly
     encountered in practice.
-    '''
+    """
     strong_list = [
         "Wicker Basket",
         "Genkent Digital Food Thermometer",
-        "Asherton 25.6\" Patio Bar Stool (Set of 2)"
+        'Asherton 25.6" Patio Bar Stool (Set of 2)',
     ]
     weak_list = [
         "Get clutter in order with this storage basket with liner."
@@ -37,13 +37,14 @@ def get_real_dataset():
         "while a little flexibility offers added comfort. Mesh strap-style "
         "construction lets rainfall right through for quick-drying, while "
         "four gases injection-molded legs include a built-in footrest and "
-        "structural stretchers."
+        "structural stretchers.",
     ]
     label_list = [0, 1, 2]
     return strong_list, weak_list, label_list
 
+
 def create_test_column_map(text_columns, labels):
-    '''
+    """
     Creates a column map from input dictionaries of strings and lists of
     integers. The text_column dictionary entries become StringColumns with
     the name equal to their key, while the labels become a NumpySparseArrayColumn
@@ -51,10 +52,11 @@ def create_test_column_map(text_columns, labels):
     Arguments:
         strong: Dictionary from string name to list of strings for strong text.
         weak: Dictionary from string name to list of strings for weak text.
-    '''
+    """
     label_list = np.array(labels, dtype=np.uint32).reshape([-1, 1])
     label_column = data.columns.NumpySparseArrayColumn(
-        array=label_list, dim=np.max(label_list)+1)
+        array=label_list, dim=np.max(label_list) + 1
+    )
     column_dict = {"labels": label_column}
     for name in text_columns.keys():
         column_dict[name] = data.columns.StringColumn(text_columns[name])
@@ -64,13 +66,13 @@ def create_test_column_map(text_columns, labels):
 
 
 def apply_augmentation_and_unigrams(columns, augmentation):
-    '''
+    """
     Applies augmentation to columns, passed in as a ColumnMap, producing a new
     ColumnMap containing the self-supervised text inputs as a StringColumn
-    named "data" and the integer labels "labels." The reason we apply 
-    unigrams is to get a more easily testable output, as currently the 
+    named "data" and the integer labels "labels." The reason we apply
+    unigrams is to get a more easily testable output, as currently the
     ColumnMap does not support introspection.
-    '''
+    """
     new_columns = augmentation.apply(columns)
 
     featurizer = data.FeaturizationPipeline(
@@ -79,7 +81,7 @@ def apply_augmentation_and_unigrams(columns, augmentation):
                 input_column="data",
                 output_column="unigrams",
                 deduplicate=False,
-                output_range=100000
+                output_range=100000,
             )
         ]
     )
@@ -88,24 +90,28 @@ def apply_augmentation_and_unigrams(columns, augmentation):
 
 
 def test_duplicated_natural_separators():
-    '''
+    """
     Tests that the natural phrase generator behaves properly when presented
     with poorly-formatted inputs that contain multiple consecutive punctuation
     marks.
-    '''
+    """
     strong_list = [""]
     weak_list = [",,.,,.,.,,weak,,,...,, weak weak"]
     label_list = [0]
     augmentation = data.augmentations.ColdStartText(
-        strong_columns=["strong"], weak_columns=["weak"],
-        label_column="labels", output_column="data")
+        strong_columns=["strong"],
+        weak_columns=["weak"],
+        label_column="labels",
+        output_column="data",
+    )
 
     columns = create_test_column_map(
-        {"strong":strong_list,"weak":weak_list}, label_list)
+        {"strong": strong_list, "weak": weak_list}, label_list
+    )
     new_columns = apply_augmentation_and_unigrams(columns, augmentation)
     new_dataset = new_columns.convert_to_dataset(["unigrams"], batch_size=10)
     # Verify that new_dataset consists of two entries, all having the same value
-    
+
     data_list = []
     for batch in new_dataset:
         for row_id in range(len(batch)):
@@ -127,47 +133,59 @@ def test_duplicated_natural_separators():
 
 
 def test_long_input():
-    '''
+    """
     Tests that the natural phrase generator properly breaks phrases into
     correctly-sized chunks when the text contains no natural delimiters.
-    '''
+    """
     strong_list = ["strong"]
     weak_list = ["a b c d e f g h i j k l m n o"]
     label_list = [0]
 
     augmentation = data.augmentations.ColdStartText(
-        strong_columns=["strong"], weak_columns=["weak"],
-        label_column="labels", output_column="data",
-        weak_max_len=3, weak_min_len=3)
+        strong_columns=["strong"],
+        weak_columns=["weak"],
+        label_column="labels",
+        output_column="data",
+        weak_max_len=3,
+        weak_min_len=3,
+    )
 
     columns = create_test_column_map(
-        {"strong":strong_list,"weak":weak_list}, label_list)
+        {"strong": strong_list, "weak": weak_list}, label_list
+    )
     new_columns = apply_augmentation_and_unigrams(columns, augmentation)
     new_dataset = new_columns.convert_to_dataset(["unigrams"], batch_size=10)
     # Verify that new_dataset consists of two entries, all having the same value
-    
+
     data_list = []
     for batch in new_dataset:
         for row_id in range(len(batch)):
             row = batch[row_id].to_numpy()[0]
             assert len(row) == 4  # 1 for the strong words, 3 for the phrase.
 
+
 def test_sample_strong_words():
-    '''
+    """
     Tests that the phrase generator samples the correct number of words from
     the strong phrase, when the strong sampling flag is enabled.
-    '''
+    """
     strong_list = ["extremely ridiculously strong language"]
     weak_list = ["a b c d e f g h i j k l m n o"]
     label_list = [0]
 
     augmentation = data.augmentations.ColdStartText(
-        strong_columns=["strong"], weak_columns=["weak"],
-        label_column="labels", output_column="data",
-        weak_max_len=3, weak_min_len=3, strong_sample_num_words=2)
+        strong_columns=["strong"],
+        weak_columns=["weak"],
+        label_column="labels",
+        output_column="data",
+        weak_max_len=3,
+        weak_min_len=3,
+        strong_sample_num_words=2,
+    )
 
     columns = create_test_column_map(
-        {"strong":strong_list,"weak":weak_list}, label_list)
+        {"strong": strong_list, "weak": weak_list}, label_list
+    )
     new_columns = apply_augmentation_and_unigrams(columns, augmentation)
     new_dataset = new_columns.convert_to_dataset(["unigrams"], batch_size=10)
 
@@ -179,20 +197,20 @@ def test_sample_strong_words():
 
 
 def test_shuffle_correct():
-    '''
+    """
     Tests that the shuffling algorithm correctly shuffles the labels and the
     phrases together.
-    '''
+    """
     strong_list = ["A", "B", "C", "D"]
-    weak_list = ["x x, x x",
-                 "x, x, x",
-                 "x x x",
-                 "x x x x"]
+    weak_list = ["x x, x x", "x, x, x", "x x x", "x x x x"]
     label_list = [2, 1, 3, 4]
 
     augmentation = data.augmentations.ColdStartText(
-        strong_columns=["strong"], weak_columns=["weak"],
-        label_column="labels", output_column="data")
+        strong_columns=["strong"],
+        weak_columns=["weak"],
+        label_column="labels",
+        output_column="data",
+    )
     # This will take all natural phrases: two 2-word phrases for the first
     # example, three 1-word phrases for the second, a 3-word phrase for the third
     # and a 4-word phrase for the fourth. We can check that shuffling is done
@@ -200,7 +218,8 @@ def test_shuffle_correct():
     # phrase length is equal to its label, plus one for the strong phrase.
 
     columns = create_test_column_map(
-        {"strong":strong_list,"weak":weak_list}, label_list)
+        {"strong": strong_list, "weak": weak_list}, label_list
+    )
     new_columns = apply_augmentation_and_unigrams(columns, augmentation)
     unigrams_dataset = new_columns.convert_to_dataset(["unigrams"], batch_size=10)
     label_dataset = new_columns.convert_to_dataset(["labels"], batch_size=10)
@@ -213,24 +232,30 @@ def test_shuffle_correct():
 
 
 def test_sample_weak_words():
-    '''
+    """
     Tests that the phrase generator samples the correct number of words from
     the weak phrases, when the weak sampling flag is enabled.
-    '''
+    """
     strong_list = ["title"]
-    weak_list = ["blah blah blah hashing blah blah lsh"
-                 " blah blah bloom filters blah blah"]
+    weak_list = [
+        "blah blah blah hashing blah blah lsh" " blah blah bloom filters blah blah"
+    ]
     label_list = [0]
 
     num_examples_per_phrase = 17
     augmentation = data.augmentations.ColdStartText(
-        strong_columns=["strong"], weak_columns=["weak"],
-        label_column="labels", output_column="data",
-        weak_sample_num_words=2, weak_sample_reps = num_examples_per_phrase)
+        strong_columns=["strong"],
+        weak_columns=["weak"],
+        label_column="labels",
+        output_column="data",
+        weak_sample_num_words=2,
+        weak_sample_reps=num_examples_per_phrase,
+    )
     # This will take all natural phrases, but downsample to just 2 tokens.
 
     columns = create_test_column_map(
-        {"strong":strong_list,"weak":weak_list}, label_list)
+        {"strong": strong_list, "weak": weak_list}, label_list
+    )
     new_columns = apply_augmentation_and_unigrams(columns, augmentation)
     new_dataset = new_columns.convert_to_dataset(["unigrams"], batch_size=10)
 
@@ -244,21 +269,25 @@ def test_sample_weak_words():
 
 
 def test_long_strong_phrase():
-    '''
+    """
     Tests that the strong phrase is cut to the correct number of words,
     when a maximum strong phrase length is provided.
-    '''
+    """
     strong_list = ["run on title that just goes on and on forever and ever"]
     weak_list = ["blah blah blah"]
     label_list = [0]
 
     augmentation = data.augmentations.ColdStartText(
-        strong_columns=["strong"], weak_columns=["weak"],
-        label_column="labels", output_column="data",
-        strong_max_len=3)
+        strong_columns=["strong"],
+        weak_columns=["weak"],
+        label_column="labels",
+        output_column="data",
+        strong_max_len=3,
+    )
 
     columns = create_test_column_map(
-        {"strong":strong_list,"weak":weak_list}, label_list)
+        {"strong": strong_list, "weak": weak_list}, label_list
+    )
     new_columns = apply_augmentation_and_unigrams(columns, augmentation)
     new_dataset = new_columns.convert_to_dataset(["unigrams"], batch_size=10)
 
@@ -272,28 +301,41 @@ def test_long_strong_phrase():
 
 
 def test_multiple_weak_columns():
-    '''
+    """
     Tests that if we have multiple weak columns, they are concatenated to get
     the weak phrase.
-    '''
+    """
     strong_list = ["NeurIPS Reviews"]
-    weak_list_0 = ["From Reviewer 1: This paper is good and contains important"
-                   ", novel results. Therefore, I have decided to reject it."]
-    weak_list_1 = ["From Reviewer 2: I detest this paper so much that I did "
-                   "not even read it. Reject (with high confidence)."]
-    weak_list_2 = ["From Reviewer 3: I have provided a list of 437 papers that"
-                   " this paper did not cite. All of them were written by me"]
+    weak_list_0 = [
+        "From Reviewer 1: This paper is good and contains important"
+        ", novel results. Therefore, I have decided to reject it."
+    ]
+    weak_list_1 = [
+        "From Reviewer 2: I detest this paper so much that I did "
+        "not even read it. Reject (with high confidence)."
+    ]
+    weak_list_2 = [
+        "From Reviewer 3: I have provided a list of 437 papers that"
+        " this paper did not cite. All of them were written by me"
+    ]
     label_list = np.array([0], dtype=np.uint32).reshape([-1, 1])
 
     augmentation = data.augmentations.ColdStartText(
-        strong_columns=["strong"], weak_columns=["weak_0", "weak_1", "weak_2"],
-        label_column="labels", output_column="data")
+        strong_columns=["strong"],
+        weak_columns=["weak_0", "weak_1", "weak_2"],
+        label_column="labels",
+        output_column="data",
+    )
 
     columns = create_test_column_map(
-        {"strong":strong_list,
-         "weak_0":weak_list_0,
-         "weak_1":weak_list_1,
-         "weak_2":weak_list_2}, label_list)
+        {
+            "strong": strong_list,
+            "weak_0": weak_list_0,
+            "weak_1": weak_list_1,
+            "weak_2": weak_list_2,
+        },
+        label_list,
+    )
     new_columns = apply_augmentation_and_unigrams(columns, augmentation)
     new_dataset = new_columns.convert_to_dataset(["unigrams"], batch_size=10)
 
@@ -305,27 +347,36 @@ def test_multiple_weak_columns():
 
 
 def test_multiple_strong_columns():
-    '''
+    """
     Tests that if we have multiple strong columns, they are concatenated to get
     the strong phrase.
-    '''
+    """
     strong_list_0 = ["hashing"]
     strong_list_1 = ["sketching"]
     strong_list_2 = ["sampling"]
-    weak_list = ["These techniques are common components of randomized "
-                 "algorithms that trade accuracy for efficiency."]
+    weak_list = [
+        "These techniques are common components of randomized "
+        "algorithms that trade accuracy for efficiency."
+    ]
     label_list = np.array([0], dtype=np.uint32).reshape([-1, 1])
 
     augmentation = data.augmentations.ColdStartText(
         strong_columns=["strong_0", "strong_1", "strong_2"],
-        weak_columns=["weak"], label_column="labels", output_column="data",
-        weak_sample_num_words=1)
+        weak_columns=["weak"],
+        label_column="labels",
+        output_column="data",
+        weak_sample_num_words=1,
+    )
 
     columns = create_test_column_map(
-        {"strong_0":strong_list_0,
-         "strong_1":strong_list_1,
-         "strong_2":strong_list_2,
-         "weak":weak_list}, label_list)
+        {
+            "strong_0": strong_list_0,
+            "strong_1": strong_list_1,
+            "strong_2": strong_list_2,
+            "weak": weak_list,
+        },
+        label_list,
+    )
     new_columns = apply_augmentation_and_unigrams(columns, augmentation)
     new_dataset = new_columns.convert_to_dataset(["unigrams"], batch_size=10)
 
@@ -336,25 +387,31 @@ def test_multiple_strong_columns():
 
 
 def test_real_input():
-    '''
+    """
     This is a behavioral test to check that the augmentation produces
     a reasonable output on real-world input data. We want to produce a
     reasonable number of phrases - not too many but not too few - and to
     confirm that 95% of the phrases meet our length requirements (there
     may be some stragglers that get cut off at the end of a weak text block).
-    '''
+    """
     strong_list, weak_list, label_list = get_real_dataset()
     augmentation = data.augmentations.ColdStartText(
-        strong_columns=["strong"], weak_columns=["weak"],
-        label_column="labels", output_column="data",
-        strong_sample_num_words=2, weak_min_len=5, weak_max_len=10,
-        weak_chunk_len=5)
+        strong_columns=["strong"],
+        weak_columns=["weak"],
+        label_column="labels",
+        output_column="data",
+        strong_sample_num_words=2,
+        weak_min_len=5,
+        weak_max_len=10,
+        weak_chunk_len=5,
+    )
 
     expected_min_length = 5  # minimally, 0 from strong and 5 from smallest weak phrase.
     expected_max_length = 12  # 2 from strong and 10 from largest strong phrase.
 
     columns = create_test_column_map(
-        {"strong":strong_list,"weak":weak_list}, label_list)
+        {"strong": strong_list, "weak": weak_list}, label_list
+    )
     new_columns = apply_augmentation_and_unigrams(columns, augmentation)
     new_dataset = new_columns.convert_to_dataset(["unigrams"], batch_size=10)
 
@@ -371,6 +428,7 @@ def test_real_input():
     # problem as it results in a very big pre-training task (100x larger).
     assert 6 <= num_data <= 300
     assert num_valid_data / num_data > 0.95
+
 
 test_duplicated_natural_separators()
 test_long_input()
