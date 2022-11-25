67b77168e5e21d8622a594999b6195247b26662d
Jerin Philip <jerin@thirdai.com>
2022-11-25 14:53:39 +0530

    Fix tests after API change

--- bolt/python_tests/test_freeze_layers.py
+++ bolt/python_tests/test_freeze_layers.py
@@ -15,39 +15,39 @@ def test_freeze_layers():
     hidden_layer_sparsity = 0.15
     output_dim = n_classes
     output_activation = "softmax"
-    loss = bolt.CategoricalCrossEntropyLoss()
+    loss = bolt.nn.losses.CategoricalCrossEntropy()
 
-    input_layer = bolt.graph.Input(dim=input_dim)
+    input_layer = bolt.nn.Input(dim=input_dim)
 
-    hidden_layer = bolt.graph.FullyConnected(
+    hidden_layer = bolt.nn.FullyConnected(
         dim=hidden_layer_dim, sparsity=hidden_layer_sparsity, activation="relu"
     )(input_layer)
 
-    output_layer = bolt.graph.FullyConnected(
-        dim=output_dim, activation=output_activation
-    )(hidden_layer)
+    output_layer = bolt.nn.FullyConnected(dim=output_dim, activation=output_activation)(
+        hidden_layer
+    )
 
-    model = bolt.graph.Model(inputs=[input_layer], output=output_layer)
+    model = bolt.nn.Model(inputs=[input_layer], output=output_layer)
     model.compile(loss)
 
     # Generate dataset.
     data, labels = gen_numpy_training_data(n_classes=n_classes, n_samples=10000)
 
-    def train_predict(model, data, labels):
+    def train_evaluate(model, data, labels):
         # Train and predict before freezing hash tables.
-        train_config = bolt.graph.TrainConfig.make(learning_rate=0.001, epochs=2)
+        train_config = bolt.TrainConfig(learning_rate=0.001, epochs=2)
         model.train(data, labels, train_config)
 
         predict_config = (
-            bolt.graph.PredictConfig.make()
+            bolt.EvalConfig()
             .enable_sparse_inference()
             .with_metrics(["categorical_accuracy"])
         )
 
-        test_metrics1 = model.predict(data, labels, predict_config)[0]
+        test_metrics1 = model.evaluate(data, labels, predict_config)[0]
         assert test_metrics1["categorical_accuracy"] >= 0.8
 
-    train_predict(model, data, labels)
+    train_evaluate(model, data, labels)
 
     before = {
         "weights": hidden_layer.weights.copy().flatten(),
@@ -56,7 +56,7 @@ def test_freeze_layers():
 
     # Freeze and train
     hidden_layer.trainable(False)
-    train_predict(model, data, labels)
+    train_evaluate(model, data, labels)
 
     after = {
         "weights": hidden_layer.weights.copy().flatten(),
@@ -69,7 +69,7 @@ def test_freeze_layers():
 
     # Undo freeze and train
     hidden_layer.trainable(True)
-    train_predict(model, data, labels)
+    train_evaluate(model, data, labels)
     after = {
         "weights": hidden_layer.weights.copy().flatten(),
         "biases": hidden_layer.biases.copy().flatten(),
