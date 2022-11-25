f702ab8dd52104d543bd54d61046e2ded040aa2c
Third AI <anshu@thirdai.com>
2022-11-25 17:54:15 +0000

    merged test cases for parquet and csv formats

--- thirdai_python_package_tests/dataset/test_get_udt_columns.py
+++ thirdai_python_package_tests/dataset/test_get_udt_columns.py
@@ -10,7 +10,7 @@ pytestmark = [pytest.mark.unit]
 
 @pytest.mark.parametrize("delimiter", _CATEGORICAL_DELIMITERS)
 def test_get_udt_columns_with_csv(delimiter):
-    with tempfile.NamedTemporaryFile(suffix=".csv", mode="w") as tmp:
+    with tempfile.NamedTemporaryFile(mode="w") as tmp:
         tmp.write(
             f"""col1,col2,col3,col4,col5,col6
                 lorem,2,3.0,label1{delimiter}label2{delimiter}label3,How vexingly quick daft zebras jump!,2021-02-01
@@ -35,35 +35,20 @@ def test_get_udt_columns_with_csv(delimiter):
         assert str(udt_types["col5"]) == '{"type": "text"}'
         assert str(udt_types["col6"]) == '{"type": "date"}'
 
+        df = pd.read_csv(tmp.name)
+        df.to_parquet(tmp.name + ".pqt")
+        udt_types_pqt = get_udt_col_types(tmp.name + ".pqt")
 
-# @pytest.mark.parametrize("delimiter", _CATEGORICAL_DELIMITERS)
-# def test_get_udt_columns_with_parquet(delimiter):
-#     with tempfile.NamedTemporaryFile(mode="w") as tmp:
-#         tmp.write(
-#             f"""col1,col2,col3,col4,col5,col6
-#                 lorem,2,3.0,label1{delimiter}label2{delimiter}label3,How vexingly quick daft zebras jump!,2021-02-01
-#                 ipsum,5,6,label4,"Sphinx of black quartz, judge my vow.",2022-02-01
-#                 dolor,8,9,label5{delimiter}label6,The quick brown fox jumps over the lazy dog,2023-02-01
-#             """
-#         )
-#         tmp.flush()
+        assert str(udt_types_pqt["col1"]) == '{"type": "categorical"}'
+        assert str(udt_types_pqt["col2"]) == '{"type": "categorical"}'
+        assert (
+            str(udt_types_pqt["col3"])
+            == '{"type": "numerical", "range": [3, 9], "granularity": "m"}'
+        )
+        assert (
+            str(udt_types_pqt["col4"])
+            == '{"type": "categorical", "delimiter": "' + delimiter + '"}'
+        )
+        assert str(udt_types_pqt["col5"]) == '{"type": "text"}'
+        assert str(udt_types_pqt["col6"]) == '{"type": "date"}'
 
-#         print('lolwa3:',tmp.name+'.pqt')
-#         df = pd.read_csv(tmp.name)
-#         df.to_parquet(tmp.name + ".pqt")
-#         udt_types = get_udt_col_types(tmp.name + ".pqt")
-
-#         assert str(udt_types["col1"]) == '{"type": "categorical"}'
-#         assert str(udt_types["col2"]) == '{"type": "categorical"}'
-#         assert (
-#             str(udt_types["col3"])
-#             == '{"type": "numerical", "range": [3, 9], "granularity": "m"}'
-#         )
-#         assert (
-#             str(udt_types["col4"])
-#             == '{"type": "categorical", "delimiter": "' + delimiter + '"}'
-#         )
-#         assert str(udt_types["col5"]) == '{"type": "text"}'
-#         assert str(udt_types["col6"]) == '{"type": "date"}'
-
-#         os.system("rm tmp.name+'.pqt'")
