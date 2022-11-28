586343a49028457b96038ad34395cd18daeccaf0
David Torres <david@thirdai.com>
2022-11-28 17:02:01 -0600

    utils file

---
+++ thirdai_python_package_tests/dataset/py_dataset_utils.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+def create_csv_and_pqt_files(csv_filename, parquet_filename, content):
+    with open(csv_filename, "w") as file:
+        file.write(content)
+    df = pd.read_csv(csv_filename)
+    df.to_parquet(parquet_filename)
