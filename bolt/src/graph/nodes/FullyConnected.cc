cefe6ff9cf3656ddd9ab0df655880f8ab2479bc4
Nicholas Meisburger <nmeisburger@gmail.com>
2022-11-28 13:28:20 -0600

    All nodes -> *.h + *.cc

---
+++ bolt/src/graph/nodes/FullyConnected.cc
@@ -0,0 +1,235 @@
+#include "FullyConnected.h"
+
+namespace thirdai::bolt {
+
+FullyConnectedNode::FullyConnectedNode(uint64_t dim,
+                                       const std::string& activation)
+    : _layer(nullptr),
+      _config(FullyConnectedLayerConfig(dim, activation)),
+      _predecessor(nullptr) {}
+
+FullyConnectedNode::FullyConnectedNode(uint64_t dim, float sparsity,
+                                       const std::string& activation)
+    : _layer(nullptr),
+      _config(FullyConnectedLayerConfig(dim, sparsity, activation)),
+      _predecessor(nullptr) {}
+
+FullyConnectedNode::FullyConnectedNode(uint64_t dim, float sparsity,
+                                       const std::string& activation,
+                                       SamplingConfigPtr sampling_config)
+    : _layer(nullptr),
+      _config(FullyConnectedLayerConfig(dim, sparsity, activation,
+                                        std::move(sampling_config))),
+      _predecessor(nullptr) {}
+
+std::shared_ptr<FullyConnectedNode> FullyConnectedNode::addPredecessor(
+    NodePtr node) {
+  if (getState() != NodeState::Constructed) {
+    throw exceptions::NodeStateMachineError(
+        "FullyConnectedNode expected to have exactly one predecessor, and "
+        "addPredecessor cannot be called twice.");
+  }
+  _predecessor = std::move(node);
+
+  return shared_from_this();
+}
+
+ActivationFunction FullyConnectedNode::getActivationFunction() const {
+  NodeState node_state = getState();
+  if (node_state == NodeState::Constructed ||
+      node_state == NodeState::PredecessorsSet) {
+    return _config->getActFunc();
+  }
+  return _layer->getActivationFunction();
+}
+
+uint32_t FullyConnectedNode::outputDim() const {
+  NodeState node_state = getState();
+  if (node_state == NodeState::Constructed ||
+      node_state == NodeState::PredecessorsSet) {
+    return _config->getDim();
+  }
+  return _layer->getDim();
+}
+
+void FullyConnectedNode::saveParameters(const std::string& filename) const {
+  std::ofstream filestream =
+      dataset::SafeFileIO::ofstream(filename, std::ios::binary);
+  cereal::BinaryOutputArchive oarchive(filestream);
+  oarchive(*_layer);
+}
+
+void FullyConnectedNode::loadParameters(const std::string& filename) {
+  std::ifstream filestream =
+      dataset::SafeFileIO::ifstream(filename, std::ios::binary);
+  cereal::BinaryInputArchive iarchive(filestream);
+  auto loaded_parameters = std::make_shared<FullyConnectedLayer>();
+  iarchive(*loaded_parameters);
+
+  if (loaded_parameters->getDim() != _layer->getDim()) {
+    std::stringstream error_msg;
+    error_msg << "Cannot load parameters from FullyConnected layer with dim="
+              << loaded_parameters->getDim()
+              << "into FullyConnected layer with dim=" << _layer->getDim()
+              << ".";
+    throw std::logic_error(error_msg.str());
+  }
+  if (loaded_parameters->getInputDim() != _layer->getInputDim()) {
+    std::stringstream error_msg;
+    error_msg
+        << "Cannot load parameters from FullyConnected layer with input_dim="
+        << loaded_parameters->getInputDim()
+        << "into FullyConnected layer with input_dim=" << _layer->getInputDim()
+        << ".";
+    throw std::logic_error(error_msg.str());
+  }
+
+  if (loaded_parameters->getActivationFunction() !=
+      _layer->getActivationFunction()) {
+    std::stringstream error_msg;
+    error_msg << "Cannot load parameters from FullyConnected layer with "
+                 "activation ="
+              << activationFunctionToStr(
+                     loaded_parameters->getActivationFunction())
+              << "into FullyConnected layer with activation="
+              << activationFunctionToStr(_layer->getActivationFunction())
+              << ".";
+    throw std::logic_error(error_msg.str());
+  }
+
+  _layer = loaded_parameters;
+}
+
+float FullyConnectedNode::getSparsity() {
+  NodeState node_state = getState();
+  if (node_state == NodeState::Constructed ||
+      node_state == NodeState::PredecessorsSet) {
+    return _config->getSparsity();
+  }
+  return _layer->getSparsity();
+}
+
+std::shared_ptr<FullyConnectedNode> FullyConnectedNode::setSparsity(
+    float sparsity) {
+  if (getState() != NodeState::Compiled) {
+    throw exceptions::NodeStateMachineError(
+        "FullyConnectedNode must be in a compiled state to call "
+        "setSparsity");
+  }
+  _layer->setSparsity(sparsity);
+  return shared_from_this();
+}
+
+float* FullyConnectedNode::getWeightsPtr() {
+  if (getState() != NodeState::PreparedForBatchProcessing &&
+      getState() != NodeState::Compiled) {
+    throw exceptions::NodeStateMachineError(
+        "FullyConnectedNode must be in a compiled state to call "
+        "getWeightsPtr.");
+  }
+  return _layer->getWeightsPtr();
+}
+
+float* FullyConnectedNode::getBiasesPtr() {
+  if (getState() != NodeState::PreparedForBatchProcessing &&
+      getState() != NodeState::Compiled) {
+    throw exceptions::NodeStateMachineError(
+        "FullyConnectedNode must be in a compiled state to call "
+        "getBiasesPtr.");
+  }
+  return _layer->getBiasesPtr();
+}
+
+float* FullyConnectedNode::getWeightGradientsPtr() {
+  if (getState() != NodeState::PreparedForBatchProcessing &&
+      getState() != NodeState::Compiled) {
+    throw exceptions::NodeStateMachineError(
+        "FullyConnectedNode must be in a compiled state to call "
+        "getWeightGradientsPtr.");
+  }
+  return _layer->getWeightGradientsPtr();
+}
+
+float* FullyConnectedNode::getBiasGradientsPtr() {
+  if (getState() != NodeState::PreparedForBatchProcessing &&
+      getState() != NodeState::Compiled) {
+    throw exceptions::NodeStateMachineError(
+        "FullyConnectedNode must be in a compiled state to call "
+        "getBiasGradientsPtr.");
+  }
+  return _layer->getBiasGradientsPtr();
+}
+
+void FullyConnectedNode::disableSparseParameterUpdates() {
+  if (getState() != NodeState::Compiled &&
+      getState() != NodeState::PreparedForBatchProcessing) {
+    throw exceptions::NodeStateMachineError(
+        "Cannot call disable_sparse_parameter_updates until the model "
+        "containing the node is compiled.");
+  }
+  _layer->disableSparseParameterUpdates();
+}
+
+void FullyConnectedNode::compileImpl() {
+  assert(_config.has_value());
+  _layer = std::make_shared<FullyConnectedLayer>(_config.value(),
+                                                 _predecessor->outputDim());
+  _config = std::nullopt;
+}
+
+void FullyConnectedNode::prepareForBatchProcessingImpl(uint32_t batch_size,
+                                                       bool use_sparsity) {
+  // TODO(Nicholas): rename createBatchState
+  _outputs =
+      _layer->createBatchState(batch_size, /* use_sparsity=*/use_sparsity);
+}
+
+void FullyConnectedNode::forwardImpl(uint32_t vec_index,
+                                     const BoltVector* labels) {
+  _layer->forward(_predecessor->getOutputVector(vec_index),
+                  this->getOutputVectorImpl(vec_index), labels);
+}
+
+void FullyConnectedNode::backpropagateImpl(uint32_t vec_index) {
+  // We are checking whether predecessor has gradients or not rather than
+  // its an input ot not because,this way will be helpful to calculate
+  // gradients for input in getInputGradientsSingle.
+  if (!_predecessor->getOutputVector(vec_index).gradients) {
+    _layer->backpropagateInputLayer(_predecessor->getOutputVector(vec_index),
+                                    this->getOutputVectorImpl(vec_index));
+  } else {
+    _layer->backpropagate(_predecessor->getOutputVector(vec_index),
+                          this->getOutputVectorImpl(vec_index));
+  }
+}
+
+void FullyConnectedNode::updateParametersImpl(float learning_rate,
+                                              uint32_t batch_cnt) {
+  // TODO(Nicholas): Abstract away these constants
+  _layer->updateParameters(learning_rate, batch_cnt, BETA1, BETA2, EPS);
+}
+
+void FullyConnectedNode::summarizeImpl(std::stringstream& summary,
+                                       bool detailed) const {
+  summary << _predecessor->name() << " -> " << name() << " (FullyConnected): ";
+  _layer->buildLayerSummary(summary, detailed);
+}
+
+Node::NodeState FullyConnectedNode::getState() const {
+  if (_predecessor == nullptr && _layer == nullptr && !_outputs.has_value()) {
+    return NodeState::Constructed;
+  }
+  if (_predecessor != nullptr && _layer == nullptr && !_outputs.has_value()) {
+    return NodeState::PredecessorsSet;
+  }
+  if (_predecessor != nullptr && _layer != nullptr && !_outputs.has_value()) {
+    return NodeState::Compiled;
+  }
+  if (_predecessor != nullptr && _layer != nullptr && _outputs.has_value()) {
+    return NodeState::PreparedForBatchProcessing;
+  }
+  throw exceptions::NodeStateMachineError(
+      "FullyConnectedNode is in an invalid internal state");
+}
+
+}  // namespace thirdai::bolt
--- bolt/src/graph/nodes/FullyConnected.h
+++ bolt/src/graph/nodes/FullyConnected.h
@@ -27,24 +27,14 @@ class FullyConnectedNode final
   friend class SwitchNode;
 
  private:
-  FullyConnectedNode(uint64_t dim, const std::string& activation)
-      : _layer(nullptr),
-        _config(FullyConnectedLayerConfig(dim, activation)),
-        _predecessor(nullptr) {}
+  FullyConnectedNode(uint64_t dim, const std::string& activation);
 
   FullyConnectedNode(uint64_t dim, float sparsity,
-                     const std::string& activation)
-      : _layer(nullptr),
-        _config(FullyConnectedLayerConfig(dim, sparsity, activation)),
-        _predecessor(nullptr) {}
+                     const std::string& activation);
 
   FullyConnectedNode(uint64_t dim, float sparsity,
                      const std::string& activation,
-                     SamplingConfigPtr sampling_config)
-      : _layer(nullptr),
-        _config(FullyConnectedLayerConfig(dim, sparsity, activation,
-                                          std::move(sampling_config))),
-        _predecessor(nullptr) {}
+                     SamplingConfigPtr sampling_config);
 
  public:
   static std::shared_ptr<FullyConnectedNode> makeDense(
@@ -74,162 +64,36 @@ class FullyConnectedNode final
     return make(dim, sparsity, activation, sampling_config);
   }
 
-  std::shared_ptr<FullyConnectedNode> addPredecessor(NodePtr node) {
-    if (getState() != NodeState::Constructed) {
-      throw exceptions::NodeStateMachineError(
-          "FullyConnectedNode expected to have exactly one predecessor, and "
-          "addPredecessor cannot be called twice.");
-    }
-    _predecessor = std::move(node);
+  std::shared_ptr<FullyConnectedNode> addPredecessor(NodePtr node);
 
-    return shared_from_this();
-  }
-
-  uint32_t outputDim() const final {
-    NodeState node_state = getState();
-    if (node_state == NodeState::Constructed ||
-        node_state == NodeState::PredecessorsSet) {
-      return _config->getDim();
-    }
-    return _layer->getDim();
-  }
+  uint32_t outputDim() const final;
 
   bool isInputNode() const final { return false; }
 
   void initOptimizer() final { _layer->initOptimizer(); }
 
-  ActivationFunction getActivationFunction() const {
-    NodeState node_state = getState();
-    if (node_state == NodeState::Constructed ||
-        node_state == NodeState::PredecessorsSet) {
-      return _config->getActFunc();
-    }
-    return _layer->getActivationFunction();
-  }
+  ActivationFunction getActivationFunction() const;
 
-  void saveParameters(const std::string& filename) const {
-    std::ofstream filestream =
-        dataset::SafeFileIO::ofstream(filename, std::ios::binary);
-    cereal::BinaryOutputArchive oarchive(filestream);
-    oarchive(*_layer);
-  }
+  void saveParameters(const std::string& filename) const;
 
-  void loadParameters(const std::string& filename) {
-    std::ifstream filestream =
-        dataset::SafeFileIO::ifstream(filename, std::ios::binary);
-    cereal::BinaryInputArchive iarchive(filestream);
-    auto loaded_parameters = std::make_shared<FullyConnectedLayer>();
-    iarchive(*loaded_parameters);
+  void loadParameters(const std::string& filename);
 
-    if (loaded_parameters->getDim() != _layer->getDim()) {
-      std::stringstream error_msg;
-      error_msg << "Cannot load parameters from FullyConnected layer with dim="
-                << loaded_parameters->getDim()
-                << "into FullyConnected layer with dim=" << _layer->getDim()
-                << ".";
-      throw std::logic_error(error_msg.str());
-    }
-    if (loaded_parameters->getInputDim() != _layer->getInputDim()) {
-      std::stringstream error_msg;
-      error_msg
-          << "Cannot load parameters from FullyConnected layer with input_dim="
-          << loaded_parameters->getInputDim()
-          << "into FullyConnected layer with input_dim="
-          << _layer->getInputDim() << ".";
-      throw std::logic_error(error_msg.str());
-    }
+  float getSparsity();
 
-    if (loaded_parameters->getActivationFunction() !=
-        _layer->getActivationFunction()) {
-      std::stringstream error_msg;
-      error_msg << "Cannot load parameters from FullyConnected layer with "
-                   "activation ="
-                << activationFunctionToStr(
-                       loaded_parameters->getActivationFunction())
-                << "into FullyConnected layer with activation="
-                << activationFunctionToStr(_layer->getActivationFunction())
-                << ".";
-      throw std::logic_error(error_msg.str());
-    }
+  std::shared_ptr<FullyConnectedNode> setSparsity(float sparsity);
 
-    _layer = loaded_parameters;
-  }
+  float* getWeightsPtr();
 
-  float getSparsity() {
-    NodeState node_state = getState();
-    if (node_state == NodeState::Constructed ||
-        node_state == NodeState::PredecessorsSet) {
-      return _config->getSparsity();
-    }
-    return _layer->getSparsity();
-  }
+  float* getBiasesPtr();
 
-  std::shared_ptr<FullyConnectedNode> setSparsity(float sparsity) {
-    if (getState() != NodeState::Compiled) {
-      throw exceptions::NodeStateMachineError(
-          "FullyConnectedNode must be in a compiled state to call setSparsity");
-    }
-    _layer->setSparsity(sparsity);
-    return shared_from_this();
-  }
+  float* getWeightGradientsPtr();
 
-  float* getWeightsPtr() {
-    if (getState() != NodeState::PreparedForBatchProcessing &&
-        getState() != NodeState::Compiled) {
-      throw exceptions::NodeStateMachineError(
-          "FullyConnectedNode must be in a compiled state to call "
-          "getWeightsPtr.");
-    }
-    return _layer->getWeightsPtr();
-  }
+  float* getBiasGradientsPtr();
 
-  float* getBiasesPtr() {
-    if (getState() != NodeState::PreparedForBatchProcessing &&
-        getState() != NodeState::Compiled) {
-      throw exceptions::NodeStateMachineError(
-          "FullyConnectedNode must be in a compiled state to call "
-          "getBiasesPtr.");
-    }
-    return _layer->getBiasesPtr();
-  }
-
-  float* getWeightGradientsPtr() {
-    if (getState() != NodeState::PreparedForBatchProcessing &&
-        getState() != NodeState::Compiled) {
-      throw exceptions::NodeStateMachineError(
-          "FullyConnectedNode must be in a compiled state to call "
-          "getWeightGradientsPtr.");
-    }
-    return _layer->getWeightGradientsPtr();
-  }
-
-  float* getBiasGradientsPtr() {
-    if (getState() != NodeState::PreparedForBatchProcessing &&
-        getState() != NodeState::Compiled) {
-      throw exceptions::NodeStateMachineError(
-          "FullyConnectedNode must be in a compiled state to call "
-          "getBiasGradientsPtr.");
-    }
-    return _layer->getBiasGradientsPtr();
-  }
-
-  void disableSparseParameterUpdates() final {
-    if (getState() != NodeState::Compiled &&
-        getState() != NodeState::PreparedForBatchProcessing) {
-      throw exceptions::NodeStateMachineError(
-          "Cannot call disable_sparse_parameter_updates until the model "
-          "containing the node is compiled.");
-    }
-    _layer->disableSparseParameterUpdates();
-  }
+  void disableSparseParameterUpdates() final;
 
  private:
-  void compileImpl() final {
-    assert(_config.has_value());
-    _layer = std::make_shared<FullyConnectedLayer>(_config.value(),
-                                                   _predecessor->outputDim());
-    _config = std::nullopt;
-  }
+  void compileImpl() final;
 
   std::vector<std::shared_ptr<FullyConnectedLayer>>
   getInternalFullyConnectedLayersImpl() const final {
@@ -237,36 +101,15 @@ class FullyConnectedNode final
   }
 
   void prepareForBatchProcessingImpl(uint32_t batch_size,
-                                     bool use_sparsity) final {
-    // TODO(Nicholas): rename createBatchState
-    _outputs =
-        _layer->createBatchState(batch_size, /* use_sparsity=*/use_sparsity);
-  }
+                                     bool use_sparsity) final;
 
   uint32_t numNonzerosInOutputImpl() const final { return (*_outputs)[0].len; }
 
-  void forwardImpl(uint32_t vec_index, const BoltVector* labels) final {
-    _layer->forward(_predecessor->getOutputVector(vec_index),
-                    this->getOutputVectorImpl(vec_index), labels);
-  }
+  void forwardImpl(uint32_t vec_index, const BoltVector* labels) final;
 
-  void backpropagateImpl(uint32_t vec_index) final {
-    // We are checking whether predecessor has gradients or not rather than its
-    // an input ot not because,this way will be helpful to calculate gradients
-    // for input in getInputGradientsSingle.
-    if (!_predecessor->getOutputVector(vec_index).gradients) {
-      _layer->backpropagateInputLayer(_predecessor->getOutputVector(vec_index),
-                                      this->getOutputVectorImpl(vec_index));
-    } else {
-      _layer->backpropagate(_predecessor->getOutputVector(vec_index),
-                            this->getOutputVectorImpl(vec_index));
-    }
-  }
+  void backpropagateImpl(uint32_t vec_index) final;
 
-  void updateParametersImpl(float learning_rate, uint32_t batch_cnt) final {
-    // TODO(Nicholas): Abstract away these constants
-    _layer->updateParameters(learning_rate, batch_cnt, BETA1, BETA2, EPS);
-  }
+  void updateParametersImpl(float learning_rate, uint32_t batch_cnt) final;
 
   BoltVector& getOutputVectorImpl(uint32_t vec_index) final {
     return (*_outputs)[vec_index];
@@ -278,30 +121,11 @@ class FullyConnectedNode final
     return {_predecessor};
   }
 
-  void summarizeImpl(std::stringstream& summary, bool detailed) const final {
-    summary << _predecessor->name() << " -> " << name()
-            << " (FullyConnected): ";
-    _layer->buildLayerSummary(summary, detailed);
-  }
+  void summarizeImpl(std::stringstream& summary, bool detailed) const final;
 
   std::string type() const final { return "fc"; }
 
-  NodeState getState() const final {
-    if (_predecessor == nullptr && _layer == nullptr && !_outputs.has_value()) {
-      return NodeState::Constructed;
-    }
-    if (_predecessor != nullptr && _layer == nullptr && !_outputs.has_value()) {
-      return NodeState::PredecessorsSet;
-    }
-    if (_predecessor != nullptr && _layer != nullptr && !_outputs.has_value()) {
-      return NodeState::Compiled;
-    }
-    if (_predecessor != nullptr && _layer != nullptr && _outputs.has_value()) {
-      return NodeState::PreparedForBatchProcessing;
-    }
-    throw exceptions::NodeStateMachineError(
-        "FullyConnectedNode is in an invalid internal state");
-  }
+  NodeState getState() const final;
 
   // Private constructor for cereal. Must create dummy config since no default
   // constructor exists for layer config.
