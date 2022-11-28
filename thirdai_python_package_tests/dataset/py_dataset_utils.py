b916e4ad80eb522946d818f67ec4338ba7d001d3
David Torres <97477655+dtr3@users.noreply.github.com>
2022-11-28 17:46:06 -0600

    Update thirdai_python_package_tests/dataset/py_dataset_utils.py
    Co-authored-by: Joshua Engels <josh@thirdai.com>

--- thirdai_python_package_tests/dataset/py_dataset_utils.py
+++ thirdai_python_package_tests/dataset/py_dataset_utils.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def create_csv_and_pqt_files(csv_filename, parquet_filename, content):
+def create_csv_and_pqt_files(csv_filename, parquet_filename, csv_string):
     with open(csv_filename, "w") as file:
         file.write(content)
     df = pd.read_csv(csv_filename)
