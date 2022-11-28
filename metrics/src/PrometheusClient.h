52c8cd332838026711ac18003c6a5c9072d48e7b
Josh Engels <josh@thirdai.com>
2022-11-28 13:10:34 -0600

    Moving to cc file

--- metrics/src/PrometheusClient.h
+++ metrics/src/PrometheusClient.h
@@ -1,15 +1,9 @@
 #pragma once
 
-#include <cryptopp/config_cxx.h>
-#include <deps/prometheus-cpp/3rdparty/civetweb/include/CivetServer.h>
 #include <prometheus/counter.h>
 #include <prometheus/exposer.h>
 #include <prometheus/histogram.h>
 #include <prometheus/registry.h>
-#include <utils/Logging.h>
-#include <iostream>
-#include <memory>
-#include <thread>
 
 namespace thirdai::metrics {
 
@@ -20,143 +14,35 @@ const inline uint32_t THIRDAI_DEFAULT_METRICS_PORT = 9929;
 
 class PrometheusMetricsClient {
  public:
-  static PrometheusMetricsClient startFromEnvVars() {
-    // TODO(Josh): Add metrics info to public docs
+  static PrometheusMetricsClient startFromEnvVars();
 
-    // I think it is safe to use std::getenv in static functions, see
-    // https://stackoverflow.com/questions/437279/is-it-safe-to-use-getenv-in-static-initializers-that-is-before-main
-    const char* env_dont_use_metrics = std::getenv("THIRDAI_DONT_USE_METRICS");
-    if (env_dont_use_metrics != NULL) {
-      return startNoop();
-    }
-
-    uint32_t port = THIRDAI_DEFAULT_METRICS_PORT;
-    const char* env_license_port = std::getenv("THIRDAI_METRICS_PORT");
-    if (env_license_port != NULL) {
-      port = std::stoi(env_license_port);
-    }
-
-    return start(port);
-  }
-
-  static PrometheusMetricsClient start(uint32_t port) {
-    std::shared_ptr<prometheus::Exposer> exposer;
-    try {
-      exposer = std::make_shared<prometheus::Exposer>(
-          /* bind_address = */ "127.0.0.1:" + std::to_string(port));
-    } catch (const CivetException& e) {
-      logging::error(
-          "Cannot start metrics server on port " + std::to_string(port) +
-          ", possibly there is already a metrics instance on this port. Please "
-          "choose a different port if you want to use metrics. Continuing "
-          "without metrics");
-      return PrometheusMetricsClient();
-    }
-    auto registry = std::make_shared<prometheus::Registry>();
-    exposer->RegisterCollectable(registry);
-    return PrometheusMetricsClient(exposer, registry);
-  }
+  static PrometheusMetricsClient start(uint32_t port);
 
   static PrometheusMetricsClient startNoop() {
     return PrometheusMetricsClient();
   }
 
   void track_predictions(double inference_time_seconds,
-                         uint32_t num_inferences) {
-    if (_registry == nullptr) {
-      return;
-    }
-    for (uint32_t i = 0; i < num_inferences; i++) {
-      _prediction_histogram->Observe(inference_time_seconds);
-    }
-  }
+                         uint32_t num_inferences);
 
   void track_explanations(double explain_time_seconds,
-                          uint32_t num_explanations) {
-    if (_registry == nullptr) {
-      return;
-    }
-    for (uint32_t i = 0; i < num_explanations; i++) {
-      _prediction_histogram->Observe(explain_time_seconds);
-    }
-  }
+                          uint32_t num_explanations);
 
-  void track_training(double training_time_seconds) {
-    if (_registry == nullptr) {
-      return;
-    }
-    _train_histogram->Observe(training_time_seconds);
-  }
+  void track_training(double training_time_seconds);
 
-  void track_evaluate(double evaluate_time_seconds) {
-    if (_registry == nullptr) {
-      return;
-    }
-    _train_histogram->Observe(evaluate_time_seconds);
-  }
+  void track_evaluate(double evaluate_time_seconds);
 
  private:
   PrometheusMetricsClient()
-      : _registry(nullptr),
-        _prediction_histogram(nullptr),
-        _explanation_histogram(nullptr),
-        _evaluation_histogram(nullptr),
-        _train_histogram(nullptr) {}
+    : _registry(nullptr),
+      _prediction_histogram(nullptr),
+      _explanation_histogram(nullptr),
+      _evaluation_histogram(nullptr),
+      _train_histogram(nullptr) {}
 
   explicit PrometheusMetricsClient(
       std::shared_ptr<prometheus::Exposer> exposer,
-      std::shared_ptr<prometheus::Registry> registry) {
-    _registry = std::move(registry);
-    _exposer = std::move(exposer);
-
-    // See https://prometheus.io/docs/practices/histograms/ for metric naming
-    // conventions
-
-    // Approximate geometric distribution with factor sqrt(10). Bins go from
-    // <0.1 ms to >=1 second. Used for inference and explanations.
-    prometheus::Histogram::BucketBoundaries fast_running_boundaries_seconds = {
-        0.0001, 0.0003, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1};
-    _prediction_histogram =
-        &prometheus::BuildHistogram()
-             .Name("thirdai_prediction_duration_seconds")
-             .Help(
-                 "Inference end to end latency. All inferences in a batch will "
-                 "have "
-                 "latency equal to the call to predict_batch.")
-             .Register(*_registry)
-             .Add(/* labels = */ {},
-                  /* buckets = */ fast_running_boundaries_seconds);
-    _explanation_histogram =
-        &prometheus::BuildHistogram()
-             .Name("thirdai_explanation_duration_seconds")
-             .Help(
-                 "Explanation end to end latency. All explanations in a "
-                 "batch "
-                 "will have "
-                 "latency equal to the call to explain_batch.")
-             .Register(*_registry)
-             .Add(/* labels = */ {},
-                  /* buckets = */ fast_running_boundaries_seconds);
-
-    // Approximate geometric distribution with factor sqrt(10). Bins go from
-    // <10 s to >=100,000 second (~30 hours)
-    prometheus::Histogram::BucketBoundaries slow_running_boundaries_seconds = {
-        10, 30, 100, 300, 1000, 3000, 10000, 30000, 100000};
-    _evaluation_histogram =
-        &prometheus::BuildHistogram()
-             .Name("thirdai_evaluation_duration_seconds")
-             .Help("Evaluation end to end latency.")
-             .Register(*_registry)
-             .Add(/* labels = */ {},
-                  /* buckets = */ slow_running_boundaries_seconds);
-    _train_histogram =
-        &prometheus::BuildHistogram()
-             .Name("thirdai_explanation_duration_seconds")
-             .Help("Training end to end latency.")
-             .Register(*_registry)
-             .Add(/* labels = */ {},
-                  /* buckets = */ slow_running_boundaries_seconds);
-  }
+      std::shared_ptr<prometheus::Registry> registry);
 
   // These variables are stored in this class to ensure the web server and
   // registry exist as long as this object exists.
