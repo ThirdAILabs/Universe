1dad157d1b015e00596aac66cb1c34efc3727b2c
Josh Engels <josh@thirdai.com>
2022-12-15 20:28:31 -0600

    Decreasing parallelism to 8

--- hashing/tests/MurmurHashTest.cc
+++ hashing/tests/MurmurHashTest.cc
@@ -9,31 +9,11 @@
 #include <random>
 #include <string>
 
-using std::chrono::duration_cast;
-using std::chrono::milliseconds;
-using std::chrono::system_clock;
 using thirdai::hashing::AvalancheTimedTestSuite;
 using thirdai::hashing::MurmurHash;
 
 uint64_t AvalancheTimedTestSuite::int_keys[num_keys];
 std::string AvalancheTimedTestSuite::str_keys[num_keys];
-
-/*
- * Tests speed of MurmurHash on integer and string keys.
- */
-TEST_F(AvalancheTimedTestSuite, MurmurHashTimeTest) {
-  auto start =
-      duration_cast<milliseconds>(system_clock::now().time_since_epoch())
-          .count();
-  for (uint32_t i = 0; i < num_keys; i++) {
-    MurmurHash(str_keys[i].c_str(),
-               static_cast<uint32_t>(strlen(str_keys[i].c_str())), seed);
-    MurmurHash(std::to_string(int_keys[i]).c_str(), sizeof(uint32_t), seed);
-  }
-  auto end = duration_cast<milliseconds>(system_clock::now().time_since_epoch())
-                 .count();
-  EXPECT_LE(end - start, 100);
-}
 
 /*
  * Tests Avalanche effect of MurmurHash on string keys.
