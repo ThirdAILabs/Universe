a7e37093bff691c7e8d8301a3f1612b882d57c85
Joshua Engels <josh@thirdai.com>
2022-12-21 10:42:17 -0600

    Moving keygen demo test to use small census dataset (#1085)
    * Moving keygen demo test to use small census dataset
    
    * Removing the created file

--- licensing/python_tests/test_demo_licensing.py
+++ licensing/python_tests/test_demo_licensing.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 import thirdai
 
@@ -9,19 +11,28 @@ from test_udt_simple import make_simple_trained_model
 
 pytestmark = [pytest.mark.release]
 
-# I created this key on Keygen, it should be good only for the census data
-CENSUS_KEY = "RRR9-XT7L-F7NH-TJYN-NCAM-9YTR-HUKL-PMAT"
+# I created this key on Keygen, it should be good only for the small census
+# dataset (the first 10 lines of the normal census training set)
+SMALL_CENSUS_KEY = "HKRW-4R97-KRLY-TMVY-79TW-3FHH-NRHR-HLYC"
 
 
-def test_census_key_works_on_census():
-    thirdai.activate(CENSUS_KEY)
+def test_census_key_works_on_small_census():
+    thirdai.activate(SMALL_CENSUS_KEY)
     train_filename, _, _ = thirdai.demos.download_census_income()
+    small_census_filename = "small_census.txt"
+    with open(small_census_filename, "w") as output:
+        with open(train_filename) as input:
+            for i, line in enumerate(input):
+                if i > 10:
+                    break
+                output.write(f"{line}")
     model = get_udt_census_income_model()
-    model.train(train_filename, epochs=1, learning_rate=0.01)
+    model.train(small_census_filename, epochs=1, learning_rate=0.01)
+    os.remove(small_census_filename)
 
 
 def test_census_key_fails_on_others():
-    thirdai.activate(CENSUS_KEY)
+    thirdai.activate(SMALL_CENSUS_KEY)
     with pytest.raises(
         RuntimeError,
         match="This dataset is not authorized under this license.",
