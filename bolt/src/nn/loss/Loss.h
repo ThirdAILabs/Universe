92324abb56f7f725e0b982b60f8d5cc8906e9863
Nicholas Meisburger <nmeisburger@gmail.com>
2022-12-28 15:16:28 -0800

    Loss function comments

--- bolt/src/nn/loss/Loss.h
+++ bolt/src/nn/loss/Loss.h
@@ -8,10 +8,25 @@ namespace thirdai::bolt::nn::loss {
 
 class Loss {
  public:
+  /**
+   * Computes the loss for the outputs used by this loss function for the given
+   * sample in the batch.
+   */
   virtual void computeGradients(uint32_t index_in_batch) = 0;
 
+  /**
+   * Returns which outputs in the computation graph have gradients computed by
+   * this loss function. This is used to ensure that all of the outputs in the
+   * computation graph have gradients computed for them.
+   */
   virtual std::vector<tensor::ActivationTensorPtr> outputsUsed() const = 0;
 
+  /**
+   * Returns the input tensor for the labels that the loss function is
+   * expecting. The labels passed into the computation graph are assigned to the
+   * inputs returned by the loss functions in the order that the loss functions
+   * are supplied to the computation graph.
+   */
   tensor::InputTensorPtr labels() const { return _labels; }
 
  protected:
