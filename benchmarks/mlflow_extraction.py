cc2da79164168cb3f03e4f30dd58ba046062a773
Vihan Lakshman <vihan@thirdai.com>
2022-12-30 21:41:02 +0000

    Address PR comments

--- benchmarks/mlflow_extraction.py
+++ benchmarks/mlflow_extraction.py
@@ -2,8 +2,7 @@ import mlflow
 import pandas as pd
 from mlflow.tracking import MlflowClient
 
-# TODO: Consolidate this constant with existing configs
-MLFLOW_URI = "http://ec2-184-73-150-35.compute-1.amazonaws.com"
+from utils import get_mlflow_uri
 
 
 def _process_mlflow_dataframe(mlflow_runs, num_runs, client):
@@ -20,16 +19,23 @@ def _process_mlflow_dataframe(mlflow_runs, num_runs, client):
     # Drop the epoch times column since it is no longer needed after calculating training time
     mlflow_runs.drop(columns=["metrics.epoch_times"], inplace=True)
 
+    # Convert the start time timestamp into a date to make it easier to read
     mlflow_runs["start_time"] = mlflow_runs.apply(lambda x: x.start_time.date(), axis=1)
 
     metric_columns = [col for col in mlflow_runs if col.startswith("metrics")]
     display_columns = ["start_time", "training_time"] + metric_columns
     df = mlflow_runs[display_columns]
+    df = df.rename(
+        columns={
+            col: col.split(".")[-1] for col in df.columns if col.startswith("metrics")
+        }
+    )
     return df
 
 
 def extract_mlflow_data(experiment_name, num_runs=5, markdown=False):
-    mlflow.set_tracking_uri(MLFLOW_URI)
+    mlflow_uri = get_mlflow_uri()
+    mlflow.set_tracking_uri(mlflow_uri)
     client = MlflowClient()
     exp_id = client.get_experiment_by_name(experiment_name).experiment_id
 
