984718094122b399f9af604063e931633a0a2778
Nicholas Meisburger <nmeisburger@gmail.com>
2022-12-29 17:03:42 -0800

    Delete mocks

--- bolt/tests/Mocks.h
+++
@@ -1,5 +0,0 @@
-#include <bolt/src/nn/loss/Loss.h>
-#include <bolt/src/nn/ops/Op.h>
-#include <bolt/src/nn/tensor/Tensor.h>
-
-namespace thirdai::bolt::nn::tests {}  // namespace thirdai::bolt::nn::tests
