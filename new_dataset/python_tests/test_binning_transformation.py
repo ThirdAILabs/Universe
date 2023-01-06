054db260a78b9a54fddf2c2f94425fdb7060ea8f
Yashwanth Adunukota <yash@thirdai.com>
2023-01-06 13:20:18 +0530

    test cases for explanation

--- new_dataset/python_tests/test_binning_transformation.py
+++ new_dataset/python_tests/test_binning_transformation.py
@@ -1,20 +1,14 @@
 import numpy as np
 import pytest
-from dataset_utils import sparse_bolt_dataset_to_numpy
+from dataset_utils import get_simple_dag_model, sparse_bolt_dataset_to_numpy
 from thirdai import data
 
 pytestmark = [pytest.mark.unit]
 
+n_rows = 100
 
-def test_binning_transformation():
-    n_rows = 100
 
-    column1_np = np.arange(n_rows, dtype=np.float32)
-    np.random.shuffle(column1_np)
-
-    column2_np = np.arange(start=10, stop=n_rows + 10, dtype=np.float32)
-    np.random.shuffle(column2_np)
-
+def get_binned_featurizer(column1_np, column2_np, prepare_for_backpropagate=False):
     column1 = data.columns.DenseFeatureColumn(column1_np)
     column2 = data.columns.DenseFeatureColumn(column2_np)
 
@@ -31,7 +25,7 @@ def test_binning_transformation():
             ),
             data.transformations.Binning(
                 input_column="column2",
-                output_column="column2",
+                output_column="out_column2",
                 inclusive_min=10,
                 exclusive_max=110,
                 num_bins=20,
@@ -39,10 +33,21 @@ def test_binning_transformation():
         ]
     )
 
-    columns = featurizer.featurize(columns)
+    columns = featurizer.featurize(columns, prepare_for_backpropagate)
+
+    return columns, featurizer
+
+
+def test_binning_transformation():
+    column1_np = np.arange(n_rows, dtype=np.float32)
+    np.random.shuffle(column1_np)
+
+    column2_np = np.arange(start=10, stop=n_rows + 10, dtype=np.float32)
+    np.random.shuffle(column2_np)
+    columns, _ = get_binned_featurizer(column1_np, column2_np)
 
     indices, values = sparse_bolt_dataset_to_numpy(
-        columns.convert_to_dataset(["column1_binned", "column2"], batch_size=29)
+        columns.convert_to_dataset(["column1_binned", "out_column2"], batch_size=29)
     )
 
     # We need to reshape the arrays so we can concatenate them on the correct axis.
@@ -61,3 +66,40 @@ def test_binning_transformation():
     assert np.array_equal(indices, expected_bins)
 
     assert np.array_equal(values, np.ones(shape=(n_rows, 2)))
+
+
+def test_binning_transformation_explanations():
+    column1_np = np.arange(n_rows, dtype=np.float32)
+    np.random.shuffle(column1_np)
+
+    column2_np = np.arange(start=10, stop=n_rows + 10, dtype=np.float32)
+    np.random.shuffle(column2_np)
+
+    columns, featurizer = get_binned_featurizer(column1_np, column2_np, True)
+
+    binned_dataset = columns.convert_to_dataset(
+        ["column1_binned", "out_column2"], batch_size=n_rows
+    )
+
+    model = get_simple_dag_model(
+        input_dim=100000, hidden_layer_dim=100, hidden_layer_sparsity=1, output_dim=151
+    )
+
+    indices, gradients = model.get_input_gradients_batch([binned_dataset[0]])
+
+    contribution_columns = columns.get_contribution_columns(
+        ["column1_binned", "out_column2"], gradients, indices
+    )
+
+    explanations = featurizer.explain(columns, contribution_columns)
+
+    for row in range(n_rows):
+        total_sum = abs(gradients[row][0]) + abs(gradients[row][1])
+        value1 = (gradients[row][0] / total_sum) * 100
+        value2 = (gradients[row][1] / total_sum) * 100
+        temp1 = explanations.getitem("column1_binned").get_row(row)
+
+        temp2 = explanations.getitem("out_column2").get_row(row)
+
+        assert abs(value1 - temp1[0].gradient) < 0.0001
+        assert abs(value2 - temp2[0].gradient) < 0.0001
--- new_dataset/python_tests/test_token_pairgram_transformation.py
+++ new_dataset/python_tests/test_token_pairgram_transformation.py
@@ -1,6 +1,7 @@
 import pytest
 from dataset_utils import (
     get_random_sentence_str_column,
+    get_simple_dag_model,
     sparse_bolt_dataset_to_numpy,
     verify_pairgrams_distribution,
 )
@@ -13,7 +14,7 @@ OUTPUT_RANGE = 1000
 NUM_WORDS = 5
 
 
-def create_random_sentence_pairgram_dataset():
+def create_random_sentence_pairgram_dataset(prepare_for_backpropagate=False):
     sentence_column = get_random_sentence_str_column(NUM_ROWS, NUM_WORDS)
 
     columns = data.ColumnMap({"sentence": sentence_column})
@@ -32,12 +33,42 @@ def create_random_sentence_pairgram_dataset():
             ),
         ]
     )
-    columns = featurizer.featurize(columns)
-    return columns.convert_to_dataset(["pairgrams"], batch_size=NUM_ROWS)
+    columns = featurizer.featurize(columns, prepare_for_backpropagate)
+
+    return columns, featurizer
 
 
 def test_sentence_pairgrams():
-    pairgram_dataset = sparse_bolt_dataset_to_numpy(
-        create_random_sentence_pairgram_dataset()
+    columns, _ = create_random_sentence_pairgram_dataset()
+    pairgram_bolt_dataset = columns.convert_to_dataset(
+        ["pairgrams"], batch_size=NUM_ROWS
     )
+    pairgram_dataset = sparse_bolt_dataset_to_numpy(pairgram_bolt_dataset)
     verify_pairgrams_distribution(pairgram_dataset, OUTPUT_RANGE, NUM_WORDS)
+
+
+def test_sentence_pairgrams_explanations():
+    columns, featurizer = create_random_sentence_pairgram_dataset(True)
+    model = get_simple_dag_model(
+        input_dim=100000, hidden_layer_dim=100, hidden_layer_sparsity=1, output_dim=151
+    )
+    pairgram_dataset = columns.convert_to_dataset(["pairgrams"], batch_size=NUM_ROWS)
+
+    indices, gradients = model.get_input_gradients_batch([pairgram_dataset[0]])
+
+    assert len(indices) == NUM_ROWS
+    assert len(gradients) == NUM_ROWS
+
+    contribution_columns = columns.get_contribution_columns(
+        ["pairgrams"], gradients, indices
+    )
+
+    explanations = featurizer.explain(columns, contribution_columns)
+
+    for row in range(NUM_ROWS):
+        temp = explanations.getitem("pairgrams").get_row(row)
+        total_sum = 0
+        for i in range(len(temp)):
+            total_sum += abs(temp[i].gradient)
+
+        assert total_sum > 99.99 and total_sum < 100.01
