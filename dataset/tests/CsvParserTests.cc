4d63f1d0b006d77c88f82f13b419cb0e1477f853
Benito Geordie <benito@thirdai.com>
2023-01-07 16:31:16 -0600

    New Csv parser

---
+++ dataset/tests/CsvParserTests.cc
@@ -0,0 +1,55 @@
+#include <gtest/gtest.h>
+#include <dataset/src/utils/CsvParser.h>
+#include <cstdint>
+#include <utility>
+
+namespace thirdai::dataset::tests {
+
+// We accept arguments by value so we can pass literals.
+// Makes it easier to write test cases, plus this isn't performance-critical.
+// NOLINTNEXTLINE
+void testCsvParser(std::string input_string, std::string delimiter,
+                   std::vector<std::string> expected_parsed_result) {
+  // TODO(Geordie): Implement
+  CsvRowParser parser(delimiter);
+  auto parsed_result = parser.parse(input_string);
+  ASSERT_EQ(parsed_result.size(), expected_parsed_result.size());
+  for (uint32_t i = 0; i < parsed_result.size(); i++) {
+    ASSERT_EQ(std::string(parsed_result[i]), expected_parsed_result[i]);
+  }
+}
+
+TEST(CsvParserTests, SupportsArbitraryDelimiters) {
+  testCsvParser("A,B", ",", {"A", "B"});
+  testCsvParser("A\tB", "\t", {"A", "B"});
+  testCsvParser("A::B", "::", {"A", "B"});
+}
+
+TEST(CsvParserTests, HandlesMultipleColumns) {
+  testCsvParser("A,B,C,D", ",", {"A", "B", "C", "D"});
+}
+
+TEST(CsvParserTests, HandlesEmptyColumns) {
+  testCsvParser("A,,C,D", ",", {"A", "", "C", "D"});
+}
+
+TEST(CsvParserTests, RemovesDoubleQuotesAroundStringColumn) {
+  testCsvParser("\"string column\"", ",", {"string column"});
+}
+
+TEST(CsvParserTests, IgnoresDelimiterInStringColumn) {
+  testCsvParser("\"I wish there was no delimiter, but there is.\"", ",",
+                {"I wish there was no delimiter, but there is."});
+}
+
+TEST(CsvParserTests, DoubleQuotesInStringColumnNotMistakenAsOuterQuotes) {
+  // CSV parsers distinguish between double quotes that wrap around a string
+  // and double quotes inside of the string by duplicating the inner quotes.
+  // Typically, the CSV parser would deduplicate the double quotes by
+  // writing a completely new string, but we don't do that and instead return a
+  // a substring for performance reasons.
+  testCsvParser(R"("""This is in quotes"", and this is not.")", ",",
+                {R"(""This is in quotes"", and this is not.)"});
+}
+
+}  // namespace thirdai::dataset::tests
