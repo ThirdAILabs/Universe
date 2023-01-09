e0f293d86886f0cd4f6b1d00273ee24f9813a2ee
Nicholas Meisburger <nmeisburger@gmail.com>
2023-01-09 09:03:05 -0600

    switch to runtime error

--- bolt/src/graph/Node.cc
+++ bolt/src/graph/Node.cc
@@ -46,7 +46,7 @@ void Node::prepareForBatchProcessing(uint32_t batch_size, bool use_sparsity) {
   prepareForBatchProcessingImpl(batch_size, use_sparsity);
 
   if (batch_size > 0 && getOutputVector(0).len == 0) {
-    throw std::invalid_argument(
+    throw std::runtime_error(
         "Node: '" + name() +
         "' allocated with output dimension=0. Please check that the the "
         "dimensions of nodes are nonzero, and that the dimension will be "
