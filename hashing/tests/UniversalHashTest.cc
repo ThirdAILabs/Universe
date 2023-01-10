6e80741b478aa8dc0e79d42d624b99b040e2077e
Josh Engels <josh@thirdai.com>
2023-01-10 11:16:52 -0600

    Fixing references to avalanch test

--- hashing/tests/UniversalHashTest.cc
+++ hashing/tests/UniversalHashTest.cc
@@ -8,6 +8,7 @@
 #include <random>
 #include <string>
 
+using thirdai::hashing::MurmurHash;
 using thirdai::hashing::UniversalHash;
 
 class UniversalHashTestSuite : public testing::Test {
@@ -69,7 +70,7 @@ UniversalHash universal_hash(time(nullptr));
  * Tests Avalanche effect of UniversalHash on string keys.
  * https://crypto.stackexchange.com/questions/40268/hash-functions-and-the-avalanche-effect
  */
-TEST_F(UniversalHashTestSuite, UniversalHashStringKeyAvalancheTest) {
+TEST_F(UniversalHashTestSuite, UniversalHashAvalancheTest) {
   // Allocate 64 bits for both hash outputs.
   uint32_t tabulation_output[2];
   uint32_t output_bits_counter[48][32] = {};
@@ -85,6 +86,42 @@ TEST_F(UniversalHashTestSuite, UniversalHashStringKeyAvalancheTest) {
       for (int k = 0; k < 32; k++) {
         output_bits_counter[j][k] +=
             ((tabulation_output[0] ^ tabulation_output[1]) >> k) & 1;
+      }
+    }
+  }
+
+  // Expect ~0.5 probability of output bit changes for each input bit flip over
+  // all 100000 keys.
+  for (auto& output_bits_counter_per_input_flip : output_bits_counter) {
+    for (auto& diff_count : output_bits_counter_per_input_flip) {
+      EXPECT_NEAR(diff_count * 1.0 / num_keys, 0.5, 0.1);
+    }
+  }
+}
+
+/*
+ * Tests Avalanche effect of MurmurHash on string keys.
+ * https://crypto.stackexchange.com/questions/40268/hash-functions-and-the-avalanche-effect
+ */
+TEST_F(UniversalHashTestSuite, MurmurHashAvalancheTest) {
+  // Allocate 64 bits for both hash outputs.
+  uint32_t murmurhash_output[2];
+  uint32_t output_bits_counter[48][32] = {};
+  for (auto& str_key : str_keys) {
+    murmurhash_output[0] = MurmurHash(
+        str_key.c_str(), static_cast<uint32_t>(strlen(str_key.c_str())), seed);
+    // Compute all possible 1 bit changes (48 bits in a string key input)
+    for (uint32_t j = 0; j < 48; j++) {
+      std::bitset<48> str_key_flipped_bitarray(convertToBitstring(str_key));
+      std::string str_key_flipped =
+          str_key_flipped_bitarray.flip(j).to_string();
+      murmurhash_output[1] = MurmurHash(
+          str_key_flipped.c_str(),
+          static_cast<uint32_t>(strlen(str_key_flipped.c_str())), seed);
+      // Compute statistics of input and 32 bit output of each bit flip.
+      for (int k = 0; k < 32; k++) {
+        output_bits_counter[j][k] +=
+            ((murmurhash_output[0] ^ murmurhash_output[1]) >> k) & 1;
       }
     }
   }
