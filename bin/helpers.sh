79ad6922903c7bd6ed9660ceac18a3ee0074f1de
Jerin Philip <jerin@thirdai.com>
2023-01-16 12:01:52 +0530

    Make file-lines end in \n
    https://stackoverflow.com/questions/729692/why-should-text-files-end-with-a-newline

---
+++ bin/helpers.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export USE_CCACHE=1 
+
+# ThirdAI builds from my local machine are license-check-free for quick
+# prototyping abilities.
+export THIRDAI_FEATURE_FLAGS=THIRDAI_EXPOSE_ALL
+export THIRDAI_DATASET_PATH="$HOME/data"
+export THIRDAI_BUILD_MODE=RelWithDebInfo
+
+
+BASEDIR=$(dirname "$0")
+THIRDAI_SOURCE_DIR="$BASEDIR/.."
+
+function thirdai-build {
+    set -x;
+    (cd $THIRDAI_SOURCE_DIR && (THIRDAI_BUILD_IDENTIFIER=$(git rev-parse --short HEAD) THIRDAI_BUILD_MODE=RelWithDebInfo python3 setup.py bdist_wheel 2>&1 | tee compile-verbose.log))
+    set +x;
+}
+
+function thirdai-install {
+    WHEEL_TAG="$(cat $THIRDAI_SOURCE_DIR/thirdai.version)+$(git -C $THIRDAI_SOURCE_DIR rev-parse --short HEAD)"
+    set -x;
+    python3 -m pip install --force-reinstall --upgrade \
+        $THIRDAI_SOURCE_DIR/dist/thirdai-${WHEEL_TAG}-cp310-cp310-macosx_12_0_arm64.whl
+    set +x;
+}
