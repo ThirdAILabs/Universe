1090fc274d656dd51aca42181ad2faa3b3fb2893
Josh Engels <josh@thirdai.com>
2023-01-23 15:20:48 -0600

    Adding udt generator test

--- licensing/python_tests/test_demo_licensing.py
+++ licensing/python_tests/test_demo_licensing.py
@@ -3,10 +3,14 @@ import os
 import pytest
 import thirdai
 
-# This line uses a hack where we can import functions from different test files
+# These lines use a hack where we can import functions from different test files
 # as long as this file is run from bin/python-format.sh. To run just this file,
 # run bin/python-test.sh -k "test_demo_licensing"
 from model_test_utils import get_udt_census_income_model
+from test_udt_generator import TRAIN_SOURCE_TARGET_FILE as udt_query_reformulation_file
+from test_udt_generator import grammar_correction_dataset
+from test_udt_generator import prepared_datasets as udt_query_reformulation_fixture
+from test_udt_generator import train_udt_query_reformulation_model
 from test_udt_simple import make_simple_trained_model
 
 pytestmark = [pytest.mark.release]
@@ -31,13 +35,22 @@ def test_census_key_works_on_small_census():
     os.remove(small_census_filename)
 
 
-def test_census_key_fails_on_others():
+def test_census_key_fails_on_udt():
     thirdai.licensing.activate(SMALL_CENSUS_KEY)
     with pytest.raises(
         RuntimeError,
         match="This dataset is not authorized under this license.",
     ):
         make_simple_trained_model()
+
+
+def test_census_key_fails_on_generator(udt_query_reformulation_fixture):
+    thirdai.licensing.activate(SMALL_CENSUS_KEY)
+    with pytest.raises(
+        RuntimeError,
+        match="This dataset is not authorized under this license.",
+    ):
+        train_udt_query_reformulation_model(udt_query_reformulation_file)
 
 
 # This fixture removes the stored access key after each test finishes, ensuring
