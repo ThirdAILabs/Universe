05b06210a19849150a9bba3788b40f10a18d2221
Benito Geordie <benito@thirdai.com>
2023-01-30 17:33:15 +0700

    udt default model comments

--- auto_ml/src/nn/UDTDefault.h
+++ auto_ml/src/nn/UDTDefault.h
@@ -6,9 +6,11 @@
 
 namespace thirdai::automl::nn {
 
+// Constructs a default bolt graph for UDT.
 bolt::BoltGraphPtr UDTDefault(const std::vector<uint32_t>& input_dims,
                               uint32_t output_dim, uint32_t hidden_layer_size);
 
+// Constructs a bolt graph for from a saved model config.
 bolt::BoltGraphPtr fromConfig(const std::vector<uint32_t>& input_dims,
                               uint32_t output_dim,
                               const std::string& saved_model_config);
