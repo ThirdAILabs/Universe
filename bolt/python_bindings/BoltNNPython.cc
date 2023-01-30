884ba9e1120ad2e5b7c43ee21a1185db0936a4db
Pratik Pranav <pratik@thirdai.com>
2023-01-30 17:27:27 +0530

    missed one change

--- bolt/python_bindings/BoltNNPython.cc
+++ bolt/python_bindings/BoltNNPython.cc
@@ -607,8 +607,7 @@ That's all for now, folks! More docs coming soon :)
       .def(
           "gradient_reference",
           [](DistributedTrainingWrapper& node) {
-            return GradientReference(
-                *node.getModel(/* should_save_optimizer = */ false).get());
+            return GradientReference(*node.getModel().get());
           },
           py::return_value_policy::reference_internal,
           "Returns gradient reference for Distributed Training Wrapper");
