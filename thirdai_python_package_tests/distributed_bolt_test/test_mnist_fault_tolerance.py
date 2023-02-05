86ea1b07782089ddf1bf7debae9378c8a3fc1a29
Pratik Pranav <pratik@thirdai.com>
2023-02-05 11:29:50 +0530

    removes if statement from assert in tests

--- thirdai_python_package_tests/distributed_bolt_test/test_mnist_fault_tolerance.py
+++ thirdai_python_package_tests/distributed_bolt_test/test_mnist_fault_tolerance.py
@@ -137,8 +137,7 @@ def train_distributed_bolt_fault_tolerance(request, ray_two_node_cluster_config)
     # if head is returned here, it connects the gcs to a new address,
     # which leads to timeout
     nodes_to_kill = get_other_nodes(mini_cluster, exclude_head=True)
-    if not nodes_to_kill:
-        assert False, "No node Found!"
+    assert nodes_to_kill, "No node Found!"
     node_to_kill = nodes_to_kill[0]
     mini_cluster.remove_node(node_to_kill)
     mini_cluster.add_node(num_cpus=1, wait=True)
