e65469ff227ce6643af360527afbc4079fdf318d
Jerin Philip <jerin@thirdai.com>
2023-02-07 22:53:18 +0530

    Add raw (for punct coverage) and tokenized samples

--- dataset/python_tests/test_vocab.py
+++ dataset/python_tests/test_vocab.py
@@ -9,7 +9,7 @@ BERT_TAG = "bert-base-uncased"
 BERT_VOCAB_PATH = "{}.vocab".format(BERT_TAG)
 BERT_VOCAB_URL = "https://huggingface.co/{}/resolve/main/vocab.txt".format(BERT_TAG)
 
-BERT_SAMPLES = [
+BERT_TOKENIZED_SAMPLES = [
     "popularity of thread ##ing has increased around 2003 , as the growth of the cpu frequency was replaced with the growth of number of cores , in turn requiring concurrency to utilize multiple cores .",
     "arkansas highway 59 business is a business route in gentry .",
     "before joining city of hope , rosen worked at northwestern university .",
@@ -20,6 +20,19 @@ BERT_SAMPLES = [
     "during the first years of japanese occupation following the first sino - japanese war , the political divisions of the island were changed frequently .",
     "october 27 – martin mu ##llen , 63 , right fielder in one game for the 1872 cleveland forest city ##s of the national association .",
     "mora ##ta de ta ##jun ##a is a municipality of the community of madrid , spain .",
+]
+
+BERT_RAW_SAMPLES = [
+    "popularity of threading has increased around 2003, as the growth of the cpu frequency was replaced with the growth of number of cores, in turn requiring concurrency to utilize multiple cores.",
+    "arkansas highway 59 business is a business route in gentry.",
+    "before joining city of hope, rosen worked at northwestern university.",
+    "the third section (8 km) was the bypass of szczuczyn opened in november 2015 as a single carriageway road.",
+    "in may 2021, the company left the swedish market due to a “unfair and discriminatory treatment” by the swedish tax agency.",
+    "since 2018, the winner of the bonaire kampionato , the top tier of football on the island qualifies for the caribbean club shield , a tertiary knockout tournament for developing caribbean football nations .",
+    "the tone of linlithgow's warnings to amery grew increasingly serious over the first half of 1943, as did amery's requests to the war cabinet; on 4august 1943 amery noted the spread of famine, and specifically stressed the effect upon calcutta and the potential effect on the morale of european troops.",
+    "during the first years of japanese occupation following the first sino-japanese war, the political divisions of the island were changed frequently.",
+    "october 27 – martin mullen, 63, right fielder in one game for the 1872 cleveland forest citys of the national association.",
+    "morata de tajuna is a municipality of the community of madrid, spain.",
 ]
 
 
@@ -39,7 +52,7 @@ def test_fixed_vocab():
         lines = vocab_file.read().splitlines()
         assert len(lines) == vocab.size()
 
-    for sample in BERT_SAMPLES:
+    for sample in BERT_TOKENIZED_SAMPLES:
         pieces = vocab.encode(sample)
 
         # Nothing maps to unknown, as the above is taken from BERT (thirdai)
@@ -66,21 +79,25 @@ def test_wordpiece_vocab():
         lines = vocab_file.read().splitlines()
         assert len(lines) == vocab.size()
 
-    for sample in BERT_SAMPLES:
-        sample = sample.replace(" ##", "")
-        pieces = vocab.encode(sample)
+    for raw, tokenized in zip(BERT_RAW_SAMPLES, BERT_TOKENIZED_SAMPLES):
+        # sample = sample.replace(" ##", "")
+        pieces = vocab.encode(raw)
 
         # Nothing maps to unknown, as the above is taken from BERT (thirdai)
         assert vocab.unk_id() not in pieces
-        tokens = sample.split()
+        tokens = tokenized.split()
+
         for piece, token in zip(pieces, tokens):
             # Assert reconstruction works
             if "THIRDAI_TEST_DEBUG" in os.environ:
                 print("{}: {}".format(token, piece), end=" ")
 
+            # Assert the piece matches the token from huggingface.
+            assert vocab.id(token) == piece
+
             # Assert sentence-level reconstruction
             decoded = vocab.decode(pieces)
-            assert decoded == sample
+            assert decoded == tokenized.replace(" ##", "")
 
             if "THIRDAI_TEST_DEBUG" in os.environ:
                 print()
