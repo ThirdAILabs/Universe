45d28a16d3e776c474c479b58af0db334d12e92d
Pratik Pranav <pratik@thirdai.com>
2023-02-09 16:23:37 +0530

    moves around positional argument

--- thirdai_python_package/_distributed_bolt/dataset_loaders.py
+++ thirdai_python_package/_distributed_bolt/dataset_loaders.py
@@ -72,7 +72,7 @@ class DistributedUDTDatasetLoader(DistributedDatasetLoader):
             self.dataset_finished = True
         else:
             load = self.generator.load_some(
-                num_batches=self.max_in_memory_batches, batch_size=self.batch_size
+                batch_size=self.batch_size, num_batches=self.max_in_memory_batches
             )
 
         return load
