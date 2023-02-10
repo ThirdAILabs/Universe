980e6a59d941164334ae39b60054fa1659477738
Josh Engels <josh@thirdai.com>
2023-02-10 14:27:33 -0600

    Starting matrix benchmarks

---
+++ test.py
@@ -0,0 +1,22 @@
+from thirdai import matrix
+import numpy as np
+import tensorflow as tf
+
+
+image = np.ones((3, 4, 4), dtype="float32")
+filters = np.ones((2, 3, 2, 2), dtype="float32")
+
+# result = matrix.eigen_2dconv(image, filters)
+
+result = matrix.eigen_2dconv_tf(image=image, filters=filters)
+
+print(result.shape, result)
+
+result = tf.nn.conv2d(
+    image,
+    filters,
+    strides = 1,
+    padding = "SAME"
+)
+
+print(result.shape, result)
