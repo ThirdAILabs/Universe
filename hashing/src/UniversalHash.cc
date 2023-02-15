a2f3b3cd3239e6cfac55a8b5ef4faa994d6c56fa
Nicholas Meisburger <43160219+nmeisburger@users.noreply.github.com>
2023-02-15 08:45:16 -0600

    Embedding layer optimization  (#1199)
    * Embedding chunk size
    
    * fix experiment
    
    * Fix script
    
    * remove benchmarking script
    
    * rename chunk_size -> update_chunk_size
    
    * Add default update chunk size
    
    * Remove changes
    
    * Comment
    
    * Resolve comments
    
    * Resolve comments

--- hashing/src/UniversalHash.cc
+++ hashing/src/UniversalHash.cc
@@ -10,25 +10,25 @@ UniversalHash::UniversalHash(uint32_t seed) : _seed(seed) {
   // We can decide to pass in a generator instead, if needed.
   std::mt19937_64 gen(_seed);
   std::uniform_int_distribution<uint64_t> dis;
-  for (auto& i : T) {
+  for (auto& i : _table) {
     for (auto& j : i) {
       j = dis(gen);
     }
   }
 }
 
-uint32_t UniversalHash::gethash(const std::string& key) const {
-  uint32_t res = 0;
+uint64_t UniversalHash::gethash(const std::string& key) const {
+  uint64_t res = 0;
   for (uint8_t ch : key) {
-    res ^= T[ch & 7][ch];
+    res ^= _table[ch & 7][ch];
   }
   return res;
 }
 
-uint32_t UniversalHash::gethash(uint64_t key) const {
-  uint32_t res = 0;
+uint64_t UniversalHash::gethash(uint64_t key) const {
+  uint64_t res = 0;
   for (uint32_t i = 0; i < sizeof(key); i++) {
-    res ^= T[i][static_cast<unsigned char>(key >> (i << 3))];
+    res ^= _table[i][static_cast<unsigned char>(key >> (i << 3))];
   }
   return res;
 }
