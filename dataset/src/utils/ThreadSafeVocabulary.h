e2ca9ad50c5127e5f260c29fd72cfe0f30621427
Benito Geordie <benito@thirdai.com>
2023-03-01 06:31:05 -0600

    Investigate source of cpu utilization issue

--- dataset/src/utils/ThreadSafeVocabulary.h
+++ dataset/src/utils/ThreadSafeVocabulary.h
@@ -138,6 +138,7 @@ class ThreadSafeVocabulary {
 
  private:
   uint32_t getExistingUid(const std::string& string) {
+    std::cout << "yep" << std::endl;
     assert(_fixed);
     if (!_string_to_uid.count(string)) {
       std::stringstream error_ss;
