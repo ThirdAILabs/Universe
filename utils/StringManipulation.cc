99ef6193bf649ce338247760337cfc239b717354
Benito Geordie <benito@thirdai.com>
2023-03-02 22:55:54 -0600

    text::join

--- utils/StringManipulation.cc
+++ utils/StringManipulation.cc
@@ -39,4 +39,18 @@ std::vector<std::string_view> split(std::string_view sentence, char delimiter) {
   return words;
 }
 
+std::string join(const std::vector<std::string>& strings,
+                 const std::string& delimiter) {
+  if (strings.empty()) {
+    return "";
+  }
+
+  std::stringstream joined_stream;
+  joined_stream << strings.front();
+  for (uint32_t i = 1; i < strings.size(); i++) {
+    joined_stream << delimiter << strings[i];
+  }
+  return joined_stream.str();
+}
+
 }  // namespace thirdai::text
