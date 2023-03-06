16fcb6567d14783acbc2ec806fbb2cc4ef282bc0
David Torres <david@thirdai.com>
2023-03-06 15:30:36 -0600

    new files

---
+++ auto_ml/src/udt/backends/UDTMachClassifier.cc
@@ -0,0 +1,43 @@
+#include "UDTMachClassifier.h"
+#include <bolt/src/graph/Graph.h>
+#include <auto_ml/src/config/ArgumentMap.h>
+#include <auto_ml/src/udt/UDTBackend.h>
+
+namespace thirdai::automl::udt {
+
+UDTMachClassifier::UDTMachClassifier(
+    uint32_t n_target_classes, uint32_t input_dim,
+    const std::optional<std::string>& model_config,
+    const config::ArgumentMap& user_args) {}
+
+void UDTMachClassifier::train(
+    const dataset::DataSourcePtr& data, float learning_rate, uint32_t epochs,
+    const std::optional<Validation>& validation,
+    std::optional<size_t> batch_size,
+    std::optional<size_t> max_in_memory_batches,
+    const std::vector<std::string>& metrics,
+    const std::vector<std::shared_ptr<bolt::Callback>>& callbacks, bool verbose,
+    std::optional<uint32_t> logging_interval) final {}
+
+py::object UDTMachClassifier::evaluate(const dataset::DataSourcePtr& data,
+                                       const std::vector<std::string>& metrics,
+                                       bool sparse_inference,
+                                       bool return_predicted_class,
+                                       bool verbose,
+                                       bool return_metrics) final {}
+
+py::object UDTMachClassifier::predict(const MapInput& sample,
+                                      bool sparse_inference,
+                                      bool return_predicted_class) final {}
+
+py::object UDTMachClassifier::predictBatch(const MapInputBatch& sample,
+                                           bool sparse_inference,
+                                           bool return_predicted_class) final {}
+
+template <class Archive>
+void serialize(Archive& archive);
+
+bolt::BoltGraphPtr _model;
+bool _freeze_hash_tables;
+
+}  // namespace thirdai::automl::udt
