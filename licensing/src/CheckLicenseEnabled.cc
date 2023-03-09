2049ea0acd4ea9b19f6504b169452ac8f36a0b76
Pratik Pranav <pratkpranav>
2023-03-09 18:16:33 +0000

    changes

--- licensing/src/CheckLicenseEnabled.cc
+++ licensing/src/CheckLicenseEnabled.cc
@@ -43,7 +43,7 @@ TrainPermissionsToken::TrainPermissionsToken(
   // making resourceName() point to a valid file, while the actual nextLine call
   // returns lines from some other file they want to train on.
   if (!dynamic_cast<dataset::FileDataSource*>(training_source.get()) &&
-      !dynamic_cast<automl::cold_start::ColdStartDataSource*>(
+      !dynamic_cast<dataset::cold_start::ColdStartDataSource*>(
           training_source.get())) {
     throw exceptions::LicenseCheckException(
         "Can only train on file data sources with a demo license");
