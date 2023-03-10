c2a482afac513417b2f81a6b7980dc2aeb15f04f
David Torres <david@thirdai.com>
2023-03-10 14:39:46 -0500

    fix test

--- bolt/python_tests/test_callbacks.py
+++ bolt/python_tests/test_callbacks.py
@@ -191,5 +191,5 @@ def test_overfitting_callback_invalid_metric_name():
 def test_overfitting_callback_metric_not_passed():
     callback = bolt.callbacks.Overfitting("categorical_accuracy", 0.97)
 
-    with pytest.raises(ValueError, match=r""):
+    with pytest.raises(ValueError, match=r"Metric: categorical_accuracy not found in training metrics."):
         train_simple_udt_with_callback(callback, [])
