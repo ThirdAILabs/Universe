6a070aefd99b41023ebe8cd3620562460687b151
Nicholas Meisburger <nmeisburger@gmail.com>
2023-03-20 15:21:04 -0500

    add link to comment

--- bolt/src/nn/ops/Embedding.cc
+++ bolt/src/nn/ops/Embedding.cc
@@ -114,7 +114,8 @@ namespace cereal {
 /**
  * This is because the Op base class only uses a serialize function, whereas
  * this Op uses a load/save pair. This tells cereal to use the load save pair
- * instead of the serialize method of the parent class.
+ * instead of the serialize method of the parent class. See docs here:
+ * https://uscilab.github.io/cereal/serialization_functions.html#inheritance
  */
 template <class Archive>
 struct specialize<Archive, thirdai::bolt::nn::ops::Embedding,
