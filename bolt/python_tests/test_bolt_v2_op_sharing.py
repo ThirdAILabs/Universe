a775a72e7eeb8410bc08cd88fc2c4c15752cc0cf
Nicholas Meisburger <nmeisburger@gmail.com>
2023-03-24 14:04:09 -0500

    python format

--- bolt/python_tests/test_bolt_v2_op_sharing.py
+++ bolt/python_tests/test_bolt_v2_op_sharing.py
@@ -1,6 +1,7 @@
-from thirdai import bolt_v2 as bolt
-from utils import gen_numpy_training_data
 import pytest
+from thirdai import bolt_v2 as bolt
+
+from utils import gen_numpy_training_data
 
 N_CLASSES = 100
 
