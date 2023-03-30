46b4cc3c68f960e0e9cfbf123bdfbdd4ed731a21
Pratik Pranav <pratik@thirdai.com>
2023-03-30 12:52:12 +0000

    formatting

--- thirdai_python_package/_distributed_bolt/dataset_loaders.py
+++ thirdai_python_package/_distributed_bolt/dataset_loaders.py
@@ -66,11 +66,13 @@ class DistributedUDTDatasetLoader(DistributedDatasetLoader):
         self.shuffle_config = shuffle_config
 
     def load(self, shuffle: bool = True):
-        shuffle_config_arg = {'shuffle_config': self.shuffle_config} if self.shuffle_config is not None else {}
+        shuffle_config_arg = (
+            {"shuffle_config": self.shuffle_config}
+            if self.shuffle_config is not None
+            else {}
+        )
         self.generator = self.data_processor.get_dataset_loader(
-            _create_data_source(self.train_file),
-            training=shuffle,
-            **shuffle_config_arg
+            _create_data_source(self.train_file), training=shuffle, **shuffle_config_arg
         )
 
     def next(self):
@@ -126,7 +128,11 @@ class DistributedColdStartDatasetLoader(DistributedUDTDatasetLoader):
                 self.cold_start_meta_data,
             )
         )
-        shuffle_config_arg = {'shuffle_config': self.shuffle_config} if self.shuffle_config is not None else {}
+        shuffle_config_arg = (
+            {"shuffle_config": self.shuffle_config}
+            if self.shuffle_config is not None
+            else {}
+        )
         self.generator = self.data_processor.get_dataset_loader(
             cold_start_data_source, training=shuffle, **shuffle_config_arg
         )
