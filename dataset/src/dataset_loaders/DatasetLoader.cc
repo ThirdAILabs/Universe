622ab24bb80e5c63cddb56cc08764c36fe16e0e4
David Torres <david@thirdai.com>
2023-04-05 15:21:07 -0500

    comment

--- dataset/src/dataset_loaders/DatasetLoader.cc
+++ dataset/src/dataset_loaders/DatasetLoader.cc
@@ -47,7 +47,13 @@ std::vector<BoltDatasetPtr> DatasetLoader::loadAll(size_t batch_size,
 
 std::optional<std::vector<BoltDatasetPtr>> DatasetLoader::loadSome(
     size_t batch_size, size_t num_batches, bool verbose) {
-  if (_featurizer->expectsHeader() && _header.has_value()) {
+  // We checked _header against _featurizer->expectsHeader() in the constructor
+  // so this check again is safe.
+  if (_featurizer->expectsHeader()) {
+    // We call _featurizer->processHeader here in case we want to use the same
+    // featurizer for two different data loaders. This way the featurizer will
+    // internalize its own column number map based on the header of the data
+    // loader at load time instead of construction time.
     _featurizer->processHeader(*_header);
   }
 #if THIRDAI_EXPOSE_ALL
