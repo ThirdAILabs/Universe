a3193726274eee7144e2b693f0105e8c9c4094da
David Torres <david@thirdai.com>
2023-04-05 16:04:27 -0500

    update comment

--- dataset/src/dataset_loaders/DatasetLoader.cc
+++ dataset/src/dataset_loaders/DatasetLoader.cc
@@ -48,9 +48,18 @@ std::vector<BoltDatasetPtr> DatasetLoader::loadAll(size_t batch_size,
 std::optional<std::vector<BoltDatasetPtr>> DatasetLoader::loadSome(
     size_t batch_size, size_t num_batches, bool verbose) {
   if (_header) {
-    // We call _featurizer->processHeader here in case we want to use the same
-    // featurizer for two different data loaders. This guarantees that the
-    // featurizer uses the correct column number map for each data loader.
+    /**
+      We call _featurizer->processHeader here in case we want to use the same
+      featurizer for two different data loaders. This guarantees that the
+      featurizer uses the correct column number map for each data loader.
+
+      If a second dataset loader is created using the same featurizer as first
+      dataset loader, but the data its featurizing has a different ordering of
+      columns, then the column number map will be updated to reflect the second
+      dataset. This will cause an issue when the first dataset loader attempt
+      to load more data. Always updating the featurizer with the current
+      dataset before loading solves this issue.
+    */
     _featurizer->processHeader(*_header);
   }
 #if THIRDAI_EXPOSE_ALL
