e37cf15d6d6feef77d0e532f7132c2e244c2f5d8
Kartik Sarangmath <kartik@thirdai.com>
2023-04-18 22:57:23 +0000

    test bolt benchmark notifications

--- benchmarks-v2/slack_benchmark_results.py
+++ benchmarks-v2/slack_benchmark_results.py
@@ -9,11 +9,7 @@ import requests
 from mlflow.tracking import MlflowClient
 
 from .runners.runner_map import runner_map
-
-# Posts to daily_udt_benchmarks channel
-SLACK_WEBHOOK = (
-    "https://hooks.slack.com/services/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"
-)
+from .utils import get_configs
 
 
 def parse_arguments():
@@ -21,6 +17,7 @@ def parse_arguments():
     parser.add_argument(
         "--runner",
         type=str,
+        nargs="+",
         required=True,
         choices=["udt", "bolt_fc", "dlrm"],
         help="The runner to retrieve benchmark results for.",
@@ -47,6 +44,12 @@ def parse_arguments():
         default=3,
         help="How many runs to display in slack message",
     )
+    parser.add_argument(
+        "--slack_webhook",
+        type=str,
+        default="",
+        help="Slack channel endpoint to ping",
+    )
     return parser.parse_args()
 
 
@@ -65,7 +68,8 @@ def process_mlflow_dataframe(mlflow_runs, num_runs, client):
     mlflow_runs.drop(columns=["metrics.epoch_times"], inplace=True)
 
     # Drop learning rate column since we don't need to display it as a recorded metric in Slack
-    mlflow_runs.drop(columns=["metrics.learning_rate"], inplace=True)
+
+    mlflow_runs.drop(columns=["metrics.learning_rate"], inplace=True, errors='ignore')
 
     # Convert the start time timestamp into a date to make it easier to read
     mlflow_runs["start_time"] = mlflow_runs.apply(lambda x: x.start_time.date(), axis=1)
@@ -99,42 +103,34 @@ def extract_mlflow_data(experiment_name, num_runs=1, markdown=False):
 if __name__ == "__main__":
     args = parse_arguments()
 
-    runner = runner_map[args.runner.lower()]
+    for runner_name in args.runner:
 
-    config_re = re.compile(args.config)
-    configs = list(
-        filter(
-            lambda config: config_re.match(config.config_name),
-            runner.config_type.__subclasses__(),
-        )
-    )
-    if len(configs) == 0:
-        raise ValueError(
-            f"Could match regular expression '{args.config}' to any configs."
-        )
+        runner = runner_map[runner_name.lower()]
 
-    slack_payload_list = [""]
-    slack_payload_idx = 0
-    for config in configs:
-        exp_name = (
-            f"{config.config_name}_benchmark"
-            if args.official_benchmark
-            else config.config_name
-        )
-        df_md = extract_mlflow_data(exp_name, num_runs=args.num_runs, markdown=True)
+        configs = get_configs(runner=runner, config_regex=args.config)
 
-        slack_payload_text = f"*{exp_name}* ```{df_md}``` \n"
-        line_length = len(slack_payload_text.split("\n")[0].split("```")[1])
+        slack_payload_list = [""]
+        slack_payload_idx = 0
+        for config in configs:
+            exp_name = (
+                f"{config.config_name}_benchmark"
+                if args.official_benchmark
+                else config.config_name
+            )
+            df_md = extract_mlflow_data(exp_name, num_runs=args.num_runs, markdown=True)
 
-        # We limit each message to under 4000 characters
-        if (
-            len(slack_payload_list[slack_payload_idx]) + len(slack_payload_text)
-            >= 4000 - line_length
-        ):
-            slack_payload_list.append(slack_payload_text)
-            slack_payload_idx += 1
-        else:
-            slack_payload_list[slack_payload_idx] += slack_payload_text
+            slack_payload_text = f"*{exp_name}* ```{df_md}``` \n"
+            line_length = len(slack_payload_text.split("\n")[0].split("```")[1])
 
-    for payload in slack_payload_list:
-        requests.post(SLACK_WEBHOOK, json.dumps({"text": payload}))
+            # We limit each message to under 4000 characters
+            if (
+                len(slack_payload_list[slack_payload_idx]) + len(slack_payload_text)
+                >= 4000 - line_length
+            ):
+                slack_payload_list.append(slack_payload_text)
+                slack_payload_idx += 1
+            else:
+                slack_payload_list[slack_payload_idx] += slack_payload_text
+
+        for payload in slack_payload_list:
+            requests.post(args.slack_webhook, json.dumps({"text": payload}))
---
+++ benchmarks-v2/utils.py
@@ -0,0 +1,14 @@
+import re
+
+def get_configs(config_regex, runner):
+    config_re = re.compile(config_regex)
+    configs = [
+        config
+        for config in runner.config_type.__subclasses__()
+        if config.config_name is not None and config_re.match(config.config_name)
+    ]
+    if len(configs) == 0:
+        raise ValueError(
+            f"Couldn't match regular expression '{config_regex}' to any configs."
+        )
+    return configs
