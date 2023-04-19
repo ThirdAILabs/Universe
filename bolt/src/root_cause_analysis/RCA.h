db9073c6bde5ced2b4bbd750ee3f664d537ce35c
Nicholas Meisburger <nmeisburger@gmail.com>
2023-04-19 12:05:54 -0500

    Add comments

--- bolt/src/root_cause_analysis/RCA.h
+++ bolt/src/root_cause_analysis/RCA.h
@@ -6,14 +6,30 @@
 
 namespace thirdai::bolt::nn::rca {
 
+/**
+ * Stores the input gradients returned by RCA explainability methods. The
+ * indices are only used if the input is sparse.
+ */
 struct RCAGradients {
   std::optional<std::vector<uint32_t>> indices;
   std::vector<float> gradients;
 };
 
+/**
+ * Commutes the gradients w.r.t the inputs by assigning a label of 1.0 to the
+ * correct label and computing gradients. These input gradients then represent
+ * what changes in the input would make the activation of the predicted neuron
+ * closer to 1.0.
+ */
 RCAGradients explainPrediction(model::ModelPtr& model,
                                const tensor::TensorList& input_vec);
 
+/**
+ * Commutes the gradients w.r.t the inputs by assigning a label of 1.0 to the
+ * specified neuron and computing gradients. These input gradients then
+ * represent what changes in the input would make the activation of the given
+ * neuron closer to 1.0.
+ */
 RCAGradients explainNeuron(model::ModelPtr& model,
                            const tensor::TensorList& input_vec,
                            uint32_t neuron);
