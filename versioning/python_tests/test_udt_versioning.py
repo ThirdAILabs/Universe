54eae39a3fd676b0b5329a7a96062889a8ec2db9
Kartik Sarangmath <kartik@thirdai.com>
2023-04-24 13:00:46 -0500

    small udt model

--- versioning/python_tests/test_udt_versioning.py
+++ versioning/python_tests/test_udt_versioning.py
@@ -1,8 +1,9 @@
+import json
 import os
 from pathlib import Path
 
 import pytest
-from thirdai import bolt
+from thirdai import bolt, deployment
 
 SERIALIZED_CLASS_DIR = Path(__file__).resolve().parent / "serialized_classes"
 ERROR_STRING = r"UDT_BASE, but got version 0"
@@ -13,22 +14,53 @@ pytestmark = [pytest.mark.unit]
 def build_udt_model():
     # Dummy UDT model to test serialization
 
+    model_config = {
+        "inputs": ["input"],
+        "nodes": [
+            {
+                "name": "hidden",
+                "type": "fully_connected",
+                "dim": 10,
+                "sparsity": 0.1,
+                "activation": "relu",
+                "predecessor": "input",
+            },
+            {
+                "name": "output",
+                "type": "fully_connected",
+                "dim": {"param_name": "output_dim"},
+                "sparsity": 0.1,
+                "activation": "sigmoid",
+                "predecessor": "hidden",
+            },
+        ],
+        "output": "output",
+        "loss": "BinaryCrossEntropyLoss",
+    }
+    model_config_path = os.path.join(SERIALIZED_CLASS_DIR, "udt_model_config")
+    deployment.dump_config(
+        config=json.dumps(model_config),
+        filename=model_config_path,
+    )
+
     model = bolt.UniversalDeepTransformer(
         data_types={
-            "sample": bolt.types.text(),
-            "target": bolt.types.categorical(),
+            "input": bolt.types.text(),
+            "output": bolt.types.categorical(),
         },
-        target="target",
-        n_target_classes=10,
+        target="output",
+        n_target_classes=2,
+        model_config=model_config_path,
     )
 
     return model
 
 
 def test_save_load_udt_model():
-    model = build_udt_model()
     model_name = "udt_model"
     model_path = os.path.join(SERIALIZED_CLASS_DIR, model_name)
+
+    model = build_udt_model()
     model.save(model_path)
     bolt.UniversalDeepTransformer.load(model_path)
 
