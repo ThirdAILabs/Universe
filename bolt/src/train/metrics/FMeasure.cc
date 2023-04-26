23971c934c508267fb96be1cd99c827feb8b94bd
Nicholas Meisburger <nmeisburger@gmail.com>
2023-04-26 11:27:56 -0500

    Fix fmeasure test

--- bolt/src/train/metrics/FMeasure.cc
+++ bolt/src/train/metrics/FMeasure.cc
@@ -1,5 +1,6 @@
 #include "FMeasure.h"
 #include <bolt/src/train/metrics/Metric.h>
+#include <iostream>
 
 namespace thirdai::bolt::train::metrics {
 
@@ -15,15 +16,26 @@ void FMeasure::record(uint32_t index_in_batch) {
   const BoltVector& output = _outputs->tensor()->getVector(index_in_batch);
   const BoltVector& labels = _labels->tensor()->getVector(index_in_batch);
 
+  std::cerr << "OUTPUT: " << output << std::endl;
+  std::cerr << "LABELS: " << labels << std::endl;
+
   auto predictions = output.getThresholdedNeurons(
       /* activation_threshold = */ _threshold,
       /* return_at_least_one = */ true,
       /* max_count_to_return = */ std::numeric_limits<uint32_t>::max());
 
+  std::cerr << "PREDICTIONS:";
+  for (uint32_t p : predictions) {
+    std::cerr << " " << p;
+  }
+  std::cerr << std::endl;
+
   for (uint32_t pred : predictions) {
     if (labels.findActiveNeuronNoTemplate(pred).activation > 0) {
+      std::cerr << "TRUE POSITIVE: " << pred << std::endl;
       _true_positives++;
     } else {
+      std::cerr << "FALSE POSITIVE: " << pred << std::endl;
       _false_positives++;
     }
   }
@@ -31,9 +43,10 @@ void FMeasure::record(uint32_t index_in_batch) {
   for (uint32_t pos = 0; pos < labels.len; pos++) {
     uint32_t label_active_neuron =
         labels.isDense() ? pos : labels.active_neurons[pos];
-    if (labels.findActiveNeuronNoTemplate(label_active_neuron).activation > 0) {
+    if (labels.activations[pos] > 0) {
       if (std::find(predictions.begin(), predictions.end(),
                     label_active_neuron) == predictions.end()) {
+        std::cerr << "FALSE NEGATIVE: " << label_active_neuron << std::endl;
         _false_negatives++;
       }
     }
@@ -52,6 +65,8 @@ float FMeasure::value() const {
   float recall = static_cast<float>(_true_positives) /
                  (_true_positives + _false_negatives);
 
+  std::cerr << "PRECISION: " << prec << std::endl;
+  std::cerr << "RECALL: " << recall << std::endl;
   /*
     P = Precision
     R = Recall
