b1a9a860dda6686c7ea533720929441d8be140b6
Nicholas Meisburger <nmeisburger@gmail.com>
2023-04-27 12:42:10 -0500

    L1 norm

---
+++ bolt/src/nn/ops/L1Normalization.cc
@@ -0,0 +1,121 @@
+#include "L1Normalization.h"
+#include <bolt/src/nn/autograd/Computation.h>
+#include <bolt_vector/src/BoltVector.h>
+
+namespace thirdai::bolt::nn::ops {
+
+std::string nextL1NormalizationName() {
+  static uint32_t constructed = 0;
+  return "l1_norm_" + std::to_string(++constructed);
+}
+
+L1Normalization::L1Normalization() : Op(nextL1NormalizationName()) {}
+
+std::shared_ptr<L1Normalization> L1Normalization::make() {
+  return std::shared_ptr<L1Normalization>(new L1Normalization());
+}
+
+void L1Normalization::forward(const autograd::ComputationList& inputs,
+                              tensor::TensorPtr& output,
+                              uint32_t index_in_batch, bool training) {
+  (void)training;
+  assert(inputs.size() == 1);
+
+  const tensor::TensorPtr& input = inputs.at(0)->tensor();
+
+  uint32_t start = output->rangeStart(index_in_batch);
+  uint32_t end = output->rangeEnd(index_in_batch);
+
+  for (uint32_t i = start; i < end; i++) {
+    l1Normalization(input->getVector(i), output->getVector(i));
+  }
+}
+
+constexpr float relu(float x) { return std::max(x, 0.F); }
+
+void L1Normalization::l1Normalization(const BoltVector& input,
+                                      BoltVector& output) {
+  assert(input.len == output.len);
+
+  /**
+   * We apply a relu here to avoid having to use an absolute value when
+   * computing the l1 norm. This is because if we include the absolute value
+   * then it requires O(N^2) computations to compute the jacobian in the
+   * backward pass. Without the absolute value we can make use of repeating
+   * structure in the jabobian to compute it in O(N) time.
+   */
+  float l1_norm = 0.0;
+  for (uint32_t i = 0; i < input.len; i++) {
+    l1_norm += relu(input.activations[i]);
+  }
+
+  for (uint32_t i = 0; i < input.len; i++) {
+    output.activations[i] = relu(input.activations[i]) / l1_norm;
+  }
+
+  if (!input.isDense()) {
+    std::copy(input.active_neurons, input.active_neurons + input.len,
+              output.active_neurons);
+  }
+}
+
+void L1Normalization::backpropagate(autograd::ComputationList& inputs,
+                                    tensor::TensorPtr& output,
+                                    uint32_t index_in_batch) {
+  assert(inputs.size() == 1);
+
+  const tensor::TensorPtr& input = inputs.at(0)->tensor();
+
+  uint32_t start = output->rangeStart(index_in_batch);
+  uint32_t end = output->rangeEnd(index_in_batch);
+
+  for (uint32_t i = start; i < end; i++) {
+    l1NormalizationGradient(input->getVector(i), output->getVector(i));
+  }
+}
+
+constexpr float reluGrad(float x) { return x > 0 ? 1 : 0; }
+
+void L1Normalization::l1NormalizationGradient(BoltVector& input,
+                                              const BoltVector& output) {
+  assert(input.len == output.len);
+
+  float l1_norm = 0.0;
+  float sum_grad_times_output = 0.0;
+
+  for (uint32_t i = 0; i < output.len; i++) {
+    sum_grad_times_output += output.gradients[i] * output.activations[i];
+    l1_norm += relu(input.activations[i]);
+  }
+
+  for (uint32_t i = 0; i < input.len; i++) {
+    float grad = (output.gradients[i] - sum_grad_times_output) / l1_norm;
+    input.gradients[i] = grad * reluGrad(input.activations[i]);
+  }
+}
+
+tensor::Dims L1Normalization::dims(
+    const autograd::ComputationList& inputs) const {
+  assert(inputs.size() == 1);
+  return inputs.at(0)->dims();
+}
+
+std::optional<uint32_t> L1Normalization::nonzeros(
+    const autograd::ComputationList& inputs, bool use_sparsity) const {
+  assert(inputs.size() == 1);
+  return inputs.at(0)->nonzeros(use_sparsity);
+}
+
+void L1Normalization::summary(std::ostream& summary,
+                              const autograd::ComputationList& inputs,
+                              const autograd::Computation* output) const {
+  summary << "L1Normalization(" << name() << "): " << inputs.at(0)->name()
+          << " -> " << output->name() << std::endl;
+}
+
+autograd::ComputationPtr L1Normalization::apply(
+    autograd::ComputationPtr input) {
+  return autograd::Computation::make(shared_from_this(), {std::move(input)});
+}
+
+}  // namespace thirdai::bolt::nn::ops
---
+++ bolt/src/nn/ops/L1Normalization.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <bolt/src/nn/ops/Op.h>
+#include <bolt_vector/src/BoltVector.h>
+#include <memory>
+
+namespace thirdai::bolt::nn::ops {
+
+class L1Normalization final
+    : public Op,
+      public std::enable_shared_from_this<L1Normalization> {
+ public:
+  static std::shared_ptr<L1Normalization> make();
+
+  void forward(const autograd::ComputationList& inputs,
+               tensor::TensorPtr& output, uint32_t index_in_batch,
+               bool training) final;
+
+  void backpropagate(autograd::ComputationList& inputs,
+                     tensor::TensorPtr& output, uint32_t index_in_batch) final;
+
+  void updateParameters(float learning_rate, uint32_t train_steps) final;
+
+  tensor::Dims dims(const autograd::ComputationList& inputs) const final;
+
+  std::optional<uint32_t> nonzeros(const autograd::ComputationList& inputs,
+                                   bool use_sparsity) const final;
+
+  void disableSparseParameterUpdates() final;
+
+  std::vector<std::vector<float>*> gradients() final;
+
+  void summary(std::ostream& summary, const autograd::ComputationList& inputs,
+               const autograd::Computation* output) const final;
+
+  autograd::ComputationPtr apply(autograd::ComputationPtr input);
+
+ private:
+  L1Normalization();
+
+  static void l1Normalization(const BoltVector& input, BoltVector& output);
+
+  static void l1NormalizationGradient(BoltVector& input,
+                                      const BoltVector& output);
+};
+
+}  // namespace thirdai::bolt::nn::ops
