f17a8291309c9d02bc85f7af5d42283ca3e34f03
David Torres <david@thirdai.com>
2023-05-02 18:18:38 -0500

    small fix

--- auto_ml/src/models/Generator.h
+++ auto_ml/src/models/Generator.h
@@ -587,8 +587,7 @@ class QueryCandidateGenerator {
   }
 
   BoltVector featurizeSingleQuery(const std::string& query) const {
-    std::vector<std::string> input_vector{query};
-    dataset::RowSampleRef input_vector_ref(input_vector);
+    dataset::RowSampleRef input_vector_ref({query});
     return _inference_featurizer->featurize(input_vector_ref).at(0);
   }
 
