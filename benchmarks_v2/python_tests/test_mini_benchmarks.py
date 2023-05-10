5f3bb64570cb4bd01847453117f16cba1a6422bd
Kartik Sarangmath <kartik@thirdai.com>
2023-05-10 23:57:48 +0000

    mini benchmarks

--- benchmarks_v2/python_tests/test_mini_benchmarks.py
+++ benchmarks_v2/python_tests/test_mini_benchmarks.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import os
 
 import pytest
@@ -16,10 +17,29 @@ def test_mini_benchmarks():
             "mini_benchmark_temporal",
         ],
         path_prefix=os.path.join(curr_path, "../src/mini_benchmark_datasets/"),
+=======
+import pytest
+from benchmarks_v2.src.main import main
+import os
+
+pytestmark = [pytest.mark.unit]
+
+def test_mini_benchmarks():
+    curr_path = os.path.dirname(os.path.abspath(__file__))
+    main(
+        runner=["mini_benchmark_udt", "mini_benchmark_query_reformulation", "mini_benchmark_temporal"],
+        path_prefix=os.path.join(curr_path, "../src/mini_benchmark_datasets/"),
+        fail_on_error=True,
+>>>>>>> 82ce2c99 (mini benchmarks)
         config="",
         mlflow_uri="",
         run_name="",
         official_slack_webhook="",
         branch_slack_webhook="",
+<<<<<<< HEAD
         branch_name="",
     )
+=======
+        branch_name=""
+    )
+>>>>>>> 82ce2c99 (mini benchmarks)
--- benchmarks_v2/src/mini_benchmark_datasets/create_mini_datasets.py
+++ benchmarks_v2/src/mini_benchmark_datasets/create_mini_datasets.py
@@ -1,9 +1,15 @@
 import argparse
 import os
 
+<<<<<<< HEAD
 import numpy as np
 import pandas as pd
 from thirdai import bolt
+=======
+import pandas as pd
+from thirdai import bolt
+import numpy as np
+>>>>>>> 82ce2c99 (mini benchmarks)
 
 from ..runners.runner_map import runner_map
 from ..utils import get_configs
@@ -17,7 +23,11 @@ def parse_arguments():
         nargs="+",
         required=True,
         choices=["udt", "bolt_fc", "dlrm", "query_reformulation", "temporal"],
+<<<<<<< HEAD
         help="The runner to retrieve configs for.",
+=======
+        help="The runner to retrieve benchmark results for.",
+>>>>>>> 82ce2c99 (mini benchmarks)
     )
     parser.add_argument(
         "--config",
@@ -29,12 +39,17 @@ def parse_arguments():
         "--read_path_prefix",
         type=str,
         default="/share/data/",
+<<<<<<< HEAD
         help="The path prefix to read the original benchmark datasets from",
+=======
+        help="The path prefex to prepend to dataset paths. Defaults to './'",
+>>>>>>> 82ce2c99 (mini benchmarks)
     )
     parser.add_argument(
         "--write_path_prefix",
         type=str,
         default="./benchmarks-v2/mini_benchmark_datasets/",
+<<<<<<< HEAD
         help="The path prefix to write the mini benchmark datasets to",
     )
     return parser.parse_args()
@@ -43,21 +58,43 @@ def parse_arguments():
 def save_non_graph_subset(file, num_lines=11):
     if not file:
         return
+=======
+        help="The path prefex to prepend to dataset paths. Defaults to './'",
+    )
+    return parser.parse_args()
+
+def save_non_graph_subset(file, num_lines=11):
+    if not file:
+        raise Exception
+    print(file)
+>>>>>>> 82ce2c99 (mini benchmarks)
 
     read_path = os.path.join(args.read_path_prefix, file)
 
     with open(read_path) as input_file:
         head = [next(input_file) for _ in range(num_lines)]
+<<<<<<< HEAD
 
+=======
+    
+>>>>>>> 82ce2c99 (mini benchmarks)
     write_path = os.path.join(args.write_path_prefix, file)
     if not os.path.exists(os.path.dirname(write_path)):
         os.makedirs(os.path.dirname(write_path))
 
+<<<<<<< HEAD
     with open(write_path, "w") as output_file:
+=======
+    with open(write_path, 'w') as output_file:
+>>>>>>> 82ce2c99 (mini benchmarks)
         output_file.writelines(head)
 
 
 def save_data_subset(config, num_lines=11):
+<<<<<<< HEAD
+=======
+
+>>>>>>> 82ce2c99 (mini benchmarks)
     try:
         has_gnn_backend = any(
             [
@@ -69,6 +106,7 @@ def save_data_subset(config, num_lines=11):
         has_gnn_backend = False
 
     if not has_gnn_backend:
+<<<<<<< HEAD
         if hasattr(config, "train_file"):
             save_non_graph_subset(config.train_file, num_lines)
 
@@ -77,6 +115,22 @@ def save_data_subset(config, num_lines=11):
 
         if hasattr(config, "cold_start_train_file"):
             save_non_graph_subset(config.cold_start_train_file, num_lines)
+=======
+        try:
+            save_non_graph_subset(config.train_file, num_lines)
+        except:
+            pass
+
+        try:
+            save_non_graph_subset(config.test_file, num_lines)
+        except:
+            pass
+
+        try:
+            save_non_graph_subset(config.cold_start_train_file, num_lines)
+        except:
+            pass
+>>>>>>> 82ce2c99 (mini benchmarks)
 
     else:
         # We create a small dummy graph dataset from a given file graph csv files
@@ -86,6 +140,7 @@ def save_data_subset(config, num_lines=11):
         data_types = config.get_data_types(args.read_path_prefix)
 
         # Get column name corresponding to node_id type from data_types
+<<<<<<< HEAD
         node_id_col = [
             k for k, v in data_types.items() if isinstance(v, bolt.types.node_id)
         ][0]
@@ -94,11 +149,18 @@ def save_data_subset(config, num_lines=11):
         neighbors_col = [
             k for k, v in data_types.items() if isinstance(v, bolt.types.neighbors)
         ][0]
+=======
+        node_id_col = (list(data_types.keys()))[[i for i,(_,v) in enumerate(data_types.items()) if isinstance(v, bolt.types.node_id)][0]]
+        
+        # Get column name corresponding to neighbors type from data_types
+        neighbors_col = (list(data_types.keys()))[[i for i,(_,v) in enumerate(data_types.items()) if isinstance(v, bolt.types.neighbors)][0]]
+>>>>>>> 82ce2c99 (mini benchmarks)
 
         read_test_file_path = os.path.join(args.read_path_prefix, config.test_file)
 
         write_train_file_path = os.path.join(args.write_path_prefix, config.train_file)
         write_test_file_path = os.path.join(args.write_path_prefix, config.test_file)
+<<<<<<< HEAD
         write_gnn_index_path = os.path.join(
             args.write_path_prefix, os.path.dirname(config.test_file), "gnn_index.csv"
         )
@@ -124,6 +186,25 @@ def save_data_subset(config, num_lines=11):
         ]
         test_df[neighbors_col] = neighbors
 
+=======
+        write_gnn_index_path = os.path.join(args.write_path_prefix, os.path.dirname(config.test_file), "gnn_index.csv")
+        if not os.path.exists(os.path.dirname(write_train_file_path)):
+            os.makedirs(os.path.dirname(write_train_file_path))
+
+        test_df = pd.read_csv(read_test_file_path, nrows=num_lines-1)
+
+        test_df[config.target] = np.random.randint(config.n_target_classes, size=num_lines-1)
+
+        # Re-index the nodes in the graph
+        test_df[node_id_col] = list(range(num_lines-1))
+
+        # Create valid neighbors
+        neighbor_matrix = np.random.randint(0,2,size=(num_lines-1,num_lines-1))
+        neighbor_matrix = np.tril(neighbor_matrix) + np.tril(neighbor_matrix, -1).T
+        neighbors = [' '.join([str(nb) for nb in np.nonzero(nbs)[0]]) for nbs in neighbor_matrix]
+        test_df[neighbors_col] = neighbors
+        
+>>>>>>> 82ce2c99 (mini benchmarks)
         test_df.to_csv(write_test_file_path)
 
         train_df = test_df.sample(frac=0.5)
@@ -143,4 +224,8 @@ if __name__ == "__main__":
         configs = get_configs(runner=runner, config_regex=args.config)
 
         for config in configs:
+<<<<<<< HEAD
             save_data_subset(config)
+=======
+            save_data_subset(config)
+>>>>>>> 82ce2c99 (mini benchmarks)
