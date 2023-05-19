d041b8d0142ebf36caaf5338aa631fa6750a951d
kartikTAI <129414343+kartikTAI@users.noreply.github.com>
2023-05-19 12:03:04 -0500

    Mini benchmarks (#1452)
    * initial commit
    
    * save
    
    * mini benchmarks
    
    * formatting
    
    * added datasets
    
    * test fix
    
    * minor changes
    
    * changes
    
    * addressed nicks comments
    
    * formatting
    
    * minor change

--- benchmarks_v2/src/runners/runner_map.py
+++ benchmarks_v2/src/runners/runner_map.py
@@ -1,5 +1,10 @@
 from .bolt_fc import BoltFullyConnectedRunner, BoltV2FullyConnectedRunner
 from .dlrm import DLRMRunner, DLRMV2Runner
+from .mini_benchmark_runners.query_reformulation import (
+    MiniBenchmarkQueryReformulationRunner,
+)
+from .mini_benchmark_runners.temporal import MiniBenchmarkTemporalRunner
+from .mini_benchmark_runners.udt import MiniBenchmarkUDTRunner
 from .query_reformulation import QueryReformulationRunner
 from .temporal import TemporalRunner
 from .udt import UDTRunner
@@ -12,4 +17,7 @@ runner_map = {
     "udt": UDTRunner,
     "query_reformulation": QueryReformulationRunner,
     "temporal": TemporalRunner,
+    "mini_benchmark_udt": MiniBenchmarkUDTRunner,
+    "mini_benchmark_query_reformulation": MiniBenchmarkQueryReformulationRunner,
+    "mini_benchmark_temporal": MiniBenchmarkTemporalRunner,
 }
