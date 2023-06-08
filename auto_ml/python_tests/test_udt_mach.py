7f38e806e060e9e45e74c75d13c8e4d44611013a
David Torres <david@thirdai.com>
2023-06-08 16:41:55 -0500

    pyformat

--- auto_ml/python_tests/test_udt_mach.py
+++ auto_ml/python_tests/test_udt_mach.py
@@ -637,7 +637,7 @@ def test_introduce_hash_regularization():
     load = regularized_introduce_helper(model, num_random_hashes=0)
     assert np.sum(load > 0) == NUM_HASHES
 
-    # when 2 of the 7 hashes in every new doc are random there should be more 
+    # when 2 of the 7 hashes in every new doc are random there should be more
     # than NUM_HASHES non-zeroes in the index's load
     load = regularized_introduce_helper(model, num_random_hashes=2)
     assert np.sum(load > 0) > NUM_HASHES
