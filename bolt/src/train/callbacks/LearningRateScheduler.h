02ab5c1369e9491dad0343f0308e9188674740d9
Gautam3AI <134595431+Gautam3AI@users.noreply.github.com>
2023-06-15 23:45:15 +0530

    Adds linear LR scheduler (#1507)
    * Adds linear LR scheduler
    
    * Adds Linear LR Scheduler
    
    * Added Linear LR Scheduler on Bolt v2. Not fully operational
    
    * Reduced Inheritance by removing LRSchedule, fix the bindings. Need attention on test and gettting erros while buiding from 'LearningRateScheduler.h'
    
    * FImplements Linear LR Scheduler.
    
    * Fixes Clang issue
    
    * Changes the inheritaance design of LearningRateScheduler
    
    * Adds some comments
    
    * Changes the wrong data type of batch_level_step in LearningRateScheduler.
    
    * * Adds Multi-step LR
    
    * Fixes some comments
    
    * Update bolt/src/train/callbacks/LearningRateScheduler.h
    
    Co-authored-by: Nicholas Meisburger <43160219+nmeisburger@users.noreply.github.com>
    
    * Update bolt/python_bindings/BoltV2TrainPython.cc
    
    Co-authored-by: Yash <yash@thirdai.com>
    
    * Update bolt/src/train/callbacks/LearningRateScheduler.h
    
    Co-authored-by: Yash <yash@thirdai.com>
    
    * Update bolt/src/train/callbacks/LearningRateScheduler.h
    
    Co-authored-by: Yash <yash@thirdai.com>
    
    * * Fix the multi-step LR step. \n * Modify the comments \n * Revert back the unnecessary changes
    
    * * Fix the multi-step LR step. * Modify the comment. * Revert back the unnecessary changes
    
    ---------
    
    Co-authored-by: Nicholas Meisburger <43160219+nmeisburger@users.noreply.github.com>
    Co-authored-by: Yash <yash@thirdai.com>

---
+++ bolt/src/train/callbacks/LearningRateScheduler.h
@@ -0,0 +1,117 @@
+#pragma once
+
+#include "Callback.h"
+#include <algorithm>
+#include <cstdint>
+#include <memory>
+#include <vector>
+
+namespace thirdai::bolt::train::callbacks {
+
+/**
+ * @brief This callback is intended to schedule learning rate changes during
+ * training.
+ * @param batch_level_steps: If true then we'll adjust the learning rate using
+ * batches as steps instead of epochs. Defaults to false.
+ */
+class LearningRateScheduler : public Callback {
+ public:
+  explicit LearningRateScheduler(bool batch_level_steps)
+      : _epoch(0), _batch_cnt(0), _batch_level_steps(batch_level_steps) {}
+
+  virtual float getNextLR(float current_learning_rate, uint32_t step) = 0;
+
+  void onEpochBegin() final {
+    // resetting the batch count
+    _batch_cnt = 0;
+
+    if (!_batch_level_steps) {
+      train_state->updateLearningRate(
+          getNextLR(train_state->learningRate(), _epoch));
+    }
+    _epoch++;
+  }
+
+  void onBatchBegin() final {
+    if (_batch_level_steps) {
+      train_state->updateLearningRate(
+          getNextLR(train_state->learningRate(), _batch_cnt));
+    }
+    _batch_cnt++;
+  }
+
+ private:
+  uint32_t _epoch, _batch_cnt;
+  bool _batch_level_steps;
+};
+
+/**
+ * @brief Schedules per-step learning rate linearly
+ * @param start_factor: The multiplicative factor in the first epoch or batch
+ * @param end_factor: The multiplicative factor at the end of the linear
+ * changing process
+ * @param total_iters: The number of iterations in which multiplicative factor
+ * reaches to end_factor
+ * @param batch_level_steps: If true then we'll adjust the learning rate using
+ * batches as steps instead of epochs. Defaults to false.
+ */
+class LinearSchedule final : public LearningRateScheduler {
+ public:
+  explicit LinearSchedule(float start_factor, float end_factor,
+                          uint32_t total_iters, bool batch_level_steps = false)
+      : LearningRateScheduler(batch_level_steps),
+        _start_factor(start_factor),
+        _end_factor(end_factor),
+        _total_iters(total_iters) {}
+
+  float getNextLR(float current_learning_rate, uint32_t step) final {
+    if (step == 0) {
+      // calculation of the lr change per step
+      _lr_change_per_step =
+          current_learning_rate * (_end_factor - _start_factor) / _total_iters;
+
+      return current_learning_rate * _start_factor;
+    }
+    if (step > _total_iters) {
+      return current_learning_rate;
+    }
+
+    return current_learning_rate + _lr_change_per_step;
+  }
+
+ private:
+  float _start_factor, _end_factor, _lr_change_per_step;
+  uint32_t _total_iters;
+};
+
+/**
+ * @brief Decays the learning rate by a factor of gamma once the number of steps
+ * reaches one of the specified milestones.
+ * @param gamma: multiplicative factor
+ * @param milestones: step milestones
+ * @param batch_level_steps: If true then we'll adjust the learning rate using
+ * batches as steps instead of epochs. Defaults to false.
+ */
+
+class MultiStepLR final : public LearningRateScheduler {
+ public:
+  MultiStepLR(float gamma, std::vector<uint32_t> milestones,
+              bool batch_level_steps = false)
+      : LearningRateScheduler(batch_level_steps),
+        _gamma(gamma),
+        _milestones(std::move(milestones)) {}
+
+  float getNextLR(float current_learning_rate, uint32_t step) final {
+    if (std::find(_milestones.begin(), _milestones.end(), step) !=
+        _milestones.end()) {
+      return current_learning_rate * _gamma;
+    }
+    return current_learning_rate;
+  }
+
+ private:
+  float _gamma;
+  std::vector<uint32_t> _milestones;
+};
+
+}  // namespace thirdai::bolt::train::callbacks
