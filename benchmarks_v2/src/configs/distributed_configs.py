6499e9213d985d1ba2a51a07ba7b62f1d60b7ba9
mj3ai <mritunjay@thirdai.com>
2023-07-02 08:30:27 +0000

    added cluster initilization

--- benchmarks_v2/src/configs/distributed_configs.py
+++ benchmarks_v2/src/configs/distributed_configs.py
@@ -1,7 +1,25 @@
 import os
+import shutil
 from abc import ABC, abstractmethod
+from thirdai.demos import download_beir_dataset
 
-from thirdai import bolt, dataset
+
+def split_into_2(
+    file_to_split, destination_file_1, destination_file_2, with_header=False
+):
+    with open(file_to_split, "r") as input_file:
+        with open(destination_file_1, "w+") as f_1:
+            with open(destination_file_2, "w+") as f_2:
+                for i, line in enumerate(input_file):
+                    if with_header and i == 0:
+                        f_1.write(line)
+                        f_2.write(line)
+                        continue
+
+                    if i % 2 == 0:
+                        f_1.write(line)
+                    else:
+                        f_2.write(line)
 
 
 class DistributedBenchmarkConfig(ABC):
@@ -19,15 +37,18 @@ class DistributedBenchmarkConfig(ABC):
     num_epochs = None
     metrics = ["categorical_accuracy"]
     callbacks = []
+    n_target_classes = None
 
 
 class FiqaConfig(DistributedBenchmarkConfig):
     config_name = "fiqa_config"
-    unsupervised_file = "/share/data/mach-doc-search/fiqa/unsupervised.csv"
-    supervised_trn = "/share/data/mach-doc-search/fiqa/trn_supervised.csv"
-    supervised_tst = "/share/data/mach-doc-search/fiqa/tst_supervised.csv"
+    dataset_name = "fiqa"
+    unsupervised_file_1 = "fiqa/unsupervised_1.csv"
+    unsupervised_file_2 = "fiqa/unsupervised_2.csv"
+    supervised_trn_1 = "fiqa/trn_supervised_1.csv"
+    supervised_trn_2 = "fiqa/trn_supervised_2.csv"
+    supervised_tst = "fiqa/tst_supervised.csv"
 
-    n_target_classes = 57638
     learning_rate = 0.001
     num_epochs = 20
     output_dim = 50000
@@ -46,13 +67,40 @@ class FiqaConfig(DistributedBenchmarkConfig):
         "recall@100",
     ]
 
+    @classmethod
+    def prepare_dataset(cls, path_prefix: str):
+        (
+            unsupervised_file,
+            trn_supervised,
+            tst_supervised,
+            cls.n_target_classes,
+        ) = download_beir_dataset("fiqa")
+
+        directory = os.path.join(path_prefix, cls.dataset_name)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        split_into_2(
+            unsupervised_file,
+            os.path.join(path_prefix, cls.unsupervised_file_1),
+            os.path.join(path_prefix, cls.unsupervised_file_2),
+            with_header=True,
+        )
+        split_into_2(
+            trn_supervised,
+            os.path.join(path_prefix, cls.supervised_trn_1),
+            os.path.join(path_prefix, cls.supervised_trn_2),
+            with_header=True,
+        )
+        shutil.move(tst_supervised, os.path.join(path_prefix, cls.supervised_tst))
+
 
 class TreqCovidConfig(DistributedBenchmarkConfig):
     config_name = "trec_covid_config"
-    unsupervised_file = "/share/data/mach-doc-search/trec-covid/unsupervised.csv"
-    supervised_tst = "/share/data/mach-doc-search/trec-covid/tst_supervised.csv"
+    dataset_name = "trec-covid"
+    unsupervised_file_1 = "trec-covid/unsupervised_1.csv"
+    unsupervised_file_2 = "trec-covid/unsupervised_2.csv"
+    supervised_tst = "trec-covid/tst_supervised.csv"
 
-    n_target_classes = 171333
     learning_rate = 0.001
     num_epochs = 20
     output_dim = 50000
@@ -70,3 +118,23 @@ class TreqCovidConfig(DistributedBenchmarkConfig):
         "recall@10",
         "recall@100",
     ]
+
+    @classmethod
+    def prepare_dataset(cls, path_prefix: str):
+        (
+            unsupervised_file,
+            trn_supervised,
+            tst_supervised,
+            n_target_classes,
+        ) = download_beir_dataset("trec-covid")
+
+        directory = os.path.join(path_prefix, cls.dataset_name)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        split_into_2(
+            unsupervised_file,
+            os.path.join(path_prefix, cls.unsupervised_file_1),
+            os.path.join(path_prefix, cls.unsupervised_file_2),
+            with_header=True,
+        )
+        shutil.move(tst_supervised, os.path.join(path_prefix, cls.supervised_tst))
--- benchmarks_v2/src/runners/distributed_v1.py
+++ benchmarks_v2/src/runners/distributed_v1.py
@@ -1,4 +1,8 @@
-from thirdai import bolt, bolt_v2
+import ray
+from ray.cluster_utils import Cluster
+from thirdai import bolt
+import thirdai.distributed_bolt as db
+import os
 
 from ..configs.distributed_configs import DistributedBenchmarkConfig
 from .runner import Runner
@@ -27,25 +31,88 @@ def create_udt_model(n_target_classes, output_dim, num_hashes):
     return model
 
 
+def make_cluster_config(num_cpu_per_node, cluster_address, communication_type="linear"):
+    # We set the working_dir for the cluster equal to this directory
+    # so that pickle works. Otherwise, unpickling functions
+    # defined in the test files would not work, since pickle needs to be
+    # able to import the file the object/function was originally defined in.
+
+    working_dir = os.path.dirname(os.path.realpath(__file__))
+    cluster_config = db.RayTrainingClusterConfig(
+        num_workers=2,
+        requested_cpus_per_node=num_cpu_per_node,
+        communication_type=communication_type,
+        cluster_address=cluster_address,
+        runtime_env={"working_dir": working_dir},
+        ignore_reinit_error=True,
+    )
+    return cluster_config
+
+
+def initilize_ray_two_node_cluster():
+    # ============ Initilize a two_node_cluster ===============
+    num_cpu_per_node = db.get_num_cpus() // 2
+
+    # case if multiprocessing import fails
+    if num_cpu_per_node == 0:
+        num_cpu_per_node = 1
+
+    mini_cluster = Cluster(
+        initialize_head=True,
+        head_node_args={
+            "num_cpus": num_cpu_per_node,
+        },
+    )
+    mini_cluster.add_node(num_cpus=num_cpu_per_node)
+    return num_cpu_per_node, mini_cluster
+    # ================ Initialized a cluster ==================
+
+
+def destroy_cluster(mini_cluster):
+    # =============== Destroying cluster ======================
+    ray.shutdown()
+    mini_cluster.shutdown()
+    # =============== Destroyed cluster =======================
+
+
 class DistributedRunner(Runner):
     config_type = DistributedBenchmarkConfig
 
     def run_benchmark(config: DistributedBenchmarkConfig, path_prefix, mlflow_logger):
+        # prepare dataset
+        config.prepare_dataset(path_prefix=path_prefix)
+
+        # Initilize ray cluster
+        num_cpu_per_node, mini_cluster = initilize_ray_two_node_cluster()
+
+        # Create model
         model = create_udt_model(
             n_target_classes=config.n_target_classes,
             output_dim=config.output_dim,
             num_hashes=config.num_hashes,
         )
-
+        # print("======== Model Created ===========")
         validation = bolt.Validation(
-            config.supervised_tst,
+            os.path.join(path_prefix, config.supervised_tst),
             interval=5000,
             metrics=config.val_metrics,
         )
 
-        if hasattr(config, "supervised_trn"):
-            model.train(
-                filename=config.supervised_trn,
+        # curr_dir = os.getcwd()
+        # print(f"{os.getcwd()} is the current working directory.")
+
+        # print("========= Training started ==========")
+        if hasattr(config, "supervised_trn_1"):
+            model.train_distributed(
+                cluster_config=make_cluster_config(
+                    num_cpu_per_node=num_cpu_per_node,
+                    cluster_address=mini_cluster.address,
+                    communication_type="linear",
+                ),
+                filenames=[
+                    os.path.join(path_prefix, config.supervised_trn_1),
+                    os.path.join(path_prefix, config.supervised_trn_2),
+                ],
                 learning_rate=config.learning_rate,
                 epochs=config.num_epochs,
                 metrics=config.train_metrics,
@@ -53,9 +120,18 @@ class DistributedRunner(Runner):
                 # callbacks=[LoggingCallback(model, supervised_tst)],
             )
 
-        if hasattr(config, "unsupervised_file"):
-            model.cold_start(
-                filename=config.unsupervised_file,
+        # print("========== Cold Start Training =============")
+        if hasattr(config, "unsupervised_file_1"):
+            model.cold_start_distributed(
+                cluster_config=make_cluster_config(
+                    num_cpu_per_node=num_cpu_per_node,
+                    cluster_address=mini_cluster.address,
+                    communication_type="linear",
+                ),
+                filenames=[
+                    os.path.join(path_prefix, config.unsupervised_file_1),
+                    os.path.join(path_prefix, config.unsupervised_file_2),
+                ],
                 strong_column_names=["TITLE"],
                 weak_column_names=["TEXT"],
                 learning_rate=config.learning_rate,
@@ -65,6 +141,7 @@ class DistributedRunner(Runner):
                     "recall@10",
                 ],
                 # callbacks=[LoggingCallback(model, supervised_tst)],
-                validation=validation,
             )
-        pass
+
+        # Destroy the cluster
+        destroy_cluster(mini_cluster)
