c6271fde579c73c83e149305efa66f246f7ef175
pratyush-saini <137160153+pratyush-saini@users.noreply.github.com>
2023-07-03 16:44:44 +0530

    Update auto_ml/src/udt/utils/Classifier.cc
    Co-authored-by: Yash <yash@thirdai.com>

--- auto_ml/src/udt/utils/Classifier.cc
+++ auto_ml/src/udt/utils/Classifier.cc
@@ -145,7 +145,7 @@ py::object Classifier::predict(const bolt::nn::tensor::TensorList& inputs,
     if (top_k > *nonzeros) {
       if (output->activeNeuronsPtr()) {
         throw std::runtime_error(
-            "top_k value is invalid. top_k > 0 and top_k <= number of target "
+            "top_k value is invalid.  top_k <= number of target "
             "classes * sparsity");
       }
       throw std::runtime_error(
