76e4c2df4e6f1d991366afe03391c51717bcd8ae
David Torres <david@thirdai.com>
2023-07-11 00:30:38 -0500

    trec-covid benchmark

--- benchmarks_v2/src/configs/mach_configs.py
+++ benchmarks_v2/src/configs/mach_configs.py
@@ -23,25 +23,41 @@ class ScifactMachUDTBenchmark(UDTBenchmarkConfig):
     num_epochs = 10
     learning_rate = 0.001
     options = {"extreme_classification": True, "embedding_dimension": 1024}
-    metrics = []
-    callbacks = [
-        AdditionalMetricCallback(
-            metric_name="precision@1",
-            metric_fn=get_mach_precision_at_k_metric_fn(
-                "DOC_ID", k=1, target_delimeter=":"
-            ),
-        ),
-        AdditionalMetricCallback(
-            metric_name="recall@5",
-            metric_fn=get_mach_recall_at_k_metric_fn(
-                "DOC_ID", k=5, target_delimeter=":"
-            ),
-        ),
-    ]
+    metrics = ["precision@1", "recall@5"]
+    callbacks = []
 
     @staticmethod
     def get_data_types(path_prefix):
         return {
             "QUERY": bolt.types.text(contextual_encoding="local"),
+            "DOC_ID": bolt.types.categorical(delimiter=":"),
+        }
+    
+
+class TrecCovidMachUDTBenchmark(UDTBenchmarkConfig):
+    config_name = "trec_covid_mach_udt"
+    dataset_name = "trec-covid"
+
+    test_file = "trec-covid/tst_supervised.csv"
+
+    target = "DOC_ID"
+    integer_target = True
+    n_target_classes = 171332
+    delimiter = ","
+
+    options = {"extreme_classification": True, "embedding_dimension": 3000, "extreme_output_dim": 20000}
+    metrics = ["precision@1", "precision@10"]
+    callbacks = []
+
+    cold_start_learning_rate = 0.001
+    cold_start_num_epochs = 5
+    cold_start_train_file = "trec-covid/unsupervised.csv"
+    strong_column_names = ["TITLE"]
+    weak_column_names = ["TEXT"]
+
+    @staticmethod
+    def get_data_types(path_prefix):
+        return {
+            "QUERY": bolt.types.text(tokenizer="char-4", contextual_encoding="local"),
             "DOC_ID": bolt.types.categorical(delimiter=":"),
         }
