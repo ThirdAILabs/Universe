6601d51fa0c2d21cba3c1a5cf7696cf013d2897c
pratyush-saini <pratyush@thirdai.com>
2023-08-03 16:16:39 +0530

    formatting

--- bolt/python_tests/utils.py
+++ bolt/python_tests/utils.py
@@ -325,7 +325,7 @@ def compressed_training(
     learning_rate=0.002,
     n_classes=10,
     epochs=30,
-    batch_size=64
+    batch_size=64,
 ):
     model = build_simple_model_for_compression(n_classes)
 
