28d7eb7440bd8b755268a23d6c92ece54102dd41
Benito Geordie <benito@thirdai.com>
2023-08-13 12:52:17 -0500

    new_data -> num_tokens

--- data/src/transformations/CountTokens.cc
+++ data/src/transformations/CountTokens.cc
@@ -7,13 +7,13 @@ thirdai::data::ColumnMap thirdai::data::CountTokens::apply(ColumnMap columns,
                                                            State& state) const {
   (void)state;
   auto tokens_column = columns.getArrayColumn<uint32_t>(_input_column);
-  std::vector<uint32_t> new_data(tokens_column->numRows());
+  std::vector<uint32_t> num_tokens(tokens_column->numRows());
 
-#pragma omp parallel for default(none) shared(tokens_column, new_data)
+#pragma omp parallel for default(none) shared(tokens_column, num_tokens)
   for (uint32_t i = 0; i < tokens_column->numRows(); ++i) {
-    new_data[i] = tokens_column->row(i).size();
-    if (_ceiling && new_data[i] > _ceiling) {
-      new_data[i] = *_ceiling;
+    num_tokens[i] = tokens_column->row(i).size();
+    if (_ceiling && num_tokens[i] > _ceiling) {
+      num_tokens[i] = *_ceiling;
     }
   }
 
@@ -21,7 +21,7 @@ thirdai::data::ColumnMap thirdai::data::CountTokens::apply(ColumnMap columns,
       _ceiling ? std::make_optional(*_ceiling + 1) : std::nullopt;
 
   auto new_column = ValueColumn<uint32_t>::make(
-      /* data= */ std::move(new_data), /* dim= */ dim);
+      /* data= */ std::move(num_tokens), /* dim= */ dim);
   columns.setColumn(/* name= */ _output_column,
                     /* column= */ new_column);
   return columns;
