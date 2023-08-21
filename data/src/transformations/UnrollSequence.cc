162126ea94994ad60185aaa3dc365c0febe7cd04
Benito Geordie <benito@thirdai.com>
2023-08-21 15:26:34 -0500

    eos in unroll sequence

--- data/src/transformations/UnrollSequence.cc
+++ data/src/transformations/UnrollSequence.cc
@@ -2,12 +2,14 @@
 #include <data/src/columns/ArrayColumns.h>
 #include <data/src/columns/Column.h>
 #include <data/src/columns/ValueColumns.h>
+#include <algorithm>
 #include <cstddef>
 #include <exception>
 #include <numeric>
 #include <optional>
 #include <sstream>
 #include <stdexcept>
+#include <string>
 #include <tuple>
 
 namespace thirdai::data {
@@ -16,7 +18,8 @@ static std::vector<size_t> offsets(const ArrayColumnBase<uint32_t>& column) {
   std::vector<size_t> offsets(column.numRows() + 1);
   offsets[0] = 0;
   for (uint32_t i = 0; i < column.numRows(); i++) {
-    offsets[i + 1] = offsets[i] + column.row(i).size();
+    // +1 for EOS token.
+    offsets[i + 1] = offsets[i] + column.row(i).size() + 1;
   }
   return offsets;
 }
@@ -50,6 +53,8 @@ ColumnMap UnrollSequence::apply(ColumnMap columns, State& state) const {
   auto source_column = columns.getArrayColumn<uint32_t>(_source_input_column);
   auto target_column = columns.getArrayColumn<uint32_t>(_target_input_column);
 
+  assertCorrectTargetInputDim(*target_column);
+
   std::vector<size_t> row_offsets = offsets(*source_column);
   const size_t total_num_rows = row_offsets.back();
   std::vector<std::vector<uint32_t>> unrolled_source_data(total_num_rows);
@@ -79,8 +84,15 @@ ColumnMap UnrollSequence::apply(ColumnMap columns, State& state) const {
         unrolled_source_data[offset + row_pos] =
             std::vector(source_row.begin(), source_row.begin() + row_pos);
         // Target is row_pos-th token; the next token to be predicted.
-        unrolled_target_data[offset + row_pos] = target_row[row_pos];
+        unrolled_target_data[offset + row_pos] =
+            tokenOffset(row_pos) + target_row[row_pos];
       }
+      unrolled_source_data[offset + source_row.size()] =
+          std::vector(source_row.begin(), source_row.end());
+      unrolled_target_data[offset + source_row.size()] =
+          _target_vocab_size * _max_position_offset +
+          tokenOffset(source_row.size());
+
     } catch (std::exception& e) {
 #pragma omp critical
       error = std::make_exception_ptr(e);
@@ -94,7 +106,9 @@ ColumnMap UnrollSequence::apply(ColumnMap columns, State& state) const {
   auto unrolled_source_column = ArrayColumn<uint32_t>::make(
       std::move(unrolled_source_data), source_column->dim());
   auto unrolled_target_column = ValueColumn<uint32_t>::make(
-      std::move(unrolled_target_data), target_column->dim());
+      std::move(unrolled_target_data),
+      // +1 for EOS.
+      (_target_vocab_size + 1) * _max_position_offset);
 
   auto permutation_indices = permutation(row_offsets);
   columns = columns.permute(permutation_indices);
@@ -103,4 +117,25 @@ ColumnMap UnrollSequence::apply(ColumnMap columns, State& state) const {
   return columns;
 }
 
+bool UnrollSequence::isEOS(uint32_t token) const {
+  return token >= _target_vocab_size * _max_position_offset;
+}
+
+void UnrollSequence::assertCorrectTargetInputDim(
+    const ArrayColumnBase<uint32_t>& target_column) const {
+  if (!target_column.dim()) {
+    throw std::invalid_argument(
+        "UnrollSequence: Expected target column to have a dimension.");
+  }
+  if (*target_column.dim() != _target_vocab_size) {
+    throw std::invalid_argument("UnrollSequence: Expected target vocab size " +
+                                std::to_string(_target_vocab_size) +
+                                " but received target column with dimension " +
+                                std::to_string(*target_column.dim()));
+  }
+}
+
+uint32_t UnrollSequence::tokenOffset(size_t position) const {
+  return std::min(position, _max_position_offset - 1);
+}
 }  // namespace thirdai::data
--- data/src/transformations/UnrollSequence.h
+++ data/src/transformations/UnrollSequence.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <data/src/columns/Column.h>
 #include <data/src/transformations/Transformation.h>
+#include <algorithm>
+#include <cstddef>
+#include <optional>
 #include <string>
 #include <vector>
 
@@ -29,19 +33,31 @@ class UnrollSequence final : public Transformation {
   UnrollSequence(std::string source_input_column,
                  std::string target_input_column,
                  std::string source_output_column,
-                 std::string target_output_column)
+                 std::string target_output_column, size_t target_vocab_size,
+                 size_t max_position_offset)
       : _source_input_column(std::move(source_input_column)),
         _target_input_column(std::move(target_input_column)),
         _source_output_column(std::move(source_output_column)),
-        _target_output_column(std::move(target_output_column)) {}
+        _target_output_column(std::move(target_output_column)),
+        _target_vocab_size(target_vocab_size),
+        _max_position_offset(max_position_offset) {}
 
   ColumnMap apply(ColumnMap columns, State& state) const final;
 
+  bool isEOS(uint32_t token) const;
+
  private:
+  void assertCorrectTargetInputDim(
+      const ArrayColumnBase<uint32_t>& target_column) const;
+
+  uint32_t tokenOffset(size_t position) const;
+
   std::string _source_input_column;
   std::string _target_input_column;
   std::string _source_output_column;
   std::string _target_output_column;
+  size_t _target_vocab_size;
+  size_t _max_position_offset;
 };
 
 }  // namespace thirdai::data
