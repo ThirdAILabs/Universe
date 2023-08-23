f2ba10515da9cf27c1374cabebb1483e02da1b2d
Benito Geordie <benito@thirdai.com>
2023-08-23 12:26:02 -0500

    pin pymupdf version

--- setup.py
+++ setup.py
@@ -153,7 +153,7 @@ with open("thirdai.version") as version_file:
 
 neural_db_deps = [
     "PyTrie",
-    "PyMuPDF",
+    "PyMuPDF==1.22.5",
     "langchain",
     "bs4",
     "trafilatura",
@@ -162,7 +162,6 @@ neural_db_deps = [
     "nltk",
     "unidecode",
     "pydantic",
-    "ray",
 ]
 
 # The information here can also be placed in setup.cfg - better separation of
