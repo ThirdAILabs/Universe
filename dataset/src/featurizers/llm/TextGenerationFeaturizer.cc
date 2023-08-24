c9869e7e0625240aa42871c081de865379665899
kartikTAI <129414343+kartikTAI@users.noreply.github.com>
2023-08-24 14:46:08 -0500

    template for std::max to fix windows build (#1729)
    * template for std::max to fix windows build
    
    ---------
    
    Co-authored-by: sid <sid@thirdai.com>

--- dataset/src/featurizers/llm/TextGenerationFeaturizer.cc
+++ dataset/src/featurizers/llm/TextGenerationFeaturizer.cc
@@ -95,7 +95,7 @@ TextGenerationFeaturizer::featurizeTextSlidingWindow(
 
   std::vector<std::vector<BoltVector>> vectors;
 
-  for (size_t i = std::max(context_size, 1UL); i < tokens.size(); i++) {
+  for (size_t i = std::max<size_t>(context_size, 1); i < tokens.size(); i++) {
     BoltVector label = BoltVector::singleElementSparseVector(tokens[i]);
 
     vectors.push_back({prompt, _context_featurizer.lrcContext(tokens, 0, i),
