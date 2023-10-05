32285b46d9b5b16b5b637fc7b8d0a0ab3432ab7b
pratyush-saini <pratyush@thirdai.com>
2023-10-05 09:48:58 +0000

    add test cases

--- thirdai_python_package_tests/neural_db/ndb_utils.py
+++ thirdai_python_package_tests/neural_db/ndb_utils.py
@@ -49,6 +49,10 @@ BASE_DIR = os.path.join(
 CSV_FILE = os.path.join(BASE_DIR, "lorem_ipsum.csv")
 PDF_FILE = os.path.join(BASE_DIR, "mutual_nda.pdf")
 DOCX_FILE = os.path.join(BASE_DIR, "four_english_words.docx")
+PPTX_FILE = os.path.join(BASE_DIR, "quantum_mechanics.pptx")
+TXT_FILE = os.path.join(BASE_DIR, "nature.txt")
+EML_FILE = os.path.join(BASE_DIR, "Message.eml")
+
 # This is a list of getter functions that return doc objects so each test can
 # use fresh doc object instances.
 all_doc_getters = [
@@ -67,6 +71,9 @@ all_doc_getters = [
         "https://en.wikipedia.org/wiki/Rice_University",
         requests.get("https://en.wikipedia.org/wiki/Rice_University"),
     ),
+    lambda: ndb.Unstructured(PPTX_FILE),
+    lambda: ndb.Unstructured(TXT_FILE),
+    lambda: ndb.Unstructured(EML_FILE),
     lambda: ndb.SentenceLevelPDF(PDF_FILE),
     lambda: ndb.SentenceLevelDOCX(DOCX_FILE),
 ]
