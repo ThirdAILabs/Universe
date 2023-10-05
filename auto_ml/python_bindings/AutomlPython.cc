0f6ffca1d9ae85c61e5523b33dadc4df19f77c9d
pratyush-saini <137160153+pratyush-saini@users.noreply.github.com>
2023-10-05 10:56:15 +0530

    Symspell (#1757)
    * add symspell support to qr
    
    * solves compilation issue
    
    * pre-training working
    
    * predict using symspell
    
    * make use_spell_checker optional
    
    * update symspellcppfork to latest
    
    * data parsing seperated from pre-training
    
    * evaluate working with symspell
    
    * sorting improved
    
    * shift generated candidates to pre-trainer
    
    * add loadall map inputs
    
    * remove parse_data from symspell
    
    * minor fix in evaluate
    
    * symspellcpp removed from submodule
    
    * adding serialization
    
    * serialization bugs fixed
    
    * remove symspell cmakelist
    
    * restructuring repo and nits resolve
    
    * python tests for symspell
    
    * cpp-test for symspell
    
    * made load map input batches generic
    
    * Update auto_ml/src/udt/utils/symspell.cc
    
    Co-authored-by: Benito Geordie <89472452+benitoThree@users.noreply.github.com>
    
    * Update auto_ml/src/udt/backends/UDTQueryReformulation.cc
    
    Co-authored-by: Benito Geordie <89472452+benitoThree@users.noreply.github.com>
    
    * generate candidates return struct
    
    * removed experimental scores
    
    * remove initialization message symspell
    
    * generate candiates parallelized
    
    * push symspell files to utils
    
    ---------
    
    Co-authored-by: Pratik Pranav <pratkpranav>
    Co-authored-by: Benito Geordie <89472452+benitoThree@users.noreply.github.com>

--- auto_ml/python_bindings/AutomlPython.cc
+++ auto_ml/python_bindings/AutomlPython.cc
@@ -56,11 +56,12 @@ std::shared_ptr<udt::UDT> makeUDT(
 
 std::shared_ptr<udt::UDT> makeQueryReformulation(
     std::string source_column, std::string target_column,
-    const std::string& dataset_size, char delimiter,
+    const std::string& dataset_size, bool use_spell_checker, char delimiter,
     const std::optional<std::string>& model_config, const py::dict& options);
 
 std::shared_ptr<udt::UDT> makeQueryReformulationTargetOnly(
-    std::string target_column, const std::string& dataset_size, char delimiter,
+    std::string target_column, const std::string& dataset_size,
+    bool use_spell_checker, char delimiter,
     const std::optional<std::string>& model_config, const py::dict& options);
 
 std::shared_ptr<udt::UDT> makeSvmClassifier(
@@ -107,11 +108,13 @@ void defineAutomlInModule(py::module_& module) {
            bolt::python::OutputRedirect())
       .def(py::init(&makeQueryReformulation), py::arg("source_column"),
            py::arg("target_column"), py::arg("dataset_size"),
-           py::arg("delimiter") = ',', py::arg("model_config") = std::nullopt,
+           py::arg("use_spell_checker") = false, py::arg("delimiter") = ',',
+           py::arg("model_config") = std::nullopt,
            py::arg("options") = py::dict(), docs::UDT_GENERATOR_INIT)
       .def(py::init(&makeQueryReformulationTargetOnly),
            py::arg("target_column"), py::arg("dataset_size"),
-           py::arg("delimiter") = ',', py::arg("model_config") = std::nullopt,
+           py::arg("use_spell_checker") = false, py::arg("delimiter") = ',',
+           py::arg("model_config") = std::nullopt,
            py::arg("options") = py::dict(), docs::UDT_GENERATOR_INIT)
       .def(py::init(&makeSvmClassifier), py::arg("file_format"),
            py::arg("n_target_classes"), py::arg("input_dim"),
@@ -427,23 +430,26 @@ std::shared_ptr<udt::UDT> makeUDT(
 
 std::shared_ptr<udt::UDT> makeQueryReformulation(
     std::string source_column, std::string target_column,
-    const std::string& dataset_size, char delimiter,
+    const std::string& dataset_size, bool use_spell_checker, char delimiter,
     const std::optional<std::string>& model_config, const py::dict& options) {
   return std::make_shared<udt::UDT>(
       /* incorrect_column_name = */ std::move(source_column),
       /* correct_column_name = */ std::move(target_column),
       /* dataset_size = */ dataset_size,
+      /** use spell checker = */ use_spell_checker,
       /* delimiter = */ delimiter, /* model_config = */ model_config,
       /* user_args= */ createArgumentMap(options));
 }
 
 std::shared_ptr<udt::UDT> makeQueryReformulationTargetOnly(
-    std::string target_column, const std::string& dataset_size, char delimiter,
+    std::string target_column, const std::string& dataset_size,
+    bool use_spell_checker, char delimiter,
     const std::optional<std::string>& model_config, const py::dict& options) {
   return std::make_shared<udt::UDT>(
       /* incorrect_column_name = */ std::nullopt,
       /* correct_column_name = */ std::move(target_column),
       /* dataset_size = */ dataset_size,
+      /** use spell checker = */ use_spell_checker,
       /* delimiter = */ delimiter, /* model_config = */ model_config,
       /* user_args= */ createArgumentMap(options));
 }
