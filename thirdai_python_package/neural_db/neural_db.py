b2441988bfbbe7e46f0797c01ff96e840971f211
Benito Geordie <benito@thirdai.com>
2023-10-12 15:22:51 -0500

    Neuraldb delete document

--- thirdai_python_package/neural_db/neural_db.py
+++ thirdai_python_package/neural_db/neural_db.py
@@ -532,6 +532,13 @@ class NeuralDB:
         on_success()
         return ids
 
+    def delete(self, source_id: str):
+        deleted_entities = self._savable_state.documents.delete(source_id)
+        self._savable_state.model.delete(deleted_entities)
+        self._savable_state.logger.log(
+            session_id=self._user_id, action="delete", args={"source_id": source_id}
+        )
+
     def clear_sources(self) -> None:
         self._savable_state.documents.clear()
         self._savable_state.model.forget_documents()
