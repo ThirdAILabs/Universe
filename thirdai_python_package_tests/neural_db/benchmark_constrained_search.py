c00e07022387adfb6f1795ea8ad2564692482631
Benito Geordie <benito@thirdai.com>
2023-10-12 14:09:10 -0500

    streamline the benchmark script

--- thirdai_python_package_tests/neural_db/benchmark_constrained_search.py
+++ thirdai_python_package_tests/neural_db/benchmark_constrained_search.py
@@ -1,8 +1,13 @@
 from typing import List
 import random
-import string
+import time
 import argparse
 from thirdai import neural_db as ndb
+from thirdai.neural_db.constraint_matcher import (
+    ConstraintMatcher,
+    ConstraintValue,
+    to_filters,
+)
 import mlflow
 import json
 
@@ -23,23 +28,10 @@ def strings_of_length(length, num_strings):
     return strings
 
 
-def write_files(folder: str, num_docs: int, num_entities_per_doc: int, entity: str):
-    if "," in entity:
-        raise ValueError("Entity cannot contain delimiter character ','")
-
-    for i in range(num_docs):
-        with open(f"{folder}/doc_{i}.csv", "w") as f:
-            f.write("id,text\n")
-            for j in range(num_entities_per_doc):
-                f.write(f"{j},{entity},\n")
-
-    return [f"{folder}/doc_{i}.csv" for i in range(num_docs)]
-
-
-def assign_metadata_to_docs(
-    metadata_fields: List[str], metadata_options: List[str], num_docs: int
+def generate_item_metadata(
+    metadata_fields: List[str], metadata_options: List[str], num_items: int
 ):
-    metadata = [{field: ""} for field in metadata_fields] * num_docs
+    metadata = [{field: ""} for field in metadata_fields] * num_items
     for i in range(num_docs):
         to_be_modded = i
         for field in metadata_fields:
@@ -75,14 +67,13 @@ def random_filters(
     return {**exact_filters, **range_filters}
 
 
-def generate_queries(
+def generate_constraints(
     num_exact_filters: int,
     num_range_filters: int,
     range_size: int,
     metadata_fields: List[str],
     metadata_options: List[str],
     num_queries: int,
-    query_length: int,
 ):
     if num_exact_filters + num_range_filters > len(metadata_fields):
         raise ValueError(
@@ -92,11 +83,6 @@ def generate_queries(
         raise ValueError(
             "Number of metadata options must be greater than or equal to range_size."
         )
-
-    queries = [
-        "".join(random.choices(string.ascii_uppercase + string.digits, k=query_length))
-        for _ in range(num_queries)
-    ]
 
     sorted_options = sorted(metadata_options)
     metadata = [
@@ -110,7 +96,7 @@ def generate_queries(
         for _ in range(num_queries)
     ]
 
-    return zip(queries, metadata)
+    return metadata
 
 
 def benchmark(
@@ -122,55 +108,46 @@ def benchmark(
     num_range_filters: int,
     range_size: int,
     num_docs: int,
-    num_entities_per_doc: int,
     num_queries: int,
-    query_length: int,
-    entity: str,
-    folder: str,
 ):
     if num_docs < num_metadata_fields * num_options_per_field:
         raise ValueError(
             "Num docs must be at least num_metadata_fields x num_options_per field."
         )
-    # Steps:
-    # Generate metadata options
+
     metadata_fields = strings_of_length(metadata_field_len, num_metadata_fields)
     metadata_options = strings_of_length(metadata_option_len, num_options_per_field)
-    filenames = write_files(folder, num_docs, num_entities_per_doc, entity)
-    doc_metadata = assign_metadata_to_docs(metadata_fields, metadata_options, num_docs)
-    documents = [
-        ndb.CSV(filename, metadata=meta)
-        for filename, meta in zip(filenames, doc_metadata)
-    ]
+    item_metadata = generate_item_metadata(metadata_fields, metadata_options, num_docs)
+    dummy_item = 0
 
-    db = ndb.NeuralDB()
-    _, insert_times = db.insert(documents, train=False, return_times=True)
+    constraint_matcher = ConstraintMatcher()
 
-    constraint_matching_time = 0
-    scoring_time = 0
+    start = time.time()
+    for meta in item_metadata:
+        constraint_matcher.index(
+            item=dummy_item,
+            constraints={key: ConstraintValue(value) for key, value in meta},
+        )
+    constraint_indexing_time = time.time() - start
 
-    for query, constraints in generate_queries(
+    constraint_queries = generate_constraints(
         num_exact_filters,
         num_range_filters,
         range_size,
         metadata_fields,
         metadata_options,
         num_queries,
-        query_length,
-    ):
-        _, times = db.search(
-            query, top_k=1000, constraints=constraints, return_times=True
-        )
-        constraint_matching_time += times["constraint_matching"]
-        scoring_time += times["scoring"]
+    )
 
-    avg_constraint_matching_time = constraint_matching_time / num_queries
-    avg_scoring_time = scoring_time / num_queries
+    start = time.time()
+    for constraints in constraint_queries:
+        start = time.time()
+        constraint_matcher.match(filters=to_filters(constraints))
+    constraint_matching_time = time.time() - start
 
     return {
-        "avg_constraint_matching_time": avg_constraint_matching_time,
-        "avg_scoring_time": avg_scoring_time,
-        **insert_times,
+        "avg_constraint_matching_time": constraint_matching_time / num_queries,
+        "total_constraint_indexing_time": constraint_indexing_time,
     }
 
 
@@ -185,7 +162,6 @@ if __name__ == "__main__":
     mlflow.set_tracking_uri(mlflow_uri)
     mlflow.set_experiment("Constrained Search Benchmarks")
     num_queries = 1000
-    query_length = 50
     metadata_field_len = 5
     metadata_option_len = 5
     range_size = 3
@@ -195,55 +171,43 @@ if __name__ == "__main__":
     run_number = 1
 
     for num_docs in [100_000, 1_000_000]:
-        for num_entities_per_doc in [100, 1000, 5000]:
-            for num_exact_filters, num_options_per_field in [
-                (3, 10),
-                (5, 7),
-                (10, 3),
-            ]:
-                entities_per_filter = (
-                    num_docs * num_entities_per_doc / num_options_per_field
-                )
+        for num_exact_filters, num_options_per_field in [
+            (3, 10),
+            (5, 7),
+            (10, 3),
+        ]:
+            params = {
+                "num_metadata_fields": num_metadata_fields,
+                "num_options_per_field": num_options_per_field,
+                "metadata_field_len": metadata_field_len,
+                "metadata_option_len": metadata_option_len,
+                "num_exact_filters": num_exact_filters,
+                "num_range_filters": num_range_filters,
+                "range_size": range_size,
+                "num_docs": num_docs,
+                "num_queries": num_queries,
+            }
 
-                params = {
-                    "num_metadata_fields": num_metadata_fields,
-                    "num_options_per_field": num_options_per_field,
-                    "metadata_field_len": metadata_field_len,
-                    "metadata_option_len": metadata_option_len,
-                    "num_exact_filters": num_exact_filters,
-                    "num_range_filters": num_range_filters,
-                    "range_size": range_size,
-                    "num_docs": num_docs,
-                    "num_entities_per_doc": num_entities_per_doc,
-                    "num_queries": num_queries,
-                    "query_length": query_length,
-                    "entities_per_filter": entities_per_filter,
-                }
+            print(
+                f"Run {run_number}\n",
+                json.dumps(params, sort_keys=True, indent=4),
+                "\n",
+            )
 
-                print(
-                    f"Run {run_number}\n",
-                    json.dumps(params, sort_keys=True, indent=4),
-                    "\n",
-                )
+            mlflow.start_run()
+            mlflow.log_params(params)
+            metrics = benchmark(
+                num_metadata_fields=num_metadata_fields,
+                num_options_per_field=num_options_per_field,
+                metadata_field_len=metadata_field_len,
+                metadata_option_len=metadata_option_len,
+                num_exact_filters=num_exact_filters,
+                num_range_filters=num_range_filters,
+                range_size=range_size,
+                num_docs=num_docs,
+                num_queries=num_queries,
+            )
+            mlflow.log_metrics(metrics)
+            mlflow.end_run()
 
-                mlflow.start_run()
-                mlflow.log_params(params)
-                metrics = benchmark(
-                    num_metadata_fields=num_metadata_fields,
-                    num_options_per_field=num_options_per_field,
-                    metadata_field_len=metadata_field_len,
-                    metadata_option_len=metadata_option_len,
-                    num_exact_filters=num_exact_filters,
-                    num_range_filters=num_range_filters,
-                    range_size=range_size,
-                    num_docs=num_docs,
-                    num_entities_per_doc=num_entities_per_doc,
-                    num_queries=num_queries,
-                    query_length=query_length,
-                    entity="paragraph.",
-                    folder=args.folder,
-                )
-                mlflow.log_metrics(metrics)
-                mlflow.end_run()
-
-                run_number += 1
+            run_number += 1
