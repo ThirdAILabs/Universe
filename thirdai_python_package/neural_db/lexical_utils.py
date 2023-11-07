83e70c47622f319ab0903fc5075864d72ff54bd2
Benito Geordie <benito@thirdai.com>
2023-11-07 13:15:08 -0600

    toolbox

---
+++ thirdai_python_package/neural_db/lexical_utils.py
@@ -0,0 +1,99 @@
+from collections import defaultdict
+import nltk
+import random
+import re
+from nltk.corpus import wordnet as wn
+
+nltk.download("averaged_perceptron_tagger")
+nltk.download("wordnet")
+
+
+def char4(sentence):
+    return [sentence[i : i + 4] for i in range(0, len(sentence) - 3, 2)]
+
+
+def custom_tokenize(sentence):
+    # print(sentence)
+    sentence = sentence.lower()
+    import re
+
+    sentence = re.sub(r"[<>=`\-,.{}:|;/@#?!&~$\[\]()\"']+\ *", "", sentence)
+    sentence = re.sub("\d+", "", sentence)
+    tokens = []
+    for word in sentence.split(" "):
+        if len(word) > 4:
+            tokens.extend(char4(word))
+    # print(tokens)
+    return set(tokens)
+
+
+def rank_documents(query_tokens, docs_tokens):
+    doc_scores = []
+    for i, doc_tokens in enumerate(docs_tokens):
+        doc_scores.append((i, len(query_tokens.intersection(doc_tokens))))
+    sorted_scores = sorted(doc_scores, key=lambda x: x[1], reverse=True)
+    return [x[0] for x in sorted_scores]
+
+
+def rerank(query, search_results):
+    query_tokens = custom_tokenize(query)
+    docs_tokens = [custom_tokenize(r.text) for r in search_results]
+    ranked_indices = rank_documents(query_tokens, docs_tokens)
+    reranked_search_results = [search_results[i] for i in ranked_indices]
+    return reranked_search_results
+
+
+def pos_to_wordnet_pos(penntag, returnNone=False):
+    morphy_tag = {"NN": wn.NOUN, "JJ": wn.ADJ, "VB": wn.VERB, "RB": wn.ADV}
+    try:
+        return morphy_tag[penntag[:2]]
+    except:
+        return None if returnNone else ""
+
+
+def get_synonyms(word, pos):
+    for synset in wn.synsets(word, pos=pos_to_wordnet_pos(pos)):
+        for lemma in synset.lemmas():
+            yield lemma.name()
+
+
+def reformulate_text(text, probability=0.2, seed=42):
+    random.seed(seed)
+    text = re.sub(r"[^a-zA-Z0-9,.?!\s]", "", text)
+    text = nltk.word_tokenize(text)
+    token_list = []
+    for word, tag in nltk.pos_tag(text):
+        # Filter for unique synonyms not equal to word and sort.
+        unique = sorted(
+            set(synonym for synonym in get_synonyms(word, tag) if synonym != word)
+        )
+        if len(unique) < 1 or random.random() > probability:
+            token_list.append(word)
+        else:
+            token_list.append(random.choice(unique).replace("_", " "))
+    return " ".join(token_list)
+
+
+def reformulate_query(query, num_candidates=25):
+    expanded_queries = [query]
+    for iteration in range(num_candidates):
+        reformulation = reformulate_text(query, seed=iteration)
+        expanded_queries.append(reformulation)
+    return expanded_queries
+
+
+def aggregate_search_results(preds_dict, top_k):
+    output = [(k, sum(v)) for k, v in preds_dict.items()]
+    output.sort(key=lambda tup: tup[1], reverse=True)
+    return [x[0] for x in output[:top_k]]
+
+
+def reformulate(db, query, constraints={}):
+    expanded_queries = reformulate_query(query)
+    preds_dict = defaultdict(list)
+    for q in expanded_queries:
+        output_results = db.search(query=q, top_k=100, constraints=constraints)
+        for result in output_results:
+            preds_dict[result].extend([result.score])
+    search_results = aggregate_search_results(preds_dict, top_k=100)
+    return search_results
---
+++ thirdai_python_package/neural_db/toolbox.py
@@ -0,0 +1,93 @@
+import pandas as pd
+from pathlib import Path
+from thirdai._thirdai import bolt
+from .documents import PDF
+from .neural_db import NeuralDB
+from .model_bazaar import Bazaar
+from .constraint_matcher import AnyOf
+from .lexical_utils import reformulate as lexical_reformulate, rerank as lexical_rerank
+
+
+def pdf_file_model(files, in_dim=50_000, emb_dim=2048, num_buckets=50_000):
+    dfs = [PDF(file).df for file in files]
+    dfs = [
+        pd.DataFrame(
+            {
+                "para": df["para"],
+                "doc_id": [i for _ in range(len(df))],
+                "file": [Path(files[i]).name for _ in range(len(df))],
+            }
+        )
+        for i, df in enumerate(dfs)
+    ]
+    pd.concat(dfs).to_csv("file_level.csv", index=False)
+    #
+    udt = bolt.UniversalDeepTransformer(
+        data_types={
+            "query": bolt.types.text(tokenizer="char-4"),
+            "doc_id": bolt.types.categorical(delimiter=" "),
+        },
+        target="doc_id",
+        n_target_classes=len(files),
+        integer_target=True,
+        options={
+            "extreme_classification": True,
+            "extreme_output_dim": num_buckets,
+            "fhr": in_dim,
+            "embedding_dimension": emb_dim,
+            "rlhf": True,
+        },
+    )
+    udt.cold_start(
+        "file_level.csv",
+        strong_column_names=[],
+        weak_column_names=["para"],
+        learning_rate=0.005,
+        epochs=10,
+    )
+    #
+    for df in dfs:
+        df["para"].iloc[0] = "\n".join(df["para"])
+    one_reference_per_file_df = pd.concat([df.iloc[:1] for df in dfs])
+    one_reference_per_file_df.to_csv("one_ref_per_file.csv", index=False)
+    return NeuralDB.from_udt(
+        udt,
+        csv="one_ref_per_file.csv",
+        csv_id_column="doc_id",
+        csv_strong_columns=[],
+        csv_weak_columns=["para"],
+        csv_reference_columns=["para"],
+    )
+
+
+def pdf_para_model(files):
+    bazaar = Bazaar(cache_dir=Path("bazaar_cache"))
+    bazaar.fetch()
+    para_db = bazaar.get_model("General QnA")
+    docs = [PDF(file, metadata={"file": Path(file).name}) for file in files]
+    para_db.insert(docs, train=True)
+    return para_db
+
+
+def hierarchical_search(
+    file_db,
+    para_db,
+    query,
+    top_k_results,
+    top_k_files=10,
+    top_k_rerank=100,
+    rerank=True,
+    reformulate=False,
+):
+    file_results = file_db.search(
+        query=query,
+        top_k=top_k_files,
+    )
+    top_k_filenames = [r.metadata["file"] for r in file_results]
+    constraints = {"file": AnyOf(top_k_filenames)}
+    results = para_db.search(query=query, top_k=top_k_rerank, constraints=constraints)
+    if reformulate:
+        results = lexical_reformulate(para_db, query, constraints=constraints)
+    if rerank:
+        results = lexical_rerank(query, results)
+    return results[:top_k_results]
