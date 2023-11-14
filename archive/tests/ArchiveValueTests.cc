943361c25720e736bed9e6d22dbafa6fd6dcf771
Nicholas Meisburger <43160219+nmeisburger@users.noreply.github.com>
2023-11-14 14:54:45 -0500

    Archive Objects (#1834)
    * Archive format
    
    * use integers instead of strings
    
    * Archive specialization
    
    * remove _uint64
    
    * Archive tests
    
    * archive map tests
    
    * List tests and fix weird cereal issues
    
    * Archive value tests
    
    * test parameter reference
    
    * ArchveList.cc
    
    * ArchiveList.cc
    
    * ArchiveMap.cc
    
    * Archive tests
    
    * format
    
    * Add pragma once
    
    * remove include
    
    * Resolve comments
    
    * Add comments
    
    * resolve last comments
    
    * Apply cipher to all strings in archives
    
    * Fix type
    
    * rename keys to seeds
    
    * downgrade pydantic
    
    * Tests for string cipher
    
    * Improve iterator test
    
    * Fix test
    
    * Fix test

---
+++ archive/tests/ArchiveValueTests.cc
@@ -0,0 +1,109 @@
+#include <gtest/gtest.h>
+#include <archive/src/Archive.h>
+#include <archive/src/ArchiveValue.h>
+#include <archive/src/StringCipher.h>
+#include <archive/tests/Utils.h>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+
+namespace thirdai::ar::tests {
+
+template <typename T, typename U>
+void testArchiveValue(T value) {
+  ConstArchivePtr archive = ArchiveValue<T>::make(value);
+
+  ASSERT_EQ(archive->as<T>(), value);
+
+  ASSERT_TRUE(archive->is<T>());
+  ASSERT_FALSE(archive->is<U>());
+
+  CHECK_EXCEPTION(archive->as<U>(),
+                  "Attempted to convert archive of type '" +
+                      ArchiveValue<T>::typeName() + "' to type '" +
+                      ArchiveValue<U>::typeName() + "'.",
+                  std::runtime_error)
+
+  std::stringstream buffer;
+  serialize(archive, buffer);
+  auto loaded = deserialize(buffer);
+
+  ASSERT_EQ(loaded->as<T>(), value);
+
+  ASSERT_TRUE(loaded->is<T>());
+  ASSERT_FALSE(loaded->is<U>());
+
+  CHECK_EXCEPTION(loaded->as<U>(),
+                  "Attempted to convert archive of type '" +
+                      ArchiveValue<T>::typeName() + "' to type '" +
+                      ArchiveValue<U>::typeName() + "'.",
+                  std::runtime_error)
+}
+
+TEST(ArchiveValueTests, TestBoolean) {
+  testArchiveValue<bool, uint64_t>(true);
+  testArchiveValue<bool, uint64_t>(false);
+}
+
+TEST(ArchiveValueTests, TestUint64) {
+  testArchiveValue<uint64_t, float>(720942);
+  testArchiveValue<uint64_t, float>(824024902);
+}
+
+TEST(ArchiveValueTests, TestInt64) {
+  testArchiveValue<int64_t, uint64_t>(-720942);
+  testArchiveValue<int64_t, uint64_t>(824024902);
+}
+
+TEST(ArchiveValueTests, TestFloat) {
+  testArchiveValue<float, uint64_t>(-2.04924);
+  testArchiveValue<float, uint64_t>(8.32902);
+}
+
+TEST(ArchiveValueTests, TestString) {
+  testArchiveValue<std::string, uint64_t>("apple");
+  testArchiveValue<std::string, uint64_t>("banana");
+}
+
+TEST(ArchiveValueTests, StringValuesAreHidden) {
+  auto str_archive = str("pineapple");
+
+  std::stringstream buffer;
+  serialize(str_archive, buffer);
+
+  std::string serialized = buffer.str();
+
+  ASSERT_EQ(serialized.find("pineapple"), std::string::npos);
+
+  ASSERT_NE(serialized.find(cipher("pineapple")), std::string::npos);
+}
+
+TEST(ArchiveValueTests, TestVecUint32) {
+  testArchiveValue<std::vector<uint32_t>, uint64_t>({1, 2, 3, 4, 5, 6, 7, 8});
+}
+
+TEST(ArchiveValueTests, TestVecInt64) {
+  testArchiveValue<std::vector<int64_t>, int64_t>({1, -2, 3, -4, 5, -6, 7, -8});
+}
+
+TEST(ArchiveValueTests, TestVecString) {
+  testArchiveValue<std::vector<std::string>, uint64_t>({"abc", "bcd", "cde"});
+}
+
+TEST(ArchiveValueTests, TestVecWstring) {
+  testArchiveValue<std::vector<std::wstring>, uint64_t>(
+      {L"abc", L"bcd", L"cde"});
+}
+
+TEST(ArchiveValueTests, TestMapUint64) {
+  testArchiveValue<std::unordered_map<uint64_t, std::vector<uint64_t>>,
+                   uint64_t>({{1, {2, 3}}, {2, {3, 4}}, {3, {4, 5}}});
+}
+
+TEST(ArchiveValueTests, TestMapFloat) {
+  testArchiveValue<std::unordered_map<uint64_t, std::vector<float>>, uint64_t>(
+      {{1, {2.0, -3.0}}, {2, {-3.1, 4.45}}, {3, {0.042, 5.42}}});
+}
+
+}  // namespace thirdai::ar::tests
