b7349a75d1a6585d15a7097b83587a88b42b775e
mj@3ai <mritunjay@thirdai.com>
2023-11-20 21:06:35 +0530

    trying some more fix

--- thirdai_python_package/neural_db/model_bazaar/client/bazaar_client.py
+++ thirdai_python_package/neural_db/model_bazaar/client/bazaar_client.py
@@ -6,7 +6,7 @@ from urllib.parse import urljoin
 from uuid import UUID
 
 import requests
-from bazaar_base import Bazaar, auth_header
+from .bazaar_base import Bazaar, auth_header
 
 from utils import (
     check_deployment_decorator,
