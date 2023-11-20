3ab9edab23e7c301192ab6efc73f13244c508b68
mj@3ai <mritunjay@thirdai.com>
2023-11-20 20:03:48 +0530

    integration to thirdai ndb

--- thirdai_python_package/neural_db/__init__.py
+++ thirdai_python_package/neural_db/__init__.py
@@ -14,8 +14,8 @@ try:
         SQLDatabase,
         Unstructured,
     )
-    from .model_bazaar import Bazaar
     from .neural_db import CancelState, NeuralDB, Strength, Sup
+    from .model_bazaar import Bazaar, ModelBazaar
 except ImportError as error:
     raise ImportError(
         "To use thirdai.neural_db, please install the additional dependencies by running 'pip install thirdai[neural_db]'"
