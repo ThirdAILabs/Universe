cfd49bba8cdf6f18301c92ed98677a85dde1d961
David Torres <detorresramos1@gmail.com>
2023-11-28 15:38:06 -0600

    perturbations and stopword manipulation

--- data/python_bindings/DataPython.cc
+++ data/python_bindings/DataPython.cc
@@ -399,7 +399,7 @@ void createTransformationsSubmodule(py::module_& dataset_submodule) {
 #if THIRDAI_EXPOSE_ALL
       .def(py::init<size_t, size_t, std::optional<uint32_t>, size_t,
                     std::optional<size_t>, uint32_t, bool, bool, uint32_t,
-                    float>(),
+                    float, float, float, float>(),
            py::arg("covering_min_length") = 5,
            py::arg("covering_max_length") = 40,
            py::arg("max_covering_samples") = std::nullopt,
@@ -408,7 +408,10 @@ void createTransformationsSubmodule(py::module_& dataset_submodule) {
            py::arg("num_slices") = 7, py::arg("add_whole_doc") = true,
            py::arg("prefilter_punctuation") = true,
            py::arg("strong_sample_num_words") = 3,
-           py::arg("word_removal_probability") = 0)
+           py::arg("stopword_removal_probability") = 0,
+           py::arg("stopword_insertion_probability") = 0,
+           py::arg("word_removal_probability") = 0,
+           py::arg("word_perturbation_probability") = 0)
 #endif
       ;
 
@@ -440,7 +443,6 @@ void createTransformationsSubmodule(py::module_& dataset_submodule) {
       .def("inference_featurization", &DyadicInterval::inferenceFeaturization,
            py::arg("columns"));
 #endif
-
 }
 
 }  // namespace thirdai::data::python
