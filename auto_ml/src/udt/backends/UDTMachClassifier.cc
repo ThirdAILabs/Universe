063a87e99b7ff6eb436e26f3bdce252973e1a3ad
Nicholas Meisburger <nmeisburger@gmail.com>
2023-12-05 09:48:44 -0500

    fix restart val data

--- auto_ml/src/udt/backends/UDTMachClassifier.cc
+++ auto_ml/src/udt/backends/UDTMachClassifier.cc
@@ -455,7 +455,9 @@ py::object UDTMachClassifier::coldstart(
     history = train(data_source, learning_rate, /* epochs= */ 1, train_metrics,
                     val_data, val_metrics, callbacks, options, comm);
     data->restart();
-    val_data->restart();
+    if (val_data) {
+      val_data->restart();
+    }
   }
 
   return history;
