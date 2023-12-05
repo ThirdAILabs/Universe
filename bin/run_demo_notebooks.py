f3b14b54d95965f38df94d4585bb6f862da7c7d0
Benito Geordie <benito@thirdai.com>
2023-12-04 20:29:42 -0600

    Run each demo in a separate container

--- bin/run_demo_notebooks.py
+++ bin/run_demo_notebooks.py
@@ -2,12 +2,8 @@ import glob
 import os
 import shutil
 import subprocess
-import sys
 import tempfile
-from pathlib import Path
 
-import nbformat
-from nbconvert.preprocessors import CellExecutionError, ExecutePreprocessor
 
 DEMO_URL = "https://github.com/ThirdAILabs/Demos.git"
 
@@ -20,52 +16,28 @@ def skip_nb(path):
     return any([nb in path for nb in IGNORED_NBS])
 
 
-def get_notebook_paths(temp_dir):
+def get_relative_notebook_paths(temp_dir):
     # Collect all of the jupyter notebooks in the Demos repo
     subprocess.call(["git", "clone", DEMO_URL], cwd=temp_dir)
     notebook_dir = os.path.join(temp_dir, "Demos", "**", "*.ipynb")
     notebook_paths = glob.glob(notebook_dir, recursive=True)
     notebook_paths = [path for path in notebook_paths if not skip_nb(path)]
+    len_demos_dir = len(str(os.path.join(temp_dir, "Demos"))) + 1  # For slash
+    relative_notebook_paths = [str(path)[len_demos_dir:] for path in notebook_paths]
 
-    return notebook_paths
+    # Clean up. We don't need to keep this since we only need it to get the paths.
+    shutil.rmtree(temp_dir)
 
-
-def run_demo_notebooks(notebook_paths):
-    errors = []
-    for notebook_path in notebook_paths:
-        with open(notebook_path) as notebook_file:
-            # Ref: https://nbformat.readthedocs.io/en/latest/format_description.html
-            nb_in = nbformat.read(notebook_file, nbformat.NO_CONVERT)
-            # The resources argument is needed to execute the notebook in a specific
-            # directory. We run the notebooks in the directory they are located in
-            # to ensure that paths work correctly to configs (or anything else).
-            working_dir = str(Path(notebook_path).parent)
-            try:
-                ep = ExecutePreprocessor(
-                    timeout=None,
-                    kernel_name="python3",
-                    resources={"metadata": {"path": working_dir}},
-                )
-                nb_out = ep.preprocess(nb_in)
-            except Exception as error:
-                notebook_name = Path(notebook_path).stem
-                errors.append((notebook_name, error))
-
-    if errors:
-        for failed_notebook, error in errors:
-            print(f"Failure in notebook: {failed_notebook}: \n {error}")
-        sys.exit(1)
-    else:
-        print("Successfully ran the following notebooks:")
-        for nb in notebook_paths:
-            print(f"\t{Path(nb).stem}")
+    return relative_notebook_paths
 
 
 def main():
     temp_dir = tempfile.mkdtemp()
-    demo_notebook_paths = get_notebook_paths(temp_dir)
-    run_demo_notebooks(demo_notebook_paths)
-    shutil.rmtree(temp_dir)  # Clean up the files used for the test
+    relative_notebook_paths = get_relative_notebook_paths(temp_dir)
+    for notebook_path in relative_notebook_paths:
+        os.system(
+            f'docker run -e "OPENAI_API_KEY=$OPENAI_API_KEY" -e "THIRDAI_KEY=$THIRDAI_KEY" thirdai/run_demos_build bash -c "python3 run_demo_notebooks.py" {notebook_path}'
+        )
 
 
 if __name__ == "__main__":
---
+++ bin/run_single_demo_notebook.py
@@ -0,0 +1,67 @@
+import glob
+import os
+import shutil
+import subprocess
+import sys
+import tempfile
+from pathlib import Path
+
+import nbformat
+from nbconvert.preprocessors import CellExecutionError, ExecutePreprocessor
+
+DEMO_URL = "https://github.com/ThirdAILabs/Demos.git"
+
+IGNORED_NBS = [
+    "DeployThirdaiwithDatabricks.ipynb",
+]
+
+
+def skip_nb(path):
+    return any([nb in path for nb in IGNORED_NBS])
+
+
+def get_notebook_path(temp_dir, relative_notebook_path):
+    # Collect all of the jupyter notebooks in the Demos repo
+    subprocess.call(["git", "clone", DEMO_URL], cwd=temp_dir)
+    return os.path.join(temp_dir, "Demos", relative_notebook_path)
+
+
+def run_demo_notebook(notebook_path):
+    errors = []
+    with open(notebook_path) as notebook_file:
+        # Ref: https://nbformat.readthedocs.io/en/latest/format_description.html
+        nb_in = nbformat.read(notebook_file, nbformat.NO_CONVERT)
+        # The resources argument is needed to execute the notebook in a specific
+        # directory. We run the notebooks in the directory they are located in
+        # to ensure that paths work correctly to configs (or anything else).
+        working_dir = str(Path(notebook_path).parent)
+        try:
+            ep = ExecutePreprocessor(
+                timeout=None,
+                kernel_name="python3",
+                resources={"metadata": {"path": working_dir}},
+            )
+            nb_out = ep.preprocess(nb_in)
+        except Exception as error:
+            notebook_name = Path(notebook_path).stem
+            errors.append((notebook_name, error))
+
+    if errors:
+        for failed_notebook, error in errors:
+            print(f"Failure in notebook: {failed_notebook}: \n {error}")
+        sys.exit(1)
+    else:
+        print("Successfully ran the following notebooks:")
+        print(f"\t{Path(notebook_path).stem}")
+
+
+def main():
+    temp_dir = tempfile.mkdtemp()
+    relative_notebook_path = sys.argv[1]
+    notebook_path = get_notebook_path(temp_dir, relative_notebook_path)
+    run_demo_notebook(notebook_path)
+    shutil.rmtree(temp_dir)  # Clean up the files used for the test
+
+
+if __name__ == "__main__":
+    main()
