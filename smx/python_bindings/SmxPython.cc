2313c304e70fc81c54d0007539250d8693f4a084
Nicholas Meisburger <nmeisburger@gmail.com>
2024-01-02 18:27:34 -0800

    sparse linear test

--- smx/python_bindings/SmxPython.cc
+++ smx/python_bindings/SmxPython.cc
@@ -1,6 +1,7 @@
 #include "SmxPython.h"
 #include <pybind11/attr.h>
 #include <pybind11/detail/common.h>
+#include <pybind11/functional.h>
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
--- smx/python_tests/test_mnist_mlp.py
+++ smx/python_tests/test_mnist_mlp.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import torch
 import torchvision
+import time
 from thirdai import smx
 
 
@@ -16,8 +17,8 @@ def load_datasets(download_dir):
         root=download_dir, train=False, download=True, transform=transform
     )
 
-    train_loader = torch.utils.data.DataLoader(train_set, shuffle=True, batch_size=128)
-    test_loader = torch.utils.data.DataLoader(test_set, shuffle=False, batch_size=128)
+    train_loader = torch.utils.data.DataLoader(train_set, shuffle=True, batch_size=250)
+    test_loader = torch.utils.data.DataLoader(test_set, shuffle=False, batch_size=250)
 
     return train_loader, test_loader
 
@@ -60,3 +61,57 @@ def test_smx_mlp_mnist():
         print(f"Epoch {epoch} accuracy={accuracy}")
 
     assert accuracy >= 0.95
+
+
+class SparseModel(smx.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.hidden = smx.SparseLinear(dim=20000, input_dim=784, sparsity=0.01)
+        self.output = smx.Linear(dim=10, input_dim=20000)
+
+    def forward(self, x):
+        out = self.hidden(x)[0]
+        out = smx.relu(out)
+        out = self.output(out)
+        return [out]
+
+    def parameters(self):
+        return self.hidden.parameters() + self.output.parameters()
+
+
+def test_smx_sparse_mlp_mnist():
+    download_dir = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "../../build/mnist_dataset"
+    )
+    train_loader, test_loader = load_datasets(download_dir=download_dir)
+
+    model = SparseModel()
+
+    optimizer = smx.optimizers.Adam(model.parameters(), lr=0.001)
+    optimizer.register_on_update_callback(model.hidden.on_update_callback())
+
+    for epoch in range(5):
+        s = time.perf_counter()
+        for x, y in train_loader:
+            out = model([flatten(x)])[0]
+
+            loss = smx.cross_entropy(out, smx.from_numpy(y.numpy().astype(np.uint32)))
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+        e = time.perf_counter()
+        print(f"epoch {e-s}")
+
+        correct, total = 0, 0
+        for x, y in test_loader:
+            out = model([flatten(x)])[0].tensor.numpy()
+
+            correct += np.sum(np.argmax(out, axis=1) == y.numpy())
+            total += len(x)
+
+        accuracy = correct / total
+        print(f"Epoch {epoch} accuracy={accuracy}")
+
+
+test_smx_sparse_mlp_mnist()
