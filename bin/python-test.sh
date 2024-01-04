34a3245b4531d8b6b4b1ecf36d1013f35214d974
David Torres <david@thirdai.com>
2024-01-04 10:29:45 -0600

    remove change

--- bin/python-test.sh
+++ bin/python-test.sh
@@ -6,8 +6,7 @@ BASEDIR=$(dirname "$0")
 # Forward all arguments.
 cd $BASEDIR/../build/
 
-# we look for tests in */python_tests/* to speed up test collection significantly
 echo "Running the following tests:"
-python3 -m pytest ../**/python_tests/ --ignore-glob=../deps --disable-warnings --collect-only "$@" 
+python3 -m pytest ../ --ignore-glob=../deps --collect-only "$@" 
 echo "Output of the tests:"
-python3 -m pytest ../**/python_tests/ --ignore-glob=../deps --disable-warnings -s "$@"
+python3 -m pytest ../ --ignore-glob=../deps -s "$@"
