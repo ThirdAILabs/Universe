4ac7d8e223ff8b479e49d4c0e5f3b2171d02ab35
shubh3ai <shubh@thirdai.com>
2024-01-17 03:44:00 +0530

    Add nltk download (#1942)
    * Add nltk download
    
    * remove wheel

--- thirdai_python_package_tests/neural_db/test_checkpoint.py
+++ thirdai_python_package_tests/neural_db/test_checkpoint.py
@@ -4,7 +4,10 @@ from collections import defaultdict
 from pathlib import Path
 from typing import List
 
+import nltk
 import pytest
+
+nltk.download("punkt")
 from ndb_utils import PDF_FILE, all_local_doc_getters, associate_works, upvote_works
 from thirdai import data
 from thirdai import neural_db as ndb
