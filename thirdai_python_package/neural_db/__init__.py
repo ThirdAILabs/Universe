bda3a95c6d23d32fa78822f72e3ad6ce26ce7c56
shubh3ai <shubh@thirdai.com>
2024-01-16 16:32:46 +0530

    Adds training tracker with resume support (#1894)
    * Adds training tracker. No resume support as of now
    
    * Refactored code a lot. Adds resume from a checkpoint support
    
    * Checkpointing working fine, resume to check
    
    * Formatting
    
    * Resume checkpoint working fine. Refactored a bit
    
    * VLC Serialization support
    
    * Formatting
    
    * Refactor a bit
    
    * Add backup config
    
    * Remove checkpoints after training and save neural db
    
    * Check if config exists
    
    * Refactored Version
    
    * Simple Tests for now
    
    * Add interrupt tests, Refactor index_documents into two parts
    
    * Reset sharded documents
    
    * Comments and small changes
    
    * Refactoring a lot, split index doc function, pickle support
    
    * Change defaults file name and add variables
    
    * Add commits, remove unnecesary checkpointing
    
    * formatting
    
    * better exception handling
    
    * Minor refactor
    
    * Minor refactor
    
    * kwargs support
    
    * Pretty to_string method
    
    * refactor datasource checkpointing
    
    * Refactor
    
    * Format
    
    * Nits
    
    * Add freeze after epoch support
    
    * nits
    
    * Format
    
    * Test
    
    * Adding tests, refactoring
    
    * CPP Format
    
    * pytest fix
    
    * Change name
    
    * Modify Test and NOLINT

--- thirdai_python_package/neural_db/__init__.py
+++ thirdai_python_package/neural_db/__init__.py
@@ -17,8 +17,7 @@ try:
         Unstructured,
     )
     from .model_bazaar import Bazaar, ModelBazaar
-    from .neural_db import CancelState, NeuralDB, Strength, Sup
-
+    from .neural_db import CancelState, CheckpointConfig, NeuralDB, Strength, Sup
 except ImportError as error:
     raise ImportError(
         "To use thirdai.neural_db, please install the additional dependencies by"
