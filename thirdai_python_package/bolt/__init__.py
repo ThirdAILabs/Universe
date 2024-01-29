da0288f459e5038800aadd803ba01a2529f40680
Nicholas Meisburger <nmeisburger@gmail.com>
2024-01-29 18:22:44 -0500

    python format

--- thirdai_python_package/bolt/__init__.py
+++ thirdai_python_package/bolt/__init__.py
@@ -1,11 +1,7 @@
 import thirdai._thirdai.bolt
 from thirdai._thirdai.bolt import *
 
-from .udt_modifications import (
-    modify_graph_udt,
-    modify_mach_udt,
-    modify_udt,
-)
+from .udt_modifications import modify_graph_udt, modify_mach_udt, modify_udt
 
 modify_udt()
 modify_graph_udt()
