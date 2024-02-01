df9cb6c96ccc09c104391a83269cfcbf4cffcc43
Nicholas Meisburger <nmeisburger@gmail.com>
2024-02-01 14:32:05 -0500

    Stemmer comments

--- utils/text/PorterStemmer.h
+++ utils/text/PorterStemmer.h
@@ -5,6 +5,13 @@
 
 namespace thirdai::text::porter_stemmer {
 
+/**
+ * Based on the Porter stemming algorithm proposed in:
+ *    Porter, M. "An algorithm for suffix stripping."
+ *        Program 14.3 (1980): 130-137.
+ *
+ * Overview of algorithm: https://tartarus.org/martin/PorterStemmer/def.txt
+ */
 std::string stem(const std::string& word, bool lowercase = true);
 
 std::vector<std::string> stem(const std::vector<std::string>& words,
