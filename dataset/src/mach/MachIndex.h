2b28b82ac7a27a8085e951fd42a2e97f5a1ce5f3
pratyush-saini <137160153+pratyush-saini@users.noreply.github.com>
2024-02-15 22:05:39 +0530

    Update dataset/src/mach/MachIndex.h
    Co-authored-by: Nicholas Meisburger <43160219+nmeisburger@users.noreply.github.com>

--- dataset/src/mach/MachIndex.h
+++ dataset/src/mach/MachIndex.h
@@ -82,7 +82,7 @@ class MachIndex {
   uint32_t approxNumHashesPerBucket(uint32_t num_new_samples) const {
     uint32_t totalHashes = ((num_new_samples + numEntities()) * numHashes());
 
-    return totalHashes / numBuckets() + ((totalHashes % numBuckets()) != 0);
+    return (totalHashes + numBuckets() - 1) / numBuckets();
   }
 
   const auto& entityToHashes() const { return _entity_to_hashes; }
