1a86a7af4f07af6d82d7e7f136a22b2b208b0ed2
Benito Geordie <benito@thirdai.com>
2024-02-21 20:57:33 +0700

    Comments on chunk

--- thirdai_python_package/neural_db_v2/core/types.py
+++ thirdai_python_package/neural_db_v2/core/types.py
@@ -7,12 +7,27 @@ ChunkId = int
 
 @dataclass
 class NewChunk:
+    """A chunk that has not been assigned a unique ID."""
+
+    # An optional identifier supplied by the user.
     user_assigned_id: Union[str, int, None]
+
+    # The text content of the chunk, e.g. a paragraph.
     text: str
+
+    # Keywords / strong signals.
     keywords: str
+
+    # Arbitrary metadata related to the chunk.
     metadata: dict
+
+    # Parent document name
+    document: str
 
 
 @dataclass
 class Chunk(NewChunk):
+    """A chunk that has been assigned a unique ID."""
+
+    # A unique identifier assigned by a chunk store.
     chunk_id: ChunkId
