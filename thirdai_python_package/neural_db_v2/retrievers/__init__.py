1cd4b660894efa20013362b22129511d2077baff
Benito Geordie <benito@thirdai.com>
2024-02-21 20:36:31 +0700

    Remove chunk_store_by_name and retriever_by_name

--- thirdai_python_package/neural_db_v2/retrievers/__init__.py
+++
@@ -1,6 +0,0 @@
-from core.retriever import Retriever
-
-
-def retriever_by_name(name: str, **kwargs) -> Retriever:
-    # TODO: Add options here
-    raise ValueError(f"Invalid retriever name {name}")
