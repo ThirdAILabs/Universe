c186753d867686fef8f057280f9042de816319bd
Benito Geordie <89472452+benitoThree@users.noreply.github.com>
2024-03-21 18:15:35 -0500

    revert ray constraint (#2072)

--- setup.py
+++ setup.py
@@ -229,7 +229,7 @@ setup(
             "scikit-learn",
             "beir",
             "pyOpenSSL>22.1.0",
-            "ray<2.10.0",
+            "ray",
             "grpcio",
             "unstructured[all-docs]<=0.10.20",
         ]
