3cb75852ffaa423a05f5daf0db41c2a4c60c8e60
Nicholas Meisburger <nmeisburger@gmail.com>
2024-03-21 20:55:06 -0400

    Fix script args

--- bin/run_demo_notebooks.py
+++ bin/run_demo_notebooks.py
@@ -36,7 +36,7 @@ def main():
     successes, failures = [], []
     for notebook_path in relative_notebook_paths:
         retcode = os.system(
-            f'docker run -e "OPENAI_API_KEY=$OPENAI_API_KEY" -e "THIRDAI_KEY=$THIRDAI_KEY" thirdai/run_demos_build bash -c "python3 run_single_demo_notebook.py" {notebook_path}'
+            f'docker run -e "OPENAI_API_KEY=$OPENAI_API_KEY" -e "THIRDAI_KEY=$THIRDAI_KEY" thirdai/run_demos_build bash -c "python3 run_single_demo_notebook.py {notebook_path}"'
         )
         if retcode == 0:
             successes.append(notebook_path)
