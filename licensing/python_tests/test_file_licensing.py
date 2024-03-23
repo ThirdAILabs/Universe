e5ab7805fa135b506222e89c62657aace7608aca
Nicholas Meisburger <43160219+nmeisburger@users.noreply.github.com>
2024-03-22 21:51:04 -0400

    Fix file licenses (#2077)

--- licensing/python_tests/test_file_licensing.py
+++ licensing/python_tests/test_file_licensing.py
@@ -59,9 +59,9 @@ def test_license_print(capfd):
     print(out)
     assert (
         out.strip()
-        == "This is a license with an expiry time of 1711083134694 epoch ms. "
+        == "This is a license with an expiry time of 1797555168817 epoch ms. "
         "Entitlements are as follows: FULL_DATASET_ACCESS, LOAD_SAVE, "
-        "MAX_OUTPUT_DIM 18446744073709551615, MAX_TRAIN_SAMPLES 100,"
+        "MAX_OUTPUT_DIM 4294967295, MAX_TRAIN_SAMPLES 100,"
     )
     assert err == ""
 
