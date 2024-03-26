ac8d2931e1c2833ee714958d79ab08713b2c1653
Nicholas Meisburger <nmeisburger@gmail.com>
2024-03-26 12:57:41 -0400

    Use np.full

--- thirdai_python_package/neural_db_v2/documents/utils.py
+++ thirdai_python_package/neural_db_v2/documents/utils.py
@@ -1,7 +1,8 @@
 from typing import Any
 
+import numpy as np
 import pandas as pd
 
 
 def series_from_value(value: Any, n: int) -> pd.Series:
-    return pd.Series(value).repeat(n).reset_index(drop=True)
+    return pd.Series(np.full(n, value))
