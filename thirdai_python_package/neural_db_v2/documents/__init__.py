793b4f2f08391697a185fcdec0e58090d2b0e581
Nicholas Meisburger <43160219+nmeisburger@users.noreply.github.com>
2024-03-27 12:59:25 -0400

    [NDB v2] Document parsers (#2057)
    * impl documents
    
    * Format and add url
    
    * Unstructured docs
    
    * Doc tests
    
    * in memory doc
    
    * Use np.full
    
    * clean up metadata concat and add fillna to text cols in csv
    
    * Csv streaming

--- thirdai_python_package/neural_db_v2/documents/__init__.py
+++ thirdai_python_package/neural_db_v2/documents/__init__.py
@@ -1,4 +1,10 @@
-from core.documents import Document
+from ..core.documents import Document
+from .csv import CSV
+from .docx import DOCX
+from .in_memory_text import InMemoryText
+from .pdf import PDF
+from .unstructured import PPTX, Email, TextFile
+from .url import URL
 
 
 def document_by_name(name: str, **kwargs) -> Document:
