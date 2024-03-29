8c47b36625f172fef28a074f02201054f26bb944
Benito Geordie <benito@thirdai.com>
2024-03-29 14:37:10 -0500

    test skeleton

---
+++ thirdai_python_package_tests/neural_db/test_handle_spaces.py
@@ -0,0 +1,13 @@
+def test_handle_spaces_in_csv_column():
+    # write simple csv, columns have spaces
+    # get references, assert that ref metadata keys doesn't have spaces
+
+    pass
+
+
+def test_handle_spaces_in_metadata():
+    pass
+
+
+def test_handle_spaces_backwards_compatibility():
+    pass
