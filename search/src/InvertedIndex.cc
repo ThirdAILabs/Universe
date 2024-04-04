d819f0a137a74f3af823d7cbea504fa472aa5631
Nicholas Meisburger <nmeisburger@gmail.com>
2024-04-03 21:51:56 -0400

    remove tested version

--- search/src/InvertedIndex.cc
+++ search/src/InvertedIndex.cc
@@ -288,9 +288,6 @@ void InvertedIndex::remove(const std::vector<DocId>& ids) {
 Tokens InvertedIndex::tokenizeText(std::string text) const {
   for (char& c : text) {
     if (std::ispunct(c)) {
-      // if (std::ispunct(c) && c != '=' && c != '/' && c != '\\' && c != '+' &&
-      //     c != '<' && c != '>' && c != '~' && c != '_' && c != '|' && c !=
-      //     '`' && c != '^') {
       c = ' ';
     }
   }
