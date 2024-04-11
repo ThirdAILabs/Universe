1543cdc480157914b04d899d8cfdf81c9801e516
pratyush-saini <pratyush@thirdai.com>
2024-04-11 06:06:33 +0000

    add pd meta object in map partition

--- thirdai_python_package/neural_db/table.py
+++ thirdai_python_package/neural_db/table.py
@@ -109,7 +109,8 @@ class DaskDataFrameTable(Table):
 
     def iter_rows_as_dicts(self) -> Generator[Tuple[int, dict], None, None]:
         index_name = self.df.index.name
-        results = self.df.map_partitions(self._partition_to_dicts, meta=("object"))
+        meta = pd.DataFrame({"data": pd.Series(dtype="object")})
+        results = self.df.map_partitions(self._partition_to_dicts, meta=meta)
         row_id = 0  # We are maintaining a global row_id because each partition will have it's local index
         for batch in results.compute():
             for row_dict in batch:
