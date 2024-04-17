c09ad1a7366bdf96a2bcfe78504fcf591ba24c9f
David Torres <david@thirdai.com>
2024-04-17 16:25:15 -0500

    fixes

--- thirdai_python_package/gen/questions.py
+++ thirdai_python_package/gen/questions.py
@@ -1,6 +1,7 @@
 from typing import List
 
 import openai
+from tqdm import tqdm
 
 
 class QAGenMethod:
@@ -9,7 +10,9 @@ class QAGenMethod:
 
 
 class OpenAI(QAGenMethod):
-    def __init__(self, api_key: str, model: str, questions_per_paragraph: int):
+    def __init__(
+        self, api_key: str, model: str, questions_per_paragraph: int, verbose=True
+    ):
         self.client = openai.OpenAI(api_key=api_key)
         self.model = model
         self.prompt = (
@@ -17,10 +20,11 @@ class OpenAI(QAGenMethod):
             "following text. Return your answers as newline separated responses "
             "without any number or bullet prefixes. Here is the content: \n\n"
         )
+        self.verbose = verbose
 
     def generate(self, texts: List[str]) -> List[List[str]]:
         questions = []
-        for text in texts:
+        for text in tqdm(texts, disable=not self.verbose):
             chat_completion = self.client.chat.completions.create(
                 messages=[{"role": "user", "content": self.prompt + text}],
                 model=self.model,
