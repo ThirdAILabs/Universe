8d1bc0e445a5d2666451f5bba6839a3ba89e7a4b
yash <yash@thirdai.com>
2024-05-01 20:24:39 +0000

    client expose

--- thirdai_python_package/neural_db/__init__.py
+++ thirdai_python_package/neural_db/__init__.py
@@ -17,7 +17,7 @@ from .documents import (
     SQLDatabase,
     Unstructured,
 )
-from .model_bazaar import Login, ModelBazaar
+from .model_bazaar import Login, ModelBazaar, NeuralDBClient
 from .neural_db import CancelState, CheckpointConfig, NeuralDB, Strength, Sup
 from .question_generation import gen_questions
 from .trainer import training_data_manager, training_progress_manager
