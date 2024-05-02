fec5a3e483a3b3198e04b774d577e70579471a7a
David Torres <97477655+dtr3@users.noreply.github.com>
2024-05-02 14:40:25 -0500

    clean up python test logging (#2163)
    * clean up python tests
    
    * comments
    
    * remove import

--- thirdai_python_package/neural_db/__init__.py
+++ thirdai_python_package/neural_db/__init__.py
@@ -1,4 +1,10 @@
 import nltk
+from nltk.data import find
+
+try:
+    find("tokenizers/punkt")
+except LookupError:
+    nltk.download("punkt")
 
 from . import parsing_utils
 from .constraint_matcher import AnyOf, EqualTo, GreaterThan, InRange, LessThan
@@ -21,5 +27,3 @@ from .model_bazaar import Login, ModelBazaar
 from .neural_db import CancelState, CheckpointConfig, NeuralDB, Strength, Sup
 from .question_generation import gen_questions
 from .trainer import training_data_manager, training_progress_manager
-
-nltk.download("punkt")
