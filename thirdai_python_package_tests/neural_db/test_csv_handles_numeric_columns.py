94e05bc9b7d5a6cb1a2968eb6244951cf6542652
Benito Geordie <benito@thirdai.com>
2024-05-02 15:01:12 -0500

    CSV handle numbers

---
+++ thirdai_python_package_tests/neural_db/test_csv_handles_numeric_columns.py
@@ -0,0 +1,23 @@
+import pytest
+from thirdai import neural_db as ndb
+import pandas as pd
+import uuid
+import os
+
+pytestmark = [pytest.mark.unit]
+
+@pytest.fixture(scope="session")
+def csv():
+    path = f"numeric_csv_{uuid.uuid4()}.csv"
+    pd.DataFrame({
+        "col": [0, 1, 2, 3, 4],
+    }).to_csv(path, index=None)
+    yield path
+    os.remove(path)
+
+
+def test_csv_handles_numeric_columns(csv):
+    # Just make sure it doesn't break, which it did previously.
+    db = ndb.NeuralDB()    
+    db.insert(ndb.CSV(csv, strong_columns=["col"], weak_columns=["col"]))
+    db.search("0", top_k=1)
