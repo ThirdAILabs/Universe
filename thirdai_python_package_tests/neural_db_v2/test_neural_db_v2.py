0749039112c7758d07c30cbe461d3276f8cedb04
dtr3 <david@thirdai.com>
2024-05-06 21:11:18 +0000

    try on large machine?

--- thirdai_python_package_tests/neural_db_v2/test_neural_db_v2.py
+++ thirdai_python_package_tests/neural_db_v2/test_neural_db_v2.py
@@ -16,7 +16,7 @@ pytestmark = [pytest.mark.unit, pytest.mark.release]
     [
         (SQLiteChunkStore, FinetunableRetriever),
         (PandasChunkStore, FinetunableRetriever),
-        # (PandasChunkStore, Mach),
+        (PandasChunkStore, Mach),
     ],
 )
 def test_neural_db_v2_save_load_integration(chunk_store, retriever):
