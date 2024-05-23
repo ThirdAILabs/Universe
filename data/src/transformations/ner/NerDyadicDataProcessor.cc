e0b8467460663b339f1ed50ab631a614e5f05a04
shubh@3ai <shubh@thirdai.com>
2024-05-23 00:00:40 +0000

    remove date regex

--- data/src/transformations/ner/NerDyadicDataProcessor.cc
+++ data/src/transformations/ner/NerDyadicDataProcessor.cc
@@ -110,12 +110,14 @@ bool isValidEmail(const std::string& email) {
 bool isValidDate(const std::string& token) {
   // Check if the token matches the regex pattern
 
-  const std::regex yyyymmdd(R"((^\d{4}[-/.]\d{2}[-/.]\d{2}$))");
-  const std::regex mmddyy(R"((^\d{2}[-/.]\d{2}[-/.]\d{4}$))");
+  // const std::regex yyyymmdd(R"((^\d{4}[-/.]\d{2}[-/.]\d{2}$))");
+  // const std::regex mmddyy(R"((^\d{2}[-/.]\d{2}[-/.]\d{4}$))");
   const std::regex month(
       R"((^(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec|january|february|march|april|may|june|july|august|september|october|november|december)))");
-  return std::regex_match(token, yyyymmdd) || std::regex_match(token, mmddyy) ||
-         std::regex_match(token, month);
+  // return std::regex_match(token, yyyymmdd) || std::regex_match(token, mmddyy)
+  // ||
+  //        std::regex_match(token, month);
+  return std::regex_match(token, month);
 }
 
 std::string NerDyadicDataProcessor::getExtraFeatures(
