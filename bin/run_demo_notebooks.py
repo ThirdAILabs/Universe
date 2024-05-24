34020fb5f51dec5edaf89be5c21f9a6a452a28bd
Pratik Pranav <pratik@thirdai.com>
2024-05-24 16:43:45 +0530

    add correct path

--- bin/run_demo_notebooks.py
+++ bin/run_demo_notebooks.py
@@ -19,8 +19,8 @@ def skip_nb(path):
 def get_relative_notebook_paths(temp_dir):
     # Collect all of the jupyter notebooks in the Demos repo
     subprocess.call(["git", "clone", DEMO_URL], cwd=temp_dir)
-    subprocess.call(["git", "fetch"], cwd=temp_dir)
-    subprocess.call(["git", "checkout", "fix-demos"], cwd=temp_dir)
+    subprocess.call(["git", "fetch"], cwd=f"{temp_dir}/Demos")
+    subprocess.call(["git", "checkout", "fix-demos"], cwd=f"{temp_dir}/Demos")
     notebook_dir = os.path.join(temp_dir, "Demos", "**", "*.ipynb")
     notebook_paths = glob.glob(notebook_dir, recursive=True)
     notebook_paths = [path for path in notebook_paths if not skip_nb(path)]
