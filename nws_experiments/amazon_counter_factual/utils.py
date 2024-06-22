23c7aeedbba9af1ba27d7759fe3d1bbea3a73348
BenGeordie <school@bengeordie@gmail.com>
2024-06-22 14:19:24 -0500

    reqs

--- nws_experiments/amazon_counter_factual/utils.py
+++ nws_experiments/amazon_counter_factual/utils.py
@@ -1,15 +1,13 @@
 import time
 import numpy as np
-import torch
-import torch.nn as nn
-import torch.optim as optim
 
 
 def run_and_time(runnable, verbose=True):
     start = time.time()
     result = runnable()
     end = time.time()
-    print(f"Finished in {end - start} seconds.")
+    if verbose:
+        print(f"Finished in {end - start} seconds.")
     return result, end - start
 
 
