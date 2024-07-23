790fdbdfd950abd389e2c363ce1166807c8cec45
Nicholas Meisburger <nmeisburger@gmail.com>
2024-07-23 15:07:16 -0700

    cpp format

--- search/src/inverted_index/OnDiskIndex.h
+++ search/src/inverted_index/OnDiskIndex.h
@@ -24,7 +24,7 @@ class OnDiskIndex final : public Retriever {
              const std::vector<std::string>& docs) final;
 
   void update(const std::vector<DocId>& ids,
-                           const std::vector<std::string>& extra_tokens) final;
+              const std::vector<std::string>& extra_tokens) final;
 
   std::vector<DocScore> query(const std::string& query, uint32_t k,
                               bool parallelize) const final;
