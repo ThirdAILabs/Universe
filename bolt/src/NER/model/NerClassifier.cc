560df2e9fad84ddc5687f70a9c199aebd877e7e2
shubh@3ai <shubh@thirdai.com>
2024-08-22 21:13:09 +0000

    remove stopword filter

--- bolt/src/NER/model/NerClassifier.cc
+++ bolt/src/NER/model/NerClassifier.cc
@@ -19,9 +19,8 @@ void applyPunctAndStopWordFilter(const std::string& token,
                                  PerTokenPredictions& predicted_tags,
                                  const std::string& default_tag) {
   // assumes that the highest activation vector is at the end
-  if (isAllPunctuation(token) ||
-      text::stop_words.count(
-          data::ner::utils::trimPunctuation(thirdai::text::lower(token))) > 0) {
+
+  if (isAllPunctuation(token)) {
     for (int i = predicted_tags.size() - 1; i >= 0; --i) {
       if (predicted_tags[i].first == default_tag) {
         predicted_tags[i].second = 1;
