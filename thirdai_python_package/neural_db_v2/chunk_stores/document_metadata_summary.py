5577261ff3af9d1237502f4bfe204511794d5306
Gautam Sharma <gautam@thirdai.com>
2025-01-27 20:36:54 +0530

    fixes

--- thirdai_python_package/neural_db_v2/chunk_stores/document_metadata_summary.py
+++ thirdai_python_package/neural_db_v2/chunk_stores/document_metadata_summary.py
@@ -39,27 +39,33 @@ class DocumentMetadataSummary:
                 if metadata_type in [MetadataType.FLOAT, MetadataType.INTEGER]
                 else StringChunkMetadataSummary(unique_values=set())
             )
-            self.summarized_metadata[(doc_id, doc_version)] = {
-                key: ChunkMetaDataSummary(metadata_type=metadata_type, summary=summary)
-            }
+            self.summarized_metadata[(doc_id, doc_version)].update(
+                {
+                    key: ChunkMetaDataSummary(
+                        metadata_type=metadata_type, summary=summary
+                    )
+                }
+            )
 
         if metadata_type in [MetadataType.FLOAT, MetadataType.INTEGER]:
             # Summarizing the Numeric metadata series
-            self.summarized_metadata[(doc_id, doc_version)].summary.min = min(
-                self.summarized_metadata[(doc_id, doc_version)].summary.min,
+            self.summarized_metadata[(doc_id, doc_version)][key].summary.min = min(
+                self.summarized_metadata[(doc_id, doc_version)][key].summary.min,
                 metadata_series.min(),
             )
-            self.summarized_metadata[(doc_id, doc_version)].summary.max = max(
-                self.summarized_metadata[(doc_id, doc_version)].summary.max,
+            self.summarized_metadata[(doc_id, doc_version)][key].summary.max = max(
+                self.summarized_metadata[(doc_id, doc_version)][key].summary.max,
                 metadata_series.max(),
             )
         else:
             # Summarizing the string/bool metadata series
-            unique_values = self.summarized_metadata[
-                (doc_id, doc_version)
+            unique_values = self.summarized_metadata[(doc_id, doc_version)][
+                key
             ].summary.unique_values
-            unique_values.add(set(metadata_series.unique()))
+            unique_values.update(metadata_series.unique().tolist())
             unique_values.discard(None)  # Remove None value
-            self.summarized_metadata[(doc_id, doc_version)].summary.unique_values = (
-                random.sample(unique_values, k=100)
+            self.summarized_metadata[(doc_id, doc_version)][
+                key
+            ].summary.unique_values = random.sample(
+                unique_values, k=min(100, len(unique_values))
             )
