5e183d8578dccbfb62c353bfb4b998435f2d2020
Nicholas Meisburger <43160219+nmeisburger@users.noreply.github.com>
2025-05-19 21:22:47 -0400

    Remove telemetry (#2404)
    * remove telemetry
    
    * format

--- utils/UUID.h
+++ utils/UUID.h
@@ -8,7 +8,7 @@ namespace thirdai::utils::uuid {
 std::string getRandomHexString(uint32_t num_bytes_randomness);
 
 // A unique identfier for this thirdai package instance. Currently used for
-// licensing and telemetry.
+// licensing.
 extern const std::string THIRDAI_UUID;
 
 }  // namespace thirdai::utils::uuid
